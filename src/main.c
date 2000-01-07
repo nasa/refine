2454f102765d244c1561db00d1ad5c1d36bfea69
Mike Park <Mike.Park@nasa.gov>
2000-01-07 18:35:46 +0000

    now the package is set up to be an exec. check has been removed

---
+++ src/main.c
@@ -0,0 +1,15 @@
+
+/* Michael A. Park
+ * Computational Modeling & Simulation Branch
+ * NASA Langley Research Center
+ * Phone:(757)864-6604
+ * Email:m.a.park@larc.nasa.gov
+ */
+
+/* $Id$ */
+
+int main( int argc, char *argv[] )
+{
+
+  return;
+}
