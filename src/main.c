37388ca5267157950931f810f433c2a1c38fd1e0
Mike Park <Mike.Park@nasa.gov>
2000-01-07 18:36:00 +0000

    imported the restart grid and ran edge swap on it

--- src/main.c
+++ src/main.c
@@ -46,6 +46,20 @@ int main( int argc, char *argv[] )
 
   grid = gridLoadPart( project );
 
+  printf("restart grid size: %d nodes %d faces %d cells.\n",
+	 gridNNode(grid),gridNFace(grid),gridNCell(grid));
+
+  printf("minimum Aspect Ratio %12f\n",gridMinAR(grid));
+  printf("minimum Volume %12.8e\n",gridMinVolume(grid));
+
+  printf("edge swapping grid...\n");
+  gridSwap(grid);
+
+  printf("new size: %d nodes %d faces %d cells.\n",
+	 gridNNode(grid),gridNFace(grid),gridNCell(grid));
+  printf("minimum Aspect Ratio %12f\n",gridMinAR(grid));
+  printf("minimum Volume %12.8e\n",gridMinVolume(grid));
+
   printf("Done.\n");
 
   return;
@@ -59,7 +73,10 @@ Grid *gridLoadPart( char *project)
   int gridDimensions[3];
   int nnode, nface, ncell;
   int maxnode, maxface, maxcell;
-  
+  int i;
+  double *xyz;
+  int *c2n, *f2n, *faceId;
+
   printf("calling CADGeom_Start ... \n");
   if ( ! CADGeom_Start( ) ){
     printf("ERROR: CADGeom_Start broke.\n%s\n",ErrMgr_GetErrStr());
@@ -95,13 +112,39 @@ Grid *gridLoadPart( char *project)
   printf("max grid size: %d nodes %d faces %d cells.\n",
 	 maxnode,maxface,maxcell);
 
+  c2n    = malloc( maxcell * 4 * sizeof(int) );
+  f2n    = malloc( maxface * 3 * sizeof(int) );
+  faceId = malloc( maxface *     sizeof(int) );
+  xyz    = malloc( maxnode * 3 * sizeof(double) );
+
+  for( i=0; i<nnode; i++ ) {
+    xyz[0+3*i] = UGrid_PtValue(ugrid,i,X);
+    xyz[1+3*i] = UGrid_PtValue(ugrid,i,Y);
+    xyz[2+3*i] = UGrid_PtValue(ugrid,i,Z);
+  }
+
+  for( i=0; i<nface; i++ ) {
+    f2n[0+3*i] = UGrid_VertValue(ugrid,i,0);
+    f2n[1+3*i] = UGrid_VertValue(ugrid,i,1);
+    f2n[2+3*i] = UGrid_VertValue(ugrid,i,2);
+    faceId[i]  = UGrid_FlagValue(ugrid,i);
+  }
+
+  for( i=0; i<ncell; i++ ) {
+    c2n[0+4*i] = UGrid_TetValue(ugrid,i,0);
+    c2n[1+4*i] = UGrid_TetValue(ugrid,i,1);
+    c2n[2+4*i] = UGrid_TetValue(ugrid,i,2);
+    c2n[3+4*i] = UGrid_TetValue(ugrid,i,3);
+  }
+
   printf("calling CADGeom_Stop ... \n");
   if ( ! CADGeom_Stop( ) ){
     printf("ERROR: CADGeom_Stop broke.\n%s\n",ErrMgr_GetErrStr());
     return NULL;
   }  
 
-  return grid;
+  return gridImport(nnode, nface, maxcell, ncell,
+		     xyz, f2n, faceId, c2n );
 }
 
  
