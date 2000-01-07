d27f1b47d562b5b7bfe6c5022449e357a8a31788
Mike Park <Mike.Park@nasa.gov>
2000-01-07 18:35:59 +0000

    load a capri/GridEx restart and get the grid dimensions

--- src/main.c
+++ src/main.c
@@ -13,6 +13,8 @@
 #include "grid.h"
 #include <CADGeom/CADGeom.h>
 
+Grid *gridLoadPart( char *project);
+
 int main( int argc, char *argv[] )
 {
   Grid *grid;
@@ -42,22 +44,64 @@ int main( int argc, char *argv[] )
 
   gridFree(grid);
 
+  grid = gridLoadPart( project );
+
+  printf("Done.\n");
+
+  return;
+}
+
+Grid *gridLoadPart( char *project)
+{
+  Grid *grid;
+  int vol=1;
+  UGridPtr ugrid;
+  int gridDimensions[3];
+  int nnode, nface, ncell;
+  int maxnode, maxface, maxcell;
+  
   printf("calling CADGeom_Start ... \n");
   if ( ! CADGeom_Start( ) ){
-    printf("Yo! it broke.\n");
+    printf("ERROR: CADGeom_Start broke.\n%s\n",ErrMgr_GetErrStr());
+    return NULL;
   }  
 
   printf("calling CADGeom_Load for project <%s> ... \n",project);
   if ( ! CADGeom_LoadPart( project ) ){
-    printf("Yo! it broke.\n");
-  }  
+    printf("ERROR: CADGeom_LoadPart broke.\n%s\n",ErrMgr_GetErrStr());
+    return NULL;
+  }
+
+  if (NULL == (ugrid = CADGeom_VolumeGrid(vol)) ) {
+    printf("ERROR: Can not find grid in restart. \n%s\n",ErrMgr_GetErrStr());
+    return NULL;
+  }
+
+  if( !UGrid_GetDims(ugrid,gridDimensions) ) {
+    printf("ERROR: Could not get grid dimensions. \n%s\n",ErrMgr_GetErrStr());
+    return NULL;
+  }
+
+  nnode = gridDimensions[0];
+  nface = gridDimensions[1];
+  ncell = gridDimensions[2];
+
+  printf("ugrid size: %d nodes %d faces %d cells.\n",nnode,nface,ncell);
+
+  maxnode = nnode * 100;
+  maxface = nface * 100;
+  maxcell = ncell * 100;
+
+  printf("max grid size: %d nodes %d faces %d cells.\n",
+	 maxnode,maxface,maxcell);
 
   printf("calling CADGeom_Stop ... \n");
   if ( ! CADGeom_Stop( ) ){
-    printf("Yo! it broke.\n");
+    printf("ERROR: CADGeom_Stop broke.\n%s\n",ErrMgr_GetErrStr());
+    return NULL;
   }  
 
-  printf("Done. \n");
-
-  return;
+  return grid;
 }
+
+ 
