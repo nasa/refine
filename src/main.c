7dbee42a339c35cf77e2a82e6fe172f7671992d4
Mike Park <Mike.Park@nasa.gov>
2000-01-07 18:35:55 +0000

    moved the read from fast in main to a function in gridImportFAST

--- src/main.c
+++ src/main.c
@@ -14,11 +14,6 @@
 
 int main( int argc, char *argv[] )
 {
-  FILE *file;
-  int i, nnode, nface, maxcell, ncell;
-  double *xyz;
-  int *f2n, *faceId;
-  int *c2n;
   Grid *grid;
   char *filename;
 
@@ -29,76 +24,21 @@ int main( int argc, char *argv[] )
     filename = "../test/om6_inv08.fgrid";
     printf("running default filename %s\n",filename);
   }
-
-  file = fopen(filename,"r");
-  fscanf(file,"%d %d %d",&nnode,&nface,&ncell);
-  printf("fast size: %d nodes %d faces %d cells.\n",nnode,nface,ncell);
-
-  printf("reading xyz...\n");
   
-  xyz = malloc(3*nnode*sizeof(double));
-
-  for( i=0; i<nnode ; i++ ) fscanf(file,"%lf",&xyz[0+3*i]);
-  for( i=0; i<nnode ; i++ ) fscanf(file,"%lf",&xyz[1+3*i]);
-  for( i=0; i<nnode ; i++ ) fscanf(file,"%lf",&xyz[2+3*i]);
-
-  printf("reading faces...\n");
+  grid = gridImportFAST( filename );
   
-  f2n = malloc(3*nface*sizeof(int));
-
-  for( i=0; i<nface ; i++ ) {
-    fscanf(file,"%d",&f2n[0+3*i]);
-    fscanf(file,"%d",&f2n[1+3*i]);
-    fscanf(file,"%d",&f2n[2+3*i]);
-    f2n[0+3*i]--;
-    f2n[1+3*i]--;
-    f2n[2+3*i]--;
-  }
-
-  printf("reading face ID tags...\n");
-  
-  faceId = malloc(nface*sizeof(int));
-
-  for( i=0; i<nface ; i++ ) {
-    fscanf(file,"%d",&faceId[i]);
-  }
-
-  printf("reading cells...\n");
-  
-  maxcell = ncell*2;
-
-  c2n = malloc(4*maxcell*sizeof(int));
-
-  for( i=0; i<ncell ; i++ ) {
-    fscanf(file,"%d",&c2n[0+4*i]);
-    fscanf(file,"%d",&c2n[1+4*i]);
-    fscanf(file,"%d",&c2n[2+4*i]);
-    fscanf(file,"%d",&c2n[3+4*i]);
-    c2n[0+4*i]--;
-    c2n[1+4*i]--;
-    c2n[2+4*i]--;
-    c2n[3+4*i]--;
-  }
-
-  fclose(file);
-
-  grid = gridImport( nnode, nface, maxcell, ncell,
-		     xyz, f2n, faceId, c2n );
-
-  printf("import size: %d nodes %d faces %d cells.\n",
+  printf("orig size: %d nodes %d faces %d cells.\n",
 	 gridNNode(grid),gridNFace(grid),gridNCell(grid));
-  printf("gridImport: minimum Aspect Ratio %12f\n",gridMinAR(grid));
-  printf("gridImport: minimum Volume %12.8e\n",gridMinVolume(grid));
+  printf("minimum Aspect Ratio %12f\n",gridMinAR(grid));
+  printf("minimum Volume %12.8e\n",gridMinVolume(grid));
 
   printf("edge swapping grid...\n");
   gridSwap(grid);
 
   printf("new size: %d nodes %d faces %d cells.\n",
 	 gridNNode(grid),gridNFace(grid),gridNCell(grid));
-  printf("gridImport: minimum Aspect Ratio %12f\n",gridMinAR(grid));
-  printf("gridImport: minimum Volume %12.8e\n",gridMinVolume(grid));
+  printf("minimum Aspect Ratio %12f\n",gridMinAR(grid));
+  printf("minimum Volume %12.8e\n",gridMinVolume(grid));
 
-  grid = gridExport( grid, &nnode, &nface, &ncell,
-		     &xyz, &f2n, &faceId, &c2n );
   return;
 }
