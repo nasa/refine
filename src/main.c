53fa1a799c6de8c2d17fb7d1f533816a146ae093
Mike Park <Mike.Park@nasa.gov>
2000-01-07 18:35:47 +0000

    read in grid

--- src/main.c
+++ src/main.c
@@ -8,8 +8,64 @@
 
 /* $Id$ */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include "grid.h"
+
 int main( int argc, char *argv[] )
 {
+  FILE *file;
+  int i, nnode, nface, ncell;
+  double *xyz;
+  int *f2n, *faceId;
+  int *c2n;
+
+  if (argc == 1) {
+    printf("\nUsage: refine project.fgrid\n\n");
+    return;
+  }
+
+  file = fopen(argv[1],"r");
+  fscanf(file,"%d %d %d",&nnode,&nface,&ncell);
+  printf("fast size: %d nodes %d faces %d cells.\n",nnode,nface,ncell);
+
+  printf("reading xyz...\n");
+  
+  xyz = malloc(3*nnode*sizeof(double));
+
+  for( i=0; i<nnode ; i++ ) fscanf(file,"%lf",&xyz[0+3*i]);
+  for( i=0; i<nnode ; i++ ) fscanf(file,"%lf",&xyz[1+3*i]);
+  for( i=0; i<nnode ; i++ ) fscanf(file,"%lf",&xyz[2+3*i]);
+
+  printf("reading faces...\n");
+  
+  f2n = malloc(3*nface*sizeof(int));
+
+  for( i=0; i<nface ; i++ ) {
+    fscanf(file,"%d",&f2n[0+3*i]);
+    fscanf(file,"%d",&f2n[1+3*i]);
+    fscanf(file,"%d",&f2n[2+3*i]);
+  }
+
+  printf("reading face ID tags...\n");
+  
+  faceId = malloc(nface*sizeof(int));
+
+  for( i=0; i<nface ; i++ ) {
+    fscanf(file,"%d",&faceId[i]);
+  }
+
+  printf("reading cells...\n");
+  
+  c2n = malloc(4*ncell*sizeof(int));
+
+  for( i=0; i<nface ; i++ ) {
+    fscanf(file,"%d",&c2n[0+4*i]);
+    fscanf(file,"%d",&c2n[1+4*i]);
+    fscanf(file,"%d",&c2n[2+4*i]);
+    fscanf(file,"%d",&c2n[3+4*i]);
+  }
+  fclose(file);
 
   return;
 }
