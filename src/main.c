cf2b99a703041a4fb4e2d4194132594bdadda705
Mike Park <Mike.Park@nasa.gov>
2000-01-07 18:36:25 +0000

    casren at the wing tip

--- src/main.c
+++ src/main.c
@@ -67,12 +67,12 @@ int main( int argc, char *argv[] )
 			 0.0,0.0,0.0,0.75,
 			 0.4);
   gridScaleSpacingSphere(grid,
-			 1.0,1.0,0.0,0.3,
-			 0.2);
+			 0.0,0.0,0.0,0.3,
+			 0.4);
   gridScaleSpacingSphere(grid,
-			 0.0,0.0,-0.8,1.0,
+			 1.0,1.0,0.0,0.4,
 			 3.0);
-  for (j=0;j<8;j++){
+  for (j=0;j<6;j++){
     gridAdapt(grid);
     printf("%02d new size: %d nodes %d faces %d cells %d edge elements.\n",
 	   j, gridNNode(grid),gridNFace(grid),gridNCell(grid),gridNEdge(grid));
