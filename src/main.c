0bdfa07f3dd8746428ba9ed46e1346af598d3862
Mike Park <Mike.Park@nasa.gov>
2000-01-07 18:36:22 +0000

    made it run quicker

--- src/main.c
+++ src/main.c
@@ -65,11 +65,8 @@ int main( int argc, char *argv[] )
   gridResetSpacing(grid);
   gridScaleSpacingSphere(grid,
 			 0.0,0.0,0.0,0.75,
-			 0.5);
-  gridScaleSpacingSphere(grid,
-			 0.0,0.0,0.0,0.4,
-			 0.5);
-  for (j=0;j<4;j++){
+			 0.4);
+  for (j=0;j<6;j++){
     gridAdapt(grid);
     printf("%02d new size: %d nodes %d faces %d cells %d edge elements.\n",
 	   j, gridNNode(grid),gridNFace(grid),gridNCell(grid),gridNEdge(grid));
