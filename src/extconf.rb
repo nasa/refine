b534a8ddc794dcd0f1fbb87d106860343da62e12
Mike Park <Mike.Park@nasa.gov>
2002-09-29 16:07:34 +0000

    now the extconf.rb selects the correct object files

--- src/extconf.rb
+++ src/extconf.rb
@@ -1,5 +1,13 @@
 
 require 'mkmf'
 
-$objs = %w{sampleunit.o sampleunit_ruby.o}
+rubyExt = Dir["*_ruby.c"]
+sourceExt = rubyExt.collect{ |r| r.sub(/_ruby.c/, ".c") }
+
+
+objC = rubyExt + sourceExt
+
+
+$objs = objC.collect{ |c| c.sub(/.c/, ".o") }
+
 create_makefile("SampleUnit")
