bfbd6311e9319200910660952d11cb1001de197d
Mike Park <Mike.Park@nasa.gov>
2002-10-18 18:33:16 +0000

    put in the test for all 12 orient cases

--- src/gridMT.rb
+++ src/gridMT.rb
@@ -99,7 +99,21 @@ class TestSampleUnit < Test::Unit::TestCase
  
 
  def testOrient
-  assert_equal [2, 3], @grid.orient(0,1,2,3,0,1)
+  assert_equal [0, 1, 2, 3], @grid.orient(0,1,2,3,0,1)
+  assert_equal [0, 1, 2, 3], @grid.orient(0,3,1,2,0,1)
+  assert_equal [0, 1, 2, 3], @grid.orient(0,2,3,1,0,1)
+
+  assert_equal [0, 1, 2, 3], @grid.orient(1,0,3,2,0,1)
+  assert_equal [0, 1, 2, 3], @grid.orient(1,2,0,3,0,1)
+  assert_equal [0, 1, 2, 3], @grid.orient(1,3,2,0,0,1)
+
+  assert_equal [0, 1, 2, 3], @grid.orient(2,3,0,1,0,1)
+  assert_equal [0, 1, 2, 3], @grid.orient(2,1,3,0,0,1)
+  assert_equal [0, 1, 2, 3], @grid.orient(2,0,1,3,0,1)
+
+  assert_equal [0, 1, 2, 3], @grid.orient(3,2,1,0,0,1)
+  assert_equal [0, 1, 2, 3], @grid.orient(3,0,2,1,0,1)
+  assert_equal [0, 1, 2, 3], @grid.orient(3,1,0,2,0,1)
  end
  
 
