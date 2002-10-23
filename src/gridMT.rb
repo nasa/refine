c83d961090588184852e4f2246f332719c5dbfc3
Mike Park <Mike.Park@nasa.gov>
2002-10-23 18:47:40 +0000

    turned on broked faceid test

--- src/gridMT.rb
+++ src/gridMT.rb
@@ -184,7 +184,7 @@ class TestSampleUnit < Test::Unit::TestCase
 
 # put faces in
 
- def XtestFace
+ def testFace
   assert_equal 0, @grid.nface 
   assert_equal 0, @grid.maxface 
   assert_not_nil grid = Grid.new(4,1,1)
@@ -193,7 +193,7 @@ class TestSampleUnit < Test::Unit::TestCase
   assert_not_nil grid.addFace(0, 1, 2, 10)
   assert_nil grid.addFace(0, 1, 2, 11)
   assert_equal 1, grid.nface 
-#  assert_equal 10, grid.faceId( 0, 1, 2 )
+  assert_equal 10, grid.faceId( 0, 1, 2 )
 #  assert_equal 10, grid.faceId( 1, 2, 0 )
 #  assert_equal 10, grid.faceId( 2, 0, 1 )
 #  assert_equal 10, grid.faceId( 2, 1, 0 )
