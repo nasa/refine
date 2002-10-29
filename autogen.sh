fc0236b483fb1ce3d0cb8407eadba9397a9a951d
Mike Park <Mike.Park@nasa.gov>
2002-10-29 19:09:54 +0000

    cleaned up the autogen script to remove extra stuff from check

--- autogen.sh
+++ autogen.sh
@@ -1,93 +1,17 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+echo "linking configure.ac to configure.in..."
+ln -s configure.ac configure.in
 
-ORIGDIR=`pwd`
-cd $srcdir
-PROJECT="refine: unstructed grid adaptation"
-TEST_TYPE=-f
+echo "Running aclocal..."
+aclocal
 
-ACLOCAL_FLAGS="-I /usr/local/share/aclocal"
+echo "Running automake..."
+automake --add-missing
 
-DIE=0
+echo "Running autoconf..."
+autoconf
 
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have autoconf installed to compile $PROJECT."
-	echo "Download the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-	DIE=1
-}
-
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have automake installed to compile $PROJECT."
-	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
-	echo "(or a newer version if it is available)"
-	DIE=1
-}
-
-if test "$DIE" -eq 1; then
-	exit 1
-fi
-
-echo "I am testing that you have the required versions of autoconf and automake"
-echo
-
-echo "Testing autoconf... "
-VER=`autoconf --version | grep -iw autoconf | sed "s/.* \([0-9.]*\)[-a-z0-9]*$/\1/"`
-if expr $VER \>= 2.13 >/dev/null; then
-	echo "looks OK."
-else
-	echo "too old! (Need 2.13, have $VER)"
-	DIE=1
-fi
-
-echo "Testing automake... "
-VER=`automake --version | grep automake | sed "s/.* \([0-9.]*\)[-a-z0-9]*$/\1/"`
-if expr $VER \>= 1.4 >/dev/null; then
-	echo "looks OK."
-else
-	echo "too old! (Need 1.4, have $VER)"
-	DIE=1
-fi
-
-echo
-
-if test "$DIE" -eq 1; then
-	exit 1
-fi
-
-# insert test that this is run form the top level dir
-
-if test -z "$*"; then
-	echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
-fi
-
-autogen_dirs="."
-
-for i in $autogen_dirs; do
-        echo "Processing $i..."
-
-        cd $i
-
-        aclocal $ACLOCAL_FLAGS
-
-        # optionally feature autoheader
-        if grep AM_CONFIG_HEADER configure.in >/dev/null ; then
-                (autoheader --version)  < /dev/null > /dev/null 2>&1 && autoheader
-        fi
-
-        automake --add-missing
-        autoconf
-done
-
-cd $ORIGDIR
-
-$srcdir/configure --enable-maintainer-mode "$@"
-
-echo 
-echo "Now type 'make' to compile $PROJECT."
+echo "Running ./configure..."
+./configure
