96fbba6cbfcced47cb2b5f690356eec7035d5d0d
Mike Park <Mike.Park@nasa.gov>
2002-11-06 19:46:24 +0000

    put in test for splitting UV vals on faces of same type put in more tests for setting values

--- src/gridMT.rb
+++ src/gridMT.rb
@@ -217,7 +217,11 @@ class TestSampleUnit < Test::Unit::TestCase
 				    3,33.0,133.0,3)
   assert_nil                 grid.nodeUV(2,3)
   assert_equal [20.0,120.0], grid.nodeUV(0,2)
+  assert_equal [21.0,121.0], grid.nodeUV(1,2)
+  assert_equal [22.0,122.0], grid.nodeUV(2,2)
   assert_equal [30.0,130.0], grid.nodeUV(0,3)
+  assert_equal [31.0,131.0], grid.nodeUV(1,3)
+  assert_equal [33.0,133.0], grid.nodeUV(3,3)
  end
 
  def testSplitEdge4
@@ -231,8 +235,12 @@ class TestSampleUnit < Test::Unit::TestCase
 
  def testSplitEdge4onSameBC
   assert_not_nil     grid=gemGrid(4, nil, nil, nil, true)
-  assert_equal grid, grid.addFace(0,1,2,11)
-  assert_equal grid, grid.addFace(1,0,5,11)
+  assert_equal grid, grid.addFaceUV(0,0.0,10.0,
+				    1,1.0,11.0,
+				    2,2.0,12.0,11)
+  assert_equal grid, grid.addFaceUV(1,1.0,11.0,
+				    0,0.0,10.0,
+				    5,5.0,15.0,11)
   assert grid.rightHandedBoundary, "original boundary is not right handed"
   assert_equal grid, grid.splitEdge(0,1)
   assert_nil         grid.faceId(0,1,2)
@@ -242,6 +250,11 @@ class TestSampleUnit < Test::Unit::TestCase
   assert_equal 11,   grid.faceId(0,6,5) 
   assert_equal 11,   grid.faceId(6,1,5) 
   assert grid.rightHandedBoundary, "split boundary is not right handed"
+  assert_equal [0.0,10.0], grid.nodeUV(0,11)
+  assert_equal [1.0,11.0], grid.nodeUV(1,11)
+  assert_equal [2.0,12.0], grid.nodeUV(2,11)
+  assert_equal [5.0,15.0], grid.nodeUV(5,11)
+  assert_equal [0.5,10.5], grid.nodeUV(6,11)
  end
 
  def testSplitEdge4onDifferentBC
@@ -313,12 +326,8 @@ class TestSampleUnit < Test::Unit::TestCase
 
  def testSplitGeometryEdge4
   assert_not_nil     grid=gemGrid(4, nil, nil, nil, true)
-  assert_equal grid, grid.addFaceUV(0,20.0,30.0,
-				    1,21.0,31.0,
-				    2,22.0,32.0,2)
-  assert_equal grid, grid.addFaceUV(1,51.0,61.0,
-				    0,50.0,60.0,
-				    5,55.0,65.0,5)
+  assert_equal grid, grid.addFace(0,1,2,2)
+  assert_equal grid, grid.addFace(1,0,5,5)
   assert grid.rightHandedBoundary, "original boundary is not right handed"
   assert_equal grid, grid.addEdge(0,1,15, 0.0, 1.0)
   assert_equal grid, grid.splitEdge(0,1)
