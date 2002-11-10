5612113a48ec82e8e8b73f75aefdc55ed41ded01
Mike Park <Mike.Park@nasa.gov>
2002-11-10 14:03:15 +0000

    the ruby extension maker needs all the headers listed

--- src/sampleunitMT.rb
+++ src/sampleunitMT.rb
@@ -4,7 +4,7 @@
 #
 # Mobility test for sampleunit c lib
 
-exit 1 unless system 'ruby makeRubyExtension.rb SampleUnit'
+exit 1 unless system 'ruby makeRubyExtension.rb SampleUnit master_header.h'
 
 require 'test/unit'
 require 'SampleUnit/SampleUnit'
