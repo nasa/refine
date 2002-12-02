a1d2d49659a0405af851c17afd99338caf89deb0
Mike Park <Mike.Park@nasa.gov>
2002-12-02 14:14:32 +0000

    correct the map example for the eigen vectors that scale the edges to length 1.0

--- src/mapExample.m
+++ src/mapExample.m
@@ -1,7 +1,6 @@
 format long
 
 p7 = 1.0/sqrt(2);
-p7 = 0.5;
 
 eVect1 = [  p7
             p7 ]
@@ -10,8 +9,8 @@ eVect2 = [ -p7
 
 eigenVector = [ eVect1 eVect2 ]
 
-eVal1 = 0.5
-eVal2 = 1.0
+eVal1 = 0.5*p7
+eVal2 = p7
 
 eigenValue  = [ eVal1   0.0
                   0.0 eVal2 ]
