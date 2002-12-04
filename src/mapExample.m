3c9ce57512c73f8ba1c8f4511e0e0f44a9d09683
Mike Park <Mike.Park@nasa.gov>
2002-12-04 14:30:03 +0000

    clarified the example

--- src/mapExample.m
+++ src/mapExample.m
@@ -9,21 +9,32 @@ eVect2 = [ -p7
 
 eigenVector = [ eVect1 eVect2 ]
 
-eVal1 = 0.5*p7
-eVal2 = p7
+h1 = 2.0*sqrt(2)
+h2 = sqrt(2)
+eVal1 = 1/h1/h1
+eVal2 = 1/h2/h2
+sqrteVal1 = 1/h1
+sqrteVal2 = 1/h2
 
 eigenValue  = [ eVal1   0.0
                   0.0 eVal2 ]
 
-map = eigenValue * eigenVector'
+sqrteigenValue  = [ sqrteVal1   0.0
+                     0.0     sqrteVal2 ]
+
+map = sqrteigenValue * eigenVector'
+
+Mmatrix = map'*map
+Mmatrix = eigenVector*eigenValue*eigenVector'
 
 x1 = [ 2
        2 ]
 e1=map*x1
 len1=norm(e1)
-
+len1=x1'*Mmatrix*x1
 x2 = [ -1
         1 ]
 
 e2=map*x2
 len2=norm(e2)
+len2=x2'*Mmatrix*x2
