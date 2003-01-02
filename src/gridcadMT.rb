28266de6a844f934c90eae8385c90e9845735abb
Mike Park <Mike.Park@nasa.gov>
2003-01-02 23:14:19 +0000

    put gridmetric.h in gridcad.c

--- src/gridcadMT.rb
+++ src/gridcadMT.rb
@@ -6,7 +6,7 @@
 
 exit 1 unless system 'ruby makeRubyExtension.rb Grid adj.c gridStruct.h master_header.h'
 exit 1 unless system 'ruby makeRubyExtension.rb GridMetric adj.c grid.c gridStruct.h master_header.h'
-exit 1 unless system 'ruby makeRubyExtension.rb GridCAD FAKEGeom adj.c grid.c gridinsert.h gridStruct.h master_header.h'
+exit 1 unless system 'ruby makeRubyExtension.rb GridCAD FAKEGeom adj.c grid.c gridmetric.h gridinsert.h gridStruct.h master_header.h'
 exit 1 unless system 'ruby makeRubyExtension.rb GridInsert adj.c grid.h gridmetric.h gridcad.h gridStruct.h master_header.h'
 
 require 'test/unit'
