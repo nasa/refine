c97c78ea033bcc2979f92526d7244722760c0d83
Mike Park <Mike.Park@nasa.gov>
2003-01-15 20:25:53 +0000

    extracted form front from main to layer

--- src/layer.h
+++ src/layer.h
@@ -19,6 +19,7 @@ BEGIN_C_DECLORATION
 typedef struct Layer Layer;
 
 Layer *layerCreate(Grid *);
+Layer *formAdvancingFront( Grid *grid, char *project );
 void layerFree(Layer *);
 void layerSortGlobalNodes(void *layer, int *o2n);
 int layerNFront(Layer *);
