084493bba73757e152da9670cb58f8c0ece8dab6
Mike Park <Mike.Park@nasa.gov>
2003-01-15 21:34:26 +0000

    added a phantom lists to store rebuild objects

--- src/mesherx.c
+++ src/mesherx.c
@@ -14,8 +14,10 @@
 #include <math.h>
 #include <limits.h>         /* Needed in some systems for DBL_MAX definition */
 #include <float.h>
+#include "gridfiller.h"
 #include "grid.h"
 #include "layer.h"
+#include "CADGeom/CADGeom.h"
 
 /******************** EXTERNAL FUNCTIONS ******************************/
 
@@ -28,6 +30,9 @@ MesherX_DiscretizeVolume( int npts, double *points, int ntri_b, int *tri_b,
   int vol=1;
   Grid *grid;
   Layer *layer;
+  int nGeomNode, nGeomEdge, nGeomFace, nGeomGroups;
+  CADCurvePtr *phantomEdge;
+  UGPatchPtr  *phantomFace;
 
   grid = gridFillFromPart( vol, npts*10 );
 
@@ -35,7 +40,23 @@ MesherX_DiscretizeVolume( int npts, double *points, int ntri_b, int *tri_b,
 
   layerAdvance(layer,0.01);
 
-  /* Make edge and face lists */
+/* Allocate Phantom Edge and Face Arrays */
+
+  if( !CADGeom_GetVolume(1,&nGeomNode,&nGeomEdge,&nGeomFace,&nGeomGroups) ) {
+    printf("ERROR: CADGeom_GetVolume, line %d of %s\n.",__LINE__, __FILE__);
+    return 1;
+  }
+
+  if( (phantomEdge=(CADCurvePtr *)calloc(nGeomEdge,sizeof(CADCurvePtr))) == NULL
+ ) {
+    printf("ERROR: Allocation of Phantom Edges\n");
+    return 1;
+  }
+
+  if( (phantomFace=(UGPatchPtr *)calloc(nGeomFace,sizeof(UGPatchPtr))) == NULL ) {
+    printf("ERROR: Allocation of Phantom Faces\n");
+    return 1;
+  }
 
   return 1;
 }
