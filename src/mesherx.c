b6b56d7da2eb4cc03a00152b40784c9c4e470e18
Mike Park <Mike.Park@nasa.gov>
2003-01-28 16:15:10 +0000

    the edges are now updated with the rebuilt versions

--- src/mesherx.c
+++ src/mesherx.c
@@ -139,6 +139,8 @@ MesherX_DiscretizeVolume( int npts, double *points, int ntri_b, int *tri_b,
   int nedgenode;
   double *newxyz;
   double *newt;
+  int *newnodes;
+  int i0, i1;
 
   grid = gridFillFromPart( vol, npts*10 );
 
@@ -185,7 +187,7 @@ MesherX_DiscretizeVolume( int npts, double *points, int ntri_b, int *tri_b,
       printf("rebuild endpoints:  %d <-> %d\n",
 	     edgeEndPoints[0],edgeEndPoints[1]);
       gridNodeXYZ(grid, edgeEndPoints[0], &edgexyz[0]);
-      gridNodeXYZ(grid, edgeEndPoints[0], &edgexyz[3]);
+      gridNodeXYZ(grid, edgeEndPoints[1], &edgexyz[3]);
       gridNodeT(grid, edgeEndPoints[0], edgeId, &tRange[0]);
       gridNodeT(grid, edgeEndPoints[1], edgeId, &tRange[1] );
 
@@ -197,13 +199,27 @@ MesherX_DiscretizeVolume( int npts, double *points, int ntri_b, int *tri_b,
 
       printf("number of rebuild edge points:  %d\n",nedgenode);
 
+      newnodes = malloc( nedgenode * sizeof(int));
+      newnodes[0] = edgeEndPoints[0];
+      newnodes[nedgenode-1] = edgeEndPoints[1];
+      for(i=1;i<(nedgenode-1);i++){
+	newnodes[i]=gridAddNode(grid,newxyz[0+3*i],newxyz[1+3*i],newxyz[2+3*i]);
+      }
+
+      gridDeleteThawedEdgeSegments(grid,edgeId);
+      for(i=1;i<(nedgenode-1);i++){
+	i0 = i-1; i1 = i;
+	gridAddEdge(grid,newnodes[i0],newnodes[i1],edgeId,newt[i0],newt[i1]);
+      }
+      i0 = nedgenode-2; i1 = nedgenode-1;
+      gridAddEdge(grid,newnodes[i0],newnodes[i1],edgeId,newt[i0],newt[i1]);
+
     }
   }
 
   outputProject = "../test/MesherX";
   printf("writing DEBUG output project %s\n",outputProject);
   gridSavePart( grid, outputProject );
-
 
   return 1;
 }
