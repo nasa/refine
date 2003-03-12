fbd160144ff64434639ab90a37496b946541837d
Mike Park <Mike.Park@nasa.gov>
2003-03-12 16:33:53 +0000

    added staus mesg

--- src/mesherx.c
+++ src/mesherx.c
@@ -90,6 +90,7 @@ int MesherX_DiscretizeVolume( int maxNodes, double scale, char *project,
   if (blendElement) {
     printf("inserting blends...\n");
     layerBlend(layer); 
+    printf("split blends...\n");
     layerSplitBlend(layer); 
   }
 
