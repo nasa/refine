d0c9ad65b4905f4b120b3ffef1228fa6091f341d
Mike Park <Mike.Park@nasa.gov>
2003-07-08 20:05:57 +0000

    pulled layer stucts out of layer

--- src/layer.c
+++ src/layer.c
@@ -22,55 +22,7 @@
 #include "grid.h"
 #include "near.h"
 
-typedef struct Normal Normal;
-struct Normal {
-  int constrained;
-  int root, tip;
-  double direction[3];
-  double height;
-  double initialheight;
-  double length;
-  double maxlength;
-  double rate;
-  bool terminated;
-};
-
-typedef struct Triangle Triangle;
-struct Triangle {
-  int normal[3];
-  int constrainedSide[3];
-  int parentGeomEdge[3];
-};
-
-typedef struct Blend Blend;
-struct Blend {
-  int nodes[2];
-  int normal[4];
-  int edgeId[2];
-  int oldnormal[4];
-};
-
-struct Layer {
-  Grid *grid;
-  int maxtriangle, ntriangle;
-  Triangle *triangle;
-  int maxParentGeomFace, nParentGeomFace, *ParentGeomFace;
-  int maxblend, nblend;
-  Blend *blend;
-  int maxnormal, nnormal;
-  Normal *normal;
-  int *globalNode2Normal;
-  int nConstrainingGeometry, *constrainingGeometry;
-  Adj *adj;
-  Near *nearTree;
-  bool mixedElementMode;
-
-  bool *cellInLayer;
-  bool *faceInLayer;
-
-  FILE *tecplotFile;
-
-};
+#include "layerStruct.h"
 
 Layer *layerCreate( Grid *grid )
 {
