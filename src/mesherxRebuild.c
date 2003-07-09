51e700e17d0de37aa1fb5cabfed7c106186fb73f
Mike Park <Mike.Park@nasa.gov>
2003-07-09 00:38:57 +0000

    removed the unneeded includes

--- src/mesherxRebuild.c
+++ src/mesherxRebuild.c
@@ -10,23 +10,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
 #include <limits.h>         /* Needed in some systems for DBL_MAX definition */
 #include <float.h>
-#include "mesherx.h"
-#include "grid.h"
-#include "gridfiller.h"
-#include "gridmetric.h"
-#include "gridswap.h"
 #include "CADGeom/CADGeom.h"
 #include "CADGeom/CADTopo.h"
-#include "Goolache/CAPrIMesh.h"
 #include "Goolache/MeshMgr.h"
-#include "Goolache/FELISASrc.h"
 #include "Goolache/UGPatch.h"
 #include "MeatLib/ErrMgr.h"
-#include "MeatLib/GeoBC.h"
+#include "grid.h"
 #include "mesherxRebuild.h"
 
 Layer *layerRebuildInterior(Layer *layer, int vol)
