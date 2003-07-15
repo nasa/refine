4a0a0d7261b12f67213b5575cf0d762fc205de70
Mike Park <Mike.Park@nasa.gov>
2003-07-15 21:10:59 +0000

    got ride of side

--- src/intersectMT.rb
+++ src/intersectMT.rb
@@ -18,18 +18,6 @@ class TestIntersect < Test::Unit::TestCase
  end
  def setup ; set_up ; end
 
- def testNodeSide
-  v0 = [0,0,0]
-  v1 = [1,0,0]
-  v2 = [0,1,0]
-  nn = [0,0,-1]
-  n0 = [0,0,0]
-  np = [0,0,1]
-  assert_equal( -1, @intersect.side(v0,v1,v2,nn))
-  assert_equal(  0, @intersect.side(v0,v1,v2,n0))
-  assert_equal(  1, @intersect.side(v0,v1,v2,np))
- end
-
  def testPlaneAndNode
   v0 = [0,0,0]
   v1 = [1,0,0]
