639c1f9d3e6835fbc537cf3a6e4bb34bc0045f33
Mike Park <Mike.Park@nasa.gov>
2003-07-15 16:51:04 +0000

    finished unitesting gridmath

--- src/gridmathMT.rb
+++ src/gridmathMT.rb
@@ -11,14 +11,42 @@ require 'GridMath/GridMath'
 
 class TestGridMath < Test::Unit::TestCase
 
+ def set_up
+  @gm = GridMath.new
+ end
+ def setup ; set_up ; end
+
  def testSubtractVector
-  gm = GridMath.new
   v1 = [1,2,3]
   v2 = [7,0,6]
-  assert_equal [-6,2,-3], gm.subtractVector(v1,v2)
+  assert_equal [-6,2,-3], @gm.subtractVector(v1,v2)
   v1 = [13,6,23]
   v2 = [2,10,16]
-  assert_equal [11,-4,7], gm.subtractVector(v1,v2)
+  assert_equal [11,-4,7], @gm.subtractVector(v1,v2)
+ end
+
+ def testDotProduct
+  v1 = [1,2,3]
+  v2 = [1,2,3]
+  assert_equal 14, @gm.dotProduct(v1,v2)
+  v1 = [2,0,5]
+  v2 = [1,20,3]
+  assert_equal 17, @gm.dotProduct(v1,v2)
+ end
+
+ def testCrossProduct
+  v1 = [1,0,0]
+  v2 = [0,1,0]
+  assert_equal [0,0,1],  @gm.crossProduct(v1,v2)
+  assert_equal [0,0,-1], @gm.crossProduct(v2,v1)
+  assert_equal [0,0,0], @gm.crossProduct(v1,v1)
+  assert_equal [0,0,0], @gm.crossProduct(v2,v2)
+ end
+
+ def testNormalizeVector
+  assert_equal [1,0,0], @gm.vectorNormalize([5,0,0])
+  assert_equal [0,1,0], @gm.vectorNormalize([0,6,0])
+  assert_equal [0,0,-1], @gm.vectorNormalize([0,0,-3])
  end
 
 end
--- src/gridmath_ruby.c
+++ src/gridmath_ruby.c
@@ -17,10 +17,50 @@ static VALUE grid_subtractVector( VALUE self, VALUE rb_v1, VALUE rb_v2)
   return rb_result;
 }
 
+static VALUE grid_dotProduct( VALUE self, VALUE rb_v1, VALUE rb_v2)
+{
+  int i;
+  double v1[3], v2[3];
+  for (i=0;i<3;i++){
+    v1[i] = NUM2DBL(rb_ary_entry(rb_v1, i));
+    v2[i] = NUM2DBL(rb_ary_entry(rb_v2, i));
+  }
+  return rb_float_new(gridDotProduct(v1,v2));
+}
+
+static VALUE grid_crossProduct( VALUE self, VALUE rb_v1, VALUE rb_v2)
+{
+  int i;
+  double v1[3], v2[3], result[3];
+  VALUE rb_result;
+  for (i=0;i<3;i++){
+    v1[i] = NUM2DBL(rb_ary_entry(rb_v1, i));
+    v2[i] = NUM2DBL(rb_ary_entry(rb_v2, i));
+  }
+  gridCrossProduct(v1,v2,result);
+  rb_result = rb_ary_new2(3);
+  for (i=0;i<3;i++) rb_ary_store(rb_result, i, rb_float_new(result[i]));
+  return rb_result;
+}
+
+static VALUE grid_vectorNormalize( VALUE self, VALUE rb_vect )
+{
+  int i;
+  double vect[3];
+  VALUE rb_result;
+  for (i=0;i<3;i++) vect[i] = NUM2DBL(rb_ary_entry(rb_vect, i));
+  gridVectorNormalize(vect);
+  for (i=0;i<3;i++) rb_ary_store(rb_vect, i, rb_float_new(vect[i]));
+  return rb_vect;
+}
+
 VALUE cGridMath;
 
 void Init_GridMath(  )
 {
   cGridMath = rb_define_class( "GridMath", rb_cObject );
   rb_define_method( cGridMath, "subtractVector", grid_subtractVector, 2 );
+  rb_define_method( cGridMath, "dotProduct", grid_dotProduct, 2 );
+  rb_define_method( cGridMath, "crossProduct", grid_crossProduct, 2 );
+  rb_define_method( cGridMath, "vectorNormalize", grid_vectorNormalize, 1 );
 }
