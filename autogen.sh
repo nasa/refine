b2423d741f4525283ed7d8a0a3e12c68eb6c3859
Mike Park <Mike.Park@nasa.gov>
2003-09-04 14:46:30 +0000

    trnasformed automake into arch dirs with kernal specific builds

--- autogen.sh
+++ autogen.sh
@@ -1,14 +1,27 @@
-#!/bin/sh
+#!/bin/csh
 # Run this to generate all the initial makefiles, etc.
 
+set arch = `uname`
+
+echo "Configure $arch..."
+
+echo "Linking required AutoTool files..."
+( cd $arch && ln -s ../AutoConfInput configure.ac )
+( cd $arch && ln -s ../AUTHORS . )
+( cd $arch && ln -s ../ChangeLog . )
+( cd $arch && ln -s ../NEWS . )
+( cd $arch && ln -s ../COPYING . )
+( cd $arch && ln -s ../INSTALL . )
+( cd $arch && ln -s ../README . )
+
 echo "Running aclocal ..."
-aclocal
+( cd $arch && aclocal )
 
 echo "Running automake ..."
-automake --add-missing
+( cd $arch && automake --add-missing )
 
 echo "Running autoconf ..."
-autoconf
+( cd $arch && autoconf )
 
-echo "Running ./configure --prefix=`pwd` ..."
-./configure --prefix=`pwd` $*
+( cd $arch && echo "Running ./configure --prefix=`pwd` " $* )
+( cd $arch && ./configure --prefix=`pwd` $* )
