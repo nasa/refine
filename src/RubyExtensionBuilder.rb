5b1f164543d4009c0a8e70ce0ded8de7cc484d4d
Mike Park <Mike.Park@nasa.gov>
2003-09-05 14:00:40 +0000

    cleaned up the extion builder to have more defaults and added layer Intersect depend

--- src/RubyExtensionBuilder.rb
+++ src/RubyExtensionBuilder.rb
@@ -9,35 +9,30 @@ class RubyExtensionBuilder
  end
 
  def buildOnly extension
-  comm = Hash.new('puts "Error, extension not known";exit 1')
-  comm['Adj'] = 'Adj'
-  comm['Near'] = 'Near'
-  comm['Intersect'] = 'Intersect gridmath.c'
-  comm['Grid'] = 'Grid adj.h'
-  comm['GridMath'] = 'GridMath'
-  comm['GridMetric'] = 'GridMetric adj.h grid.h gridmath.c'
-  comm['GridSwap'] = 'GridSwap adj.h grid.h gridmath.h gridmetric.h'
-  comm['GridCAD'] = 'GridCAD FAKEGeom adj.h grid.h gridmath.h gridmetric.h gridinsert.h'
-  comm['GridInsert'] = 'GridInsert adj.h grid.h gridmath.h gridmetric.h gridcad.h'
-  comm['Layer'] = 'Layer layerStruct.h adj.h near.h intersect.h grid.h gridmath.h gridmetric.h gridcad.h gridinsert.h'
-  exit 1 unless system('ruby makeRubyExtension.rb ' + comm[extension] + ' master_header.h')
+  extraFiles = Hash.new('')
+  extraFiles['Intersect'] = 'gridmath.c'
+  extraFiles['Grid'] = 'adj.h'
+  extraFiles['GridMetric'] = 'adj.h grid.h gridmath.c'
+  extraFiles['GridSwap'] = 'adj.h grid.h gridmath.h gridmetric.h'
+  extraFiles['GridCAD'] = 'FAKEGeom adj.h grid.h gridmath.h gridmetric.h gridinsert.h'
+  extraFiles['GridInsert'] = 'adj.h grid.h gridmath.h gridmetric.h gridcad.h'
+  extraFiles['Layer'] = 'layerStruct.h adj.h near.h intersect.h grid.h gridmath.h gridmetric.h gridcad.h gridinsert.h'
+
+  systemCall = ['ruby makeRubyExtension.rb',extension,extraFiles[extension],'master_header.h'].join(' ')
+  exit 1 unless system(systemCall )
  end
 
  def build
-  depend = Hash.new
-  depend['Adj'] = %w{ Adj }
-  depend['Near'] = %w{ Near }
-  depend['Intersect'] = %w{ Intersect }
-  depend['Grid'] = %w{ Adj Grid }
-  depend['GridMath'] = %w{ GridMath }
-  depend['GridMetric'] = %w{ Adj Grid GridMetric }
-  depend['GridSwap'] = %w{ Adj Grid GridMetric GridSwap }
-  depend['GridCAD'] = %w{ Adj Grid GridMetric GridCAD }
-  depend['GridInsert'] = %w{ Adj Grid GridMetric GridSwap GridCAD GridInsert }
-  depend['Layer'] = %w{ Adj Near Grid GridMetric GridCAD GridInsert Layer }
-  depend[@extension].each { |extension|
-   buildOnly extension
-  }
+  requiredPackages = Hash.new([])
+  requiredPackages['Grid'] = %w{ Adj }
+  requiredPackages['GridMetric'] = %w{ Adj Grid }
+  requiredPackages['GridSwap'] = %w{ Adj Grid GridMetric }
+  requiredPackages['GridCAD'] = %w{ Adj Grid GridMetric }
+  requiredPackages['GridInsert'] = %w{ Adj Grid GridMetric GridSwap GridCAD }
+  requiredPackages['Layer'] = %w{ Adj Near Intersect Grid GridMetric GridCAD GridInsert }
+
+  requiredPackages[@extension].each { |extension| buildOnly extension }
+  buildOnly @extension
  end
 
 end
