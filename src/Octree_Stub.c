d6115f3be43e54da9790716fd8bcf90b0d1432aa
Mike Park <Mike.Park@nasa.gov>
2003-11-26 19:43:00 +0000

    added octree stub lib

---
+++ src/Octree_Stub.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include "MeatLib/ErrMgr.h"
+/*============================================================================
+ * func OctreeUtils_GetBackgroundSolution - Stub for Octree evaluator 
+ *============================================================================*/
+int OctreeUtils_GetBackgroundSolution(double *x, double *y, double *z, double *s)
+{
+  ErrMgr_Set(__FILE__,__LINE__,"OctreeUtils_GetBackgroundSolution: NOT yet supproted");
+  return( 1 );
+}
