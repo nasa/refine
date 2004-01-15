1094671955b18962bbb016e40db3924981ad5364
Mike Park <Mike.Park@nasa.gov>
2004-01-15 21:00:02 +0000

    reduce interface to one procedure. started adde xyz nodes to grid

--- src/gridfortran.c
+++ src/gridfortran.c
@@ -15,20 +15,12 @@
 
 static Grid *grid;
 
-int gridcreate_( int *maxnode, int *maxcell, int *maxface, int *maxedge )
+int gridcreate_( int *nnode, double *x, double *y, double *z )
 {
-  grid = gridCreate( *maxnode, *maxcell, *maxface, *maxedge);
-  printf("%s:%d: created grid obj with %d nodes %d cells\n",
-	 __FILE__, __LINE__,gridMaxNode(grid),gridMaxCell(grid));
+  int node;
+  grid = gridCreate( *nnode, 5000, 5000, 0);
+  for ( node=0; node<*nnode; node++) gridAddNode(grid,x[node],y[node],z[node]);
+  printf("populated grid object with %d nodes\n",gridNNode(grid));
 }
-
-int gridcreate( int *maxnode, int *maxcell, int *maxface, int *maxedge )
-{ return gridcreate_(maxnode, maxcell, maxface, maxedge); }
-
-int gridcreate__( int *maxnode, int *maxcell, int *maxface, int *maxedge )
-{ return gridcreate_(maxnode, maxcell, maxface, maxedge); }
-
-int GRIDCREATE( int *maxnode, int *maxcell, int *maxface, int *maxedge )
-{ return gridcreate_(maxnode, maxcell, maxface, maxedge); }
 
 
