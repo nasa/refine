f0faf30788e82aa89b645cd12d63bb90d72df766
Mike Park <Mike.Park@nasa.gov>
2004-01-15 21:52:04 +0000

    clean up debug stuff

--- src/gridfortran.c
+++ src/gridfortran.c
@@ -25,22 +25,13 @@ int gridcreate_( int *nnode, double *x, double *y, double *z ,
   grid = gridCreate( *nnode, *ncell, 5000, 0);
   for ( node=0; node<*nnode; node++) gridAddNode(grid,x[node],y[node],z[node]);
   printf("populated grid object with %d nodes\n",gridNNode(grid));
-  for ( cell=0; cell<*ncell; cell++) {
-    gridAddCell( grid,
-		 c2n[cell+0*(*maxcell)] - 1,
-		 c2n[cell+1*(*maxcell)] - 1,
-		 c2n[cell+2*(*maxcell)] - 1,
-		 c2n[cell+3*(*maxcell)] - 1 );
-    gridCell(grid,cell,nodes);
-    printf(" %8d AR %25.15e\n",cell,gridAR(grid,nodes));
-    for(node=0;node<4;node++){
-      gridNodeXYZ(grid,nodes[node],xyz);
-      printf(" %2d AR %23.15e%23.15e%23.15e\n",
-	     node,xyz[0],xyz[1],xyz[2]);
-    }
-  }
+  for ( cell=0; cell<*ncell; cell++) gridAddCell( grid,
+						  c2n[cell+0*(*maxcell)] - 1,
+						  c2n[cell+1*(*maxcell)] - 1,
+						  c2n[cell+2*(*maxcell)] - 1,
+						  c2n[cell+3*(*maxcell)] - 1 );
   printf("populated grid object with %d cells\n",gridNCell(grid));
-  printf(" min AR %25.15e\n",gridMinAR(grid));
+  printf(" min AR %17.15f\n",gridMinAR(grid));
 }
 
 
