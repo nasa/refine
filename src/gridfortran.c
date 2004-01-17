8aaad7a3137e536c2e23f8e0b301662071fbc2cb
Mike Park <Mike.Park@nasa.gov>
2004-01-17 02:59:35 +0000

    got rid to max node bump illistrating the cell problem

--- src/gridfortran.c
+++ src/gridfortran.c
@@ -25,7 +25,7 @@ int gridcreate_( int *nnode, double *x, double *y, double *z ,
   int node, cell;
   int nodes[4];
   double xyz[3];
-  grid = gridCreate( *nnode+5000, *ncell+30000, 5000, 0);
+  grid = gridCreate( *nnode, *ncell+30000, 5000, 0);
   for ( node=0; node<*nnode; node++) gridAddNode(grid,x[node],y[node],z[node]);
   printf("populated grid object with %d nodes\n",gridNNode(grid));
   for ( cell=0; cell<*ncell; cell++) gridAddCell( grid,
