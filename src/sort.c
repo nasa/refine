e91d72ddad8500739027da73b67e345f6342d706
Mike Park <Mike.Park@nasa.gov>
2004-01-30 16:04:51 +0000

    put in Num Rec in C code now debugging

--- src/sort.c
+++ src/sort.c
@@ -15,8 +15,41 @@
 
 void sortHeap( int length, int *arrayInput, int *sortedIndex  )
 {
-  int i;
+  int i,l,j,ir, indxt,q;
+
+  if(0==length) return;
   for(i=0;i<length;i++) sortedIndex[i] = i;
+  if(1==length) return;
+
+  l=(length >> 1)+1; 
+  ir=length;
+  for (;;) {
+    if (l > 1) {
+      indxt=sortedIndex[--l]; 
+      q=arrayInput[indxt];
+    } else {
+      indxt=sortedIndex[ir];  
+      q=arrayInput[indxt];
+      sortedIndex[ir]=sortedIndex[1];
+      if (--ir == 1) {
+	sortedIndex[1]=indxt;
+	break; 
+      } 
+    }
+    i=l;
+    j=l+l;
+
+    while (j <= ir) { 
+      if ( j < ir && 
+	   arrayInput[sortedIndex[j]] < arrayInput[sortedIndex[j+1]]) j++; 
+      if (q < arrayInput[sortedIndex[j]]) { 
+	sortedIndex[i]=sortedIndex[j];
+	i=j; 
+	j <<= 1; 
+      } else break; 
+    }
+    sortedIndex[i]=indxt;
+  } 
 }
 
 int sortSearch( int length, int *arrayInput, int index  )
