014affe2f6090c10ec671cc883ecc6ae5ba6782b
Mike Park <Mike.Park@nasa.gov>
2004-01-30 16:47:18 +0000

    put in test for all pertibations of three number ordering

--- src/sortMT.rb
+++ src/sortMT.rb
@@ -23,11 +23,39 @@ class TestSort < Test::Unit::TestCase
   assert_equal [0], Sort.Heap([7])
  end
 
- def testLengthTwoHeapSort
+ def testLengthTwoHeapSortOrdered
   assert_equal [0,1], Sort.Heap([5,7])
  end
 
- def testLengthZeroBinarySearch
+ def testLengthTwoHeapSortReversed
+  assert_equal [1,0], Sort.Heap([67,2])
+ end
+
+ def testLengthThreeHeapSort012
+  assert_equal [0,1,2], Sort.Heap([5,7,9])
+ end
+
+ def testLengthThreeHeapSort021
+  assert_equal [0,2,1], Sort.Heap([5,10,9])
+ end
+
+ def testLengthThreeHeapSort102
+  assert_equal [1,0,2], Sort.Heap([2,1,3])
+ end
+
+ def testLengthThreeHeapSort120
+  assert_equal [1,2,0], Sort.Heap([17,5,15])
+ end
+
+ def testLengthThreeHeapSort201
+  assert_equal [2,0,1], Sort.Heap([234,465,2])
+ end
+
+ def testLengthThreeHeapSort210
+  assert_equal [2,1,0], Sort.Heap([1234,465,2])
+ end
+
+ def XtestLengthZeroBinarySearch
   assert_equal EMPTY, Sort.Search([],-1)
   assert_equal EMPTY, Sort.Search([],0)
   assert_equal EMPTY, Sort.Search([],1)
