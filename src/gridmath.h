e5eb2954e1d460328d519c27065d65330dbdddcb
Mike Park <Mike.Park@nasa.gov>
2004-05-11 13:48:13 +0000

    added a add in place menthod

--- src/gridmath.h
+++ src/gridmath.h
@@ -23,6 +23,11 @@ BEGIN_C_DECLORATION
 (result)[1] = (v1)[1] - (v2)[1]; \
 (result)[2] = (v1)[2] - (v2)[2];
 
+#define gridAddToVector(v,add) \
+(v)[0] += (add)[0]; \
+(v)[1] += (add)[1]; \
+(v)[2] += (add)[2];
+
 #define gridDotProduct(v1, v2) \
 ( (v1)[0]*(v2)[0] +  (v1)[1]*(v2)[1] + (v1)[2]*(v2)[2] )
 
