e36ad66be0a3082d0a4ad20206b2908e17be88e1
Beth Lee-Rausch <Elizabeth.M.Lee-Rausch@nasa.gov>
2004-06-09 12:51:00 +0000

    removed warning message now that libtool works correctly on cfdlab computers

--- autogen.sh
+++ autogen.sh
@@ -13,22 +13,6 @@ if [ ! -d $arch ]; then
 fi
 
 echo "Running bootstrap ..."
-if [ ! -e $arch/acinclude.m4 ] ; then
-  echo "***********************************************************************"
-  echo "**"
-  echo "** WARNING: No \"acinclude.m4\" file found in \"$arch/\"."
-  echo "**          You may wish to copy \"libtool.m4\" into"
-  echo "**          \"$arch/acinclude.m4\" from your libtool/aclocal"
-  echo "**          installation for the most current libtool configuration."
-  echo "**"
-  echo "**          Check your libtool installation for location."
-  echo "**"
-  echo "**          Example: \"/usr/local/libtool/share/aclocal/libtool.m4\""
-  echo "**                   or \"/usr/share/aclocal/libtool.m4\""
-  echo "**"
-  echo "***********************************************************************"
-fi
-
 ./bootstrap
 
 echo "Running ../configure --prefix=`pwd` $* ..."
