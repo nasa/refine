e8b4f31d83ec9217c5105505ca4dc135f9bad29e
Mike Park <Mike.Park@nasa.gov>
2004-09-03 12:25:22 +0000

    fixed up to reposrt the number of cells

--- util/cellVerify.m
+++ util/cellVerify.m
@@ -1,41 +1,32 @@
 
 
-# cat fort.2?? | sort | sed -e 's/[a-z]/ /g' > clean
+# cat fort.2?? | sort > allSorted
+# sed -e 's/[a-z]/ /g' < allSorted > clean
 
 if (exist('clean')!=1)
  load clean
 endif
 
-cell_col =1
+cell_col = 3
+local_node_1 = 8
+nnodes0 = 12;
 length = size(clean,1)
 
-if (1==0)
-  cell = clean(:,cell_col);
-  shift = [clean(2:length,cell_col);clean(1,cell_col)];
-  dup = (cell==shift);
-
-  for i=1:length;
-    if (dup(i))
-      printf( "%6d%6d%6d%6d%6d %6d%6d p%3d\n",
-	     clean(i,cell_col),clean(i,4:7),clean(i,8),clean(i,12),clean(i,2));
-    endif
-    if (dup(i))
-      printf( "%6d%6d%6d%6d%6d %6d%6d p%3d\n",
-	     clean(i+1,cell_col),clean(i+1,4:7),clean(i+1,8),clean(i+1,12),clean(i+1,2));
-    endif
-  endfor
-endif
-
-local = (clean(:,8)<=clean(:,12));
-local_test = sum(local);
-local_cells = find(local)
+local = (clean(:,local_node_1)<=clean(:,nnodes0));
+local_test = sum(local)
+local_cells = find(local);
 
 max_cell = max(clean(:,cell_col))
 min_cell = min(clean(:,cell_col))
 
-
 hits = zeros(max_cell,1);
 hits(clean(local_cells,cell_col))++;
+
+[max_hits, max_hits_index] = max(hits)
+[min_hits, min_hits_index] = min(hits)
 hits--;
 
 bad_cells = find(hits)
+
+hits(bad_cells)
+
