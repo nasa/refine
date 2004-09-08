1e964af4679ed86a41b72db3d1e2e092323465fa
Mike Park <Mike.Park@nasa.gov>
2004-09-08 00:08:09 +0000

    found a cell global used twice

--- util/cellVerify.m
+++ util/cellVerify.m
@@ -9,6 +9,7 @@ endif
 
 cell_col = 1
 local_node_1 = 8
+global_node1 = 4
 nnodes0 = 12;
 length = size(clean,1)
 
@@ -30,3 +31,29 @@ bad_cells = find(hits)
 
 hits(bad_cells)
 
+nodes = zeros(4,max_cell);
+for line = 1:max_cell
+  cell = clean(line,cell_col);
+  if (nodes(1,cell)==0)
+    nodes(1,cell) = clean(line,global_node1+0);
+    nodes(2,cell) = clean(line,global_node1+1);
+    nodes(3,cell) = clean(line,global_node1+2);
+    nodes(4,cell) = clean(line,global_node1+3);
+  else
+    if (nodes(1,cell) != clean(line,global_node1+0) || \
+	nodes(2,cell) != clean(line,global_node1+1) || \
+	nodes(3,cell) != clean(line,global_node1+2) || \
+	nodes(4,cell) != clean(line,global_node1+3) ) 
+      line
+      cell
+      nodes(:,cell)
+      clean(line,global_node1:global_node1+3)
+      clean(line,:)
+    endif
+  endif
+endfor
+
+				# line 283532
+				# cell 251211
+				# first 3952 4351 4222 136
+				# next 32065  57518  46704  18643
