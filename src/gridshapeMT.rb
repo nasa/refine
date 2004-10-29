768e71a25bb2c7ee564ec16ddbd2a86f914f5309
Mike Park <Mike.Park@nasa.gov>
2004-10-29 21:29:19 +0000

    fixed up unit tests for fix to second order phi

--- src/gridshapeMT.rb
+++ src/gridshapeMT.rb
@@ -165,6 +165,34 @@ class TestGridShape < Test::Unit::TestCase
   assert_in_delta(0.0,jac[6],tol)
   assert_in_delta(0.0,jac[7],tol)
   assert_in_delta(1.0,jac[8],tol)
+
+  where = [0.0, 0.0, 0.0]
+  
+  jac = @g.shapeJacobian2(n0,n1,n2,n3,e01,e02,e03,e12,e13,e23,where)
+  tol=1.0e-14
+  assert_in_delta(1.0,jac[0],tol)
+  assert_in_delta(0.0,jac[1],tol)
+  assert_in_delta(0.0,jac[2],tol)
+  assert_in_delta(0.0,jac[3],tol)
+  assert_in_delta(1.0,jac[4],tol)
+  assert_in_delta(0.0,jac[5],tol)
+  assert_in_delta(0.0,jac[6],tol)
+  assert_in_delta(0.0,jac[7],tol)
+  assert_in_delta(1.0,jac[8],tol)
+
+  where = [0.0, 1.0, 0.0]
+  
+  jac = @g.shapeJacobian2(n0,n1,n2,n3,e01,e02,e03,e12,e13,e23,where)
+  tol=1.0e-14
+  assert_in_delta(1.0,jac[0],tol)
+  assert_in_delta(0.0,jac[1],tol)
+  assert_in_delta(0.0,jac[2],tol)
+  assert_in_delta(0.0,jac[3],tol)
+  assert_in_delta(1.0,jac[4],tol)
+  assert_in_delta(0.0,jac[5],tol)
+  assert_in_delta(0.0,jac[6],tol)
+  assert_in_delta(0.0,jac[7],tol)
+  assert_in_delta(1.0,jac[8],tol)
  end
 
  def testSecondOrderLagrangeJacobianIceCreamCone
@@ -206,7 +234,7 @@ class TestGridShape < Test::Unit::TestCase
   assert_in_delta(1.0,jac[5],tol)
   assert_in_delta(0.0,jac[6],tol)
   assert_in_delta(0.0,jac[7],tol)
-  assert_in_delta(5.0,jac[8],tol)
+  assert_in_delta(3.0,jac[8],tol)
  end
 
  def testSecondOrderLagrangeJacobianPartialIceCreamCone
@@ -234,7 +262,7 @@ class TestGridShape < Test::Unit::TestCase
   assert_in_delta(1.0,jac[5],tol)
   assert_in_delta(0.0,jac[6],tol)
   assert_in_delta(0.0,jac[7],tol)
-  assert_in_delta(5.0,jac[8],tol)
+  assert_in_delta(3.0,jac[8],tol)
  end
 
  def testSecondOrderLagrangeJacobianInvertedEdge01
