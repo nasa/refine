9b82d1e0bce9b6db3efaa97301f87a60bc48236b
Mike Park <Mike.Park@nasa.gov>
2004-11-17 23:19:10 +0000

    corrected it to run in linear metric not spacing

--- util/variableMetricRatio.m
+++ util/variableMetricRatio.m
@@ -1,25 +1,27 @@
 1;
 
-function s = matric(x)
-  s0 = 0.1;
-  s1 = 1.0;
-  s = s0 * (1-x) + s1 * x; 
+function m = metric(x)
+  m0 = (0.1)^-2;
+  m1 = (1.0)^-2;
+  m = m0 * (1-x) + m1 * x;
 endfunction
 
-s0_0 = matric(0)
-s1_0 = matric(1)
-s0_5 = matric(0.5)
+s0_0 = metric(0)
+s1_0 = metric(1)
+s0_5 = metric(0.5)
 
 x = 0;
 ratio = 0;
 step = 0;
-w = 0.5
+w = 1.0
 while (x < 1)
   step+=1
   x0 = x
-  s0 = matric(x0)
+  m0 = metric(x0)
+  s0 = 1/sqrt(m0)
   x1 = min(1.0,x0+w*s0)
-  s1 = matric(x1)
+  m1 = metric(x1)
+  s1 = 1/sqrt(m1)
   dx = (x1-x0)
   x += dx
   ratio += 0.5*(s0+s1)*dx
