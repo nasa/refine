76d9e57c9d7f31e59b7450431e46ad9a4badd087
Mike Park <Mike.Park@nasa.gov>
2004-11-23 04:47:55 +0000

    reformatted to give the correct metric calculation

--- util/variableMetricRatio.m
+++ util/variableMetricRatio.m
@@ -1,28 +1,39 @@
 1;
 
 function m = metric(x)
-  m0 = (0.1)^-2;
+  m0 = (0.001)^-2;
   m1 = (1.0)^-2;
   m = m0 * (1-x) + m1 * x;
 endfunction
 
-s0_0 = metric(0)
-s1_0 = metric(1)
-s0_5 = metric(0.5)
+s0_0 = sqrt(metric(0))
+s1_0 = sqrt(metric(1))
+s0_5 = sqrt(metric(0.5))
 
-x = 0;
-ratio = 0;
-step = 0;
-w = 1.0
-while (x < 1)
-  step+=1
-  x0 = x
-  m0 = metric(x0)
-  s0 = 1/sqrt(m0)
-  x1 = min(1.0,x0+w*s0)
-  m1 = metric(x1)
-  s1 = 1/sqrt(m1)
-  dx = (x1-x0)
-  x += dx
-  ratio += 0.5*(s0+s1)*dx
-endwhile
+function integrated_length(w)
+  w
+  x = 0;
+  ratio = 0;
+  step = 0;
+  while (x < 1)
+    step+=1;
+    x0 = x;
+    m0 = metric(x0);
+    s0 = 1/sqrt(m0);
+    x1 = min(1.0,x0+w*s0);
+    m1 = metric(x1);
+    s1 = 1/sqrt(m1);
+    dx = (x1-x0);
+    x += dx;
+    m = 0.5*(m0+m1);
+    ratio += sqrt(dx*m*dx);
+  endwhile
+  steps=step
+  ratio
+endfunction
+
+integrated_length(4)
+integrated_length(2)
+integrated_length(1)
+integrated_length(.1)
+integrated_length(.05)
