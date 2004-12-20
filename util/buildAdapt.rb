2c803beb3a0012c841c1390f7bd8ddc68e70b41c
Mike Park <Mike.Park@nasa.gov>
2004-12-20 18:49:31 +0000

    created objectsw for each package

--- util/buildAdapt.rb
+++ util/buildAdapt.rb
@@ -1,21 +1,43 @@
 #!/usr/bin/env ruby
 
-def kill working_copy
- `rm -rf ${working_copy}`
+class Package
+
+ def initialize name
+  @name = name
+  @repository = case name
+		when "CAPRI", "SDK"
+		 "-d :ext:geolab:/usr/local/inhouse/CVS"
+		when "refine"
+		 "-d :ext:cmb20:/ump/fldmd/home/mikepark/cvsroot"
+		when "HEFSS.rps"
+		 "-d :ext:hefss-core:/usr/local/cvsroot"
+		else
+		 puts "repository unknown: "+package
+		 nil
+		end
+  @where=`pwd`
+ end
+
+ def kill
+  `rm -rf #@name`
+  self
+ end
+
+ def check_out
+  command = "cvs -q #@repository co -P #@name"
+  puts command
+  `#{command}`
+  self
+ end
+
+ def clean_check_out
+  kill
+  check_out
+  self
+ end
 end
 
-def check_out package
- repository = case package
-	      when "CAPRI", "SDK"
-	       "-d :ext:geolab:/usr/local/inhouse/CVS"
-	      when "refine"
-	       "-d :ext:cmb20:/ump/fldmd/home/mikepark/cvsroot"
-	      when "HEFSS.rps"
-	       "-d :ext:hefss-core:/usr/local/cvsroot"
-	      else
-	       puts "repository unknown: "+package
-	       nil
-	      end
- `cvs -q ${repository} co -P ${package}`
-end
-
+capri = Package.new("CAPRI")
+sdk = Package.new("SDK")
+refine = Package.new("refine")
+hefss = Package.new("HEFSS.rps")
