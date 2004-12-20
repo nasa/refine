ceb5229d8b5f996a1e3ca53d5427341c623ae3b3
Mike Park <Mike.Park@nasa.gov>
2004-12-20 18:26:03 +0000

    added code to manipulate repositories

--- util/buildAdapt.rb
+++ util/buildAdapt.rb
@@ -1,3 +1,21 @@
 #!/usr/bin/env ruby
 
-puts "hello world"
+def kill working_copy
+ `rm -rf ${working_copy}`
+end
+
+def check_out package
+ repository = case package
+	      when "CAPRI", "SDK"
+	       "-d :ext:geolab:/usr/local/inhouse/CVS"
+	      when "refine"
+	       "-d :ext:cmb20:/ump/fldmd/home/mikepark/cvsroot"
+	      when "HEFSS.rps"
+	       "-d :ext:hefss-core:/usr/local/cvsroot"
+	      else
+	       puts "repository unknown: "+package
+	       nil
+	      end
+ `cvs -q ${repository} co -P ${package}`
+end
+
