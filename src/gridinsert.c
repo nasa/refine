db4d7288b34029b676bc3f477138b6d867f99a9b
Mike Park <Mike.Park@nasa.gov>
2005-02-01 02:27:38 +0000

    (gridSplitEdgeAt,gridCollapseEdge) only perform projection when gridCostConstraint contains gridCOST_CNST_VALID

--- src/gridinsert.c
+++ src/gridinsert.c
@@ -282,7 +282,8 @@ int gridSplitEdgeAt(Grid *grid, Queue *queue, int n0, int n1,
     newedge1 = gridAddEdgeAndQueue(grid,queue,n1,newnode,edgeId,t1,newT);
   }
 
-  gridProjectNode(grid, newnode );
+  if (gridCostConstraint(grid)&gridCOST_CNST_VALID)
+    gridProjectNode(grid, newnode );
 
   /* find the worst cell */
   minAR = 2.0; 
@@ -795,7 +796,8 @@ Grid *gridCollapseEdge(Grid *grid, Queue *queue, int n0, int n1,
 
   /* project and match node locations and parmeters */
   gridSetNodeXYZ( grid, n0, xyzAvg);
-  gridProjectNode(grid, n0 );
+  if (gridCostConstraint(grid)&gridCOST_CNST_VALID)
+    gridProjectNode(grid, n0 );
   gridNodeXYZ( grid, n0, xyzAvg);
   gridSetNodeXYZ( grid, n1, xyzAvg); 
  
