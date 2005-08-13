48dfd27cbdcfddfe746cb5822fce9c2a2e73f8b0
Mike Park <Mike.Park@nasa.gov>
2005-08-13 17:15:29 +0000

    made tri and square methods

--- src/ringMT.rb
+++ src/ringMT.rb
@@ -20,10 +20,17 @@ class TestRing < Test::Unit::TestCase
  end
  def setup ; set_up ; end
 
- def add_right_ring_segments(n0,n1,n2)
+ def add_triangular_ring_segments(n0,n1,n2)
   @ring.addSegment(0,1,n0,n1)
   @ring.addSegment(1,2,n1,n2)
   @ring.addSegment(2,0,n2,n0)
+ end
+ 
+ def add_square_ring_segments(n0,n1,n2,n3)
+  @ring.addSegment(0,1,n0,n1)
+  @ring.addSegment(1,2,n1,n2)
+  @ring.addSegment(2,3,n2,n3)
+  @ring.addSegment(3,0,n3,n0)
  end
  
  # init
@@ -111,7 +118,7 @@ class TestRing < Test::Unit::TestCase
 
  def test_ring_does_not_intersect_a_completing_segment
   n0 = [0.0,0.0] ; n1 = [1.0,0.0] ; n2 = [0.0,1.0]
-  add_right_ring_segments(n0,n1,n2)
+  add_triangular_ring_segments(n0,n1,n2)
   assert !@ring.intersectsSegment(1,0,n1,n0)
   assert !@ring.intersectsSegment(2,1,n2,n1)
   assert !@ring.intersectsSegment(0,2,n0,n2)  
@@ -119,7 +126,7 @@ class TestRing < Test::Unit::TestCase
 
  def test_ring_intersects_a_duplicate_segment
   n0 = [0.0,0.0] ; n1 = [1.0,0.0] ; n2 = [0.0,1.0]
-  add_right_ring_segments(n0,n1,n2)
+  add_triangular_ring_segments(n0,n1,n2)
   assert @ring.intersectsSegment(0,1,n0,n1)
   assert @ring.intersectsSegment(1,2,n1,n2)
   assert @ring.intersectsSegment(2,0,n2,n0)  
@@ -127,40 +134,40 @@ class TestRing < Test::Unit::TestCase
 
  def test_ring_does_not_intersect_an_interior_segment_connected_to_ring
   n0 = [0.0,0.0] ; n1 = [1.0,0.0] ; n2 = [0.0,1.0]
-  add_right_ring_segments(n0,n1,n2)
+  add_triangular_ring_segments(n0,n1,n2)
   assert !@ring.intersectsSegment(0,3,n0,[1.0/3.0,1.0/3.0])
  end
 
  def test_ring_intersects_an_intersecting_segment_connected_to_ring
   n0 = [0.0,0.0] ; n1 = [1.0,0.0] ; n2 = [0.0,1.0]
-  add_right_ring_segments(n0,n1,n2)
+  add_triangular_ring_segments(n0,n1,n2)
   assert @ring.intersectsSegment(0,3,n0,[1.0,1.0])
  end
 
  def test_parallel_overlapping_segment_with_different_nodes_intersects
   n0 = [0.0,0.0] ; n1 = [1.0,0.0] ; n2 = [0.0,1.0]
-  add_right_ring_segments(n0,n1,n2)
+  add_triangular_ring_segments(n0,n1,n2)
   assert @ring.intersectsSegment(3,4,[2.0,-1.0],[-1.00,1.00])
   assert @ring.intersectsSegment(3,4,[0.5, 0.5],[ 0.75,0.25])
  end
 
  def test_parallel_non_overlapping_segment_does_not_intersect
   n0 = [0.0,0.0] ; n1 = [1.0,0.0] ; n2 = [0.0,1.0]
-  add_right_ring_segments(n0,n1,n2)
+  add_triangular_ring_segments(n0,n1,n2)
   assert @ring.intersectsSegment(3,4,[2.0, 0.0],[3.0, 0.0])
   assert @ring.intersectsSegment(3,4,[2.0,-1.0],[3.0,-2.0])
  end
 
  def test_offset_parallel_segment_does_not_intersect
   n0 = [0.0,0.0] ; n1 = [1.0,0.0] ; n2 = [0.0,1.0]
-  add_right_ring_segments(n0,n1,n2)
+  add_triangular_ring_segments(n0,n1,n2)
   assert !@ring.intersectsSegment(3,4,[1.0,1.0],[0.0,2.0])
   assert !@ring.intersectsSegment(3,4,[1.0,2.0],[1.0,3.0])
  end
 
  def test_intersect_ring_point
   n0 = [0.0,0.0] ; n1 = [1.0,0.0] ; n2 = [0.0,1.0]
-  add_right_ring_segments(n0,n1,n2)
+  add_triangular_ring_segments(n0,n1,n2)
   assert @ring.intersectsSegment(3,4,[-1.0, 1.0],[1.0, 1.0])
  end
 
@@ -270,65 +277,65 @@ class TestRing < Test::Unit::TestCase
 
  def test_ring_surrounds_a_completion_triangle_right_handed_uvs
   n0 = [0.0,0.0] ; n1 = [1.0,0.0] ; n2 = [0.0,1.0]
-  add_right_ring_segments(n0,n1,n2)
+  add_triangular_ring_segments(n0,n1,n2)
   assert @ring.surroundsTriangle(0,1,2,n2)
  end
 
  def test_ring_does_not_surrounds_a_negative_area_triangle_right_handed_uvs
   n0 = [0.0,0.0] ; n1 = [1.0,0.0] ; n2 = [0.0,1.0]
-  add_right_ring_segments(n0,n1,n2)
+  add_triangular_ring_segments(n0,n1,n2)
   assert !@ring.surroundsTriangle(0,1,3,[0.0,-1.0])
  end
 
  def test_ring_surrounds_a_completion_triangle_left_handed_uvs
   n0 = [0.0,0.0] ; n1 = [-1.0,0.0] ; n2 = [0.0,1.0]
-  add_right_ring_segments(n0,n1,n2)
+  add_triangular_ring_segments(n0,n1,n2)
   assert @ring.surroundsTriangle(0,1,2,n2)
  end
 
  def test_ring_does_not_surrounds_a_negative_area_triangle_left_handed_uvs
   n0 = [0.0,0.0] ; n1 = [-1.0,0.0] ; n2 = [0.0,1.0]
-  add_right_ring_segments(n0,n1,n2)
+  add_triangular_ring_segments(n0,n1,n2)
   assert !@ring.surroundsTriangle(0,1,3,[0.0,-1.0])
  end
 
  def test_reject_zero_area_triangle_right_handed_uvs
   n0 = [0.0,0.0] ; n1 = [1.0,0.0] ; n2 = [0.0,1.0]
-  add_right_ring_segments(n0,n1,n2)
+  add_triangular_ring_segments(n0,n1,n2)
   assert !@ring.surroundsTriangle(0,1,3,[0.5,0.0])
  end
 
  def test_reject_zero_area_triangle_left_handed_uvs
   n0 = [0.0,0.0] ; n1 = [-1.0,0.0] ; n2 = [0.0,1.0]
-  add_right_ring_segments(n0,n1,n2)
+  add_triangular_ring_segments(n0,n1,n2)
   assert !@ring.surroundsTriangle(0,1,3,[-0.5,0.0])
+ end
+
+ def test_reject_zero_area_triangle_right_handed_uvs
+  n0 = [0.0,0.0] ; n1 = [1.0,0.0] ; n2 = [0.0,1.0]
+  add_triangular_ring_segments(n0,n1,n2)
+  assert !@ring.surroundsTriangle(0,1,3,[0.5,0.0])
  end
 
  # compute total area of the ring
 
  def test_triangle_ring_area_with_right_handed_uvs
   n0 = [0.0,0.0] ; n1 = [1.0,0.0] ; n2 = [0.0,1.0]
-  add_right_ring_segments(n0,n1,n2)
+  add_triangular_ring_segments(n0,n1,n2)
   tol = 1.0e-15
   assert_in_delta(  0.5, @ring.area, tol )
  end
 
  def test_square_ring_area_with_right_handed_uvs
   n0 = [0.0,0.0] ; n1 = [1.0,0.0] ; n2 = [1.0,1.0] ; n3 = [0.0,1.0]
-  @ring.addSegment(0,1,n0,n1)
-  @ring.addSegment(1,2,n1,n2)
-  @ring.addSegment(2,3,n2,n3)
-  @ring.addSegment(3,0,n3,n0)
+  add_square_ring_segments(n0,n1,n2,n3)
   tol = 1.0e-15
   assert_in_delta(  1.0, @ring.area, tol )
  end
 
  def test_square_ring_area_with_left_handed_uvs
   n0 = [0.0,0.0] ; n1 = [-1.0,0.0] ; n2 = [-1.0,1.0] ; n3 = [0.0,1.0]
-  @ring.addSegment(0,1,n0,n1)
-  @ring.addSegment(1,2,n1,n2)
-  @ring.addSegment(2,3,n2,n3)
-  @ring.addSegment(3,0,n3,n0)
+  add_square_ring_segments(n0,n1,n2,n3)
   tol = 1.0e-15
   assert_in_delta( -1.0, @ring.area, tol )
  end
