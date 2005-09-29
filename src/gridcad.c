8559c515ea07d1deadeeaa4f2a7bcbbbfcc7b1c4
Mike Park <Mike.Park@nasa.gov>
2005-09-29 21:11:02 +0000

    cut tries to 4 and put in face smooth if > 5

--- src/gridcad.c
+++ src/gridcad.c
@@ -412,7 +412,11 @@ Grid *gridSequentialEvaluation(Grid *grid)
       tries = 0;
       while (1.0e-14 > gridMinVolume( grid )) {
 	tries++;
-	if (tries>10) return NULL;
+	if (tries>4) return NULL;
+	if (tries>5) {
+	  gridSmoothNodeFaceAreaUV(grid, node );
+	  gridSmoothNodeFaceAreaUV(grid, node );
+	}
 	active_nodes = 0;
 	for( fix_node=0; fix_node < gridMaxNode(grid);fix_node++) {
 	  if ( gridValidNode( grid, fix_node ) ) {
