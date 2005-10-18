5061db2fbe8185884e9a7df7d7562dc0ee1cab59
Mike Park <Mike.Park@nasa.gov>
2005-10-18 15:25:29 +0000

    made two edge grdi and fixed one edge grid

--- src/gridedgerMT.rb
+++ src/gridedgerMT.rb
@@ -34,28 +34,25 @@ class TestGridEdger < Test::Unit::TestCase
   edgeId = 1
   grid.addEdge(0,1,edgeId,0.0,1.0)
 
+  #  2
+  # 0 1
+  #  3
+
   grid.addFaceUV(0, 10.0, 20.0,
+                 1, 11.0, 20.0,
                  2, 10.5, 20.5,
-                 1, 11.0, 20.5,
                  1)
 
   grid.addFaceUV(0, 10.0, 20.0,
-                 1, 11.0, 20.5,
                  3, 10.5, 19.5,
+                 1, 11.0, 20.0,
                  2)
+
   grid.setNGeomNode(2)
   grid.setNGeomEdge(1)
   grid.setNGeomFace(2)
 
   grid.addGeomEdge(edgeId,0,1)
-
- end
-
- def test_initialize
-  grid = Grid.new(0,0,0,0)
-  edgeId = 5
-  assert_not_nil ge = GridEdger.new(grid,edgeId)
-  assert_equal( edgeId, ge.edgeId )
  end
 
  def test_single_edge_setup
@@ -70,6 +67,70 @@ class TestGridEdger < Test::Unit::TestCase
 
   assert_equal [0, 1],    grid.geomCurve(1,0)
   assert_equal [0.0, 1.0], grid.geomCurveT(1,0)
+ end
+
+ def two_edge_grid
+  grid = Grid.new(5,0,4,2)
+  grid.addNode(0.0,0.0,0.0)
+  grid.addNode(3.0,0.0,0.0)
+  grid.addNode(1.0,0.0,0.0)
+  grid.addNode(0.5,0.5,0.0)
+  grid.addNode(0.5,-0.5,0.0)
+
+  edgeId = 1
+  grid.addEdge(0,2,edgeId,0.0,1.0)
+  grid.addEdge(2,1,edgeId,1.0,3.0)
+
+  #  3
+  # 0 2  1
+  #  4
+
+  grid.addFaceUV(0, 10.0, 20.0,
+                 2, 11.0, 20.0,
+                 3, 10.5, 20.5,
+                 1)
+  grid.addFaceUV(2, 11.0, 20.0,
+                 1, 13.0, 20.0,
+                 3, 10.5, 20.5,
+                 1)
+
+  grid.addFaceUV(0, 10.0, 20.0,
+                 4, 10.5, 19.5,
+                 2, 11.0, 20.0,
+                 2)
+  grid.addFaceUV(2, 11.0, 20.0,
+                 3, 10.5, 19.5,
+                 1, 13.0, 20.0,
+                 2)
+
+  grid.setNGeomNode(2)
+  grid.setNGeomEdge(1)
+  grid.setNGeomFace(2)
+
+  grid.addGeomEdge(edgeId,0,1)
+ end
+
+ def test_two_edge_setup
+  grid = two_edge_grid
+
+  assert grid.geometryNode(0)
+  assert grid.geometryNode(1)
+  assert grid.geometryEdge(2)
+  assert grid.geometryFace(3)
+  assert grid.geometryFace(4)
+
+  assert_equal [0, 2, 1], grid.geomEdge(1)
+
+  assert_equal [0, 2, 1],    grid.geomCurve(1,0)
+  assert_equal [1, 2, 0],    grid.geomCurve(1,1)
+  assert_equal [0.0, 1.0, 3.0], grid.geomCurveT(1,0)
+ end
+
+ def test_initialize
+  grid = Grid.new(0,0,0,0)
+  edgeId = 5
+  assert_not_nil ge = GridEdger.new(grid,edgeId)
+  assert_equal( edgeId, ge.edgeId )
  end
 
 end
