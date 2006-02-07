af54f06151c5e58b5e93efa8df58a41b219deb85
Mike Park <Mike.Park@nasa.gov>
2006-02-07 13:47:07 +0000

    added an octave spike for lp untangling

---
+++ util/tableau.m
@@ -0,0 +1,62 @@
+function [optx,zmin] = tableau(c,A,b)
+
+[m,n]=size(A)
+
+B = eye(m)
+Binv = B
+
+x = zeros(m,1)
+x(m)=1
+
+cB = c(n-m+1:n)
+
+t = [ -cB'*b  c'-cB'*Binv*A
+      Binv*b Binv*A]
+
+basis = [n-m+1:n]
+
+
+[min_reducted_cost, pivot_col] = min(t(1,2:n+1));
+min_reducted_cost
+pivot_col = pivot_col +1
+
+while (min_reducted_cost<0)
+
+  pivot_row = 0;
+  step_length = inf;
+  for i=2:m+1
+    if (t(i,pivot_col)>0)
+      this_step = t(i,1)./t(i,pivot_col)
+      if (this_step<step_length)
+	pivot_row=i;
+	step_length = this_step;
+      end
+    end
+  end
+  pivot_row
+  step_length
+
+
+  disp('flip basis')
+  basis(pivot_row-1) = pivot_col-1
+  disp('normalize row')
+  t(pivot_row,:) = t(pivot_row,:)./t(pivot_row,pivot_col)
+
+  disp('elimate other rows')
+  for i=1:m+1
+    if (i!=pivot_row)
+      t(i,:) = t(i,:) - t(pivot_row,:)*t(i,pivot_col);
+    end
+  end
+  t
+  [min_reducted_cost, pivot_col] = min(t(1,2:n+1));
+  min_reducted_cost
+  pivot_col = pivot_col +1
+
+end
+
+optx = zeros(n,1);
+for i = 1:m
+  optx(basis(i)) = t(i+1,1);
+end
+zmin = -t(1,1);
---
+++ util/untangle2.m
@@ -0,0 +1,62 @@
+
+grid_x = [ 0 1 1 0 ]';
+grid_y = [ 0 0 1 1 ]';
+#grid_x = [ 0 1.2 1.3 0 ]';
+#grid_y = [ 0 0 1.4 1.5 ]';
+#grid_x = [ 0 1 0.5 0 ]';
+#grid_y = [ 0 0 0.5 1 ]';
+#grid_x = [    0 1.2 0.3 0.1 ]';
+#grid_y = [ -0.1 0.0 0.2 1.5 ]';
+
+degree = size(grid_x,1)
+
+a=[];
+c=[];
+for i = 1:degree
+  j = i+1;
+  if (i==degree)
+    j = 1;
+  end
+  ax=-0.5*(grid_y(i)-grid_y(j));
+  ay=-0.5*(grid_x(j)-grid_x(i));
+  a=[a [ax;ay;1] ];
+  ci = 0.5*(grid_x(i)*grid_y(j) - grid_x(j)*grid_y(i));
+  c=[c;ci];
+end
+a = [a eye(3) ];
+M=sum(c);
+c = [c; [ M; M; M ]];
+
+b=[0;0;1];
+
+a
+c
+b
+
+[optx,zmin] = tableau(c,a,b)
+
+r = find(optx)
+
+xy = a(:,r)'\c(r)
+
+
+data = [];
+for i = 1:degree
+  j = i+1;
+  if (i==degree)
+    j = 1;
+  end
+  data = [ data 
+	  xy(1) xy(2)
+	  grid_x(i) grid_y(i) 
+	  grid_x(j) grid_y(j) 
+	  xy(1) xy(2)
+	  ];
+end
+
+if (1)
+  plot(data(:,1),data(:,2))
+  disp('press key')
+  pause
+end
+
