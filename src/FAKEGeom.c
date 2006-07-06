7312902e08072281499015181e7f7c995282f710
Mike Park <Mike.Park@nasa.gov>
2006-07-06 16:31:21 +0000

    * src/FAKEGeom.c rewound to previous version, needed for unit tests

--- src/FAKEGeom.c
+++ src/FAKEGeom.c
@@ -13,20 +13,47 @@
 GridBool CADGeom_NearestOnEdge(int vol, int edgeId, 
 			   double *xyz, double *t, double *xyznew)
 {
-  return FALSE;
+  *t = xyz[0];
+  xyznew[0] = xyz[0];
+  xyznew[1] = 0.0;
+  xyznew[2] = 0.0;
+
+  return TRUE;
 }
 
 GridBool CADGeom_NearestOnFace(int vol, int faceId, 
 			   double *xyz, double *uv, double *xyznew)
 {
-  return FALSE;
+  uv[0] = xyz[0]+10.0;
+  uv[1] = xyz[1]+20.0;
+  xyznew[0] = xyz[0];
+  xyznew[1] = xyz[1];
+  xyznew[2] = 0.0;
+
+  return TRUE;
 }
 
 GridBool CADGeom_PointOnEdge(int vol, int edgeId, 
 			 double t, double *xyz, 
 			 int derivativeFlag, double *dt, double *dtdt )
 {
-  return FALSE;
+  xyz[0] = t;
+  xyz[1] = 0.0;
+  xyz[2] = 0.0;
+
+  if (derivativeFlag > 0){
+    dt[0] = 1.0;
+    dt[1] = 0.0;
+    dt[2] = 0.0;
+  }
+
+  if (derivativeFlag > 1){
+    dtdt[0] = 0.0;
+    dtdt[1] = 0.0;
+    dtdt[2] = 0.0;
+  }
+
+  return TRUE;
 }
 
 GridBool CADGeom_PointOnFace(int vol, int faceId, 
@@ -34,11 +61,44 @@ GridBool CADGeom_PointOnFace(int vol, int faceId,
 			 int derivativeFlag, double *du, double *dv,
 			 double *dudu, double *dudv, double *dvdv )
 {
-  return FALSE;
+  xyz[0] = uv[0]-10.0;
+  xyz[1] = uv[1]-20.0;
+  xyz[2] = 0.0;
+
+  if (derivativeFlag > 0){
+    du[0] = 1.0;
+    du[1] = 0.0;
+    du[2] = 0.0;
+    dv[0] = 0.0;
+    dv[1] = 1.0;
+    dv[2] = 0.0;
+  }
+
+  if (derivativeFlag > 1){
+    dudu[0] = 0.0;
+    dudu[1] = 0.0;
+    dudu[2] = 0.0;
+    dudv[0] = 0.0;
+    dudv[1] = 0.0;
+    dudv[2] = 0.0;
+    dvdv[0] = 0.0;
+    dvdv[1] = 0.0;
+    dvdv[2] = 0.0;
+  }
+
+  return TRUE;
 }
 
 GridBool CADGeom_NormalToFace( int vol, int faceId, 
 			       double *uv, double *xyz, double *normal)
 {
-  return FALSE;
+  xyz[0] = uv[0]-10.0;
+  xyz[1] = uv[1]-20.0;
+  xyz[2] = 0.0;
+
+  normal[0] =  0.0;
+  normal[1] =  0.0;
+  normal[2] = -1.0;
+
+  return TRUE;
 }
