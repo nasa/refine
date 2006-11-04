c4ce945a206a134a338aa512befa929cc242729f
Mike Park <Mike.Park@nasa.gov>
2006-11-04 14:11:35 +0000

    to amek fast tec

---
+++ util/fast2tec.rb
@@ -0,0 +1,11 @@
+
+refine_path = File.expand_path("~/GRIDEX/refine/src")
+
+$:.push refine_path
+
+require 'Adj/Adj'
+require 'Line/Line'
+require 'Grid/Grid'
+
+Grid.from_FAST(ARGV[0]).writeTecplotSurfaceGeom
+
