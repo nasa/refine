f7e4320537b39997ef3c0b1f18795f6cf0e34dd0
Mike Park <MikePark.rb@GMail.com>
2008-12-16 11:10:41 -0500

    suppress unused from grid callbacks

--- src/gridedger.c
+++ src/gridedger.c
@@ -71,6 +71,19 @@ void gridedgerPack(void *voidGridEdger,
 {
   int i;
   GridEdger *ge = (GridEdger *)voidGridEdger;
+
+  SUPRESS_UNUSED_COMPILER_WARNING(nnode);
+  SUPRESS_UNUSED_COMPILER_WARNING(maxnode);
+  SUPRESS_UNUSED_COMPILER_WARNING(ncell);
+  SUPRESS_UNUSED_COMPILER_WARNING(maxcell);
+  SUPRESS_UNUSED_COMPILER_WARNING(cello2n);
+  SUPRESS_UNUSED_COMPILER_WARNING(nface);
+  SUPRESS_UNUSED_COMPILER_WARNING(maxface);
+  SUPRESS_UNUSED_COMPILER_WARNING(faceo2n);
+  SUPRESS_UNUSED_COMPILER_WARNING(nedge);
+  SUPRESS_UNUSED_COMPILER_WARNING(maxedge);
+  SUPRESS_UNUSED_COMPILER_WARNING(edgeo2n);
+
   for ( i = 0 ; i < gridedgerUnusedNodes(ge) ; i++ ) {
     ge->unused[i] = nodeo2n[ge->unused[i]];
   }
@@ -80,6 +93,9 @@ void gridedgerSortNode(void *voidGridEdger, int maxnode, int *o2n)
 {
   int i;
   GridEdger *ge = (GridEdger *)voidGridEdger;
+
+  SUPRESS_UNUSED_COMPILER_WARNING(maxnode);
+
   for ( i = 0 ; i < gridedgerUnusedNodes(ge) ; i++ ) {
     ge->unused[i] = o2n[ge->unused[i]];
   }
@@ -88,7 +104,10 @@ void gridedgerSortNode(void *voidGridEdger, int maxnode, int *o2n)
 void gridedgerReallocator(void *voidGridEdger, int reallocType, 
 			  int lastSize, int newSize)
 {
-
+  SUPRESS_UNUSED_COMPILER_WARNING(voidGridEdger);
+  SUPRESS_UNUSED_COMPILER_WARNING(reallocType);
+  SUPRESS_UNUSED_COMPILER_WARNING(lastSize);
+  SUPRESS_UNUSED_COMPILER_WARNING(newSize);
 }
 
 void gridedgerGridHasBeenFreed(void *voidGridEdger )
