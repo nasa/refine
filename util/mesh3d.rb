d53294e01ecf5ba04a9f12a798d0e539f5ef0169
Mike Park <Mike.Park@NASA.Gov>
2010-03-30 15:26:21 -0400

    to postpedn description

--- util/mesh3d.rb
+++ util/mesh3d.rb
@@ -30,3 +30,35 @@ grid = Grid.from_mesh3d(root+'.mesh3D')
 grid.writeVTK(root+'.vtk')
 grid.writeTecplotVolumeGeom
 
+__END__
+
+#Number of Verts
+nVerts
+nVert xyzs
+#Number of Elements
+nTets nPyramids nPrisms nHexas
+nTet indices (4 per element -- 1 bias)
+nPyramid indices (5 per element -- 1 bias)
+nPrism indices (6 per element -- 1 bias)
+nHexa indices (8 per element -- 1 bias)
+#Number of Boundaries, volume element flag
+nBounds eflag
+nBound times:
+ Boundary Name
+ Boundary type
+ Geom index
+ nTris nQuads
+ nTri indices (3 per facet -- 1 bias)
+ nQuad indices (4 per facet -- 1 bias)
+
+The "Geom index" will contain some indication of the originating
+geometry (for adaptation and higher-order vertex fill-in). The
+"Boundary type" should be something like "Wall", "InFlow", "OutFlow",
+"FarField", "Symmetry" or an index pointing to that...
+
+If the eflag is set to 1 then the lines containing the surface element
+indices will contain the number of the attached volume element.
+Otherwise the triangle connectivity will contain 3 numbers and the
+quadrilaterals will contain 4 numbers.
+
+
