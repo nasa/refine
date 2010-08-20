5fff7816ef6e384f2a735935141fa4a0064b05f7
Mike Park <Mike.Park@NASA.Gov>
2010-08-20 08:22:59 -0400

    to discover location of src and build ruby wrapper automatically

--- util/fgrid2gri.rb
+++ util/fgrid2gri.rb
@@ -1,13 +1,20 @@
 #!/usr/bin/env ruby
 
-refine_path = File.expand_path("~/GRIDEX/refine/src")
+starting_path = Dir.pwd
+
+refine_path = "#{File.dirname $0}/../src"
 
 $:.push refine_path
 
-# for Grid...
+Dir.chdir refine_path
+require 'RubyExtensionBuilder'
+
+RubyExtensionBuilder.new('Grid').build
 require 'Adj/Adj'
 require 'Line/Line'
 require 'Grid/Grid'
+
+Dir.chdir starting_path
 
 filename = ARGV[0]
 grid = Grid.from_FAST File.expand_path(filename)
