e7bc1f11f33224a706a5e8f03b4dc06684c8cdc3
Mike Park <Mike.Park@NASA.Gov>
2010-08-23 13:09:42 -0400

    to clean out unit_main.c, saving gridHistogram to px_utils.c

--- src/px_utils.c
+++ src/px_utils.c
@@ -212,3 +212,72 @@ Grid *gridDumpBentEdgesForPX(Grid *grid, int order, char *filename)
 
   return grid;
 }
+
+Grid *gridHistogram( Grid *grid, char *filename ) 
+{
+  int nodes[4];
+  int edge, edgeId;
+  int face, faceId;
+  int cell;
+  double length, volume;
+
+  int bins[200];
+  int bin;
+
+  char fn[1024];
+  FILE *file;
+  
+  if (NULL == filename) {
+    hist_index++;
+    sprintf( fn, "histogram%04d.m", hist_index );
+    printf("dump %s\n",fn);
+    file = fopen( fn, "w" );
+  }else{
+    printf("dump %s\n",filename);
+    file = fopen( filename, "w" );
+  }
+
+  for (bin=0;bin<200;bin++) bins[bin] = 0;
+  gridCreateConn(grid);
+  for ( edge = 0 ; edge < gridNConn(grid) ; edge++ ) {
+    gridConn2Node( grid, edge, nodes );
+    length = gridEdgeRatio(grid, nodes[0], nodes[1]);
+    bin = (int)(log10(length)*100.0)+100;
+    bin = MAX(0,bin);
+    bin = MIN(bin,199);
+    bins[bin]++;
+  }
+  gridEraseConn(grid);
+
+  fprintf( file, "edge_length = [\n" );
+  for (bin=0;bin<200;bin++) 
+    fprintf( file, "%f %d\n", (((double)bin)-100.0)/100.0, bins[bin] );
+  fprintf( file, "];\n" );
+
+  for (bin=0;bin<200;bin++) bins[bin] = 0;
+  for ( cell = 0 ; cell < gridMaxCell(grid) ; cell++ ) {
+    if ( grid == gridCell( grid, cell, nodes ) ) {
+      volume = gridVolume(grid, nodes);
+      bin = (int)(-log10(volume)*10.0);
+      bin = MAX(0,bin);
+      bin = MIN(bin,199);
+      bins[bin]++;
+    }
+  }
+  fprintf( file, "tet_volume = [\n" );
+  for (bin=199;bin>=0;bin--) 
+    fprintf( file, "%f %d\n", -(((double)bin))/10.0, bins[bin] );
+  fprintf( file, "];\n" );
+
+  fprintf( file, "[x,y]=bar(edge_length(:,1), edge_length(:,2));plot(x,y,';;');\n" );
+  fprintf( file, "xlabel('log_{10}(L)')\n" );
+  fprintf( file, "print -deps histogram_edge.eps\n" );
+  fprintf( file, "closeplot\n" );
+  fprintf( file, "[x,y]=bar(tet_volume(:,1), tet_volume(:,2));plot(x,y,';Tetrahedral Volume;');\n" );
+  fprintf( file, "print -deps histogram_vol.eps\n" );
+  fprintf( file, "closeplot\n" );
+
+  fclose(file);
+
+  return grid;
+}
