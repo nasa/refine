b1cd4199b9b981b3ce1ec9336405c50afb21bd14
Mike Park <Mike.Park@NASA.Gov>
2010-09-23 08:14:16 -0400

    to create ruby wrappers

--- util/fast2tec.rb
+++ util/fast2tec.rb
@@ -1,12 +1,20 @@
 #!/usr/bin/env ruby
 
-refine_path = File.expand_path("~/GRIDEX/refine/src")
+starting_path = Dir.pwd
+
+refine_path = "#{File.dirname $0}/../src"
 
 $:.push refine_path
 
+Dir.chdir refine_path
+require 'RubyExtensionBuilder'
+
+RubyExtensionBuilder.new('Grid').build
 require 'Adj/Adj'
 require 'Line/Line'
 require 'Grid/Grid'
+
+Dir.chdir starting_path
 
 Grid.from_FAST(ARGV[0]).writeTecplotSurfaceGeom
 
