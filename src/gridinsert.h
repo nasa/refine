74c95ad631226c780f9b6900749673ec6e61f63e
Mike Park <Mike.Park@NASA.Gov>
2011-02-21 09:27:13 +0100

    toward a single gridsplitedge, removed Repeat and Force

--- src/gridinsert.h
+++ src/gridinsert.h
@@ -21,8 +21,6 @@ Grid *gridAdapt(Grid *g, double minLength, double maxLength );
 
 int gridSplitEdge(Grid *g, int n0, int n1 );
 int gridSplitEdgeRatio(Grid *g, Queue *q, int n0, int n1, double ratio);
-int gridSplitEdgeRepeat(Grid *g, Queue *q, int n0, int n1, GridBool debug_split );
-int gridSplitEdgeForce(Grid *g, Queue *q, int n0, int n1, GridBool debug_split );
 int gridSplitEdgeIfNear(Grid *g, int n0, int n1, double *xyz);
 int gridSplitFaceAt(Grid *g, int *face_nodes, double *xyz);
 int gridSplitCellAt(Grid *g, int cell, double *xyz);
