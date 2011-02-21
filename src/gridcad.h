e12d1246931911742fe402851e8cf2692d1460cd
Mike Park <Mike.Park@NASA.Gov>
2011-02-21 13:31:25 +0100

    to remove thesis spike, gridNewGeometryEdgeSiteAllowedAt, ...IntersectsFace

--- src/gridcad.h
+++ src/gridcad.h
@@ -49,17 +49,6 @@ Grid *gridRobustProject(Grid *g);
 
 Grid *gridWholesaleEvaluation(Grid *g);
 
-GridBool gridNewGeometryEdgeSiteAllowedAt(Grid *g, 
-					  int node0, int node1, double t);
-
-Grid *gridCurveIntersectsFace(Grid *g, int *face_nodes, int parent,
-			      double *tuv0, double *tuv1,
-			      double *tuv, double *xyz, double *bary);
-
-Grid *gridLineSegmentIntersectsFace(Grid *g, int node0, int node1,
-				    int faceId, 
-				    double *uv, double *xyz, double *bary );
-
 /* the {optimzation,laplacian}Limits will be set to a default if < 0.0 */ 
 Grid *gridSmooth(Grid *g, double optimizationLimit, double laplacianLimit );
 Grid *gridSmoothFaceMR(Grid *g, double optimizationLimit );
