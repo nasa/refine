691c75533f53cec29c0a8d25b9e6160783670c59
Mike Park <Mike.Park@NASA.Gov>
2011-08-12 10:46:14 -0400

    pull in test deps

--- two/test.sh
+++ two/test.sh
@@ -9,7 +9,18 @@ for test in $tests;
 do
   root=${test%_test.c}
   echo $root
-  compile="${CC} ${CFLAGS} -o ${root}_test ${root}.c ${root}_test.c"
+
+  dependencies=''
+  for dep_header in `grep '#include "ref_' $root.h`
+  do
+    source_with_leading_quote=${dep_header%.h\"}.c
+    source=${source_with_leading_quote#\"}
+    if [ -a ${source} ]; then
+      dependencies=${dependencies}' '${source}
+    fi
+  done
+
+  compile="${CC} ${CFLAGS} -o ${root}_test ${dependencies} ${root}.c ${root}_test.c"
   (eval ${compile} && eval valgrind --leak-check=full ./${root}_test) || \
       ( echo FAIL: ./${root}_test ; exit 1 ) ||  exit 1
 done
