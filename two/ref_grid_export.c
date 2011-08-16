2b909de632f863aab2ab9c481cdc418849589e0b
Mike Park <MikePark.rb@GMail.com>
2011-08-16 16:54:52 -0400

    to dumb xyz

--- two/ref_grid_export.c
+++ two/ref_grid_export.c
@@ -7,6 +7,9 @@
 REF_STATUS ref_grid_export_vtk( REF_GRID ref_grid, char *filename  )
 {
   FILE *file;
+  REF_NODE ref_node;
+  int node;
+  ref_node = ref_grid->nodes;
 
   file = fopen(filename,"w");
   if (NULL == (void *)file) printf("unable to open %s\n",filename);
@@ -17,7 +20,14 @@ REF_STATUS ref_grid_export_vtk( REF_GRID ref_grid, char *filename  )
   fprintf(file,"ASCII\n");
 
   fprintf(file,"DATASET UNSTRUCTURED_GRID\n");
-  fprintf(file,"POINTS %d\n",ref_node_n(ref_grid->nodes));
+  fprintf(file,"POINTS %d double\n",ref_node_n(ref_node));
+
+  for ( node = 0; node < ref_node_max(ref_node); node++ )
+    if ( ref_node_valid(ref_node,node) )
+      fprintf(file, " %.16e %16e %16e\n",
+	      ref_node_xyz(ref_node,0,node),
+	      ref_node_xyz(ref_node,1,node),
+	      ref_node_xyz(ref_node,2,node) ) ;
 
   fclose(file);
 
