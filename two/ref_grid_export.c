dbbac01a84145d1996f5b728ce8d066279293911
Mike Park <MikePark.rb@GMail.com>
2011-08-18 16:42:42 -0400

    to use compact

--- two/ref_grid_export.c
+++ two/ref_grid_export.c
@@ -9,6 +9,7 @@ REF_STATUS ref_grid_export_vtk( REF_GRID ref_grid, char *filename  )
   FILE *file;
   REF_NODE ref_node;
   REF_INT node;
+  REF_INT *o2n;
 
   ref_node = ref_grid->nodes;
 
@@ -20,15 +21,19 @@ REF_STATUS ref_grid_export_vtk( REF_GRID ref_grid, char *filename  )
   fprintf(file,"ref_grid_export_vtk\n");
   fprintf(file,"ASCII\n");
 
+  RSS( ref_node_compact( ref_node, &o2n), "compact" );
+
   fprintf(file,"DATASET UNSTRUCTURED_GRID\n");
   fprintf(file,"POINTS %d double\n",ref_node_n(ref_node));
 
   for ( node = 0; node < ref_node_max(ref_node); node++ )
-    if ( ref_node_valid(ref_node,node) )
+    if ( REF_EMPTY != o2n[node] )
       fprintf(file, " %.16e %16e %16e\n",
 	      ref_node_xyz(ref_node,0,node),
 	      ref_node_xyz(ref_node,1,node),
 	      ref_node_xyz(ref_node,2,node) ) ;
+
+  free(o2n);
 
   fclose(file);
 
