a209e866a6a9b1c504ca296021cf018df002dd12
Mike Park <Mike.Park@NASA.Gov>
2011-08-19 12:39:02 -0400

    fixed header name

--- two/ref_grid_export.h
+++ two/ref_grid_export.h
@@ -1,6 +1,6 @@
 
-#ifndef REF_GRID_IMPORT_H
-#define REF_GRID_IMPORT_H
+#ifndef REF_GRID_EXPORT_H
+#define REF_GRID_EXPORT_H
 
 #include "ref_grid.h"
 #include "ref_node.h"
@@ -12,4 +12,4 @@ REF_STATUS ref_grid_export_vtk( REF_GRID ref_grid, char *filename );
 
 END_C_DECLORATION
 
-#endif /* REF_GRID_IMPORT_H */
+#endif /* REF_GRID_EXPORT_H */
