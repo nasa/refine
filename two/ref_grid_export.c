c59eed8d9c634aef3c35ab5b13d021cd0a2c317b
Mike Park <Mike.Park@NASA.Gov>
2011-08-19 11:38:43 -0400

    dumped out a tet

--- two/ref_grid_export.c
+++ two/ref_grid_export.c
@@ -4,13 +4,22 @@
 
 #include "ref_grid_import.h"
 
+#define VTK_TETRA      (10)
+#define VTK_HEXAHEDRON (12)
+#define VTK_WEDGE      (13)
+#define VTK_PYRAMID    (14)
+
+
 REF_STATUS ref_grid_export_vtk( REF_GRID ref_grid, char *filename  )
 {
   FILE *file;
   REF_NODE ref_node;
+  REF_CELL ref_cell;
   REF_INT node;
   REF_INT *o2n;
-  REF_INT ncell;
+  REF_INT ncell,size;
+  REF_INT *nodes;
+  REF_INT node_per, cell;
 
   ref_node = ref_grid->nodes;
 
@@ -39,8 +48,33 @@ REF_STATUS ref_grid_export_vtk( REF_GRID ref_grid, char *filename  )
   ncell += ref_cell_n(ref_grid->cells[5]);
   ncell += ref_cell_n(ref_grid->cells[6]);
   ncell += ref_cell_n(ref_grid->cells[8]);
+  size = 0;
+  size += 5*ref_cell_n(ref_grid->cells[4]);
+  size += 6*ref_cell_n(ref_grid->cells[5]);
+  size += 7*ref_cell_n(ref_grid->cells[6]);
+  size += 9*ref_cell_n(ref_grid->cells[8]);
 
-  fprintf(file,"CELLS %d\n",ref_node_n(ref_node));
+  fprintf(file,"CELLS %d %d\n",ncell,size);
+
+  node_per = 4;
+  ref_cell = ref_grid->cells[node_per];
+  nodes = (REF_INT *) malloc( node_per * sizeof(REF_INT) );
+  for ( cell = 0 ; cell < ref_cell_max(ref_cell) ; cell++ )
+    if ( ref_cell_valid( ref_cell, cell ) )
+      {
+	fprintf(file," %d",node_per);
+	for ( node = 0; node < node_per; node++ )
+	  fprintf(file," %d",o2n[ref_cell_c2n(ref_cell,node,cell)]+1);
+	fprintf(file,"\n");
+      }
+  free(nodes);
+
+  fprintf(file,"CELL_TYPES %d\n",ncell);
+  node_per = 4;
+  ref_cell = ref_grid->cells[node_per];
+  for ( cell = 0 ; cell < ref_cell_n(ref_cell) ; cell++ )
+    fprintf(file," %d\n",VTK_TETRA);
+
 
   free(o2n);
 
