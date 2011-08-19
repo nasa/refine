9dfd022fa736c4342157b4ae93487050642aff81
Mike Park <Mike.Park@NASA.Gov>
2011-08-19 12:54:48 -0400

    to pull in more source (once)

--- two/test.sh
+++ two/test.sh
@@ -13,19 +13,32 @@ fi
 for test in $tests;
 do
   root=${test%_test.c}
-  echo $root
+  echo $root ---------------------
 
   dependencies=''
-  for dep_header in `grep '#include "ref_' $root.h`
+  for dep_header in `grep '#include "ref_' ${root}.h`
   do
     source_with_leading_quote=${dep_header%.h\"}.c
     source=${source_with_leading_quote#\"}
     if [ -a ${source} ]; then
-      dependencies=${dependencies}' '${source}
+      if ! echo ${dependencies} | grep -q "${source}" ; then
+        dependencies=${dependencies}' '${source}
+      fi
+    fi
+  done
+  for dep_header in `grep '#include "ref_' ${root}_test.c`
+  do
+    source_with_leading_quote=${dep_header%.h\"}.c
+    source=${source_with_leading_quote#\"}
+    if [ -a ${source} ]; then
+      if ! echo ${dependencies} | grep -q "${source}" ; then
+        dependencies=${dependencies}' '${source}
+      fi
     fi
   done
 
-  compile="${CC} ${CFLAGS} -o ${root}_test ${dependencies} ${root}.c ${root}_test.c"
+  compile="${CC} ${CFLAGS} -o ${root}_test ${dependencies} ${root}_test.c"
   (eval ${compile} && eval valgrind --leak-check=full ./${root}_test) || \
       ( echo FAIL: ./${root}_test ; exit 1 ) ||  exit 1
 done
+
