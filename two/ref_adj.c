aaa61a726ae5dcf5cb8bfa40d5fe94e3e1bbfa78
Mike Park <Mike.Park@NASA.Gov>
2011-08-22 12:36:14 -0400

    started the link list data

--- two/ref_adj.c
+++ two/ref_adj.c
@@ -7,13 +7,32 @@
 REF_STATUS ref_adj_create( REF_ADJ *ref_adj_ptr )
 {
   REF_ADJ ref_adj;
+  REF_INT i;
 
   (*ref_adj_ptr) = NULL;
   (*ref_adj_ptr) = (REF_ADJ)malloc( sizeof(REF_ADJ_STRUCT) );
   RNS(*ref_adj_ptr,"malloc ref_adj NULL");
   ref_adj = (*ref_adj_ptr);
 
-  ref_adj->blank = REF_EMPTY;
+  ref_adj_nnode(ref_adj) = 10;
+  ref_adj_nitem(ref_adj) = 20;
+
+  ref_adj->first = (REF_INT *)malloc( ref_adj_nnode(ref_adj) * 
+				      sizeof(REF_INT) );
+  RNS(ref_adj->first,"malloc ref_adj->first NULL");
+  for (i = 0; i < ref_adj_nnode(ref_adj) ; i++ )
+    ref_adj->first[i] = REF_EMPTY;
+
+  ref_adj->item = (REF_ADJ_ITEM)malloc( ref_adj_nitem(ref_adj) * 
+					sizeof(REF_ADJ_ITEM_STRUCT) );
+  RNS(ref_adj->item,"malloc ref_adj->item NULL");
+  for (i = 0; i < ref_adj_nitem(ref_adj) ; i++ )
+    {
+      ref_adj->item[i].reference = REF_EMPTY;
+      ref_adj->item[i].next = i+1;
+    }
+  ref_adj->item[ref_adj_nitem(ref_adj)-1].next = REF_EMPTY;
+  ref_adj->blank = 0;
 
   return REF_SUCCESS;
 }
@@ -21,6 +40,8 @@ REF_STATUS ref_adj_create( REF_ADJ *ref_adj_ptr )
 REF_STATUS ref_adj_free( REF_ADJ ref_adj )
 {
   if ( NULL == (void *)ref_adj ) return REF_NULL;
+  ref_cond_free( ref_adj->first );
+  ref_cond_free( ref_adj->item );
   ref_cond_free( ref_adj );
   return REF_SUCCESS;
 }
--- two/ref_adj.h
+++ two/ref_adj.h
@@ -7,18 +7,31 @@
 BEGIN_C_DECLORATION
 typedef struct REF_ADJ_STRUCT REF_ADJ_STRUCT;
 typedef REF_ADJ_STRUCT * REF_ADJ;
+typedef struct REF_ADJ_ITEM_STRUCT REF_ADJ_ITEM_STRUCT;
+typedef REF_ADJ_ITEM_STRUCT * REF_ADJ_ITEM;
 END_C_DECLORATION
 
 BEGIN_C_DECLORATION
 
 struct REF_ADJ_STRUCT {
+  REF_INT nnode, nitem;
+  REF_INT *first;
+  REF_ADJ_ITEM item;
   REF_INT blank;
+};
+
+struct REF_ADJ_ITEM_STRUCT {
+  REF_INT reference;
+  REF_INT next;
 };
 
 REF_STATUS ref_adj_create( REF_ADJ *ref_adj );
 REF_STATUS ref_adj_free( REF_ADJ ref_adj );
 
 REF_STATUS ref_adj_inspect( REF_ADJ ref_adj );
+
+#define ref_adj_nnode( ref_adj ) ((ref_adj)->nnode)
+#define ref_adj_nitem( ref_adj ) ((ref_adj)->nitem)
 
 END_C_DECLORATION
 
