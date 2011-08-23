a941f478e1c05b6fca84499a4e475647c2187e49
Mike Park <MikePark.rb@GMail.com>
2011-08-23 11:37:34 -0400

    more use of macros

--- two/ref_cell.c
+++ two/ref_cell.c
@@ -6,26 +6,30 @@
 
 REF_STATUS ref_cell_create( REF_INT node_per, REF_CELL *ref_cell_ptr )
 {
-  REF_INT i;
+  REF_INT cell;
   REF_INT max;
+  REF_CELL ref_cell;
+
   (*ref_cell_ptr) = NULL;
   (*ref_cell_ptr) = (REF_CELL)malloc( sizeof(REF_CELL_STRUCT) );
   RNS(*ref_cell_ptr,"malloc ref_cell NULL");
 
+  ref_cell = (*ref_cell_ptr);
+
   max = 100;
 
-  (*ref_cell_ptr)->node_per = node_per;
-  (*ref_cell_ptr)->n = 0;
-  (*ref_cell_ptr)->max = max;
-  (*ref_cell_ptr)->c2n = (REF_INT *)malloc(node_per*max*sizeof(REF_INT));
-  RNS((*ref_cell_ptr)->c2n,"malloc c2n NULL");
-  for ( i=0 ; i < max ; i++ ) 
+  ref_cell_node_per(ref_cell) = node_per;
+  ref_cell_n(ref_cell) = 0;
+  ref_cell_max(ref_cell) = max;
+  ref_cell->c2n = (REF_INT *)malloc(node_per*max*sizeof(REF_INT));
+  RNS(ref_cell->c2n,"malloc c2n NULL");
+  for ( cell = 0 ; cell < max ; cell++ ) 
     {
-      (*ref_cell_ptr)->c2n[0+node_per*i] = REF_EMPTY; 
-      (*ref_cell_ptr)->c2n[1+node_per*i] = i+1; 
+      ref_cell_c2n(ref_cell,0,cell) = REF_EMPTY;
+      ref_cell_c2n(ref_cell,1,cell) = cell+1;
     }
-  (*ref_cell_ptr)->c2n[1+node_per*(max-1)] = REF_EMPTY;
-  (*ref_cell_ptr)->blank = 0;
+  ref_cell_c2n(ref_cell,1,max-1) = REF_EMPTY;
+  ref_cell_blank(ref_cell) = 0;
 
   return REF_SUCCESS;
 }
@@ -85,11 +89,11 @@ REF_STATUS ref_cell_add( REF_CELL ref_cell, REF_INT *nodes, REF_INT *new_cell )
       RNS(ref_cell->c2n,"remalloc c2n NULL");
       for (cell=orig;cell < ref_cell_max(ref_cell); cell++ ) 
 	{
-	  ref_cell->c2n[0+ref_cell_node_per(ref_cell)*cell] = REF_EMPTY; 
-	  ref_cell->c2n[1+ref_cell_node_per(ref_cell)*cell] = cell+1; 
+	  ref_cell_c2n(ref_cell,0,cell)= REF_EMPTY; 
+	  ref_cell_c2n(ref_cell,1,cell) = cell+1; 
 	}
       ref_cell_c2n(ref_cell,1,(ref_cell->max)-1) = REF_EMPTY; 
-      ref_cell->blank = orig;
+      ref_cell_blank(ref_cell) = orig;
     }
 
   cell = ref_cell_blank(ref_cell);
