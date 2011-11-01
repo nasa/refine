7a8e3026f42bd03ce2e29c04e6401299cf505161
Mike Park <Mike.Park@NASA.Gov>
2011-11-01 15:57:38 -0400

    put tests in blocks

--- two/ref_subdiv_test.c
+++ two/ref_subdiv_test.c
@@ -19,7 +19,6 @@ int main( void )
   REF_INT nodes[6] = {0,1,2,3,4,5};
   REF_INT cell;
 
-  REF_SUBDIV ref_subdiv;
 
   TSS(ref_grid_create(&ref_grid),"create");
 
@@ -27,33 +26,39 @@ int main( void )
 
   TSS(ref_cell_add(ref_cell,nodes,&cell),"add prism");
 
-  TSS(ref_subdiv_create(&ref_subdiv,ref_grid),"create");
+  {
+    REF_SUBDIV ref_subdiv;
+    TSS(ref_subdiv_create(&ref_subdiv,ref_grid),"create");
 
-  TES(0,ref_subdiv_mark(ref_subdiv,0),"init mark");
-  TES(0,ref_subdiv_mark(ref_subdiv,6),"init mark");
+    TES(0,ref_subdiv_mark(ref_subdiv,0),"init mark");
+    TES(0,ref_subdiv_mark(ref_subdiv,6),"init mark");
 
-  TSS(ref_subdiv_mark_to_split(ref_subdiv,0,1),"mark edge 0-1");
+    TSS(ref_subdiv_mark_to_split(ref_subdiv,0,1),"mark edge 0-1");
 
-  TES(1,ref_subdiv_mark(ref_subdiv,0),"been marked");
-  TES(0,ref_subdiv_mark(ref_subdiv,6),"not been marked");
+    TES(1,ref_subdiv_mark(ref_subdiv,0),"been marked");
+    TES(0,ref_subdiv_mark(ref_subdiv,6),"not been marked");
 
-  TSS(ref_subdiv_mark_relax(ref_subdiv),"relax");
+    TSS(ref_subdiv_mark_relax(ref_subdiv),"relax");
 
-  TES(1,ref_subdiv_mark(ref_subdiv,0),"been marked");
-  TES(1,ref_subdiv_mark(ref_subdiv,6),"been relaxed");
+    TES(1,ref_subdiv_mark(ref_subdiv,0),"been marked");
+    TES(1,ref_subdiv_mark(ref_subdiv,6),"been relaxed");
 
-  TSS(ref_subdiv_free(ref_subdiv),"free");
+    TSS(ref_subdiv_free(ref_subdiv),"free");
+  }
 
-  TSS(ref_subdiv_create(&ref_subdiv,ref_grid),"create");
+  {
+    REF_SUBDIV ref_subdiv;
+    TSS(ref_subdiv_create(&ref_subdiv,ref_grid),"create");
 
-  TSS(ref_subdiv_mark_to_split(ref_subdiv,0,1),"mark edge 0-1");
-  TSS(ref_subdiv_mark_to_split(ref_subdiv,1,2),"mark edge 1-2");
+    TSS(ref_subdiv_mark_to_split(ref_subdiv,0,1),"mark edge 0-1");
+    TSS(ref_subdiv_mark_to_split(ref_subdiv,1,2),"mark edge 1-2");
 
-  TES(0,ref_subdiv_mark(ref_subdiv,1),"no yet");
-  TSS(ref_subdiv_mark_relax(ref_subdiv),"relax");
-  TES(1,ref_subdiv_mark(ref_subdiv,1),"yet");
+    TES(0,ref_subdiv_mark(ref_subdiv,1),"no yet");
+    TSS(ref_subdiv_mark_relax(ref_subdiv),"relax");
+    TES(1,ref_subdiv_mark(ref_subdiv,1),"yet");
 
-  TSS(ref_subdiv_free(ref_subdiv),"free");
+    TSS(ref_subdiv_free(ref_subdiv),"free");
+  }
 
   TSS( ref_grid_free(ref_grid),"free" );
 
