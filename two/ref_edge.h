0f898dd6e1b47220d4ac77b2dc3d4aae2a554e23
Mike Park <Mike.Park@NASA.Gov>
2011-12-07 15:43:01 -0500

    another macro

--- two/ref_edge.h
+++ two/ref_edge.h
@@ -30,6 +30,10 @@ REF_STATUS ref_edge_free( REF_EDGE ref_edge );
 
 #define ref_edge_adj( ref_edge ) ((ref_edge)->adj)
 
+#define each_edge_having_node( ref_edge, node, item, edge ) \
+  each_ref_adj_node_item_with_ref( ref_edge_adj( ref_edge ), node, item, edge)
+
+
 REF_STATUS ref_edge_with( REF_EDGE ref_edge, 
 			  REF_INT node0, REF_INT node1,
 			  REF_INT *edge );
