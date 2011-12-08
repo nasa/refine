789bce0da947a056e720be1cd25d64cd0edf67b4
Mike Park <Mike.Park@NASA.Gov>
2011-12-08 14:19:33 -0500

    refactored

--- two/ref_grid_test.c
+++ two/ref_grid_test.c
@@ -6,35 +6,35 @@
 #include "ref_grid.h"
 #include "ref_test.h"
 
-int main( int argc, char *argv[] )
+int main( void )
 {
   REF_GRID ref_grid;
-  REF_CELL ref_cell;
-  REF_INT node_per;
-  REF_INT group;
 
-  if (argc>1) {printf("%s ignored\n",argv[0]);}
+  {  /* init */
+    TFS(ref_grid_free(NULL),"dont free NULL");
 
-  /* init */
+    TSS(ref_grid_create(&ref_grid),"create");
+    TSS(ref_grid_free(ref_grid),"free");
+  }
 
-  TFS(ref_grid_free(NULL),"dont free NULL");
+  {  /* each element */
 
-  TSS(ref_grid_create(&ref_grid),"create");
-  TSS(ref_grid_free(ref_grid),"free");
+    REF_CELL ref_cell;
+    REF_INT node_per;
+    REF_INT group;
 
-  /* each element */
+    TSS(ref_grid_create(&ref_grid),"create");
 
-  TSS(ref_grid_create(&ref_grid),"create");
+    node_per = 3;
+    each_ref_grid_ref_cell( ref_grid, group, ref_cell )
+      {
+	node_per += 1;
+	if ( 7 == node_per ) node_per = 8;
+	TES( node_per, ref_cell_node_per( ref_cell), "cells in order" );
+      }
 
-  node_per = 3;
-  each_ref_grid_ref_cell( ref_grid, group, ref_cell )
-    {
-      node_per += 1;
-      if ( 7 == node_per ) node_per = 8;
-      TES( node_per, ref_cell_node_per( ref_cell), "cells in order" );
-    }
-
-  TSS(ref_grid_free(ref_grid),"free"); 
+    TSS(ref_grid_free(ref_grid),"free"); 
+  }
 
   return 0;
 }
