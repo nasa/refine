1bdb40c65851002639082c9b5b4c5338df20032f
Mike Park <Mike.Park@NASA.Gov>
2011-12-08 14:17:19 -0500

    started loop

--- two/ref_quality.c
+++ two/ref_quality.c
@@ -6,11 +6,21 @@
 
 #include "ref_cell.h"
 
+
 REF_STATUS ref_quality_hex( REF_GRID ref_grid )
 {
   REF_CELL ref_cell;
+  REF_INT cell, cell_edge;
+  REF_INT nodes[8];
 
   ref_cell = ref_grid_hex(ref_grid);
+
+  each_ref_cell_valid_cell_with_nodes( ref_cell, cell, nodes)
+    each_ref_cell_cell_edge( ref_cell, cell_edge )      
+      {
+	
+      }
+
 
   return REF_SUCCESS;
 }
