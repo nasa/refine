d949b60c7745289db393e88f8875a3f3cdc97c79
Mike Park <MikePark.rb@GMail.com>
2011-12-13 12:14:18 -0500

    more test coverage

--- two/ref_math_test.c
+++ two/ref_math_test.c
@@ -10,10 +10,22 @@
 int main( void )
 {
 
-  {
+  { /* same 1 */
     REF_DBL vect1[3]={1.0,0.0,0.0};
     REF_DBL vect2[3]={1.0,0.0,0.0};
     TWDS(1.0,ref_math_dot(vect1,vect2),-1.0,"dot");
+  }
+
+  { /* orth 1 */
+    REF_DBL vect1[3]={1.0,0.0,0.0};
+    REF_DBL vect2[3]={0.0,1.0,0.0};
+    TWDS(0.0,ref_math_dot(vect1,vect2),-1.0,"dot");
+  }
+
+  { /* two */
+    REF_DBL vect1[3]={1.0,0.0,0.0};
+    REF_DBL vect2[3]={2.0,1.0,0.0};
+    TWDS(2.0,ref_math_dot(vect1,vect2),-1.0,"dot");
   }
 
   return 0;
