09cfe2d20f281bce14a2685f09d73d24e1eb85eb
Mike Park <Mike.Park@NASA.Gov>
2011-12-15 16:44:44 -0500

    add the exporter

--- two/ref2_test.c
+++ two/ref2_test.c
@@ -51,6 +51,9 @@ int main( int argc, char *argv[] )
   printf("vtk.\n");
   TSS(ref_grid_export_vtk(ref_grid, "ref2.vtk"),"to vtk");
 
+  printf("ugrid.\n");
+  TSS(ref_grid_export_ugrid(ref_grid, "ref2.ugrid"),"to ugrid");
+
   printf("free.\n");
   TSS(ref_grid_free(ref_grid),"free");
 
