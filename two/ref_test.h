7f57dca9432f99d2bd5af3204c8c6b579715d721
Mike Park <Mike.Park@NASA.Gov>
2011-12-19 10:55:09 -0500

    revent namespace clash

--- two/ref_test.h
+++ two/ref_test.h
@@ -11,11 +11,12 @@ BEGIN_C_DECLORATION
 
 #define TSS(fcn,msg)							\
   {									\
-    REF_STATUS code;							\
-    code = (fcn);							\
-    if (REF_SUCCESS != code){						\
-      printf("%s: %d: %s: %d %s\n",__FILE__,__LINE__,__func__,code,(msg)); \
-      return code;							\
+    REF_STATUS ref_private_testing_code;				\
+    ref_private_testing_code = (fcn);					\
+    if (REF_SUCCESS != ref_private_testing_code){			\
+      printf("%s: %d: %s: %d %s\n",__FILE__,__LINE__,__func__,		\
+	     ref_private_testing_code,(msg));				\
+      return ref_private_testing_code;					\
     }else{								\
       TEST_ECH0(msg);							\
     }									\
@@ -43,12 +44,13 @@ BEGIN_C_DECLORATION
 
 #define TEIS(a,b,msg)							\
   {									\
-    REF_INT ai,bi;							\
-    ai = (a);								\
-    bi = (b);								\
-    if (ai!=bi){							\
+    REF_INT ref_private_testing_ai,ref_private_testing_bi;		\
+    ref_private_testing_ai = (a);					\
+    ref_private_testing_bi = (b);					\
+    if (ref_private_testing_ai!=ref_private_testing_bi){		\
       printf("%s: %d: %s: %s\nexpected %d was %d\n",			\
-	     __FILE__,__LINE__,__func__,(msg),ai,bi);			\
+	     __FILE__,__LINE__,__func__,(msg),				\
+	     ref_private_testing_ai,ref_private_testing_bi);		\
       return REF_FAILURE;						\
     }else{								\
       TEST_ECH0(msg);							\
@@ -57,15 +59,19 @@ BEGIN_C_DECLORATION
 
 #define TWDS(a,b,tol,msg)						\
   {									\
-    REF_DBL ad,bd,del,allowed;						\
-    ad = (a);								\
-    bd = (b);								\
-    del = ABS(ad-bd);							\
-    allowed = (tol);							\
-    if ( tol < 0.0 ) allowed = 1.0E-12;					\
-    if (del>allowed){							\
+    REF_DBL ref_private_testing_ad,ref_private_testing_bd;		\
+    REF_DBL ref_private_testing_del,ref_private_testing_allowed;	\
+    ref_private_testing_ad = (a);					\
+    ref_private_testing_bd = (b);					\
+    ref_private_testing_del =						\
+      ABS(ref_private_testing_ad-ref_private_testing_bd);		\
+    ref_private_testing_allowed = (tol);				\
+    if ( tol < 0.0 ) ref_private_testing_allowed = 1.0E-12;		\
+    if (ref_private_testing_del>ref_private_testing_allowed){		\
       printf("%s: %d: %s: %s\nexpected %e, was %e, %e outside of %e\n",	\
-	     __FILE__,__LINE__,__func__,(msg),ad,bd,del,allowed);	\
+	     __FILE__,__LINE__,__func__,(msg),				\
+	     ref_private_testing_ad,ref_private_testing_bd,		\
+	     ref_private_testing_del,ref_private_testing_allowed);	\
       return REF_FAILURE;						\
     }else{								\
       TEST_ECH0(msg);							\
@@ -84,11 +90,11 @@ BEGIN_C_DECLORATION
 
 #define TFS(fcn,msg)							\
   {									\
-    REF_STATUS code;							\
-    code = (fcn);							\
-    if (REF_SUCCESS == code){						\
+    REF_STATUS ref_private_testing_code;				\
+    ref_private_testing_code = (fcn);					\
+    if (REF_SUCCESS == ref_private_testing_code){			\
       printf("%s: %d: %s: %s\n",__FILE__,__LINE__,__func__,(msg));	\
-      return code;							\
+      return ref_private_testing_code;					\
     }else{								\
       TEST_ECH0(msg);							\
     }									\
