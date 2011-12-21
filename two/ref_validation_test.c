61756177888c932ae47a030a5e3343a07f61dfe8
Mike Park <Mike.Park@NASA.Gov>
2011-12-21 09:29:17 -0500

    comments

--- two/ref_validation_test.c
+++ two/ref_validation_test.c
@@ -27,16 +27,19 @@ int main( int argc, char *argv[] )
       printf("validating\n");
 
       printf("reading %s\n",argv[1]);
-      TSS(ref_import_by_extension( &ref_grid, argv[1] ),"from ugrid");
+      RSS(ref_import_by_extension( &ref_grid, argv[1] ),"from ugrid");
       printf("complete.\n");
       
+      RSS(ref_grid_inspect( ref_grid ), "inspection");
+
       printf("validate.\n");
-      TSS( ref_validation_all( ref_grid ), "invalid grid" );
+      RSS( ref_validation_all( ref_grid ), "invalid grid" );
 
       printf("vtk.\n");
-      TSS( ref_export_vtk( ref_grid, "validate.vtk" ), "vtk" );
+      RSS( ref_export_vtk( ref_grid, "validate.vtk" ), "vtk" );
+
       printf("tec.\n");
-      TSS( ref_export_tec( ref_grid, "validate.tec" ), "tec" );
+      RSS( ref_export_tec( ref_grid, "validate.tec" ), "tec" );
 
       printf("done.\n");
       return 0;
