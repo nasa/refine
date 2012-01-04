ae6a3be384ddce1e18668ef431b0e0ec66806a2c
Mike Park <Mike.Park@NASA.Gov>
2012-01-04 11:10:27 -0500

    split boundary quads

--- two/ref_subdiv_test.c
+++ two/ref_subdiv_test.c
@@ -34,6 +34,37 @@ static REF_STATUS set_up_prism_for_subdiv( REF_SUBDIV *ref_subdiv_ptr )
   return REF_SUCCESS;
 }
 
+static REF_STATUS set_up_prism_with_boundary( REF_SUBDIV *ref_subdiv_ptr )
+{
+  REF_GRID ref_grid;
+  REF_INT nodes[6] = {0,1,2,3,4,5};
+  REF_INT cell, node;
+
+  TSS(ref_grid_create(&ref_grid),"create");
+
+  TSS(ref_node_add( ref_grid_node(ref_grid), 0, &node),"n0");
+  TSS(ref_node_add( ref_grid_node(ref_grid), 1, &node),"n1");
+  TSS(ref_node_add( ref_grid_node(ref_grid), 2, &node),"n2");
+  TSS(ref_node_add( ref_grid_node(ref_grid), 3, &node),"n3");
+  TSS(ref_node_add( ref_grid_node(ref_grid), 4, &node),"n4");
+  TSS(ref_node_add( ref_grid_node(ref_grid), 5, &node),"n5");
+
+  TSS(ref_cell_add(ref_grid_pri(ref_grid),nodes,&cell),"add prism");
+
+  nodes[0] = 0; nodes[1] = 3; nodes[2] = 4; nodes[3] = 1; nodes[4] = 10;
+  TSS(ref_cell_add(ref_grid_qua(ref_grid),nodes,&cell),"add quad");
+
+  nodes[0] = 3; nodes[1] = 5; nodes[2] = 4; nodes[3] = 100;
+  TSS(ref_cell_add(ref_grid_tri(ref_grid),nodes,&cell),"add tri");
+
+  nodes[0] = 0; nodes[1] = 1; nodes[2] = 2; nodes[3] = 101;
+  TSS(ref_cell_add(ref_grid_tri(ref_grid),nodes,&cell),"add tri");
+
+  TSS(ref_subdiv_create(ref_subdiv_ptr,ref_grid),"create");
+
+  return REF_SUCCESS;
+}
+
 static REF_STATUS tear_down( REF_SUBDIV ref_subdiv )
 {
   REF_GRID ref_grid;
@@ -117,6 +148,27 @@ int main( void )
 
     TSS(ref_subdiv_split(ref_subdiv),"split");
     TEIS(2, ref_cell_n(ref_grid_pri(ref_grid)),"two");
+
+    TSS( tear_down( ref_subdiv ), "tear down");
+  }
+
+  {
+    REF_SUBDIV ref_subdiv;
+    REF_GRID ref_grid;
+    TSS(set_up_prism_with_boundary(&ref_subdiv),"set up");
+    ref_grid = ref_subdiv_grid(ref_subdiv);
+
+    TSS(ref_subdiv_mark_to_split(ref_subdiv,0,1),"mark edge 0-1");
+
+    TSS(ref_subdiv_mark_relax(ref_subdiv),"relax");
+    TSS(ref_subdiv_new_node(ref_subdiv),"new nodes");
+
+    TSS(ref_subdiv_split(ref_subdiv),"split");
+    TEIS(2, ref_cell_n(ref_grid_pri(ref_grid)),"two pri");
+
+    TEIS(2, ref_cell_n(ref_grid_qua(ref_grid)),"two qua");
+
+    TEIS(4, ref_cell_n(ref_grid_tri(ref_grid)),"four tri");
 
     TSS( tear_down( ref_subdiv ), "tear down");
   }
