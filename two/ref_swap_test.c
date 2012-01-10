f45d72939ca7aeea545975ad89a16ba3943a9bbd
Mike Park <MikePark.rb@GMail.com>
2012-01-10 13:00:09 -0500

    broken test to eomulate removal

--- two/ref_swap_test.c
+++ two/ref_swap_test.c
@@ -25,6 +25,33 @@ int main( void )
     TSS(ref_fixture_tet_grid(&ref_grid),"set up");
 
     TEIS(REF_INVALID,ref_swap_remove_two_face_cell(ref_grid,0),"cell 0");
+    TEIS(1, ref_cell_n(ref_grid_tet(ref_grid)),"tet");
+
+    TSS( ref_grid_free( ref_grid ), "free grid");
+  }
+
+  { /* leave different faces alone */
+    REF_GRID ref_grid;
+    REF_INT nodes[4] = {0,3,1,50}, cell;
+
+    TSS(ref_fixture_tet_grid(&ref_grid),"set up");
+    TSS(ref_cell_add(ref_grid_tri(ref_grid),nodes,&cell),"other tri");
+    
+    TEIS(REF_INVALID,ref_swap_remove_two_face_cell(ref_grid,0),"cell 0");
+    TEIS(1, ref_cell_n(ref_grid_tet(ref_grid)),"tet");
+
+    TSS( ref_grid_free( ref_grid ), "free grid");
+  }
+
+  { /* same faces alone */
+    REF_GRID ref_grid;
+    REF_INT nodes[4] = {0,3,1,3}, cell;
+
+    TSS(ref_fixture_tet_grid(&ref_grid),"set up");
+    TSS(ref_cell_add(ref_grid_tri(ref_grid),nodes,&cell),"other tri");
+    
+    TSS(ref_swap_remove_two_face_cell(ref_grid,0),"removal failed");
+    TEIS(0, ref_cell_n(ref_grid_tet(ref_grid)),"tet");
 
     TSS( ref_grid_free( ref_grid ), "free grid");
   }
