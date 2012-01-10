2c49d0a7fb679c4179ad0489dfa8ce7df100f3a5
Mike Park <MikePark.rb@GMail.com>
2012-01-10 14:26:31 -0500

    finish routine

--- two/ref_swap.c
+++ two/ref_swap.c
@@ -13,7 +13,9 @@ REF_STATUS ref_swap_remove_two_face_cell( REF_GRID ref_grid, REF_INT cell )
   REF_INT face_nodes[4];
   REF_INT found;
   REF_INT face0, face1;
+  REF_INT cell_face0, cell_face1;
   REF_INT faceid0, faceid1;
+  REF_INT temp, face;
 
   ref_cell = ref_grid_tet(ref_grid);
  
@@ -21,6 +23,8 @@ REF_STATUS ref_swap_remove_two_face_cell( REF_GRID ref_grid, REF_INT cell )
   face1 = REF_EMPTY;
   faceid0 = REF_EMPTY;
   faceid1 = REF_EMPTY;
+  cell_face0 = REF_EMPTY;
+  cell_face1 = REF_EMPTY;
   for (cell_face=0;cell_face<4;cell_face++)
     {
       for(node=0;node<4;node++)
@@ -34,6 +38,7 @@ REF_STATUS ref_swap_remove_two_face_cell( REF_GRID ref_grid, REF_INT cell )
 	    {
 	      face0 = found;
 	      faceid0 = face_nodes[3];
+	      cell_face0 = cell_face;
 	    }
 	  else
 	    {
@@ -43,6 +48,7 @@ REF_STATUS ref_swap_remove_two_face_cell( REF_GRID ref_grid, REF_INT cell )
 		}
 	      face1 = found;
 	      faceid1 = face_nodes[3];
+	      cell_face1 = cell_face;
 	    }
 	}
     }
@@ -50,6 +56,24 @@ REF_STATUS ref_swap_remove_two_face_cell( REF_GRID ref_grid, REF_INT cell )
   if ( REF_EMPTY == face0 ) return REF_INVALID;
   if ( REF_EMPTY == face1 ) return REF_INVALID;
   if ( faceid0 != faceid1 ) return REF_INVALID;
+
+  RSS( ref_cell_remove( ref_grid_tri( ref_grid ), face0 ), "remove tri0" );
+  RSS( ref_cell_remove( ref_grid_tri( ref_grid ), face1 ), "remove tri1" );
+
+  for (cell_face=0;cell_face<4;cell_face++)
+    {
+      for(node=0;node<4;node++)
+	cell_nodes[node]=ref_cell_f2n(ref_cell,node,cell,cell_face);
+      if ( cell_face != cell_face0 && cell_face != cell_face1 )
+	{
+	  cell_nodes[3] = faceid0;
+	  temp = cell_nodes[0]; 
+	  cell_nodes[0] = cell_nodes[1];
+	  cell_nodes[1] = temp;
+	  RSS( ref_cell_add( ref_grid_tri( ref_grid ), cell_nodes, &face ), 
+	       "add tri" );
+	}
+    }
 
   RSS( ref_cell_remove( ref_cell, cell ), "remove tet" );
 
