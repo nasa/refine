60251df1bc2baf8b40317a71e8ae0763c92d3499
Mike Park <Mike.Park@NASA.Gov>
2012-01-11 08:35:21 -0500

    install here

--- setup-build.sh
+++ setup-build.sh
@@ -3,4 +3,5 @@
 set -x
 
 mkdir -p strict
-( cd strict && ../configure CFLAGS='-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused' TESTS_ENVIRONMENT='valgrind --quiet --leak-check=full' )
+( cd strict && ../configure --prefix=`pwd` CFLAGS='-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused' TESTS_ENVIRONMENT='valgrind --quiet --leak-check=full' )
+
