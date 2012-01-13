39d44d633ea1c8b94d7f5230e5c3b2be6ee171ee
Mike Park <Mike.Park@NASA.Gov>
2012-01-13 09:39:15 -0500

    a mpi build too

--- setup-build.sh
+++ setup-build.sh
@@ -5,3 +5,6 @@ set -x
 mkdir -p strict
 ( cd strict && ../configure --prefix=`pwd` CFLAGS='-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused' TESTS_ENVIRONMENT='valgrind --quiet --leak-check=full' )
 
+mkdir -p mpi
+( cd mpi && ../configure --prefix=`pwd` CC=mpicc CFLAGS='-DHAVE_MPI -g -O2 -traceback' TESTS_ENVIRONMENT='valgrind --quiet --leak-check=full' )
+
