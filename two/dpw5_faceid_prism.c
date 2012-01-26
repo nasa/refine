263c27c9cbd86c553500d9795d561b1d69aba222
Mike Park <Mike.Park@NASA.Gov>
2012-01-26 17:31:53 -0500

    split everyone

--- two/dpw5_faceid_prism.c
+++ two/dpw5_faceid_prism.c
@@ -30,20 +30,21 @@
 int main( int argc, char *argv[] )
 {
   REF_GRID ref_grid;
+  REF_DICT ref_dict;
 
   REF_NODE ref_node;
   REF_CELL ref_cell;
   REF_INT cell;
   REF_INT nodes[REF_CELL_MAX_NODE_PER];
-  REF_INT faceid;
+  REF_INT faceid, boundary_condition;
   REF_INT open_node0, open_node1;
 
   REF_SHARD ref_shard;
   REF_INT face_marks, hex_marks;
 
-  if (argc<2) 
+  if (argc<3) 
     {
-      printf("usage: %s filename.extension\n",argv[0]);
+      printf("usage: %s filename.extension filename.mapbc\n",argv[0]);
       return 0;
     }
 
@@ -53,28 +54,36 @@ int main( int argc, char *argv[] )
 
   RSS(ref_grid_inspect( ref_grid ), "inspection");
 
+  printf("reading %s\n",argv[2]);
+  RSS(ref_import_mapbc( &ref_dict, argv[2] ),"mapbc");
+  printf("complete.\n");
+
+  RSS(ref_dict_inspect( ref_dict ), "inspection");
+
   printf("create shard.\n");
   RSS( ref_shard_create( &ref_shard, ref_grid ), "make shard");
 
   ref_node = ref_grid_node(ref_grid);
   ref_cell = ref_grid_qua(ref_grid);
 
-  faceid = 1;
-
   each_ref_cell_valid_cell_with_nodes( ref_cell, cell, nodes)
-    if ( faceid == nodes[4] )
-      {
-	RSS( ref_face_open_node( &ref_node_xyz(ref_node,0,nodes[0]),
-				 &ref_node_xyz(ref_node,0,nodes[1]),
-				 &ref_node_xyz(ref_node,0,nodes[2]),
-				 &ref_node_xyz(ref_node,0,nodes[3]),
-				 &open_node0 ), "find open" );
-	open_node1 = open_node0+2;
-	if ( open_node1 >= 4 ) open_node1 -= 4;
-	RSS( ref_shard_mark_to_split( ref_shard, 
-				      nodes[open_node0], nodes[open_node1]), 
-	     "mark");
-      }
+    {
+      faceid = nodes[4];
+      RSS( ref_dict_value(ref_dict,faceid,&boundary_condition), "get bc");
+      if ( boundary_condition == 4000 )
+	{
+	  RSS( ref_face_open_node( &ref_node_xyz(ref_node,0,nodes[0]),
+				   &ref_node_xyz(ref_node,0,nodes[1]),
+				   &ref_node_xyz(ref_node,0,nodes[2]),
+				   &ref_node_xyz(ref_node,0,nodes[3]),
+				   &open_node0 ), "find open" );
+	  open_node1 = open_node0+2;
+	  if ( open_node1 >= 4 ) open_node1 -= 4;
+	  RSS( ref_shard_mark_to_split( ref_shard, 
+					nodes[open_node0], nodes[open_node1]), 
+	       "mark");
+	}
+    }
 
   RSS( ref_shard_mark_n( ref_shard, &face_marks, &hex_marks ), "count marks");
   printf("marked faces %d hexes %d\n",face_marks,hex_marks);
@@ -91,7 +100,7 @@ int main( int argc, char *argv[] )
   RSS(ref_grid_inspect( ref_grid ), "inspection");
 
   printf("ugrid.\n");
-  RSS(ref_export_b8_ugrid(ref_grid, "ref2.b8.ugrid"),"to ugrid");
+  RSS(ref_export_b8_ugrid(ref_grid, "prism.b8.ugrid"),"to ugrid");
 
   printf("free.\n");
   RSS(ref_grid_free(ref_grid),"free");
