2b4c01c60e7500282cd3db627703977161eda895
Mike Park <Mike.Park@NASA.Gov>
2012-02-10 08:27:45 -0500

    split out projectable

---
+++ two/projectable.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include "ref_import.h"
+#include "ref_export.h"
+
+#include "ref_adj.h"
+#include "ref_grid.h"
+#include "ref_node.h"
+#include "ref_metric.h"
+#include "ref_cell.h"
+
+#include "ref_edge.h"
+
+#include "ref_face.h"
+#include "ref_sort.h"
+#include "ref_dict.h"
+
+#include "ref_quality.h"
+#include "ref_shard.h"
+#include "ref_subdiv.h"
+
+#include "ref_validation.h"
+
+#include "ref_math.h"
+#include "ref_swap.h"
+
+int main( int argc, char *argv[] )
+{
+  REF_GRID ref_grid;
+
+  if (argc<2) 
+    {
+      printf("usage: %s filename.extension\n",argv[0]);
+      return 0;
+    }
+
+  printf("reading %s\n",argv[1]);
+  RSS(ref_import_by_extension( &ref_grid, argv[1] ),"from ugrid");
+  printf("complete.\n");
+      
+  RSS(ref_grid_inspect( ref_grid ), "inspection");
+
+  printf("check for multiple_face_cell.\n");
+  RSS( ref_quality_report_multiple_face_cell( ref_grid, "qual-orig.tec"), 
+       "report" );
+
+  printf("try swapping.\n");
+  RSS( ref_quality_swap_multiple_face_cell( ref_grid), "swap" );
+
+  printf("check for multiple_face_cell.\n");
+  RSS( ref_quality_report_multiple_face_cell( ref_grid, "qual-swap.tec"), 
+       "report" );
+
+  printf("try split.\n");
+  RSS( ref_quality_split_multiple_face_cell( ref_grid ), "split" );
+
+  printf("check for multiple_face_cell.\n");
+  RSS( ref_quality_report_multiple_face_cell( ref_grid, "qual-split.tec"), 
+       "report" );
+
+  printf("export\n");
+  RSS(ref_export_b8_ugrid( ref_grid, "quality.b8.ugrid" ),"from ugrid");
+
+  printf("done.\n");
+  return 0;
+}
--- two/ref_quality_test.c
+++ two/ref_quality_test.c
@@ -28,49 +28,8 @@
 #include "ref_test.h"
 #include "ref_fixture.h"
 
-int main( int argc, char *argv[] )
+int main( void )
 {
-  REF_GRID ref_grid;
-
-  if (argc>1) 
-    {
-
-      printf("reading %s\n",argv[1]);
-      RSS(ref_import_by_extension( &ref_grid, argv[1] ),"from ugrid");
-      printf("complete.\n");
-      
-      RSS(ref_grid_inspect( ref_grid ), "inspection");
-
-      printf("tec surf.\n");
-      RSS(ref_export_tec_surf( ref_grid, "qual-surf-orig.tec" ), "tec surf");
-
-      printf("check for multiple_face_cell.\n");
-      RSS( ref_quality_report_multiple_face_cell( ref_grid, "qual-orig.tec"), 
-	   "report" );
-
-      printf("try swapping.\n");
-      RSS( ref_quality_swap_multiple_face_cell( ref_grid), "swap" );
-
-      printf("tec surf.\n");
-      RSS(ref_export_tec_surf( ref_grid, "qual-surf-swap.tec" ), "tec surf");
-
-      printf("check for multiple_face_cell.\n");
-      RSS( ref_quality_report_multiple_face_cell( ref_grid, "qual-swap.tec"), 
-	   "report" );
-
-      printf("try split.\n");
-      RSS( ref_quality_split_multiple_face_cell( ref_grid ), "split" );
-
-      printf("check for multiple_face_cell.\n");
-      RSS( ref_quality_report_multiple_face_cell( ref_grid, "qual-split.tec"), 
-	   "report" );
-
-      printf("export\n");
-      RSS(ref_export_b8_ugrid( ref_grid, "quality.b8.ugrid" ),"from ugrid");
-
-      printf("done.\n");
-      return 0;
-    }
 
   { /* find mark */
     REF_GRID ref_grid;
@@ -83,7 +42,6 @@ int main( int argc, char *argv[] )
 
     TSS( ref_grid_free( ref_grid ), "free" );
   }
-
 
   return 0;
 }
