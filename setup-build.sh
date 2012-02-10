19c07c88d64605f651e0949f3fd5ce485bbddad6
Mike Park <Mike.Park@NASA.Gov>
2012-02-10 08:32:30 -0500

    adde profile target

--- setup-build.sh
+++ setup-build.sh
@@ -8,3 +8,6 @@ mkdir -p strict
 mkdir -p mpi
 ( cd mpi && ../configure --prefix=`pwd` CC=mpicc CFLAGS='-DHAVE_MPI -g -O2 -traceback' TESTS_ENVIRONMENT='valgrind --quiet --leak-check=full' )
 
+mkdir -p profile
+( cd profile && ../configure --prefix=`pwd` CFLAGS='-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -pg' )
+
