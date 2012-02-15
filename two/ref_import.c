cf601bba43d0faf3221de9a75bfb15fbaa06932c
Mike Park <Mike.Park@NASA.Gov>
2012-02-14 20:31:29 -0500

    .msh reader placeholder

--- two/ref_import.c
+++ two/ref_import.c
@@ -65,6 +65,11 @@ REF_STATUS ref_import_by_extension( REF_GRID *ref_grid_ptr, char *filename )
 	  RSS( ref_import_fgrid( ref_grid_ptr, filename ), "fgrid failed");
 	} 
       else 
+	if( strcmp(&filename[end_of_string-4],".msh") == 0 ) 
+	  {
+	    RSS( ref_import_msh( ref_grid_ptr, filename ), "msh failed");
+	  } 
+      else 
 	{
 	  printf("%s: %d: %s %s\n",__FILE__,__LINE__,
 		 "input file name extension unknown", filename);
@@ -436,6 +441,23 @@ REF_STATUS ref_import_b8_ugrid( REF_GRID *ref_grid_ptr, char *filename )
   fclose(file);
 
   return REF_SUCCESS;
+}
+
+REF_STATUS ref_import_msh( REF_GRID *ref_grid_ptr, char *filename )
+{
+  REF_GRID ref_grid;
+  REF_NODE ref_node;
+  FILE *file;
+
+  RSS( ref_grid_create( ref_grid_ptr ), "create grid");
+  ref_grid = (*ref_grid_ptr);
+  ref_node = ref_grid_node(ref_grid);
+
+  file = fopen(filename,"r");
+  if (NULL == (void *)file) printf("unable to open %s\n",filename);
+  RNS(file, "unable to open file" );
+
+  return REF_IMPLEMENT;
 }
 
 REF_STATUS ref_import_mapbc( REF_DICT *ref_dict_ptr, char *filename )
--- two/ref_import.h
+++ two/ref_import.h
@@ -14,6 +14,7 @@ REF_STATUS ref_import_by_extension( REF_GRID *ref_grid, char *filename );
 REF_STATUS ref_import_fgrid( REF_GRID *ref_grid, char *filename );
 REF_STATUS ref_import_ugrid( REF_GRID *ref_grid, char *filename );
 REF_STATUS ref_import_b8_ugrid( REF_GRID *ref_grid, char *filename );
+REF_STATUS ref_import_msh( REF_GRID *ref_grid, char *filename );
 
 REF_STATUS ref_import_mapbc( REF_DICT *ref_dict, char *filename );
 
