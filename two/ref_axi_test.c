60243f1b2d8711f83dbf81db8443f4b6bdc5a8a4
Mike Park <Mike.Park@NASA.Gov>
2012-02-16 09:25:41 -0500

    to allow test code to convert grid

--- two/ref_axi_test.c
+++ two/ref_axi_test.c
@@ -16,6 +16,9 @@
 #include "ref_test.h"
 #include "ref_axi.h"
 
+#include "ref_import.c"
+#include "ref_export.c"
+
 REF_STATUS ref_quad_grid( REF_GRID *ref_grid_ptr, REF_DBL z0, REF_DBL z1 )
 {
   REF_GRID ref_grid;
@@ -105,8 +108,29 @@ REF_STATUS ref_prism_grid( REF_GRID *ref_grid_ptr,
   return REF_SUCCESS;
 }
 
-int main( void )
+int main( int argc, char *argv[] )
 {
+
+  if (argc==3) 
+    {
+      REF_GRID ref_grid;
+
+      printf("importing %s\n",argv[1]);
+      RSS(ref_import_by_extension( &ref_grid, argv[1] ),"from msh");
+      printf("complete.\n");
+
+      RSS(ref_grid_inspect( ref_grid ), "inspection");
+
+      RSS(ref_axi_wedge(ref_grid),"axi wedge");
+
+      RSS(ref_grid_inspect( ref_grid ), "inspection");
+
+      printf("exporting %s\n",argv[2]);
+      RSS(ref_export_by_extension( ref_grid, argv[2] ),"to file");
+      printf("done.\n");
+
+      return 0;
+    }
 
   { /* collapse quad 0 */
     REF_GRID ref_grid;
