141ed555669ffb88aaae13d91c598b2eee337467
Mike Park <Mike.Park@NASA.Gov>
2012-02-22 14:44:57 -0500

    array bounda fpe

--- setup-build.sh
+++ setup-build.sh
@@ -6,7 +6,7 @@ mkdir -p strict
 ( cd strict && ../configure --prefix=`pwd` CFLAGS='-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused' TESTS_ENVIRONMENT='valgrind --quiet --leak-check=full' )
 
 mkdir -p mpi
-( cd mpi && ../configure --prefix=`pwd` CC=mpicc CFLAGS='-DHAVE_MPI -g -O2 -traceback' TESTS_ENVIRONMENT='valgrind --quiet --leak-check=full' )
+( cd mpi && ../configure --prefix=`pwd` CC=mpicc CFLAGS='-DHAVE_MPI -g -O2 -traceback -check -check noarg_temp_created -ftrapuv -fp-stack-check -fpe0' TESTS_ENVIRONMENT='valgrind --quiet --leak-check=full' )
 
 mkdir -p profile
 ( cd profile && ../configure --prefix=`pwd` CFLAGS='-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -pg' )
