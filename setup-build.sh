9d44397e8a7c74f5bfb5dc3703f27f2ed5df1b72
Mike Park <Mike.Park@NASA.Gov>
2012-02-23 10:33:36 -0500

    clean up setup and add zoltan

--- setup-build.sh
+++ setup-build.sh
@@ -2,12 +2,31 @@
 
 set -x
 
+# TESTS_ENVIRONMENT='valgrind --quiet --leak-check=full'
+
 mkdir -p strict
-( cd strict && ../configure --prefix=`pwd` CFLAGS='-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused' TESTS_ENVIRONMENT='valgrind --quiet --leak-check=full' )
+( cd strict && \
+    ../configure \
+    --prefix=`pwd` \
+    CFLAGS='-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused' \
+    ) \
+    || exit
 
 mkdir -p mpi
-( cd mpi && ../configure --prefix=`pwd` CC=mpicc CFLAGS='-DHAVE_MPI -g -O2 -traceback -check -check noarg_temp_created -ftrapuv -fp-stack-check -fpe0' TESTS_ENVIRONMENT='valgrind --quiet --leak-check=full' )
+( cd mpi && \
+    ../configure \
+    --prefix=`pwd` \
+    --with-zoltan=${HOME}/local/pkgs/zoltan_distrib_v3.6/Zoltan_v3.6/build \
+    CC=mpicc \
+    CFLAGS='-DHAVE_MPI -g -O2 -traceback -Wall -Werror-all -Wcheck -ftrapuv -fp-stack-check -check-uninit -fstack-security-check -fpe0' \
+    ) \
+    || exit
 
 mkdir -p profile
-( cd profile && ../configure --prefix=`pwd` CFLAGS='-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -pg' )
+( cd profile && \
+    ../configure \
+    --prefix=`pwd` \
+    CFLAGS='-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -pg' \
+    ) \
+    || exit
 
