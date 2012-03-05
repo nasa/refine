ff4613770fcbc43acac5e48cd7a25bc02829c79d
Mike Park <Mike.Park@NASA.Gov>
2012-03-05 11:06:02 -0500

    check it out

---
+++ check.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -x
+
+script_name_with_path=`which "${0}"`
+script_path=`dirname "${script_name_with_path}"`
+
+# TESTS_ENVIRONMENT='valgrind --quiet --leak-check=full'
+
+( cd ${script_path}/mpi/two && \
+    make check ) && \
+( cd ${script_path}/profile/two && \
+    make check ) && \
+( cd ${script_path}/strict/two && \
+    make check TESTS_ENVIRONMENT='valgrind --quiet --leak-check=full' ) && \
+true
+
