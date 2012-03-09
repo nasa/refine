6f2a89e577b25857da14dcc1261f69cacd6644c5
Mike Park <Mike.Park@NASA.Gov>
2012-03-09 14:12:33 -0500

    test two

--- two/ref_fixture_test.c
+++ two/ref_fixture_test.c
@@ -27,19 +27,33 @@ int main( int argc, char *argv[] )
 
   RSS( ref_mpi_start( argc, argv ), "start" );
 
-  if ( 1 < ref_mpi_n ) 
-    {
-      REF_GRID ref_grid;
+  {
+    REF_GRID ref_grid;
 
-      RSS( ref_fixture_pri_stack_grid( &ref_grid ), "fix" );
+    RSS( ref_fixture_pri_grid( &ref_grid ), "fix" );
+
+    RSS( ref_validation_cell_node( ref_grid ), "invalid pri" );
+
+    RSS( ref_grid_free(ref_grid),"free");
+  }
+
+  {
+    REF_GRID ref_grid;
+
+    RSS( ref_fixture_pri_stack_grid( &ref_grid ), "fix" );
+
+    if ( 1 < ref_mpi_n ) 
       RSS( ref_export_tec_part( ref_grid, "orig_part" ), "see" );
 
-      RSS( ref_validation_cell_node( ref_grid ), "valid nodes" );
+    RSS( ref_validation_cell_node( ref_grid ), "invalid stack" );
 
-      RSS( ref_migrate_to_balance( ref_grid ), "bal" );
+    RSS( ref_migrate_to_balance( ref_grid ), "bal" );
+
+    if ( 1 < ref_mpi_n ) 
       RSS( ref_export_tec_part( ref_grid, "bal_part" ), "see" );
 
-    }
+    RSS( ref_grid_free(ref_grid),"free");
+  }
 
   RSS( ref_mpi_stop( ), "stop" );
 
