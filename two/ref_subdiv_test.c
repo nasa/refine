1b204aa4ffd205eef5a7a8c111e4c78b2f7ffb93
Mike Park <Mike.Park@NASA.Gov>
2012-03-12 12:45:12 -0400

    removed proc checks now that all seq tests are run with mpi shutoff

--- two/ref_subdiv_test.c
+++ two/ref_subdiv_test.c
@@ -174,92 +174,88 @@ int main( int argc, char *argv[] )
     RSS( tear_down( ref_subdiv ), "tear down");
   }
 
-  if ( 1 == ref_mpi_n )
-    { /* new nodes */
-      REF_SUBDIV ref_subdiv;
-      REF_NODE ref_node;
-      RSS(set_up_prism_for_subdiv(&ref_subdiv),"set up");
-      ref_node = ref_grid_node(ref_subdiv_grid(ref_subdiv));
+  { /* new nodes */
+    REF_SUBDIV ref_subdiv;
+    REF_NODE ref_node;
+    RSS(set_up_prism_for_subdiv(&ref_subdiv),"set up");
+    ref_node = ref_grid_node(ref_subdiv_grid(ref_subdiv));
 
-      RSS(ref_subdiv_mark_to_split(ref_subdiv,0,1),"mark edge 0 0-1");
-      RSS(ref_subdiv_mark_to_split(ref_subdiv,3,4),"mark edge 6 3-4");
+    RSS(ref_subdiv_mark_to_split(ref_subdiv,0,1),"mark edge 0 0-1");
+    RSS(ref_subdiv_mark_to_split(ref_subdiv,3,4),"mark edge 6 3-4");
 
-      REIS(6, ref_node_n(ref_node), "6 node prism" );
-      RSS(ref_subdiv_new_node(ref_subdiv),"new nodes");
+    REIS(6, ref_node_n(ref_node), "6 node prism" );
+    RSS(ref_subdiv_new_node(ref_subdiv),"new nodes");
 
-      REIS(8, ref_node_n(ref_node), "two new nodes" );
+    REIS(8, ref_node_n(ref_node), "two new nodes" );
 
-      REIS(6, ref_subdiv_node(ref_subdiv,0), "new 6" );
-      REIS(7, ref_subdiv_node(ref_subdiv,6), "new 6" );
+    REIS(6, ref_subdiv_node(ref_subdiv,0), "new 6" );
+    REIS(7, ref_subdiv_node(ref_subdiv,6), "new 6" );
 
-      RSS( tear_down( ref_subdiv ), "tear down");
-    }
+    RSS( tear_down( ref_subdiv ), "tear down");
+  }
 
-  if ( 1 == ref_mpi_n )
-    { /* split prism in two */
-      REF_SUBDIV ref_subdiv;
-      REF_GRID ref_grid;
-      RSS(set_up_prism_for_subdiv(&ref_subdiv),"set up");
-      ref_grid = ref_subdiv_grid(ref_subdiv);
+  { /* split prism in two */
+    REF_SUBDIV ref_subdiv;
+    REF_GRID ref_grid;
+    RSS(set_up_prism_for_subdiv(&ref_subdiv),"set up");
+    ref_grid = ref_subdiv_grid(ref_subdiv);
 
-      RSS(ref_subdiv_mark_to_split(ref_subdiv,0,1),"mark edge 0-1");
+    RSS(ref_subdiv_mark_to_split(ref_subdiv,0,1),"mark edge 0-1");
 
-      RSS(ref_subdiv_mark_relax(ref_subdiv),"relax");
-      RSS(ref_subdiv_new_node(ref_subdiv),"new nodes");
+    RSS(ref_subdiv_mark_relax(ref_subdiv),"relax");
+    RSS(ref_subdiv_new_node(ref_subdiv),"new nodes");
 
-      RSS(ref_subdiv_split(ref_subdiv),"split");
-      REIS(2, ref_cell_n(ref_grid_pri(ref_grid)),"two");
+    RSS(ref_subdiv_split(ref_subdiv),"split");
+    REIS(2, ref_cell_n(ref_grid_pri(ref_grid)),"two");
 
-      RSS( tear_down( ref_subdiv ), "tear down");
-    }
+    RSS( tear_down( ref_subdiv ), "tear down");
+  }
 
-  if ( 1 == ref_mpi_n )
-    { /* split prism in two with bcs */
-      REF_SUBDIV ref_subdiv;
-      REF_GRID ref_grid;
-      RSS(set_up_prism_for_subdiv(&ref_subdiv),"set up");
-      ref_grid = ref_subdiv_grid(ref_subdiv);
+  { /* split prism in two with bcs */
+    REF_SUBDIV ref_subdiv;
+    REF_GRID ref_grid;
+    RSS(set_up_prism_for_subdiv(&ref_subdiv),"set up");
+    ref_grid = ref_subdiv_grid(ref_subdiv);
 
-      RSS(ref_subdiv_mark_to_split(ref_subdiv,0,1),"mark edge 0-1");
+    RSS(ref_subdiv_mark_to_split(ref_subdiv,0,1),"mark edge 0-1");
 
-      RSS(ref_subdiv_mark_relax(ref_subdiv),"relax");
-      RSS(ref_subdiv_new_node(ref_subdiv),"new nodes");
+    RSS(ref_subdiv_mark_relax(ref_subdiv),"relax");
+    RSS(ref_subdiv_new_node(ref_subdiv),"new nodes");
 
-      RSS(ref_subdiv_split(ref_subdiv),"split");
-      REIS(2, ref_cell_n(ref_grid_pri(ref_grid)),"two pri");
+    RSS(ref_subdiv_split(ref_subdiv),"split");
+    REIS(2, ref_cell_n(ref_grid_pri(ref_grid)),"two pri");
 
-      REIS(2, ref_cell_n(ref_grid_qua(ref_grid)),"two qua");
+    REIS(2, ref_cell_n(ref_grid_qua(ref_grid)),"two qua");
 
-      REIS(4, ref_cell_n(ref_grid_tri(ref_grid)),"four tri");
+    REIS(4, ref_cell_n(ref_grid_tri(ref_grid)),"four tri");
 
-      RSS( tear_down( ref_subdiv ), "tear down");
-    }
+    RSS( tear_down( ref_subdiv ), "tear down");
+  }
 
-  if ( 1 == ref_mpi_n )
-    { /* split prism in four with bcs */
-      REF_SUBDIV ref_subdiv;
-      REF_GRID ref_grid;
-      RSS(set_up_prism_for_subdiv(&ref_subdiv),"set up");
-      ref_grid = ref_subdiv_grid(ref_subdiv);
+  { /* split prism in four with bcs */
+    REF_SUBDIV ref_subdiv;
+    REF_GRID ref_grid;
+    RSS(set_up_prism_for_subdiv(&ref_subdiv),"set up");
+    ref_grid = ref_subdiv_grid(ref_subdiv);
 
-      RSS(ref_subdiv_mark_to_split(ref_subdiv,0,1),"mark edge 0-1");
-      RSS(ref_subdiv_mark_to_split(ref_subdiv,1,2),"mark edge 1-2");
-      RSS(ref_subdiv_mark_to_split(ref_subdiv,2,0),"mark edge 2-0");
+    RSS(ref_subdiv_mark_to_split(ref_subdiv,0,1),"mark edge 0-1");
+    RSS(ref_subdiv_mark_to_split(ref_subdiv,1,2),"mark edge 1-2");
+    RSS(ref_subdiv_mark_to_split(ref_subdiv,2,0),"mark edge 2-0");
 
-      RSS(ref_subdiv_mark_relax(ref_subdiv),"relax");
-      RSS(ref_subdiv_new_node(ref_subdiv),"new nodes");
+    RSS(ref_subdiv_mark_relax(ref_subdiv),"relax");
+    RSS(ref_subdiv_new_node(ref_subdiv),"new nodes");
 
-      RSS(ref_subdiv_split(ref_subdiv),"split");
-      REIS(4, ref_cell_n(ref_grid_pri(ref_grid)),"two pri");
+    RSS(ref_subdiv_split(ref_subdiv),"split");
+    REIS(4, ref_cell_n(ref_grid_pri(ref_grid)),"two pri");
 
-      REIS(2, ref_cell_n(ref_grid_qua(ref_grid)),"two qua");
+    REIS(2, ref_cell_n(ref_grid_qua(ref_grid)),"two qua");
 
-      REIS(8, ref_cell_n(ref_grid_tri(ref_grid)),"four tri");
+    REIS(8, ref_cell_n(ref_grid_tri(ref_grid)),"four tri");
 
-      /*    ref_export_tec(ref_grid,"pri4.tec");  */
+    /*    ref_export_tec(ref_grid,"pri4.tec");  */
 
-      RSS( tear_down( ref_subdiv ), "tear down");
-    }
+    RSS( tear_down( ref_subdiv ), "tear down");
+  }
 
   { /* split tet in two, map 1 */
     REF_SUBDIV ref_subdiv;
