c7919faa2d5262e2813ab135c5144547d5975561
Mike Park <MikePark.rb@GMail.com>
2012-03-13 07:59:40 -0400

    clean up on ref_fixture_pyr_grid

--- two/ref_fixture.c
+++ two/ref_fixture.c
@@ -51,7 +51,7 @@ REF_STATUS ref_fixture_pyr_grid( REF_GRID *ref_grid_ptr )
 {
   REF_GRID ref_grid;
   REF_NODE ref_node;
-  REF_INT nodes[5] = {0,3,4,1,2};
+  REF_INT nodes[5] = {0,1,2,3,4};
   REF_INT cell, node;
 
   RSS(ref_grid_create(ref_grid_ptr),"create");
@@ -70,77 +70,37 @@ REF_STATUS ref_fixture_pyr_grid( REF_GRID *ref_grid_ptr )
   ref_node_xyz(ref_node,2,node) = 0.0;
 
   RSS(ref_node_add(ref_node,2,&node),"add node");
-  ref_node_xyz(ref_node,0,node) = 1.0;
+  ref_node_xyz(ref_node,0,node) = 0.0;
   ref_node_xyz(ref_node,1,node) = 1.0;
   ref_node_xyz(ref_node,2,node) = 0.0;
 
   RSS(ref_node_add(ref_node,3,&node),"add node");
   ref_node_xyz(ref_node,0,node) = 0.0;
-  ref_node_xyz(ref_node,1,node) = 1.0;
-  ref_node_xyz(ref_node,2,node) = 0.0;
+  ref_node_xyz(ref_node,1,node) = 0.0;
+  ref_node_xyz(ref_node,2,node) = 1.0;
 
   RSS(ref_node_add(ref_node,4,&node),"add node");
-  ref_node_xyz(ref_node,0,node) = 0.5;
-  ref_node_xyz(ref_node,1,node) = 0.5;
+  ref_node_xyz(ref_node,0,node) = 0.0;
+  ref_node_xyz(ref_node,1,node) = 1.0;
   ref_node_xyz(ref_node,2,node) = 1.0;
 
   RSS( ref_node_initialize_n_global( ref_node, 5 ), "init glob" );
 
   RSS(ref_cell_add(ref_grid_pyr(ref_grid),nodes,&cell),"add pyr");
 
-  nodes[0] = 0; nodes[1] = 1; nodes[2] = 2; nodes[3] = 3; nodes[4] = 10;
+  nodes[0] = 0; nodes[1] = 3; nodes[2] = 4; nodes[3] = 1; nodes[4] = 10;
   RSS(ref_cell_add(ref_grid_qua(ref_grid),nodes,&cell),"add qua");
 
-  nodes[0] = 0; nodes[1] = 1; nodes[2] = 4; nodes[3] = 20;
+  nodes[0] = 0; nodes[1] = 1; nodes[2] = 2; nodes[3] = 20;
   RSS(ref_cell_add(ref_grid_tri(ref_grid),nodes,&cell),"add tri");
 
-  nodes[0] = 1; nodes[1] = 2; nodes[2] = 4; nodes[3] = 20;
+  nodes[0] = 1; nodes[1] = 4; nodes[2] = 2; nodes[3] = 20;
   RSS(ref_cell_add(ref_grid_tri(ref_grid),nodes,&cell),"add tri");
 
-  nodes[0] = 2; nodes[1] = 3; nodes[2] = 4; nodes[3] = 20;
+  nodes[0] = 4; nodes[1] = 3; nodes[2] = 2; nodes[3] = 20;
   RSS(ref_cell_add(ref_grid_tri(ref_grid),nodes,&cell),"add tri");
 
-  nodes[0] = 3; nodes[1] = 0; nodes[2] = 4; nodes[3] = 20;
-  RSS(ref_cell_add(ref_grid_tri(ref_grid),nodes,&cell),"add tri");
-
-  RSS(ref_node_add(ref_node,5,&node),"add node");
-  ref_node_xyz(ref_node,0,node) = 0.0;
-  ref_node_xyz(ref_node,1,node) = 0.0;
-  ref_node_xyz(ref_node,2,node) = 2.0;
-
-  RSS(ref_node_add(ref_node,6,&node),"add node");
-  ref_node_xyz(ref_node,0,node) = 1.0;
-  ref_node_xyz(ref_node,1,node) = 0.0;
-  ref_node_xyz(ref_node,2,node) = 2.0;
-
-  RSS(ref_node_add(ref_node,7,&node),"add node");
-  ref_node_xyz(ref_node,0,node) = 0.0;
-  ref_node_xyz(ref_node,1,node) = 1.0;
-  ref_node_xyz(ref_node,2,node) = 2.0;
-
-  RSS(ref_node_add(ref_node,8,&node),"add node");
-  ref_node_xyz(ref_node,0,node) = 0.0;
-  ref_node_xyz(ref_node,1,node) = 0.0;
-  ref_node_xyz(ref_node,2,node) = 3.0;
-
-  /*
-  8    
-    7
-  5    6
-  */
-  nodes[0] = 5; nodes[1] = 6; nodes[2] = 7; nodes[3] = 8;
-  RSS(ref_cell_add(ref_grid_tet(ref_grid),nodes,&cell),"add tet");
-
-  nodes[0] = 6; nodes[1] = 8; nodes[2] = 7; nodes[3] = 30;
-  RSS(ref_cell_add(ref_grid_tri(ref_grid),nodes,&cell),"add tri");
-
-  nodes[0] = 5; nodes[1] = 7; nodes[2] = 8; nodes[3] = 30;
-  RSS(ref_cell_add(ref_grid_tri(ref_grid),nodes,&cell),"add tri");
-
-  nodes[0] = 5; nodes[1] = 8; nodes[2] = 6; nodes[3] = 30;
-  RSS(ref_cell_add(ref_grid_tri(ref_grid),nodes,&cell),"add tri");
-
-  nodes[0] = 5; nodes[1] = 6; nodes[2] = 7; nodes[3] = 30;
+  nodes[0] = 0; nodes[1] = 2; nodes[2] = 3; nodes[3] = 20;
   RSS(ref_cell_add(ref_grid_tri(ref_grid),nodes,&cell),"add tri");
 
   return REF_SUCCESS;
