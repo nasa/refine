3dd31cec6e33c00f88de06e1eb947c67186ed58e
Mike Park <MikePark.rb@GMail.com>
2012-03-20 13:57:01 -0400

    use ref_math_divisible macro

--- two/ref_math.c
+++ two/ref_math.c
@@ -10,9 +10,12 @@ REF_STATUS ref_math_normalize( REF_DBL *normal )
   REF_DBL length;
 
   length = ref_math_dot(normal,normal);
-  if (ABS(length) < 1.0e-15) return REF_DIV_ZERO;
 
   length = sqrt(length);
+
+  if ( !ref_math_divisible(normal[0],length) ||
+       !ref_math_divisible(normal[1],length) ||
+       !ref_math_divisible(normal[2],length) ) return REF_DIV_ZERO;
   
   normal[0] /= length;
   normal[1] /= length;
