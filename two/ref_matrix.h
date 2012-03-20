71ed14c2ac7322fc922f36a0ef2b11a3939d2231
Mike Park <MikePark.rb@GMail.com>
2012-03-20 14:16:58 -0400

    to create length in metric macro

--- two/ref_matrix.h
+++ two/ref_matrix.h
@@ -9,6 +9,11 @@ BEGIN_C_DECLORATION
 #define ref_matrix_eig( d, e ) ((d)[(e)])
 #define ref_matrix_vec( d, xyz, v) ((d)[(xyz)+3+3*(v)])
 
+#define ref_matrix_sqrt_vt_m_v( m, v ) \
+  sqrt( (v)[0] * ( (m)[0]*(v)[0] + (m)[1]*(v)[1] + (m)[2]*(v)[2] ) + \
+	(v)[1] * ( (m)[1]*(v)[0] + (m)[3]*(v)[1] + (m)[4]*(v)[2] ) + \
+	(v)[2] * ( (m)[2]*(v)[0] + (m)[4]*(v)[1] + (m)[5]*(v)[2] ) )
+
 REF_STATUS ref_matrix_diagonalize( REF_DBL *m_upper_tri, 
 				   REF_DBL *diagonal_system );
 
