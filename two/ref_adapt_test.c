204e9a7dfd36b9bf3d8898b0e2560926be73e201
Mike Park <Mike.Park@NASA.Gov>
2012-03-23 09:35:53 -0400

    sort of an example

--- two/ref_adapt_test.c
+++ two/ref_adapt_test.c
@@ -15,6 +15,7 @@
 #include "ref_migrate.h"
 
 #include "ref_fixture.h"
+#include "ref_import.h"
 #include "ref_export.h"
 #include "ref_dict.h"
 
@@ -36,16 +37,52 @@ int main( int argc, char *argv[] )
   if ( 1 < argc )
     {
       REF_GRID ref_grid;
+      REF_NODE ref_node;
+      REF_INT node;
+      REF_DBL d[12];
+      REF_DBL x;
+      REF_DBL hmin, hmax, h;
+      REF_INT i;
 
       ref_mpi_stopwatch_start();
       RSS(ref_part_b8_ugrid( &ref_grid, argv[1] ), "import" );
+      ref_node = ref_grid_node(ref_grid);
       ref_mpi_stopwatch_stop("read");
       RSS(ref_migrate_new_part(ref_grid),"new part");
       ref_mpi_stopwatch_stop("new part");
       RSS( ref_migrate_shufflin( ref_grid ), "shufflin");
       ref_mpi_stopwatch_stop("shufflin");
 
-      RSS( ref_adapt_pass( ref_grid ), "pass");
+      each_ref_node_valid_node( ref_node, node )
+	{
+	  hmax = 0.25;
+	  hmin = 0.25;
+	  x = ref_node_xyz(ref_node,0, node);
+	  h = MIN( hmax, hmin+(hmax-hmin)*ABS(x));
+	  ref_matrix_eig( d, 0 ) = 1/(h*h);
+	  h = hmax;
+	  ref_matrix_eig( d, 1 ) = 1/(h*h);
+	  ref_matrix_eig( d, 2 ) = 1/(h*h);
+	  ref_matrix_vec( d, 0, 0) = 1.0;
+	  ref_matrix_vec( d, 1, 0) = 0.0;
+	  ref_matrix_vec( d, 2, 0) = 0.0;
+	  ref_matrix_vec( d, 0, 1) = 0.0;
+	  ref_matrix_vec( d, 1, 1) = 1.0;
+	  ref_matrix_vec( d, 2, 1) = 0.0;
+	  ref_matrix_vec( d, 0, 2) = 0.0;
+	  ref_matrix_vec( d, 1, 2) = 0.0;
+	  ref_matrix_vec( d, 2, 2) = 1.0;
+	  RSS( ref_matrix_form_m(d, ref_node_metric_ptr(ref_node,node) ), "m" );
+	}
+
+      for (i = 0; i<10 ; i++ )
+	{
+	  RSS( ref_adapt_pass( ref_grid ), "pass");
+	  ref_mpi_stopwatch_stop("pass");
+	  RSS(ref_migrate_new_part(ref_grid),"new part");
+	  RSS( ref_migrate_shufflin( ref_grid ), "shufflin");
+	  ref_mpi_stopwatch_stop("bal");
+	}
 
       ref_mpi_stopwatch_start();
       RSS( ref_gather_b8_ugrid( ref_grid, "ref_adapt_test.b8.ugrid" ), 
@@ -53,6 +90,15 @@ int main( int argc, char *argv[] )
       ref_mpi_stopwatch_stop("gather");
 
       RSS( ref_grid_free( ref_grid ), "free");
+
+      if ( ref_mpi_master )
+	{
+	  RSS(ref_import_by_extension( &ref_grid, 
+				       "ref_adapt_test.b8.ugrid" ), "imp" );
+	  RSS(ref_export_by_extension( ref_grid, "ref_adapt_test.tec" ),"ex" );
+	  RSS( ref_grid_free( ref_grid ), "free");
+	}
+      ref_mpi_stopwatch_stop("post");
     }
 
   RSS( ref_mpi_stop(  ), "stop" );
