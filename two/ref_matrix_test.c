abd67c354654c22689b7d3bd60a9cd4c8d23c962
Mike Park <Mike.Park@NASA.Gov>
2012-04-06 09:57:39 -0400

    test streched tet/m

--- two/ref_matrix_test.c
+++ two/ref_matrix_test.c
@@ -268,5 +268,59 @@ int main( void )
     RWDS( 1.0, m[5], tol, "m[5]");
   }
 
+  { /* imply m from from short z iso tet nodes */
+    REF_DBL tol = -1.0;
+    REF_DBL m[6];
+    REF_DBL xyz0[]={ 1.0/3.0*sqrt(3.0),0.0,0.0};
+    REF_DBL xyz1[]={-1.0/6.0*sqrt(3.0),0.5,0.0};
+    REF_DBL xyz2[]={-1.0/6.0*sqrt(3.0),-0.5,0.0};
+    REF_DBL xyz3[]={0.0,0.0, 0.1 * 1.0/3.0*sqrt(6.0)};
+
+    RSS( ref_matrix_imply_m( m, xyz0, xyz1, xyz2, xyz3), "imply");
+
+    RWDS(   1.0, m[0], tol, "m[0]");
+    RWDS(   0.0, m[1], tol, "m[1]");
+    RWDS(   0.0, m[2], tol, "m[2]");
+    RWDS(   1.0, m[3], tol, "m[3]");
+    RWDS(   0.0, m[4], tol, "m[4]");
+    RWDS( 100.0, m[5], tol, "m[5]");
+  }
+
+  { /* imply m from from right tet nodes */
+    REF_DBL tol = -1.0;
+    REF_DBL m[6];
+    REF_DBL xyz0[]={0.0,0.0,0.0};
+    REF_DBL xyz1[]={1.0,0.0,0.0};
+    REF_DBL xyz2[]={0.0,1.0,0.0};
+    REF_DBL xyz3[]={0.0,0.0,1.0};
+
+    RSS( ref_matrix_imply_m( m, xyz0, xyz1, xyz2, xyz3), "imply");
+
+    RWDS( 1.0, m[0], tol, "m[0]");
+    RWDS( 0.5, m[1], tol, "m[1]");
+    RWDS( 0.5, m[2], tol, "m[2]");
+    RWDS( 1.0, m[3], tol, "m[3]");
+    RWDS( 0.5, m[4], tol, "m[4]");
+    RWDS( 1.0, m[5], tol, "m[5]");
+  }
+
+  { /* imply m from from short z right tet nodes */
+    REF_DBL tol = -1.0;
+    REF_DBL m[6];
+    REF_DBL xyz0[]={0.0,0.0,0.0};
+    REF_DBL xyz1[]={1.0,0.0,0.0};
+    REF_DBL xyz2[]={0.0,1.0,0.0};
+    REF_DBL xyz3[]={0.0,0.0,0.1};
+
+    RSS( ref_matrix_imply_m( m, xyz0, xyz1, xyz2, xyz3), "imply");
+
+    RWDS(   1.0, m[0], tol, "m[0]");
+    RWDS(   0.5, m[1], tol, "m[1]");
+    RWDS(   5.0, m[2], tol, "m[2]");
+    RWDS(   1.0, m[3], tol, "m[3]");
+    RWDS(   5.0, m[4], tol, "m[4]");
+    RWDS( 100.0, m[5], tol, "m[5]");
+  }
+
   return 0;
 }
