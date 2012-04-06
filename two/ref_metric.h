d987d1d964e37347dba5eec9aadf7fd147e1e10d
Mike Park <Mike.Park@NASA.Gov>
2012-04-06 13:06:17 -0400

    infer from bl

--- two/ref_metric.h
+++ two/ref_metric.h
@@ -10,6 +10,8 @@ BEGIN_C_DECLORATION
 
 REF_STATUS ref_metric_imply_from( REF_DBL *metric, REF_GRID ref_grid );
 
+REF_STATUS ref_metric_imply_non_tet( REF_DBL *metric, REF_GRID ref_grid );
+
 END_C_DECLORATION
 
 #endif /* REF_METRIC_H */
--- two/ref_metric_test.c
+++ two/ref_metric_test.c
@@ -75,5 +75,67 @@ int main( void )
     RSS(ref_grid_free(ref_grid),"free"); 
   }
 
+  {  /* imply metric non tet prism */
+    REF_DBL tol = 0.00001;
+    REF_GRID ref_grid;
+    REF_DBL *metric;
+    REF_INT node;
+
+    RSS( ref_fixture_pri_grid( &ref_grid ), "tet" );
+
+    ref_malloc( metric, 6*ref_node_max(ref_grid_node(ref_grid)), REF_DBL );
+
+    RSS( ref_metric_imply_non_tet( metric, ref_grid ), "imply" );
+
+    each_ref_node_valid_node( ref_grid_node(ref_grid), node )
+      {
+	RWDS( 0.97569,  metric[0+6*node], tol, "m[0]");
+	RWDS( 0.864269, metric[1+6*node], tol, "m[1]");
+	RWDS(-0.43566,  metric[2+6*node], tol, "m[2]");
+	RWDS( 1.714524, metric[3+6*node], tol, "m[3]");
+	RWDS(-0.864269, metric[4+6*node], tol, "m[4]");
+	RWDS( 0.97569,  metric[5+6*node], tol, "m[5]");
+      }
+
+    ref_free( metric );
+
+    RSS(ref_grid_free(ref_grid),"free"); 
+  }
+
+  {  /* imply metric tet prism */
+    REF_DBL tol = 0.00001;
+    REF_GRID ref_grid;
+    REF_DBL *metric;
+    REF_INT node;
+
+    RSS( ref_fixture_pri_tet_cap_grid( &ref_grid ), "tet" );
+
+    ref_malloc( metric, 6*ref_node_max(ref_grid_node(ref_grid)), REF_DBL );
+
+    RSS( ref_metric_imply_from( metric, ref_grid ), "imply" );
+
+    node = 6;
+    RWDS( 1.00, metric[0+6*node], tol, "m[0]");
+    RWDS( 0.50, metric[1+6*node], tol, "m[1]");
+    RWDS( 0.05, metric[2+6*node], tol, "m[2]");
+    RWDS( 1.00, metric[3+6*node], tol, "m[3]");
+    RWDS( 0.05, metric[4+6*node], tol, "m[4]");
+    RWDS( 0.67, metric[5+6*node], tol, "m[5]");
+
+    RSS( ref_metric_imply_non_tet( metric, ref_grid ), "imply" );
+
+    node = 6;
+    RWDS( 1.0,  metric[0+6*node], tol, "m[0]");
+    RWDS( 0.5, metric[1+6*node], tol, "m[1]");
+    RWDS( 0.05,  metric[2+6*node], tol, "m[2]");
+    RWDS( 1.0, metric[3+6*node], tol, "m[3]");
+    RWDS( 0.05, metric[4+6*node], tol, "m[4]");
+    RWDS( 0.67,  metric[5+6*node], tol, "m[5]");
+
+    ref_free( metric );
+
+    RSS(ref_grid_free(ref_grid),"free"); 
+  }
+
   return 0;
 }
