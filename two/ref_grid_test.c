2a49245299e7a8a805d3c440923834d52899f4cc
Mike Park <Mike.Park@NASA.Gov>
2012-04-06 11:21:01 -0400

    imply tet metric

--- two/ref_grid_test.c
+++ two/ref_grid_test.c
@@ -9,13 +9,16 @@
 #include "ref_sort.h"
 #include "ref_matrix.h"
 
+#include "ref_fixture.h"
+#include "ref_malloc.h"
+
 #include "ref_mpi.h"
 
 int main( void )
 {
-  REF_GRID ref_grid;
 
   {  /* init */
+    REF_GRID ref_grid;
     REIS(REF_NULL,ref_grid_free(NULL),"dont free NULL");
 
     RSS(ref_grid_create(&ref_grid),"create");
@@ -23,7 +26,7 @@ int main( void )
   }
 
   {  /* each element */
-
+    REF_GRID ref_grid;
     REF_CELL ref_cell;
     REF_INT node_per;
     REF_INT group;
@@ -37,6 +40,33 @@ int main( void )
 	if ( 7 == node_per ) node_per = 8;
 	RES( node_per, ref_cell_node_per( ref_cell), "cells in order" );
       }
+
+    RSS(ref_grid_free(ref_grid),"free"); 
+  }
+
+  {  /* imply metric tet */
+    REF_DBL tol = -1.0;
+    REF_GRID ref_grid;
+    REF_DBL *metric;
+    REF_INT node;
+
+    RSS( ref_fixture_tet_grid( &ref_grid ), "tet" );
+
+    ref_malloc( metric, 6*ref_node_max(ref_grid_node(ref_grid)), REF_DBL );
+
+    RSS( ref_grid_imply_metric( ref_grid, metric ), "imply" );
+
+    each_ref_node_valid_node( ref_grid_node(ref_grid), node )
+      {
+	RWDS( 1.0, metric[0+6*node], tol, "m[0]");
+	RWDS( 0.5, metric[1+6*node], tol, "m[1]");
+	RWDS( 0.5, metric[2+6*node], tol, "m[2]");
+	RWDS( 1.0, metric[3+6*node], tol, "m[3]");
+	RWDS( 0.5, metric[4+6*node], tol, "m[4]");
+	RWDS( 1.0, metric[5+6*node], tol, "m[5]");
+      }
+
+    ref_free( metric );
 
     RSS(ref_grid_free(ref_grid),"free"); 
   }
