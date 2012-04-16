b4b6ab4dc330ec7088f316df9d386e6c16530a39
Mike Park <Mike.Park@NASA.Gov>
2012-04-16 18:37:35 -0400

    to remove the hand coded fortra interfaces with FC_FUNC_ macro

--- src/unravel_fortran.c
+++ src/unravel_fortran.c
@@ -18,9 +18,7 @@
 
 static Grid *grid = NULL;
 
-void unravel_start__( int *unravel_api_version, int *status )
-{unravel_start_( unravel_api_version, status );}
-void unravel_start_( int *unravel_api_version, int *status )
+void FC_FUNC_(unravel_start,UNRAVEL_START)( int *unravel_api_version, int *status )
 {
   *unravel_api_version = 1;
   *status = 0;
@@ -32,9 +30,7 @@ void unravel_start_( int *unravel_api_version, int *status )
   if (NULL == grid) *status = 1;
 }
 
-void unravel_tet__( int *c2n, double *x, double *y, double *z, int *status )
-{unravel_tet_( c2n, x, y, z, status );}
-void unravel_tet_( int *c2n, double *x, double *y, double *z, int *status )
+void FC_FUNC_(unravel_tet,UNRAVEL_TET)( int *c2n, double *x, double *y, double *z, int *status )
 {
   int cell_node;
   int local_nodes[4];
@@ -58,9 +54,7 @@ void unravel_tet_( int *c2n, double *x, double *y, double *z, int *status )
 	      local_nodes[2], local_nodes[3] );
 }
 
-void unravel_thaw__( int *nodeid, int *status )
-{unravel_thaw_( nodeid, status ); }
-void unravel_thaw_( int *nodeid, int *status )
+void FC_FUNC_(unravel_thaw,UNRAVEL_THAW)( int *nodeid, int *status )
 {
   int local_node;
   *status = 0;
@@ -70,9 +64,7 @@ void unravel_thaw_( int *nodeid, int *status )
   if ( grid != gridThawNode( grid, local_node ) ) *status = 1;
 }
 
-void unravel_it__( int *status )
-{ unravel_it_( status ); }
-void unravel_it_( int *status )
+void FC_FUNC_(unravel_it,UNRAVEL_IT)( int *status )
 {
   int node, try;
   double volume, smallest_volume;
@@ -97,9 +89,7 @@ void unravel_it_( int *status )
 
 }
 
-void unravel_xyz__( int *nodeid, double *x, double *y, double *z, int *status )
-{ unravel_xyz_( nodeid, x, y, z, status ); }
-void unravel_xyz_( int *nodeid, double *x, double *y, double *z, int *status )
+void FC_FUNC_(unravel_xyz,UNRAVEL_XYZ)( int *nodeid, double *x, double *y, double *z, int *status )
 {
   int local_node;
   double xyz[3];
@@ -117,9 +107,7 @@ void unravel_xyz_( int *nodeid, double *x, double *y, double *z, int *status )
   *z = xyz[2];
 }
 
-void unravel_cleanup__( int *status )
-{ unravel_cleanup_( status ); }
-void unravel_cleanup_( int *status )
+void FC_FUNC_(unravel_cleanup,UNRAVEL_CLEANUP)( int *status )
 {
 
   *status = 0;
--- src/unravel_fortran.h
+++ src/unravel_fortran.h
@@ -11,13 +11,17 @@
 
 #include "refine_defs.h"
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 BEGIN_C_DECLORATION
-void unravel_start_( int *unravel_api_version, int *status );
-void unravel_tet_( int *c2n, double *x, double *y, double *z, int *status );
-void unravel_thaw_( int *nodeid, int *status );
-void unravel_it_( int *status );
-void unravel_xyz_( int *nodeid, double *x, double *y, double *z, int *status );
-void unravel_cleanup_( int *status );
+void FC_FUNC_(unravel_start,UNRAVEL_START)( int *unravel_api_version, int *status );
+void FC_FUNC_(unravel_tet,UNRAVEL_TET)( int *c2n, double *x, double *y, double *z, int *status );
+void FC_FUNC_(unravel_thaw,UNRAVEL_THAW)( int *nodeid, int *status );
+void FC_FUNC_(unravel_it,UNRAVEL_IT)( int *status );
+void FC_FUNC_(unravel_xyz,UNRAVEL_XYZ)( int *nodeid, double *x, double *y, double *z, int *status );
+void FC_FUNC_(unravel_cleanup,UNRAVEL_CLEANUP)( int *status );
 END_C_DECLORATION
 
 #endif /* UNRAVEL_FORTRAN_H */
