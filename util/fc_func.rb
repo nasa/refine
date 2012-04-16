a435324ca41ed5ec2e34c6aa86e810aadd365910
Mike Park <MikePark.rb@GMail.com>
2012-04-16 18:43:54 -0400

    to run on new ruby and set ret_val

--- util/fc_func.rb
+++ util/fc_func.rb
@@ -2,9 +2,10 @@
 
 lines = IO.readlines(ARGV[0])
 
-File.open(ARGV[0],'wt') do |f|
+File.open(ARGV[0],'w') do |f|
   lines.each do |line|
-    if line =~ /void (.*)\(/
+    ret_val = 'void'
+    if line =~ /#{ret_val} (.*)\(/
       func_ = $1
       func = $1.gsub(/_$/,'')
       puts line.sub!(/#{func_}/,"FC_FUNC_(#{func},#{func.upcase})")
