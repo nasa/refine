ae8894b2cb5addf804848659d2b6aac71f7f471c
Mike Park <Mike.Park@NASA.Gov>
2012-06-15 10:29:11 -0400

    debug helper name mangle, add arry

--- two/ref_debug.h
+++ two/ref_debug.h
@@ -6,9 +6,19 @@
 
 BEGIN_C_DECLORATION
 
-#define ref_print_int( i ) printf( " " #i " = %d\n", i); 
-#define ref_print_dbl( d ) printf( " " #d " = %e\n", i); 
-#define ref_print_str( s ) printf( " " #s "\n"); 
+#define ref_debug_print_int( i ) printf( " " #i " = %d\n", i); 
+#define ref_debug_print_dbl( d ) printf( " " #d " = %e\n", i); 
+#define ref_debug_print_str( s ) printf( " " #s "\n"); 
+
+#define ref_debug_print_int_array( a, n )		\
+  { REF_INT ref_debug_print_int_array_i;		\
+    for( ref_debug_print_int_array_i = 0 ;		\
+	 ref_debug_print_int_array_i<(n);		\
+	 ref_debug_print_int_array_i++ )		\
+      printf( " " #a "[%d] = %d\n",			\
+	      ref_debug_print_int_array_i,		\
+	      (a)[ref_debug_print_int_array_i]);	\
+  }
 
 END_C_DECLORATION
 
