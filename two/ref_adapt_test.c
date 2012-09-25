1140893ab7e90426431a2c9092c3cc553cf2e225
Mike Park <Mike.Park@NASA.Gov>
2012-09-25 14:54:18 -0400

    add output format

--- two/ref_adapt_test.c
+++ two/ref_adapt_test.c
@@ -173,6 +173,8 @@ int main( int argc, char *argv[] )
 	   "gather");
       ref_mpi_stopwatch_stop("gather");
 
+      RSS( ref_gather_plot( ref_grid, "ref_adapt_test.eps" ), 
+	   "gather");
       RSS( ref_gather_tec_part( ref_grid, "ref_adapt_test_part.tec" ), 
 	   "part_viz");
 
