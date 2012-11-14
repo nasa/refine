c3d3e7ed08b9450a3a4cbda0daccd0a0b52a4af2
Mike Park <Mike.Park@NASA.Gov>
2012-11-14 14:53:15 -0500

    more sweeps and status to fortran interface

--- two/ref_fortran.c
+++ two/ref_fortran.c
@@ -115,12 +115,14 @@ REF_STATUS FC_FUNC_(ref_fortran_adapt,REF_FORTRAN_ADAPT)( void )
   RSS( ref_metric_sanitize(ref_grid),"sant");
   ref_mpi_stopwatch_stop("metric sant");
 
-  passes = 5;
+  passes = 10;
   for (i = 0; i<passes ; i++ )
     {
       RSS( ref_adapt_pass( ref_grid ), "pass");
       ref_mpi_stopwatch_stop("pass");
       RSS(ref_validation_cell_volume(ref_grid),"vol");
+      RSS( ref_histogram_ratio( ref_grid ), "gram");
+      RSS( ref_histogram_quality( ref_grid ), "qual");
       RSS(ref_migrate_to_balance(ref_grid),"balance");
       ref_mpi_stopwatch_stop("balance");
     }
