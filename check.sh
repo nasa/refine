62407fbaa49a81e4dbb0141c930cf2235fd77981
Mike Park <Mike.Park@NASA.Gov>
2013-01-17 10:41:22 -0500

    to test the right path

--- check.sh
+++ check.sh
@@ -7,13 +7,13 @@ script_path=`dirname "${script_name_with_path}"`
 
 # TESTS_ENVIRONMENT='valgrind --quiet --leak-check=full'
 
-if test -d mpi
+if test -d ${script_path}/mpi
 then
   ( cd ${script_path}/mpi/two && \
      make -j check ) || exit
 fi
 
-if test -d strict
+if test -d ${script_path}/strict
 then
   ( cd ${script_path}/strict/two && \
     make -j check TESTS_ENVIRONMENT='valgrind --quiet --leak-check=full' ) || exit
