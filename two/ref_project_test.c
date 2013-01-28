8703b2f0aa3969670748b603d07eb72dfcb915f2
Mike Park <Mike.Park@NASA.Gov>
2013-01-28 08:16:19 -0500

    to embed grid to take a look

--- two/ref_project_test.c
+++ two/ref_project_test.c
@@ -24,7 +24,8 @@
 
 #include "ref_math.h"
 
-#include "ref_fixture.h"
+#include "ref_subdiv.h"
+#include "ref_metric.h"
 
 int main( int argc, char *argv[] )
 {
@@ -32,15 +33,31 @@ int main( int argc, char *argv[] )
   if (argc==2) 
     {
       REF_GRID ref_grid;
+      REF_SUBDIV ref_subdiv;
+      REF_DBL *metric;
 
       printf("import from %s\n",argv[1]);
       RSS( ref_import_by_extension( &ref_grid, argv[1] ), "examine header" );
       printf(" complete.\n");
 
+      ref_malloc( metric, 6*ref_node_max(ref_grid_node(ref_grid)), REF_DBL );
+      RSS( ref_metric_imply_from( metric, ref_grid ), "imply" );
+      RSS( ref_metric_to_node( metric, ref_grid_node(ref_grid) ), "to n" );
+      ref_free( metric );
+
       printf("export ref_project_test_orig.tec\n");
-      RSS(ref_export_by_extension( ref_grid, "ref_project_test_orig" ),"exp");
+      RSS(ref_export_by_extension( ref_grid, "ref_project_test_orig.tec" ),"e");
       printf(" complete.\n");
 
+      RSS( ref_subdiv_create( &ref_subdiv, ref_grid ), "subdiv c");
+      RSS( ref_subdiv_mark_all( ref_subdiv ), "mark all");
+      RSS( ref_subdiv_split( ref_subdiv ), "split");
+
+      printf("export ref_project_test_embed.tec\n");
+      RSS(ref_export_by_extension( ref_grid, "ref_project_test_embed.tec"),"e");
+      printf(" complete.\n");
+
+      RSS( ref_subdiv_free( ref_subdiv), "free" );
       RSS( ref_grid_free( ref_grid ), "free" );
       printf("done.\n");
 
