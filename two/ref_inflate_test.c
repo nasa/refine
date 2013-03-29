68d384ce96360af895992bb8d77cb94f861dadf6
Mike Park <Mike.Park@NASA.Gov>
2013-03-29 08:51:54 -0400

    bring back inflate test

---
+++ two/ref_inflate_test.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include "ref_inflate.h"
+
+#include "ref_math.h"
+
+#include "ref_import.h"
+#include "ref_export.h"
+
+#include "ref_cell.h"
+#include "ref_grid.h"
+#include "ref_sort.h"
+#include "ref_adj.h"
+#include "ref_node.h"
+#include "ref_matrix.h"
+#include "ref_mpi.h"
+#include "ref_dict.h"
+#include "ref_list.h"
+
+#include "ref_edge.h"
+
+int main(  )
+{
+
+  return 0;
+}
