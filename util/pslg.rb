89e01aad535b11cd2d68adc8a843486bd0547f7b
Mike Park <Mike.Park@NASA.Gov>
2013-05-05 12:28:29 -0400

    upstream from embed-2d//pslg/pslg.rb to include Curve

--- util/pslg.rb
+++ util/pslg.rb
@@ -1,12 +1,16 @@
+
+require 'scanf'
+
 #  node, x-y pair
 
 class Node
  
  attr_accessor :x, :y, :bc, :i
 
- def initialize(x, y)
+ def initialize(x, y, bc = nil)
   self.x = x
   self.y = y
+  self.bc = bc
  end
 
  def to_s
@@ -14,16 +18,49 @@ class Node
  end
 
 end
-#  
 
-class Segment
+
+class Curve < Array
+
+  attr_accessor :bc
+
+  def initialize(bc,filename, scale=1.0, format="%f,%f")
+    self.bc = bc
+
+    File.open(filename).each do |line|
+      values = line.scanf(format)
+      if (values.length >= 2)
+        if ( !last.nil? &&
+             values[0]*scale == last.x &&
+             values[1]*scale == last.y ) 
+          puts "duplicate node dropped"
+        else
+          self << Node.new(values[0]*scale,values[1]*scale,bc) 
+        end
+      end
+    end
+
+  end
+
+  def to_s
+    out=""
+    (self.length-1).times do |i|
+      out += "#{self[i].i} #{self[i+1].i} #{bc}\n"
+    end
+    out
+  end
+
+end
+
+
+class Segment < Array
  
- attr_accessor :bc, :first, :last
+ attr_accessor :bc
 
- def initialize(bc,first,last)
+ def initialize(bc,node0,node1)
   self.bc = bc
-  self.first = first
-  self.last = last
+  self[0] = node0
+  self[1] = node1
   first.bc = bc
  end
 
@@ -32,36 +69,47 @@ class Segment
  end
 
 end
-#  node, x-y pair
+
 
 class Msh < Array
 
+ def initialize(angle=30)
+   @verts = Array.new
+   @angle=angle
+ end
+
  def header
-  "Dimension\n2\nMaximalAngleOfCorner\n0.001\n"
+   "AngleOfCornerBound\n#{@angle}\nDimension\n2\n"
  end
 
  def set_node_i_based_on_segment_list
-  i = 0
-  self.each do |segment|
-   node = segment.first
-   i += 1
-   node.i = i
-  end
+   i = 0
+   self.each do |segment|
+     segment.each do |node|
+       unless node.i
+         i += 1
+         node.i = i
+         @verts << node
+       end
+     end
+   end
  end
 
  def vertices
   set_node_i_based_on_segment_list
-  n = self.length
+  n = @verts.length
   out = "Vertices\n#{n}\n"
-  self.each do |segment|
-   node = segment.first
+  @verts.each do |node|
    out += node.to_s
   end
   out
  end
 
  def edges
-  n = self.length
+   n = 0
+   self.each do |segment|
+     n += (segment.length-1)
+   end
   out = "Edges\n#{n}\n"
   self.each do |segment|
    out += segment.to_s
