36420ecc3f6854ef4bc273fbf6cd67889d3ff78d
Mike Park <Mike.Park@NASA.Gov>
2013-05-05 12:42:45 -0400

    to convert a fun3d metric file to a sol file

---
+++ util/metric2sol.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [ ! "$#" -eq 2 ];then
+  echo usage: $0 file.metric file.sol
+fi
+
+cat > $2 <<EOF
+MeshVersionFormatted 2
+
+Dimension 3
+
+SolAtVertices
+`wc -l $1`
+1 3
+
+EOF
+
+awk '{print $1, $2, $4, $3, $5, $6}' $1 >> $2
+
