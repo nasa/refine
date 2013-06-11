91e126b3271f94682d987a4a8cf628a041136c77
Mike Park <Mike.Park@NASA.Gov>
2013-06-11 07:53:42 -0400

    report quality durring adapt

--- two/ref_adapt_test.c
+++ two/ref_adapt_test.c
@@ -107,6 +107,7 @@ int main( int argc, char *argv[] )
 	  RSS( ref_adapt_pass( ref_grid ), "pass");
 	  ref_mpi_stopwatch_stop("pass");
 	  RSS(ref_validation_cell_volume(ref_grid),"vol");
+	  RSS( ref_histogram_quality( ref_grid ), "gram");
 	  RSS( ref_histogram_ratio( ref_grid ), "gram");
 	  RSS(ref_migrate_to_balance(ref_grid),"balance");
 	  ref_mpi_stopwatch_stop("balance");
