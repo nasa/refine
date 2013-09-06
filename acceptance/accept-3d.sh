cc507e77efef305994410159f77d0a3db1263678
Mike Park <Mike.Park@NASA.Gov>
2013-09-06 12:44:20 -0400

    start acceptance

---
+++ acceptance/accept-3d.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+bin=${HOME}/refine/strict/two
+
+${bin}/ref_acceptance 1 ref_adapt_test.b8.ugrid
+
+function adapt_cycle {
+    proj=$1
+
+    cp ref_adapt_test.b8.ugrid ${proj}.b8.ugrid
+
+    ${bin}/ref_translate ${proj}.b8.ugrid ${proj}.html
+    ${bin}/ref_translate ${proj}.b8.ugrid ${proj}.tec
+
+    ${bin}/ref_acceptance ${proj}.b8.ugrid ${proj}.metric 0.01
+    ${bin}/ref_adapt_test ${proj}.b8.ugrid ${proj}.metric
+
+}
+
+adapt_cycle accept-3d-00
+adapt_cycle accept-3d-01
+adapt_cycle accept-3d-02
+adapt_cycle accept-3d-03
+adapt_cycle accept-3d-04
+
---
+++ two/ref_acceptance.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include "ref_inflate.h"
+
+#include "ref_math.h"
+
+#include "ref_import.h"
+#include "ref_export.h"
+
+#include "ref_cell.h"
+#include "ref_grid.h"
+#include "ref_sort.h"
+#include "ref_adj.h"
+#include "ref_node.h"
+#include "ref_matrix.h"
+#include "ref_mpi.h"
+#include "ref_dict.h"
+#include "ref_list.h"
+
+#include "ref_edge.h"
+
+#include "ref_fixture.h"
+#include "ref_metric.h"
+#include "ref_gather.h"
+
+int main( int argc, char *argv[] )
+{
+  REF_GRID ref_grid;
+  
+  if ( 3 == argc )
+    {
+      switch ( atoi(argv[1]) )
+	{
+	case 1:
+	  RSS( ref_fixture_tet_brick_grid( &ref_grid ), "brick");
+	  break;
+	case 2:
+	  RSS( ref_fixture_twod_brick_grid( &ref_grid ), "brick");
+	  break;
+	default:
+	  THROW("case not recognized");
+	  break;
+	}
+      RSS( ref_export_by_extension( ref_grid, argv[2] ), "out" );
+
+      return 0;
+    }
+
+  if ( 4 == argc )
+    {
+      RSS( ref_import_by_extension( &ref_grid, argv[1] ), "in");
+      RSS( ref_metric_olympic_node( ref_grid_node(ref_grid), atof(argv[3]) ),
+	   "oly" );
+      RSS( ref_gather_metric( ref_grid, argv[2] ), "in");
+
+      return 0;
+    }
+
+  return 0;
+}
