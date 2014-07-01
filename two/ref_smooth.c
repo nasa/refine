689819d390f69a3041cd001410aac08ace8738ab
Mike Park <Mike.Park@NASA.Gov>
2014-07-01 11:04:47 -0400

    start with query method for tri node qual

--- two/ref_smooth.c
+++ two/ref_smooth.c
@@ -46,3 +46,34 @@ REF_STATUS ref_smooth_twod( REF_GRID ref_grid, REF_INT node )
 
   return REF_SUCCESS;
 }
+
+REF_STATUS ref_smooth_tri_quality_around( REF_GRID ref_grid, 
+					  REF_INT node,
+					  REF_DBL *min_quality )
+{
+  REF_NODE ref_node = ref_grid_node(ref_grid);
+  REF_CELL ref_cell = ref_grid_tri(ref_grid);
+  REF_INT item, cell;
+  REF_INT nodes[REF_CELL_MAX_SIZE_PER];
+  REF_BOOL none_found = REF_TRUE;
+  REF_DBL quality;
+
+  *min_quality = 1.0;
+  each_ref_cell_having_node( ref_cell, node, item, cell )
+    {
+      RSS( ref_cell_nodes( ref_cell, cell, nodes ), "nodes" );
+      none_found = REF_FALSE;
+      RSS( ref_node_tri_quality( ref_node, 
+				 nodes,  
+				 &quality ), "qual" );
+      *min_quality = MIN( *min_quality, quality );
+    }
+      
+  if ( none_found )
+    {
+      *min_quality = -2.0;
+      return REF_NOT_FOUND;
+    }
+
+  return REF_SUCCESS;
+}
--- two/ref_smooth.h
+++ two/ref_smooth.h
@@ -10,6 +10,10 @@ BEGIN_C_DECLORATION
 
 REF_STATUS ref_smooth_twod( REF_GRID ref_grid, REF_INT node );
 
+REF_STATUS ref_smooth_tri_quality_around( REF_GRID ref_grid, 
+					  REF_INT node,
+					  REF_DBL *min_quality );
+
 END_C_DECLORATION
 
 #endif /* REF_SMOOTH_H */
