3aadee50e84a2a7969619a1eba5679ae1f95d5b7
Mike Park <Mike.Park@NASA.Gov>
2014-09-16 14:15:33 -0400

    test with export/import

--- two/ref_import_test.c
+++ two/ref_import_test.c
@@ -29,6 +29,23 @@ int main( int argc, char *argv[] )
       return 0;
     }
 
+  { /* export import .msh brick */
+    REF_GRID export_grid, import_grid;
+    char file[] = "ref_import_test.msh";
+    RSS(ref_fixture_twod_brick_grid( &export_grid ), "set up tet" );
+    RSS(ref_export_twod_msh( export_grid, file ), "export" );
+    RSS(ref_import_msh( &import_grid, file ), "import" );
+    REIS( ref_node_n(ref_grid_node(export_grid)),
+	  ref_node_n(ref_grid_node(import_grid)), "node count" );
+    REIS( ref_cell_n(ref_grid_qua(export_grid)),
+	  ref_cell_n(ref_grid_qua(import_grid)), "qua count" );
+    REIS( ref_cell_n(ref_grid_tri(export_grid)),
+	  ref_cell_n(ref_grid_tri(import_grid)), "tri count" );
+    RSS(ref_grid_free(import_grid),"free");
+    RSS(ref_grid_free(export_grid),"free");
+    REIS(0, remove( file ), "test clean up");
+  }
+
   { /* export import .fgrid tet */
     REF_GRID export_grid, import_grid;
     char file[] = "ref_import_test.fgrid";
