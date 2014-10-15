1d14bd6f2c25b317ae92cb4fe120b6c4e8108967
Mike Park <Mike.Park@NASA.Gov>
2014-10-15 11:25:53 -0400

    unify

--- acceptance/3d/linear/fun3d/accept-3d-fun3d.sh
+++ acceptance/3d/linear/fun3d/accept-3d-fun3d.sh
@@ -10,19 +10,19 @@ cat > faux_input <<EOF
 6 zplane 1
 EOF
 
-two=${HOME}/refine/strict/two
+bin=${HOME}/refine/strict/two
 
-${two}/ref_acceptance 1 ref_adapt_test.lb8.ugrid
+${bin}/ref_acceptance 1 ref_adapt_test.b8.ugrid
 
 function adapt_cycle {
     proj=$1
 
-     cp ref_adapt_test.lb8.ugrid ${proj}.lb8.ugrid
+    cp ref_adapt_test.b8.ugrid ${proj}.b8.ugrid
 
-#     ${two}/ref_translate ${proj}.fgrid ${proj}.html
-    ${two}/ref_translate ${proj}.lb8.ugrid ${proj}.tec
+    ${bin}/ref_translate ${proj}.b8.ugrid ${proj}.html
+    ${bin}/ref_translate ${proj}.b8.ugrid ${proj}.tec
 
-    ${two}/ref_acceptance ${proj}.lb8.ugrid ${proj}.metric 0.001
+    ${bin}/ref_acceptance ${proj}.b8.ugrid ${proj}.metric 0.001
 
 cat > ${proj}.mapbc <<EOF
 6
@@ -59,14 +59,21 @@ adapt_cycles = 6
 EOF
 
     nodet_mpi --adapt
+    ${bin}/ref_translate ref_adapt_test.lb8.ugrid ref_adapt_test.b8.ugrid 
 
+    ${bin}/ref_metric_test ${proj}.b8.ugrid ${proj}.metric > ${proj}.status
+    cp ref_metric_test_surf.tec ${proj}_metric_surf.tec
+    cp ref_metric_test_s00_n1_p0_ellipse.tec ${proj}_metric_ellipse.tec
+    gnuplot ref_histogram_edge-ratio.gnuplot
+    epstopdf ref_histogram_edge-ratio.eps
+    cp ref_histogram_edge-ratio.pdf ${proj}_edge-ratio.pdf
 }
 
-adapt_cycle accept-3d-00
-adapt_cycle accept-3d-01
-adapt_cycle accept-3d-02
-adapt_cycle accept-3d-03
-adapt_cycle accept-3d-04
-adapt_cycle accept-3d-05
-adapt_cycle accept-3d-06
+adapt_cycle accept-3d-fun3d-00
+adapt_cycle accept-3d-fun3d-01
+adapt_cycle accept-3d-fun3d-02
+adapt_cycle accept-3d-fun3d-03
+adapt_cycle accept-3d-fun3d-04
+adapt_cycle accept-3d-fun3d-05
+adapt_cycle accept-3d-fun3d-06
 
