08c3d71375826bfe617a48924cfd325c5feac727
Mike Park <Mike.Park@NASA.gov>
2014-10-15 10:17:26 -0400

    standardize one

--- acceptance/accept-3d-one.sh
+++ acceptance/accept-3d-one.sh
@@ -10,30 +10,38 @@ cat > faux_input <<EOF
 6 zplane 1
 EOF
 
-two=${HOME}/refine/strict/two
+bin=${HOME}/refine/strict/two
 one=${HOME}/refine/strict/src
 
-${two}/ref_acceptance 1 ref_adapt_test.b8.ugrid
-${two}/ref_translate ref_adapt_test.b8.ugrid refine-one.fgrid
+${bin}/ref_acceptance 1 ref_adapt_test.b8.ugrid
 
 function adapt_cycle {
     proj=$1
 
-    cp refine-one.fgrid ${proj}.fgrid
+    cp ref_adapt_test.b8.ugrid ${proj}.b8.ugrid
 
-#     ${two}/ref_translate ${proj}.fgrid ${proj}.html
-    ${two}/ref_translate ${proj}.fgrid ${proj}.tec
+    ${bin}/ref_translate ${proj}.b8.ugrid ${proj}.html
+    ${bin}/ref_translate ${proj}.b8.ugrid ${proj}.tec
 
-    ${two}/ref_acceptance ${proj}.fgrid ${proj}.metric 0.001
+    ${bin}/ref_acceptance ${proj}.b8.ugrid ${proj}.metric 0.001
+
+    ${bin}/ref_translate ${proj}.b8.ugrid ${proj}.fgrid
     ${one}/refine-px -g ${proj}.fgrid -m ${proj}.metric -o refine-one.fgrid
+    ${bin}/ref_translate ${proj}.fgrid ref_adapt_test.b8.ugrid
 
+    ${bin}/ref_metric_test ${proj}.b8.ugrid ${proj}.metric
+    cp ref_metric_test_surf.tec ${proj}_metric_surf.tec
+    cp ref_metric_test_s00_n1_p0_ellipse.tec ${proj}_metric_ellipse.tec
+    gnuplot ref_histogram_edge-ratio.gnuplot
+    epstopdf ref_histogram_edge-ratio.eps
+    cp ref_histogram_edge-ratio.pdf ${proj}_edge-ratio.pdf
 }
 
-adapt_cycle accept-3d-00
-adapt_cycle accept-3d-01
-adapt_cycle accept-3d-02
-adapt_cycle accept-3d-03
-adapt_cycle accept-3d-04
-adapt_cycle accept-3d-05
-adapt_cycle accept-3d-06
+adapt_cycle accept-3d-one-00
+adapt_cycle accept-3d-one-01
+adapt_cycle accept-3d-one-02
+adapt_cycle accept-3d-one-03
+adapt_cycle accept-3d-one-04
+adapt_cycle accept-3d-one-05
+adapt_cycle accept-3d-one-06
 
