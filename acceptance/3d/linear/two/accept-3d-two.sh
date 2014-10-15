9d43b8c3cc548e2e40a4b535dc5a50d8dfe54807
Mike Park <Mike.Park@NASA.Gov>
2014-10-15 11:18:56 -0400

    unify

--- acceptance/3d/linear/two/accept-3d-two.sh
+++ acceptance/3d/linear/two/accept-3d-two.sh
@@ -13,9 +13,10 @@ function adapt_cycle {
     ${bin}/ref_translate ${proj}.b8.ugrid ${proj}.tec
 
     ${bin}/ref_acceptance ${proj}.b8.ugrid ${proj}.metric 0.001
+
     ${bin}/ref_adapt_test ${proj}.b8.ugrid ${proj}.metric
 
-    ${bin}/ref_metric_test ${proj}.b8.ugrid ${proj}.metric
+    ${bin}/ref_metric_test ${proj}.b8.ugrid ${proj}.metric > ${proj}.status
     cp ref_metric_test_surf.tec ${proj}_metric_surf.tec
     cp ref_metric_test_s00_n1_p0_ellipse.tec ${proj}_metric_ellipse.tec
     gnuplot ref_histogram_edge-ratio.gnuplot
