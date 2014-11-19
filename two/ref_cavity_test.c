901134d5d7e7d6df74956a7c1dd1579c06a3d582
Mike Park <Mike.Park@NASA.gov>
2014-11-19 14:09:46 -0500

    start new system

--- two/ref_cavity_test.c
+++ two/ref_cavity_test.c
@@ -401,9 +401,6 @@ int main( int argc, char *argv[] )
     RSS(ref_cavity_replace_tri(ref_cavity, ref_grid, node, opp ),"free");
     RSS(ref_cavity_free(ref_cavity),"free");
 
-    if ( 2 == argc )
-      RSS( ref_export_by_extension( ref_grid, argv[1] ), "export" );
-
     REIS( 28, ref_node_n(ref_grid_node(ref_grid)), "nodes" );
     REIS( 28, ref_cell_n(ref_grid_tri(ref_grid)), "nodes" );
     REIS( 14, ref_cell_n(ref_grid_pri(ref_grid)), "nodes" );
@@ -455,6 +452,29 @@ int main( int argc, char *argv[] )
     REIS( 1, ref_cell_n(ref_grid_pri(ref_grid)), "nodes" );
 
     RSS(ref_grid_free(ref_grid),"free");
+  }
+
+  { /* small edge */
+    REF_GRID ref_grid;
+    REF_NODE ref_node;
+    REF_CAVITY ref_cavity;
+    REF_INT node, opp;
+
+    RSS( ref_fixture_twod_brick_grid( &ref_grid ), "brick" );
+    ref_node = ref_grid_node(ref_grid);
+
+    node = 10;
+    RSS(ref_twod_opposite_node(ref_grid_pri(ref_grid), node, &opp), "opp");
+    RSS(ref_cavity_create(&ref_cavity,2),"create");
+    RSS(ref_cavity_add_disk(ref_cavity,ref_grid,node),"insert first");
+    ref_node_xyz(ref_node,2,node) = 0.6;
+    ref_node_xyz(ref_node,2,opp ) = 0.6;
+    RSS(ref_cavity_free(ref_cavity),"free");
+
+    if ( 2 == argc )
+      RSS( ref_export_by_extension( ref_grid, argv[1] ), "export" );
+
+    RSS( ref_grid_free(ref_grid),"free");
   }
 
   return 0;
