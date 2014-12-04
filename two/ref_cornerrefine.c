78f634f216f2c0aa627b70175bea49201d761b2f
Mike Park <Mike.Park@NASA.Gov>
2014-12-04 08:16:14 -0500

    refine opts

--- two/ref_cornerrefine.c
+++ two/ref_cornerrefine.c
@@ -31,9 +31,11 @@
 int main( int argc, char *argv[] )
 {
   REF_GRID ref_grid;
+  REF_CELL ref_cell;
   REF_SUBDIV ref_subdiv;
   REF_BOOL valid_inputs;
   REF_INT cell, nodes[REF_CELL_MAX_SIZE_PER];
+  REF_INT node, item;
   valid_inputs = (3 == argc);  
 
   if ( !valid_inputs )
@@ -44,15 +46,28 @@ int main( int argc, char *argv[] )
 
   RSS( ref_import_by_extension( &ref_grid, argv[1] ), "in");
   RSS( ref_metric_unit_node( ref_grid_node(ref_grid) ), "met");
-  RSS( ref_subdiv_create( &ref_subdiv, ref_grid ), "init" );
 
-  each_ref_cell_valid_cell_with_nodes( ref_grid_pri(ref_grid), cell, nodes)
+  ref_cell = ref_grid_pri(ref_grid);
+
+  RSS( ref_subdiv_create( &ref_subdiv, ref_grid ), "init" );
+  each_ref_cell_valid_cell_with_nodes( ref_cell, cell, nodes)
     {
       RSS( ref_subdiv_mark_to_split( ref_subdiv, nodes[1], nodes[2] ), "o0" );
       RSS( ref_subdiv_mark_to_split( ref_subdiv, nodes[2], nodes[0] ), "o1" );
       RSS( ref_subdiv_mark_to_split( ref_subdiv, nodes[0], nodes[1] ), "o2" );
     }
+  RSS(ref_subdiv_split(ref_subdiv),"split");
+  RSS(ref_subdiv_free(ref_subdiv),"free");
 
+  RSS( ref_subdiv_create( &ref_subdiv, ref_grid ), "init" );
+  node = 0;
+  each_ref_cell_having_node( ref_cell, node, item, cell )
+    {
+      RSS( ref_cell_nodes( ref_cell, cell, nodes ), "nodes" );
+      RSS( ref_subdiv_mark_to_split( ref_subdiv, nodes[1], nodes[2] ), "o0" );
+      RSS( ref_subdiv_mark_to_split( ref_subdiv, nodes[2], nodes[0] ), "o1" );
+      RSS( ref_subdiv_mark_to_split( ref_subdiv, nodes[0], nodes[1] ), "o2" );
+    }
   RSS(ref_subdiv_split(ref_subdiv),"split");
   RSS(ref_subdiv_free(ref_subdiv),"free");
 
