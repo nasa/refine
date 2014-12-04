f8427d5dfdbb6b39e6a5ef656c965ef6282bc382
Mike Park <Mike.Park@NASA.Gov>
2014-12-04 13:11:30 -0500

    r pow meshes

--- two/ref_acceptance.c
+++ two/ref_acceptance.c
@@ -29,6 +29,7 @@
 int main( int argc, char *argv[] )
 {
   REF_GRID ref_grid;
+  REF_NODE ref_node;
   
   if ( 3 == argc )
     {
@@ -55,6 +56,41 @@ int main( int argc, char *argv[] )
 
       RSS( ref_metric_olympic_node( ref_grid_node(ref_grid), atof(argv[3]) ),
 	   "oly" );
+      if (ref_grid_twod(ref_grid))
+	RSS( ref_metric_twod_node( ref_grid_node(ref_grid) ), "2d" );
+
+      RSS( ref_gather_metric( ref_grid, argv[2] ), "in");
+
+      return 0;
+    }
+
+  if ( 5 == argc )
+    {
+      REF_DBL x, z, h, c, r, k;
+      REF_INT node;
+
+      c = atof(argv[3]);
+      k = atof(argv[4]);
+
+      RSS( ref_import_by_extension( &ref_grid, argv[1] ), "in");
+      ref_node = ref_grid_node(ref_grid);
+
+      each_ref_node_valid_node( ref_node, node )
+      {
+	x = ref_node_xyz(ref_node,0,node);
+	z = ref_node_xyz(ref_node,2,node);
+	r = sqrt( x*x + z*z );
+	r = MAX( r, 4e-3*c );
+	h = c * pow(r,k); 
+ 
+	ref_node_metric(ref_node,0,node) = 1.0/(h*h);
+	ref_node_metric(ref_node,1,node) = 0.0;
+	ref_node_metric(ref_node,2,node) = 0.0;
+	ref_node_metric(ref_node,3,node) = 1.0;
+	ref_node_metric(ref_node,4,node) = 0.0;
+	ref_node_metric(ref_node,5,node) = 1.0/(h*h);
+      } 
+
       if (ref_grid_twod(ref_grid))
 	RSS( ref_metric_twod_node( ref_grid_node(ref_grid) ), "2d" );
 
