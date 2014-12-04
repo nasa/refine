2c71d744fe505a5d84ee4d9405e8dca7aaf712a4
Mike Park <Mike.Park@NASA.Gov>
2014-12-04 13:14:25 -0500

    only metric last one

--- acceptance/2d/crh/bamg/accept-2d-bamg.sh
+++ acceptance/2d/crh/bamg/accept-2d-bamg.sh
@@ -4,6 +4,9 @@ bin=${HOME}/refine/strict/two
 
 ${bin}/ref_acceptance 2 ref_bamg_test.b8.ugrid
 
+c=0.02
+k=0.5
+
 function adapt_cycle {
     proj=$1
 
@@ -12,7 +15,7 @@ function adapt_cycle {
     ${bin}/ref_translate ${proj}.b8.ugrid ${proj}.html
     ${bin}/ref_translate ${proj}.b8.ugrid ${proj}.tec
 
-    ${bin}/ref_acceptance ${proj}.b8.ugrid ${proj}.metric 0.02 0.5
+    ${bin}/ref_acceptance ${proj}.b8.ugrid ${proj}.metric $c $k
     ${bin}/ref_bamg ${proj}.b8.ugrid ${proj}.metric
 
     ${bin}/ref_metric_test ${proj}.b8.ugrid ${proj}.metric
@@ -20,14 +23,23 @@ function adapt_cycle {
     cp ref_metric_test_s00_n1_p0_ellipse.tec ${proj}_metric_ellipse.tec
 }
 
-adapt_cycle accept-2d-bamg-00
-adapt_cycle accept-2d-bamg-01
-adapt_cycle accept-2d-bamg-02
-adapt_cycle accept-2d-bamg-03
-adapt_cycle accept-2d-bamg-04
-adapt_cycle accept-2d-bamg-05
-adapt_cycle accept-2d-bamg-06
-adapt_cycle accept-2d-bamg-07
-adapt_cycle accept-2d-bamg-08
+function gen_only {
+    proj=$1
+
+    cp ref_bamg_test.b8.ugrid ${proj}.b8.ugrid
+
+    ${bin}/ref_acceptance ${proj}.b8.ugrid ${proj}.metric $c $k
+    ${bin}/ref_bamg ${proj}.b8.ugrid ${proj}.metric
+}
+
+gen_only accept-2d-bamg-00
+gen_only accept-2d-bamg-01
+gen_only accept-2d-bamg-02
+gen_only accept-2d-bamg-03
+gen_only accept-2d-bamg-04
+gen_only accept-2d-bamg-05
+gen_only accept-2d-bamg-06
+gen_only accept-2d-bamg-07
+gen_only accept-2d-bamg-08
 adapt_cycle accept-2d-bamg-09
 
