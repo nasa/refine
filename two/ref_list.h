8b7e08de1e0fd78555596b23f62077766e0e3d74
Mike Park <Mike.Park@NASA.Gov>
2014-12-16 15:50:12 -0500

    iterator

--- two/ref_list.h
+++ two/ref_list.h
@@ -24,6 +24,9 @@ REF_STATUS ref_list_inspect( REF_LIST ref_list );
 #define ref_list_max( ref_list ) ((ref_list)->max)
 #define ref_list_value( ref_list, i ) ((ref_list)->value[(i)])
 
+#define each_ref_list_item( ref_list, item )		\
+  for ( ( item ) = 0; ( item ) < ref_list_n( ref_list ); ( item )++ )
+
 REF_STATUS ref_list_add( REF_LIST ref_list, REF_INT value );
 REF_STATUS ref_list_pop( REF_LIST ref_list, REF_INT *value );
 REF_STATUS ref_list_delete( REF_LIST ref_list, REF_INT value );
