cd9fdec7314920f35baf678087849dc41c19b189
Mike Park <Mike.Park@NASA.gov>
2015-03-21 11:53:01 -0400

    run a number of sweeps

--- acceptance/2d/linear/feflo/accept-2d-feflo.sh
+++ acceptance/2d/linear/feflo/accept-2d-feflo.sh
@@ -13,15 +13,20 @@ function adapt_cycle {
     ${bin}/ref_translate ${proj}.b8.ugrid ${proj}.tec
 
     ${bin}/ref_acceptance ${proj}.b8.ugrid ${proj}.metric 0.0001
-    ${bin}/ref_fefloa ${proj}.b8.ugrid ${proj}.metric
+    ${bin}/ref_fefloa2d ${proj}.b8.ugrid ${proj}.metric
 
+    feflo.a-cart \
+	-in fefloa.2d.meshb \
+	-met fefloa.2d.sol \
+	-out ref_fefloa_test.meshb 
+    ${bin}/ref_translate ref_fefloa_test.meshb ref_fefloa_test.b8.ugrid
+    
     ${bin}/ref_metric_test ${proj}.b8.ugrid ${proj}.metric
     cp ref_metric_test_surf.tec ${proj}_metric_surf.tec
     cp ref_metric_test_s00_n1_p0_ellipse.tec ${proj}_metric_ellipse.tec
 }
 
 adapt_cycle accept-2d-fefloa-00
-exit
 adapt_cycle accept-2d-fefloa-01
 adapt_cycle accept-2d-fefloa-02
 adapt_cycle accept-2d-fefloa-03
