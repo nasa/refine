6256858fc46819059744c475beacff092598dc4b
Mike Park <Mike.Park@NASA.gov>
2015-03-21 11:52:41 -0400

    fefloa driver

---
+++ two/ref_fefloa2d.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include "ref_export.h"
+#include "ref_import.h"
+#include "ref_part.h"
+#include "ref_grid.h"
+#include "ref_validation.h"
+#include "ref_histogram.h"
+
+int main( int argc, char *argv[] )
+{
+  REF_GRID ref_grid;
+  REF_BOOL valid_inputs;
+
+  valid_inputs = (3 == argc);
+
+  if ( !valid_inputs )
+    {
+      printf("usage: %s project.extension project.metric\n",argv[0]);
+      return 0;
+    }
+
+  RSS( ref_import_by_extension( &ref_grid, argv[1] ), "import" );
+  RSS( ref_export_by_extension( ref_grid, "fefloa.2d.meshb" ), "export" );
+  RSS( ref_part_metric( ref_grid_node(ref_grid), argv[2] ), "part metric" );
+  RSS( ref_export_twod_sol( ref_grid, "fefloa.2d.sol" ), "export" );
+
+  RSS(ref_grid_free(ref_grid),"free");
+
+  return 0;
+}
