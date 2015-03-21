5e2f2be66f9ee1e4d3e0b0ab0344a171ae43caa2
Mike Park <Mike.Park@NASA.gov>
2015-03-21 09:30:51 -0400

    make sure error codes are emmited for memeory bugs

--- check.sh
+++ check.sh
@@ -5,8 +5,6 @@ set -x
 script_name_with_path=`which "${0}"`
 script_path=`dirname "${script_name_with_path}"`
 
-# TESTS_ENVIRONMENT='valgrind --quiet --leak-check=full'
-
 if test -d ${script_path}/zoltan
 then
   ( cd ${script_path}/zoltan && \
@@ -22,7 +20,7 @@ fi
 if test -d ${script_path}/strict
 then
   ( cd ${script_path}/strict && \
-    make -j check TESTS_ENVIRONMENT='valgrind --quiet --leak-check=full' ) || exit
+    make -j check TESTS_ENVIRONMENT='valgrind --quiet  --error-exitcode=1 --leak-check=full' ) || exit
 
 fi
 
