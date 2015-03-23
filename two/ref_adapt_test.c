53e0802166a559eb2956418c71da8600be9ff733
Mike Park <Mike.Park@NASA.Gov>
2015-03-23 10:41:54 -0400

    note about 2d mod for troy lake

--- two/ref_adapt_test.c
+++ two/ref_adapt_test.c
@@ -60,6 +60,10 @@ int main( int argc, char *argv[] )
       ref_node = ref_grid_node(ref_grid);
       ref_mpi_stopwatch_stop("read grid");
 
+      /* for Troy Lake Pointwise extruded 2D grids
+	 ref_node_twod_mid_plane(ref_node)=-1;
+      */
+
       RSS(ref_migrate_to_balance(ref_grid),"balance");
       ref_mpi_stopwatch_stop("balance");
 
