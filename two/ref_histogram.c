618b9cac289ae265e1e6e661c4fc2fa93fa527ec
Mike Park <Mike.Park@NASA.Gov>
2015-04-17 10:48:55 -0400

    sort methods

--- two/ref_histogram.c
+++ two/ref_histogram.c
@@ -66,90 +66,6 @@ REF_STATUS ref_histogram_add( REF_HISTOGRAM ref_histogram, REF_DBL observation )
   return REF_SUCCESS;
 }
 
-REF_STATUS ref_histogram_ratio( REF_GRID ref_grid )
-{
-  REF_HISTOGRAM ref_histogram;
-  REF_EDGE ref_edge;
-  REF_INT edge, part;
-  REF_DBL ratio;
-  REF_BOOL active;
-
-  RSS( ref_histogram_create(&ref_histogram),"create");
-  RSS( ref_edge_create( &ref_edge, ref_grid ), "make edges" );
-
-  for (edge=0;edge< ref_edge_n(ref_edge);edge++)
-    {
-      RSS( ref_edge_part( ref_edge, edge, &part ), "edge part");
-      RSS( ref_node_edge_twod( ref_grid_node(ref_grid), 
-			       ref_edge_e2n(ref_edge, 0, edge),
-			       ref_edge_e2n(ref_edge, 1, edge), 
-			       &active ), "twod edge");
-      active = ( active || !ref_grid_twod(ref_grid) );
-      if ( part == ref_mpi_id && active )
-	{
-	  RSS( ref_node_ratio( ref_grid_node(ref_grid), 
-			       ref_edge_e2n(ref_edge, 0, edge),
-			       ref_edge_e2n(ref_edge, 1, edge), 
-			       &ratio ), "rat");
-	  RSS( ref_histogram_add( ref_histogram, ratio ), "add");
-	}
-    }
-
-  RSS( ref_histogram_gather( ref_histogram ), "gather");
-  if ( ref_mpi_master ) RSS( ref_histogram_print( ref_histogram,
-						  "edge ratio"), "print");
-  if ( 1 == ref_mpi_n ) RSS( ref_histogram_export( ref_histogram,
-						   "edge-ratio"), "print");
-
-  RSS( ref_edge_free(ref_edge), "free edge" );
-  RSS( ref_histogram_free(ref_histogram), "free gram" );
-  return REF_SUCCESS;
-}
-
-REF_STATUS ref_histogram_quality( REF_GRID ref_grid )
-{
-  REF_HISTOGRAM ref_histogram;
-  REF_CELL ref_cell;
-  REF_INT cell;
-  REF_INT nodes[REF_CELL_MAX_SIZE_PER];
-  REF_DBL quality;
-
-  RSS( ref_histogram_create(&ref_histogram),"create");
-
-  if ( ref_grid_twod(ref_grid) )
-    {
-      ref_cell = ref_grid_tri(ref_grid);
-    }
-  else
-    {
-      ref_cell = ref_grid_tet(ref_grid);
-    }
-  each_ref_cell_valid_cell_with_nodes( ref_cell, cell, nodes)
-    {
-      if ( ref_node_part(ref_grid_node(ref_grid),nodes[0]) == ref_mpi_id )
-	{
-	  if ( ref_grid_twod(ref_grid) )
-	    {
-	      RSS( ref_node_tri_quality( ref_grid_node(ref_grid),
-					 nodes,&quality ), "qual");
-	    }
-	  else
-	    {
-	      RSS( ref_node_tet_quality( ref_grid_node(ref_grid),
-					 nodes,&quality ), "qual");
-	    }
-	  RSS( ref_histogram_add( ref_histogram, quality ), "add");
-	}
-    }
-
-  RSS( ref_histogram_gather( ref_histogram ), "gather");
-  if ( ref_mpi_master ) RSS( ref_histogram_print( ref_histogram,
-						  "quality"), "print");
-
-  RSS( ref_histogram_free(ref_histogram), "free gram" );
-  return REF_SUCCESS;
-}
-
 REF_STATUS ref_histogram_gather( REF_HISTOGRAM ref_histogram )
 {
   REF_INT *bins;
@@ -272,6 +188,90 @@ REF_STATUS ref_histogram_export( REF_HISTOGRAM ref_histogram,
 
   fclose(f);
 
+  return REF_SUCCESS;
+}
+
+REF_STATUS ref_histogram_ratio( REF_GRID ref_grid )
+{
+  REF_HISTOGRAM ref_histogram;
+  REF_EDGE ref_edge;
+  REF_INT edge, part;
+  REF_DBL ratio;
+  REF_BOOL active;
+
+  RSS( ref_histogram_create(&ref_histogram),"create");
+  RSS( ref_edge_create( &ref_edge, ref_grid ), "make edges" );
+
+  for (edge=0;edge< ref_edge_n(ref_edge);edge++)
+    {
+      RSS( ref_edge_part( ref_edge, edge, &part ), "edge part");
+      RSS( ref_node_edge_twod( ref_grid_node(ref_grid), 
+			       ref_edge_e2n(ref_edge, 0, edge),
+			       ref_edge_e2n(ref_edge, 1, edge), 
+			       &active ), "twod edge");
+      active = ( active || !ref_grid_twod(ref_grid) );
+      if ( part == ref_mpi_id && active )
+	{
+	  RSS( ref_node_ratio( ref_grid_node(ref_grid), 
+			       ref_edge_e2n(ref_edge, 0, edge),
+			       ref_edge_e2n(ref_edge, 1, edge), 
+			       &ratio ), "rat");
+	  RSS( ref_histogram_add( ref_histogram, ratio ), "add");
+	}
+    }
+
+  RSS( ref_histogram_gather( ref_histogram ), "gather");
+  if ( ref_mpi_master ) RSS( ref_histogram_print( ref_histogram,
+						  "edge ratio"), "print");
+  if ( 1 == ref_mpi_n ) RSS( ref_histogram_export( ref_histogram,
+						   "edge-ratio"), "print");
+
+  RSS( ref_edge_free(ref_edge), "free edge" );
+  RSS( ref_histogram_free(ref_histogram), "free gram" );
+  return REF_SUCCESS;
+}
+
+REF_STATUS ref_histogram_quality( REF_GRID ref_grid )
+{
+  REF_HISTOGRAM ref_histogram;
+  REF_CELL ref_cell;
+  REF_INT cell;
+  REF_INT nodes[REF_CELL_MAX_SIZE_PER];
+  REF_DBL quality;
+
+  RSS( ref_histogram_create(&ref_histogram),"create");
+
+  if ( ref_grid_twod(ref_grid) )
+    {
+      ref_cell = ref_grid_tri(ref_grid);
+    }
+  else
+    {
+      ref_cell = ref_grid_tet(ref_grid);
+    }
+  each_ref_cell_valid_cell_with_nodes( ref_cell, cell, nodes)
+    {
+      if ( ref_node_part(ref_grid_node(ref_grid),nodes[0]) == ref_mpi_id )
+	{
+	  if ( ref_grid_twod(ref_grid) )
+	    {
+	      RSS( ref_node_tri_quality( ref_grid_node(ref_grid),
+					 nodes,&quality ), "qual");
+	    }
+	  else
+	    {
+	      RSS( ref_node_tet_quality( ref_grid_node(ref_grid),
+					 nodes,&quality ), "qual");
+	    }
+	  RSS( ref_histogram_add( ref_histogram, quality ), "add");
+	}
+    }
+
+  RSS( ref_histogram_gather( ref_histogram ), "gather");
+  if ( ref_mpi_master ) RSS( ref_histogram_print( ref_histogram,
+						  "quality"), "print");
+
+  RSS( ref_histogram_free(ref_histogram), "free gram" );
   return REF_SUCCESS;
 }
 
--- two/ref_histogram.h
+++ two/ref_histogram.h
@@ -39,14 +39,14 @@ REF_STATUS ref_histogram_free( REF_HISTOGRAM ref_histogram );
 REF_STATUS ref_histogram_add( REF_HISTOGRAM ref_histogram, 
 			      REF_DBL observation );
 
-REF_STATUS ref_histogram_ratio( REF_GRID ref_grid );
-REF_STATUS ref_histogram_quality( REF_GRID ref_grid );
-
 REF_STATUS ref_histogram_gather( REF_HISTOGRAM ref_histogram );
 REF_STATUS ref_histogram_print( REF_HISTOGRAM ref_histogram, 
 				char *description );
 REF_STATUS ref_histogram_export( REF_HISTOGRAM ref_histogram, 
 				 char *description );
+
+REF_STATUS ref_histogram_ratio( REF_GRID ref_grid );
+REF_STATUS ref_histogram_quality( REF_GRID ref_grid );
 
 REF_STATUS ref_histogram_tec_ratio( REF_GRID ref_grid );
 
