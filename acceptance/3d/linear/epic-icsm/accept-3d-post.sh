36265c063eb24fa1b7945333c8fffe58f22a61bb
Mike Park <Mike.Park@NASA.gov>
2015-04-18 11:09:46 -0400

    setup

---
+++ acceptance/3d/linear/epic-icsm/accept-3d-post.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+bin=${HOME}/refine/strict/two
+
+proj=epic_L2_qShape_InsertCollapse
+
+${bin}/ref_translate ${proj}.b8.ugrid accept-3d.tec
+
+${bin}/ref_acceptance ${proj}.b8.ugrid ${proj}.metric 0.001
+
+${bin}/ref_metric_test ${proj}.b8.ugrid ${proj}.metric > ${proj}.status
+cp ref_metric_test_surf.tec ${proj}_metric_surf.tec
+cp ref_metric_test_s00_n1_p0_ellipse.tec ${proj}_metric_ellipse.tec
+gnuplot ref_histogram_edge-ratio.gnuplot
+epstopdf ref_histogram_edge-ratio.eps
+cp ref_histogram_edge-ratio.pdf ${proj}_edge-ratio.pdf
