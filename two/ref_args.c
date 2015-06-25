a9cdfd527ef40e02ea5312a3785d514d8a1b0b6f
Mike Park <Mike.Park@NASA.gov>
2015-06-25 10:01:25 -0500

    missing

--- two/ref_args.c
+++ two/ref_args.c
@@ -1,6 +1,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "ref_args.h"
 
@@ -13,3 +14,22 @@ REF_STATUS ref_args_inspect( REF_INT n, char **args )
 
   return REF_SUCCESS;
 }
+
+REF_STATUS ref_args_find( REF_INT n, char **args, char *target, REF_INT *pos )
+{
+  REF_INT i;
+
+  *pos = REF_EMPTY;
+  
+  for (i=0;i<n;i++)
+    {
+      if ( 0 == strcmp( target, args[i] ) )
+	{
+	  *pos = i;
+	  return REF_SUCCESS;
+	}
+    }
+  
+  return REF_NOT_FOUND;
+}
+  
--- two/ref_args_test.c
+++ two/ref_args_test.c
@@ -18,9 +18,14 @@ int main( int argc, char *argv[] )
     char *a1 = "-1";
     char *a2 = "-2";
     char *as[3];
+    REF_INT pos;
     as[0]=a0; as[1]=a1;as[2]=a2;
 
-    RSS(ref_args_inspect( n, as ), "echo");
+    RSS(ref_args_find( n, as, "-1", &pos ), "echo");
+    REIS( 1, pos, "location" );
+    
+    REIS(REF_NOT_FOUND, ref_args_find( n, as, "-h", &pos ), "not found");
+    REIS( REF_EMPTY, pos, "location" );
   }
 
 
