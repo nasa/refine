e25b9c4ca0cf9fc93b35727bcad5fcabf0ca0464
Mike Park <Mike.Park@NASA.gov>
2015-06-25 09:47:58 -0500

    echo tester fixture

--- two/ref_args_test.c
+++ two/ref_args_test.c
@@ -14,5 +14,20 @@ int main( int argc, char *argv[] )
 	printf("%d : '%s'\n",i,argv[i]);
     }
 
+  {
+    REF_INT n = 3;
+    char *a0 = "program";
+    char *a1 = "-1";
+    char *a2 = "-2";
+    char *as[3];
+    REF_INT i;
+    as[0]=a0; as[1]=a1;as[2]=a2;
+    
+    for (i=0;i<n;i++)
+      printf("%d : '%s'\n",i,as[i]);
+  }
+
+
+  
   return 0;
 }
