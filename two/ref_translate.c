988d25f861e5f1fa42f672a91915a9f694fe3b44
Mike Park <Mike.Park@NASA.Gov>
2016-02-08 13:36:47 -0500

    refactored trans for extension

--- two/ref_translate.c
+++ two/ref_translate.c
@@ -9,30 +9,40 @@
 #include "ref_dict.h"
 #include "ref_list.h"
 
+static int print_usage(char *name)
+{
+  printf("usage:\n");
+  printf("  %s input_grid.extension output_grid.extension\n",name);
+  printf("     [--shift dx dy dz]\n");
+  printf("     [--scale s]\n");
+  return 0;
+}
+
 int main( int argc, char *argv[] )
 {
   REF_GRID ref_grid;
-  REF_BOOL valid_inputs, shift;
   REF_INT node;
   REF_DBL dx, dy, dz;
   REF_NODE ref_node;
+  char *endptr;
+  REF_INT pos;
 
-  valid_inputs = ( (3 == argc) || (7 == argc) );
-  shift = (7 == argc);
-
-  if ( !valid_inputs )
-    {
-      printf("usage: %s input_grid.extension output_grid.extension [--shift dx dy dz]\n",argv[0]);
-      return 0;
-    }
+  if ( 3 > argc )
+    return(print_usage(argv[0]));
 
   RSS( ref_import_by_extension( &ref_grid, argv[1] ), "import" );
-  if ( shift )
-    {
+
+  pos = 3;
+  while( pos < argc ) {
+    if( strcmp(argv[pos],"--shift") == 0 ) {
+      printf("%d: --shift\n",pos);
+      if ( pos+4 > argc )
+	return(print_usage(argv[0]));
       ref_node = ref_grid_node(ref_grid);
-      dx = atof( argv[4] );
-      dy = atof( argv[5] );
-      dz = atof( argv[6] );
+      pos++; dx = strtod( argv[pos], &endptr);RAS(argv[pos]!=endptr,"parse dx");
+      pos++; dy = strtod( argv[pos], &endptr);RAS(argv[pos]!=endptr,"parse dy");
+      pos++; dz = strtod( argv[pos], &endptr);RAS(argv[pos]!=endptr,"parse dz");
+      printf("%f %f %f\n",dx,dy,dy);
       each_ref_node_valid_node( ref_node, node )
 	{
 	  ref_node_xyz(ref_node,0,node) += dx;
@@ -40,6 +50,8 @@ int main( int argc, char *argv[] )
 	  ref_node_xyz(ref_node,2,node) += dz;
 	}
     }
+    pos++; 
+  }
   RSS( ref_export_by_extension( ref_grid, argv[2] ), "export" );
 
   RSS(ref_grid_free(ref_grid),"free");
