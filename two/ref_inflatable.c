852162ad52138a89e24dae2b1c13effe604764bc
Mike Park <Mike.Park@NASA.Gov>
2016-03-10 14:38:55 -0500

    timings

--- two/ref_inflatable.c
+++ two/ref_inflatable.c
@@ -64,7 +64,9 @@ int main( int argc, char *argv[] )
       return 1;
     }
 
+  ref_mpi_stopwatch_start();
   RSS( ref_import_by_extension( &ref_grid, argv[1] ), "read grid" );
+  ref_mpi_stopwatch_stop("import");
 
   nlayers = atoi( argv[2] );
   if ( nlayers < 0 )
@@ -202,6 +204,8 @@ int main( int argc, char *argv[] )
 	     ref_node_n(ref_grid_node(ref_grid)));
     }
 
+  ref_mpi_stopwatch_stop("inflate");
+
   if ( REF_EMPTY != scale_pos )
     {
       printf("scale grid after inflation by %f\n",scale);
@@ -215,7 +219,9 @@ int main( int argc, char *argv[] )
     }
 
   RSS( ref_export_tec_surf( ref_grid, "inflated_boundary.tec" ), "tec" );
+  ref_mpi_stopwatch_stop("surf");
   RSS( ref_export_by_extension( ref_grid, "inflated.b8.ugrid" ), "b8" );
+  ref_mpi_stopwatch_stop("export");
 
   RSS(ref_dict_free( faceids ), "free" );
   RSS(ref_grid_free(ref_grid),"free");
