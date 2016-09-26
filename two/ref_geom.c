609a623b8749a0bf2c8bb686d485fd922d0cac25
Mike Park <Mike.Park@NASA.gov>
2016-09-26 13:09:27 -0400

    save

--- two/ref_geom.c
+++ two/ref_geom.c
@@ -23,8 +23,29 @@ REF_STATUS ref_geom_egads_fixture( char *filename )
 {
 #ifdef HAVE_EGADS
   ego context;
-  printf("EGAGS project %s\n",filename);
+  int stype;
+  double data[6];
+  ego body;
+  ego model = NULL;
+  
   REIS( EGADS_SUCCESS, EG_open(&context), "EG open");
+  stype = 1;
+  data[0]=0.0; /* corner */
+  data[1]=0.0;
+  data[2]=0.0;
+  data[3]=1.0; /* length of sides */
+  data[4]=1.0;
+  data[5]=1.0;
+  REIS( EGADS_SUCCESS,
+	EG_makeSolidBody(context, stype, data, &body), "EG box");
+  REIS( EGADS_SUCCESS,
+	EG_makeTopology(context, NULL, MODEL, 0,
+			NULL, 1, &body,
+			NULL, &model), "topo");
+  REIS( EGADS_SUCCESS,
+	EG_saveModel(model, filename), "save");
+  printf("wrote EGAGS project %s\n",filename);
+
 #else
   printf("No EGAGS linked %s\n",filename);
 #endif
