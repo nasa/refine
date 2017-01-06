c6e1a655c29c27c93e909ca57082b1433ddb0b47
Mike Park <Mike.Park@NASA.gov>
2017-01-06 15:19:55 -0500

    reapaird

--- two/ref_geom.c
+++ two/ref_geom.c
@@ -645,7 +645,7 @@ REF_STATUS ref_geom_constrain( REF_GRID ref_grid, REF_INT node )
    [x_tt,y_tt,z_tt]
    [x_u,y_u,z_u] [x_v,y_v,z_v] face
    [x_uu,y_uu,z_uu] [x_uv,y_uv,z_uv] [x_vv,y_vv,z_vv]
- *\
+*/
 REF_STATUS ref_geom_eval( REF_GEOM ref_geom, REF_INT geom,
 			  REF_DBL *xyz, REF_DBL *dxyz_dtuv )
 {
