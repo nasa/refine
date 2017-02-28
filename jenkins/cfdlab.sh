98a90c0e90af89744ac60b6b7d9114bb695fd9ae
Mike Park <Mike.Park@NASA.Gov>
2017-02-28 14:50:26 -0500

    module echo crusft

--- jenkins/cfdlab.sh
+++ jenkins/cfdlab.sh
@@ -4,13 +4,14 @@ set -x # echo commands
 set -e # exit on first error
 set -u # Treat unset variables as error
 
+set +x # echo commands
 . /usr/local/pkgs/modules/init/bash
-
 module use --append /ump/fldmd/home/wtjones1/Modules/modulefiles
 module load OpenCASCADE/6.6.0
 module load ESP/svn
-
 module add gcc_4.9.1_64
+module list
+set -x # echo commands
 
 module_path="/ump/fldmd/home/casb-shared/fun3d/fun3d_users/modules"
 parmetis_path="${module_path}/ParMETIS/4.0.3-1.10.2_intel_2013-2013.4.183_64"
