57c15492abcfd44ccb50f22325cf963a87535fc4
Mike Park <Mike.Park@NASA.gov>
2017-03-07 12:13:26 -0500

    cleanup driver with pdf

--- acceptance/2d/linear/two/accept-2d-two.sh
+++ acceptance/2d/linear/two/accept-2d-two.sh
@@ -1,23 +1,36 @@
 #!/usr/bin/env bash
 
-bin=${HOME}/refine/strict/two
+set -x # echo commands
+set -e # exit on first error
+set -u # Treat unset variables as error
 
-${bin}/ref_acceptance 2 ref_adapt_test.b8.ugrid
+if [ $# -gt 0 ] ; then
+    one=$1/src
+    two=$1/two
+else
+    one=${HOME}/refine/strict/src
+    two=${HOME}/refine/strict/two
+fi
+
+${two}/ref_acceptance 2 ref_adapt_test.b8.ugrid
 
 function adapt_cycle {
     proj=$1
 
     cp ref_adapt_test.b8.ugrid ${proj}.b8.ugrid
 
-    ${bin}/ref_translate ${proj}.b8.ugrid ${proj}.html
-    ${bin}/ref_translate ${proj}.b8.ugrid ${proj}.tec
+    ${two}/ref_translate ${proj}.b8.ugrid ${proj}.html
+    ${two}/ref_translate ${proj}.b8.ugrid ${proj}.tec
 
-    ${bin}/ref_acceptance ${proj}.b8.ugrid ${proj}.metric 0.0001
-    ${bin}/ref_adapt_test ${proj}.b8.ugrid ${proj}.metric | tee ${proj}.out || exit 1
+    ${two}/ref_acceptance ${proj}.b8.ugrid ${proj}.metric 0.0001
+    ${two}/ref_adapt_test ${proj}.b8.ugrid ${proj}.metric | tee ${proj}.out || exit 1
 
-    ${bin}/ref_metric_test ${proj}.b8.ugrid ${proj}.metric
+    ${two}/ref_metric_test ${proj}.b8.ugrid ${proj}.metric
     cp ref_metric_test_surf.tec ${proj}_metric_surf.tec
     cp ref_metric_test_s00_n1_p0_ellipse.tec ${proj}_metric_ellipse.tec
+    gnuplot ref_histogram_edge-ratio.gnuplot
+    epstopdf ref_histogram_edge-ratio.eps
+    cp ref_histogram_edge-ratio.pdf ${proj}_edge-ratio.pdf
 }
 
 adapt_cycle accept-2d-two-00
