70cc43a1132fd749589c37386c62aad1150c9953
Mike Park <Mike.Park@NASA.gov>
2017-03-31 07:30:42 -0400

    egads and install in check too

--- check.sh
+++ check.sh
@@ -6,22 +6,29 @@ set -u
 script_name_with_path=`which "${0}"`
 script_path=`dirname "${script_name_with_path}"`
 
+if test -d ${script_path}/egads
+then
+  ( cd ${script_path}/egads && \
+     make -j check install ) || exit
+fi
+
 if test -d ${script_path}/zoltan
 then
   ( cd ${script_path}/zoltan && \
-     make -j check ) || exit
+     make -j check install ) || exit
 fi
 
 if test -d ${script_path}/parmetis
 then
   ( cd ${script_path}/parmetis && \
-     make -j check ) || exit
+     make -j check install ) || exit
 fi
 
 if test -d ${script_path}/strict
 then
   ( cd ${script_path}/strict && \
     make -j check TESTS_ENVIRONMENT='valgrind --quiet  --error-exitcode=1 --leak-check=full' ) || exit
-
+  ( cd ${script_path}/strict && \
+    make -j install ) || exit
 fi
 
