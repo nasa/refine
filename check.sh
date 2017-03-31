05429bae848cecfefef6c4d6b9e44a54809ebf6f
Mike Park <Mike.Park@NASA.gov>
2017-03-31 07:32:23 -0400

    check-then-install

--- check.sh
+++ check.sh
@@ -9,19 +9,25 @@ script_path=`dirname "${script_name_with_path}"`
 if test -d ${script_path}/egads
 then
   ( cd ${script_path}/egads && \
-     make -j check install ) || exit
+     make -j check ) || exit
+  ( cd ${script_path}/egads && \
+     make -j install ) || exit
 fi
 
 if test -d ${script_path}/zoltan
 then
   ( cd ${script_path}/zoltan && \
-     make -j check install ) || exit
+     make -j check ) || exit
+  ( cd ${script_path}/zoltan && \
+     make -j install ) || exit
 fi
 
 if test -d ${script_path}/parmetis
 then
   ( cd ${script_path}/parmetis && \
-     make -j check install ) || exit
+     make -j check ) || exit
+  ( cd ${script_path}/parmetis && \
+     make -j install ) || exit
 fi
 
 if test -d ${script_path}/strict
