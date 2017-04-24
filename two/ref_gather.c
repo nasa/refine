59c4b2fae894849ea156c564bf94ff6dd01f6e04
Mike Park <Mike.Park@NASA.gov>
2017-04-24 18:49:09 -0400

    add time to gather movie

--- two/ref_gather.c
+++ two/ref_gather.c
@@ -80,6 +80,7 @@ REF_STATUS ref_gather_tec_movie_record_button( REF_BOOL on_or_off )
 }
 
 static FILE *movie_file = NULL;
+static REF_DBL movie_time = 0.0;
 
 REF_STATUS ref_gather_tec_movie_frame( REF_GRID ref_grid, char *zone_title )
 {
@@ -109,15 +110,16 @@ REF_STATUS ref_gather_tec_movie_frame( REF_GRID ref_grid, char *zone_title )
       if ( NULL == zone_title )
 	{
 	  fprintf(movie_file,
-		  "zone t=\"part\", nodes=%d, elements=%d, datapacking=%s, zonetype=%s\n",
-		  nnode, ntri, "point", "fetriangle" );
+		  "zone t=\"part\", nodes=%d, elements=%d, datapacking=%s, zonetype=%s, solutiontime=%f\n",
+		  nnode, ntri, "point", "fetriangle", movie_time );
 	}
       else
 	{
 	  fprintf(movie_file,
-		  "zone t=\"%s\", nodes=%d, elements=%d, datapacking=%s, zonetype=%s\n",
-		  zone_title, nnode, ntri, "point", "fetriangle" );
+		  "zone t=\"%s\", nodes=%d, elements=%d, datapacking=%s, zonetype=%s, solutiontime=%f\n",
+		  zone_title, nnode, ntri, "point", "fetriangle", movie_time );
 	}
+      movie_time += 1.0;
     }
 
   RSS( ref_gather_node_tec_part( ref_node, movie_file ), "nodes");
