6eb8736dd539bbe4f509508a87c2f1b5181a0ab0
Mike Park <Mike.Park@NASA.gov>
2017-04-28 14:54:09 -0700

    setup

--- acceptance/cube/polar-1/two/accept-cube-linear.sh
+++ acceptance/cube/polar-1/two/accept-cube-linear.sh
@@ -12,7 +12,7 @@ else
     two=${HOME}/refine/egads/two
 fi
 
-field=linear
+field=polar-1
 
 tecplot=-t
 
@@ -50,30 +50,30 @@ adapt_cycle_sant cycle02 cycle03 2
 adapt_cycle_sant cycle03 cycle04 2
 adapt_cycle_sant cycle04 cycle05 2
 adapt_cycle_sant cycle05 cycle06 2
-adapt_cycle cycle06 cycle07 2
-adapt_cycle cycle07 cycle08 2
-adapt_cycle cycle08 cycle09 2
-adapt_cycle cycle09 cycle10 2
-adapt_cycle cycle10 cycle11 2
-adapt_cycle cycle11 cycle12 2
-adapt_cycle cycle12 cycle13 2
-adapt_cycle cycle13 cycle14 2
-adapt_cycle cycle14 cycle15 2
-adapt_cycle cycle15 cycle16 2
-adapt_cycle cycle16 cycle17 2
-adapt_cycle cycle17 cycle18 2
-adapt_cycle cycle18 cycle19 2
-adapt_cycle cycle19 cycle20 2
-adapt_cycle cycle20 cycle21 2
-adapt_cycle cycle21 cycle22 2
-adapt_cycle cycle22 cycle23 2
-adapt_cycle cycle23 cycle24 2
-adapt_cycle cycle24 cycle25 2
-adapt_cycle cycle25 cycle26 2
-adapt_cycle cycle26 cycle27 2
-adapt_cycle cycle27 cycle28 2
-adapt_cycle cycle28 cycle29 2
-adapt_cycle cycle29 cycle30 2
+adapt_cycle cycle06 cycle07 10
+adapt_cycle cycle07 cycle08 10
+adapt_cycle cycle08 cycle09 10
+adapt_cycle cycle09 cycle10 10
+adapt_cycle cycle10 cycle11 10
+adapt_cycle cycle11 cycle12 10
+adapt_cycle cycle12 cycle13 10
+adapt_cycle cycle13 cycle14 10
+adapt_cycle cycle14 cycle15 10
+adapt_cycle cycle15 cycle16 10
+adapt_cycle cycle16 cycle17 10
+adapt_cycle cycle17 cycle18 10
+adapt_cycle cycle18 cycle19 10
+adapt_cycle cycle19 cycle20 10
+adapt_cycle cycle20 cycle21 10
+adapt_cycle cycle21 cycle22 10
+adapt_cycle cycle22 cycle23 10
+adapt_cycle cycle23 cycle24 10
+adapt_cycle cycle24 cycle25 10
+adapt_cycle cycle25 cycle26 10
+adapt_cycle cycle26 cycle27 10
+adapt_cycle cycle27 cycle28 10
+adapt_cycle cycle28 cycle29 10
+adapt_cycle cycle29 cycle30 10
 
 cat cycle30.status
 ../../../check.rb cycle30.status 0.08 1.8
