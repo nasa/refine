f295183a5ffbc49a078b910db155091f72a46e97
Mike Park <Mike.Park@NASA.gov>
2017-07-06 13:58:35 -0400

    no longer export histogram by default

--- acceptance/3d/linear/epic-ics/accept-3d-post.sh
+++ acceptance/3d/linear/epic-ics/accept-3d-post.sh
@@ -11,6 +11,3 @@ ${bin}/ref_acceptance ${proj}.b8.ugrid ${proj}.metric 0.001
 ${bin}/ref_metric_test ${proj}.b8.ugrid ${proj}.metric > ${proj}.status
 cp ref_metric_test_surf.tec ${proj}_metric_surf.tec
 cp ref_metric_test_s00_n1_p0_ellipse.tec ${proj}_metric_ellipse.tec
-gnuplot ref_histogram_edge-ratio.gnuplot
-epstopdf ref_histogram_edge-ratio.eps
-cp ref_histogram_edge-ratio.pdf ${proj}_edge-ratio.pdf
--- acceptance/3d/linear/two/accept-3d-two.sh
+++ acceptance/3d/linear/two/accept-3d-two.sh
@@ -31,9 +31,6 @@ function adapt_cycle {
     ${two}/ref_metric_test ${proj}.b8.ugrid ${proj}.metric > ${proj}.status
     cp ref_metric_test_surf.tec ${proj}_metric_surf.tec
     cp ref_metric_test_s00_n1_p0_ellipse.tec ${proj}_metric_ellipse.tec
-    gnuplot ref_histogram_edge-ratio.gnuplot
-    epstopdf ref_histogram_edge-ratio.eps
-    cp ref_histogram_edge-ratio.pdf ${proj}_edge-ratio.pdf
 }
 
 adapt_cycle accept-3d-two-00
