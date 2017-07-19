7f878fd6a821dc12f733f6e8221a738662a3597b
Mike Park <Mike.Park@NASA.Gov>
2017-07-19 13:55:54 -0400

    accept short edge, with metric const

--- acceptance/cube-cylinder/uniform/two/accept-cube-cylinder-uniform-two.sh
+++ acceptance/cube-cylinder/uniform/two/accept-cube-cylinder-uniform-two.sh
@@ -25,7 +25,7 @@ ${two}/ref_acceptance ref_driver2.b8.ugrid ref_driver2.metric 0.1
 ${two}/ref_metric_test ref_driver2.b8.ugrid ref_driver2.metric > accept-cube-cylinder-uniform-two-02.status
 
 cat accept-cube-cylinder-uniform-two-02.status
-../../../check.rb accept-cube-cylinder-uniform-two-02.status 0.4 1.6
+../../../check.rb accept-cube-cylinder-uniform-two-02.status 0.15 1.5
 
 
 
