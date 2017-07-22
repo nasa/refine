fa1e4ce45557961c5fb6200819b3871485dce9a7
Mike Park <Mike.Park@NASA.gov>
2017-07-22 10:40:30 -0400

    remove unused ref_grid_empty_cell_clone infrastructure

--- two/ref_grid.c
+++ two/ref_grid.c
@@ -73,34 +73,6 @@ REF_STATUS ref_grid_deep_copy( REF_GRID *ref_grid_ptr, REF_GRID original )
   return REF_SUCCESS;
 }
 
-REF_STATUS ref_grid_empty_cell_clone( REF_GRID *ref_grid_ptr, REF_GRID parent )
-{
-  REF_GRID ref_grid;
-
-  ref_malloc( *ref_grid_ptr, 1, REF_GRID_STRUCT );
-
-  ref_grid = *ref_grid_ptr;
-
-  ref_grid_node(ref_grid) = ref_grid_node(parent);
-
-  RSS( ref_cell_create( &ref_grid_tet(ref_grid), 4, REF_FALSE ), "tet create" );
-  RSS( ref_cell_create( &ref_grid_pyr(ref_grid), 5, REF_FALSE ), "pyr create" );
-  RSS( ref_cell_create( &ref_grid_pri(ref_grid), 6, REF_FALSE ), "pri create" );
-  RSS( ref_cell_create( &ref_grid_hex(ref_grid), 8, REF_FALSE ), "hex create" );
-
-  ref_grid_cell(ref_grid,4) = NULL;
-
-  RSS( ref_cell_create( &ref_grid_edg(ref_grid), 2, REF_TRUE ), "edg create" );
-  RSS( ref_cell_create( &ref_grid_tri(ref_grid), 3, REF_TRUE ), "tri create" );
-  RSS( ref_cell_create( &ref_grid_qua(ref_grid), 4, REF_TRUE ), "qua create" );
-
-  RSS( ref_geom_create( &ref_grid_geom(ref_grid) ), "geom create" );
-
-  ref_grid_twod(ref_grid) = ref_grid_twod(parent);
-
-  return REF_SUCCESS;
-}
-
 REF_STATUS ref_grid_free( REF_GRID ref_grid )
 {
   if ( NULL == (void *)ref_grid ) return REF_NULL;
@@ -117,25 +89,6 @@ REF_STATUS ref_grid_free( REF_GRID ref_grid )
   RSS( ref_cell_free( ref_grid_tet(ref_grid) ), "tet free");
 
   RSS( ref_node_free( ref_grid_node(ref_grid) ), "node free");
-
-  ref_free( ref_grid );
-  return REF_SUCCESS;
-}
-
-REF_STATUS ref_grid_free_cell_clone( REF_GRID ref_grid )
-{
-  if ( NULL == (void *)ref_grid ) return REF_NULL;
-
-  RSS( ref_geom_free( ref_grid_geom(ref_grid) ), "geom free");
-  
-  RSS( ref_cell_free( ref_grid_qua(ref_grid) ), "qua free");
-  RSS( ref_cell_free( ref_grid_tri(ref_grid) ), "tri free");
-  RSS( ref_cell_free( ref_grid_edg(ref_grid) ), "edg free");
-
-  RSS( ref_cell_free( ref_grid_hex(ref_grid) ), "hex free");
-  RSS( ref_cell_free( ref_grid_pri(ref_grid) ), "pri free");
-  RSS( ref_cell_free( ref_grid_pyr(ref_grid) ), "pyr free");
-  RSS( ref_cell_free( ref_grid_tet(ref_grid) ), "tet free");
 
   ref_free( ref_grid );
   return REF_SUCCESS;
--- two/ref_grid.h
+++ two/ref_grid.h
@@ -34,9 +34,6 @@ REF_STATUS ref_grid_free( REF_GRID ref_grid );
 
 REF_STATUS ref_grid_deep_copy( REF_GRID *ref_grid, REF_GRID original );
 
-REF_STATUS ref_grid_empty_cell_clone( REF_GRID *ref_grid, REF_GRID parent );
-REF_STATUS ref_grid_free_cell_clone( REF_GRID ref_grid );
-
 #define ref_grid_node(ref_grid) ((ref_grid)->node)
 #define ref_grid_cell(ref_grid,group) ((ref_grid)->cell[(group)])
 
