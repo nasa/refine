648f8f9d14118e511f7e57e30e5d8f8d313132f4
Mike Park <Mike.Park@NASA.Gov>
2017-08-14 09:02:15 -0400

    working

--- acceptance/cube-cylinder/uniform/two/accept-cube-cylinder-uniform-two-mpi.sh
+++ acceptance/cube-cylinder/uniform/two/accept-cube-cylinder-uniform-two-mpi.sh
@@ -20,14 +20,14 @@ nproc=16
 ${two}/ref_acceptance ega.meshb ega.metric 0.1
 mpiexec -np ${nproc} ${two}/ref_driver -i ega.meshb -g ega.egads -m ega.metric -o ref_driver1 -t
 ${two}/ref_acceptance ref_driver1.meshb ref_driver1.metric 0.1
-${two}/ref_metric_test ref_driver1.meshb ref_driver1.metric > accept-cube-cylinder-uniform-two-01.status
+${two}/ref_metric_test ref_driver1.meshb ref_driver1.metric > accept-cube-cylinder-uniform-two-mpi-01.status
 
 mpiexec -np ${nproc} ${two}/ref_driver -i ref_driver1.meshb -g ega.egads -m ref_driver1.metric -o ref_driver2 -t
 ${two}/ref_acceptance ref_driver2.meshb ref_driver2.metric 0.1
-${two}/ref_metric_test ref_driver2.meshb ref_driver2.metric > accept-cube-cylinder-uniform-two-02.status
+${two}/ref_metric_test ref_driver2.meshb ref_driver2.metric > accept-cube-cylinder-uniform-two-mpi-02.status
 
-cat accept-cube-cylinder-uniform-two-02.status
-../../../check.rb accept-cube-cylinder-uniform-two-02.status 0.20 1.6
+cat accept-cube-cylinder-uniform-two-mpi-02.status
+../../../check.rb accept-cube-cylinder-uniform-two-mpi-02.status 0.15 1.8
 
 
 
