ed35ac49fbf149250b7d01468e180672c5d5c3f1
Mike Park <Mike.Park@NASA.gov>
2017-08-30 11:41:49 -0400

    update year

--- misc/headerfier.rb
+++ misc/headerfier.rb
@@ -11,7 +11,7 @@ ARGV.each do |file|
   File.open(file,"w") do |f|
     f.puts <<EOF
 
-/* Copyright 2007 United States Government as represented by the
+/* Copyright 2014 United States Government as represented by the
  * Administrator of the National Aeronautics and Space
  * Administration. No copyright is claimed in the United States under
  * Title 17, U.S. Code.  All Other Rights Reserved.
