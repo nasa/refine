2ea133ccce11a1a2edf7026225caedc149c57ae2
Mike Park <Mike.Park@NASA.gov>
2017-09-14 09:04:38 -0400

    made broken test

---
+++ acceptance/cone-cone/uniform/two/accept-cone-cone-uniform-two.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -x # echo commands
+set -e # exit on first error
+set -u # Treat unset variables as error
+
+if [ $# -gt 0 ] ; then
+    one=$1/one
+    two=$1/src
+else
+    one=${HOME}/refine/egads/one
+    two=${HOME}/refine/egads/src
+fi
+
+geomfile=cone-cone.egads
+
+# ${two}/ref_geom_test ${geomfile} cone-cone.meshb
+
+${two}/ref_acceptance cone-cone.meshb cone-cone.metric 0.1
+${two}/ref_driver -i cone-cone.meshb -g ${geomfile} -m cone-cone.metric -o ref_driver1
+${two}/ref_acceptance ref_driver1.meshb ref_driver1.metric 0.1
+${two}/ref_metric_test ref_driver1.meshb ref_driver1.metric > accept-cone-cone-uniform-two-01.status
+
+${two}/ref_driver -i ref_driver1.meshb -g ${geomfile} -m ref_driver1.metric -o ref_driver2
+${two}/ref_acceptance ref_driver2.meshb ref_driver2.metric 0.1
+${two}/ref_metric_test ref_driver2.meshb ref_driver2.metric > accept-cone-cone-uniform-two-02.status
+
+cat accept-cone-cone-uniform-two-02.status
+../../../check.rb accept-cone-cone-uniform-two-02.status 0.05 2.1
+
+
+
