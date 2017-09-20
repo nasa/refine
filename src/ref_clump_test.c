dcd0dcb838d5bc6f7f706f4859b1108b403940c9
Mike Park <Mike.Park@NASA.gov>
2017-09-20 09:46:46 +0200

    see qual too

--- src/ref_clump_test.c
+++ src/ref_clump_test.c
@@ -23,12 +23,12 @@
 #include    "ref_math.h"
 #include   "ref_smooth.h"
 #include    "ref_twod.h"
-#include   "ref_gather.h"
 #include   "ref_metric.h"
 
 #include   "ref_import.h"
 #include   "ref_part.h"
 #include   "ref_export.h"
+#include   "ref_gather.h"
 
 int main( int argc, char *argv[] )
 {
@@ -47,6 +47,9 @@ int main( int argc, char *argv[] )
 	}
       RSS( ref_clump_stuck_edges( ref_grid, 0.5 ), "stuck edge" );
       RSS( ref_export_tec_surf( ref_grid, "clump_surf.tec" ), "surf" );
+      RSS( ref_gather_tec_movie_record_button( ref_grid_gather(ref_grid),
+					       REF_TRUE ), "gather on" );      
+      RSS( ref_gather_tec_movie_frame( ref_grid, "clump" ), "gather" );      
       RSS( ref_grid_free(ref_grid),"free");
     }
 
