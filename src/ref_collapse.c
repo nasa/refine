adf07586ca531c832118585f0ded922f627f2ba0
Mike Park <Mike.Park@NASA.Gov>
2017-09-28 15:38:06 -0400

    comment on addition

--- src/ref_collapse.c
+++ src/ref_collapse.c
@@ -473,6 +473,7 @@ REF_STATUS ref_collapse_edge_quality( REF_GRID ref_grid,
     RSS( ref_node_tet_quality( ref_node,nodes,&quality ), "qual");
     if ( quality < ref_grid_adapt(ref_grid,collapse_quality_absolute) )
       return REF_SUCCESS;
+    /* reject more than one tri on a tet? */
   }
 
   RSS( ref_geom_supported( ref_geom, node0, &has_support), "support");
