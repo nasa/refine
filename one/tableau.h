5e95497e494d1b99ae2c103e914f7d9fe752ef8e
Mike Park <Mike.Park@NASA.Gov>
2017-10-03 09:15:55 -0400

    headerfy one *.h code

--- one/tableau.h
+++ one/tableau.h
@@ -1,15 +1,20 @@
 
-/* Tableau, an implementation of the tableau method for solving linear
- *   programming problems.
+/* Copyright 2014 United States Government as represented by the
+ * Administrator of the National Aeronautics and Space
+ * Administration. No copyright is claimed in the United States under
+ * Title 17, U.S. Code.  All Other Rights Reserved.
  *
- * Michael A. Park
- * Computational AeroSciences Branch
- * NASA Langley Research Center
- * Phone: (757) 864-6604
- * Email: Mike.Park@NASA.Gov
+ * The refine platform is licensed under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
  */
-
-
 
 #ifndef TABLEAU_H
 #define TABLEAU_H
