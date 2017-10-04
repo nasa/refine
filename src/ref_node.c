4792fca566573a2368e31a930d0c6e5df04e0268
Mike Park <Mike.Park@NASA.Gov>
2017-10-04 11:04:16 -0400

    return worst direction when div by zero

--- src/ref_node.c
+++ src/ref_node.c
@@ -2002,11 +2002,19 @@ REF_STATUS ref_node_bary4( REF_NODE ref_node, REF_INT *nodes,
     }
   else
     {
-      REF_INT i;
-      printf("%s: %d: %s: div zero total %.18e vol %.18e %.18e %.18e %.18e\n",
+      REF_INT i, smallest;
+      REF_DBL volume;
+      smallest = 0;
+      for(i=1;i<4;i++) 
+	if ( bary[i] < bary[smallest])
+	  smallest = i;
+      for(i=0;i<4;i++)
+	bary[i] = 0.0;
+      bary[smallest] = -1.0;
+      RSS( ref_node_tet_vol( ref_node, nodes, &volume ), "bary vol chk" );
+      printf("%s: %d: %s: div zero tot %.18e %.18e vol %.18e %.18e %.18e %.18e\n",
 	     __FILE__,__LINE__,__func__,
-	     total, bary[0], bary[1], bary[2], bary[3] );
-      for(i=0;i<4;i++) bary[i] = 0.0;
+	     total, volume, bary[0], bary[1], bary[2], bary[3] );
       return REF_DIV_ZERO;
     }
   
