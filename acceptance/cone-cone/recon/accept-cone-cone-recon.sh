c46bfe5c8cb7cb6b322e79dac87755ca41d8f862
Mike Park <Mike.Park@NASA.Gov>
2017-11-13 09:39:16 -0500

    driver

---
+++ acceptance/cone-cone/recon/accept-cone-cone-recon.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -x # echo commands
+set -e # exit on first error
+set -u # Treat unset variables as error
+
+if [ $# -gt 0 ] ; then
+    one=$1/one
+    two=$1/src
+else
+    one=${HOME}/refine/egads/one
+    two=${HOME}/refine/egads/src
+fi
+
+geomfile=cone-cone.egads
+
+${two}/ref_geom_test --recon cone-cone-no-geom.meshb ${geomfile} > accept-cone-cone-recon.status
+
+../../recon.rb accept-cone-cone-recon.status 
