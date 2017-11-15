19088a34a568591a426660e1b82ae332567bca32
Mike Park <Mike.Park@NASA.Gov>
2017-11-15 11:30:28 -0500

    taylor sweeps for faster exec

--- acceptance/3d/linear/two/accept-3d-two-para.sh
+++ acceptance/3d/linear/two/accept-3d-two-para.sh
@@ -16,6 +16,7 @@ ${two}/ref_acceptance 1 ref_adapt_test.b8.ugrid
 
 function adapt_cycle {
     proj=$1
+    sweeps=$2
 
     cp ref_adapt_test.b8.ugrid ${proj}.b8.ugrid
 
@@ -25,16 +26,16 @@ function adapt_cycle {
     ${two}/ref_acceptance ${proj}.b8.ugrid ${proj}.metric 0.01
 
     rm ref_adapt_test.b8.ugrid
-    mpiexec -np 2 ${two}/ref_driver -i ${proj}.b8.ugrid -m ${proj}.metric -t -o ref_adapt_test
+    mpiexec -np 2 ${two}/ref_driver -i ${proj}.b8.ugrid -m ${proj}.metric -t -s ${sweeps} -o ref_adapt_test
     
     ${two}/ref_metric_test ${proj}.b8.ugrid ${proj}.metric > ${proj}.status
     cp ref_metric_test_s00_n1_p0_ellipse.tec ${proj}_metric_ellipse.tec
 }
 
-adapt_cycle accept-3d-two-para-00
-adapt_cycle accept-3d-two-para-01
-adapt_cycle accept-3d-two-para-02
-adapt_cycle accept-3d-two-para-03
+adapt_cycle accept-3d-two-para-00 10
+adapt_cycle accept-3d-two-para-01 20
+adapt_cycle accept-3d-two-para-02 10
+adapt_cycle accept-3d-two-para-03 10
 
 cat accept-3d-two-para-03.status
 ../../../check.rb accept-3d-two-para-03.status 0.15 2.0
