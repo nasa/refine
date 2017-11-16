038636e74cb2fbbef1c7c32ad24753ff1286fe85
Mike Park <Mike.Park@NASA.gov>
2017-11-16 16:31:40 -0500

    migrate_test missed one

--- src/ref_migrate_test.c
+++ src/ref_migrate_test.c
@@ -138,7 +138,7 @@ int main( int argc, char *argv[] )
       REF_GRID import_grid;
 
       if ( ref_mpi_once(ref_mpi) ) 
-	printf("%d procs, read %s\n",ref_mpi_n,argv[1]);
+	printf("%d procs, read %s\n",ref_mpi_rank(ref_mpi),argv[1]);
 
       ref_mpi_stopwatch_start(ref_mpi);
       RSS(ref_part_by_extension( &import_grid, ref_mpi, argv[1] ), "import" );
