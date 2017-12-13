6e49228835f10107f3254d094319d41ae49c8ee3
Mike Park <Mike.Park@NASA.Gov>
2017-12-13 14:05:56 -0500

    sketched out agents

--- src/ref_agents.c
+++ src/ref_agents.c
@@ -24,9 +24,12 @@
 
 #include "ref_malloc.h"
 
-/* REF_EMPTY is terminatior, next avalable is shifted by 2*/
-#define next2index(next) (-(next)-2)
-#define index2next(index) (-2-(index))
+#define ref_agent_node(ref_agents,id) ((ref_agents)->agent[(id)].node)
+#define ref_agent_previous(ref_agents,id) ((ref_agents)->agent[(id)].previous)
+#define ref_agent_next(ref_agents,id) ((ref_agents)->agent[(id)].next)
+#define ref_agent_valid(ref_agents,id) (ref_agent_node(ref_agents,id) >= 0)
+
+#define ref_agent_max(ref_agents) ((ref_agents)->max)
 
 REF_STATUS ref_agents_create( REF_AGENTS *ref_agents_ptr, REF_MPI ref_mpi )
 {
@@ -44,9 +47,14 @@ REF_STATUS ref_agents_create( REF_AGENTS *ref_agents_ptr, REF_MPI ref_mpi )
   ref_malloc( ref_agents->agent, ref_agents->max, REF_AGENT_STRUCT );
 
   for (id=0;id<ref_agents->max;id++)
-    ref_agents->agent[id].node = index2next(id+1);
-  ref_agents->agent[(ref_agents->max)-1].node = REF_EMPTY;
-  ref_agents->blank = index2next(0);
+    {
+      ref_agent_node(ref_agents,id) = REF_EMPTY;
+      ref_agent_previous(ref_agents,id) = REF_EMPTY;
+      ref_agent_next(ref_agents,id) = id+1;
+    }
+  ref_agent_next(ref_agents,ref_agent_max(ref_agents)-1) = REF_EMPTY;
+  ref_agents->blank = 0;
+  ref_agents->last = REF_EMPTY;
 
   return REF_SUCCESS;
 }
@@ -57,6 +65,105 @@ REF_STATUS ref_agents_free( REF_AGENTS ref_agents )
     return REF_NULL;
   ref_free( ref_agents->agent );
   ref_free( ref_agents );
+  return REF_SUCCESS;
+}
+
+REF_STATUS ref_agents_push( REF_AGENTS ref_agents, REF_INT node )
+{
+  REF_INT id;
+
+  if ( REF_EMPTY == ref_agents->blank )
+    {
+      REF_INT orig, chunk, extra;
+      orig = ref_agents->max;
+      chunk = MAX(5000,(REF_INT)(1.5*(REF_DBL)orig));
+      ref_agents->max = orig + chunk;
+      ref_realloc( ref_agents->agent, ref_agents->max, REF_AGENT_STRUCT);
+      
+      for (extra=orig;extra < ref_agent_max(ref_agents); extra++ ) 
+	{
+	  ref_agent_node(ref_agents,extra) = REF_EMPTY;
+	  ref_agent_previous(ref_agents,extra) = REF_EMPTY;
+	  ref_agent_next(ref_agents,extra) = extra+1;
+	}
+      ref_agent_next(ref_agents,ref_agent_max(ref_agents)-1) = REF_EMPTY;
+      ref_agents->blank = orig;
+    }
+
+  id = ref_agents->blank;
+
+  if ( REF_EMPTY != ref_agents->last )
+    ref_agent_next(ref_agents,ref_agents->last) = id;
+
+  ref_agent_previous(ref_agents,id) = ref_agents->last;
+  ref_agent_next(ref_agents,id) = REF_EMPTY;
+  ref_agent_node(ref_agents,id) = node;
+
+  ref_agents->blank = ref_agent_next(ref_agents,id);
+  ref_agents->last = id;
+
+  ref_agents_n(ref_agents)++;
+
+  return REF_SUCCESS;
+}
+
+REF_STATUS ref_agents_remove( REF_AGENTS ref_agents, REF_INT id )
+{
+
+  if ( id < 0 || ref_agent_max(ref_agents) <= id )
+    return REF_INVALID;
+  if ( !ref_agent_valid(ref_agents,id) )
+    return REF_INVALID;
+
+  if ( ref_agents->last == id )
+    ref_agents->last = ref_agent_previous(ref_agents,id);
+
+  if ( REF_EMPTY != ref_agent_previous(ref_agents,id) )
+    ref_agent_next(ref_agents,ref_agent_previous(ref_agents,id)) = 
+      ref_agent_next(ref_agents,id);
+
+  if ( REF_EMPTY != ref_agent_next(ref_agents,id) )
+    ref_agent_previous(ref_agents,ref_agent_next(ref_agents,id)) =
+      ref_agent_previous(ref_agents,id);
+
+  ref_agent_node(ref_agents,id) = REF_EMPTY;
+  ref_agent_previous(ref_agents,id) = REF_EMPTY;
+  ref_agent_next(ref_agents,id) = ref_agents->blank;
+  ref_agents->blank = id;
+
+  ref_agents_n(ref_agents)--;
+
+  return REF_SUCCESS;
+}
+
+REF_STATUS ref_agents_pop( REF_AGENTS ref_agents, REF_INT *node )
+{
+  REF_INT id;
+
+  if ( REF_EMPTY == ref_agents->last )
+    return REF_NOT_FOUND;
+
+  id = ref_agents->last;
+
+  *node = ref_agent_node(ref_agents,id);
+
+  RSS( ref_agents_remove( ref_agents, id ), "rm" );
+
+  return REF_SUCCESS;
+}
+
+REF_STATUS ref_agents_delete( REF_AGENTS ref_agents, REF_INT node )
+{
+  REF_INT id;
+
+  if ( node < 0 )
+    return REF_INVALID;
+
+  for ( id = 0 ; id < ref_agent_max(ref_agents) ; id++ )
+    {
+      if ( node == ref_agent_node(ref_agents,id) )
+	RSS( ref_agents_remove( ref_agents, id ), "rm" );
+    }
   return REF_SUCCESS;
 }
 
--- src/ref_agents.h
+++ src/ref_agents.h
@@ -34,17 +34,26 @@ END_C_DECLORATION
 BEGIN_C_DECLORATION
 struct REF_AGENT_STRUCT {
   REF_INT node;
-  REF_INT guess;
+  REF_INT previous;
+  REF_INT next;
 };
 struct REF_AGENTS_STRUCT {
   REF_INT n, max;
   REF_INT blank;
+  REF_INT last;
   REF_AGENT_STRUCT *agent;
   REF_MPI ref_mpi;
 };
 
+#define ref_agents_n(ref_agents) ( (ref_agents)->n )
+
 REF_STATUS ref_agents_create( REF_AGENTS *ref_agents, REF_MPI ref_mpi );
 
 REF_STATUS ref_agents_free( REF_AGENTS ref_agents );
+
+REF_STATUS ref_agents_push( REF_AGENTS ref_agents, REF_INT node );
+REF_STATUS ref_agents_remove( REF_AGENTS ref_agents, REF_INT id );
+REF_STATUS ref_agents_pop( REF_AGENTS ref_agents, REF_INT *node );
+REF_STATUS ref_agents_delete( REF_AGENTS ref_agents, REF_INT node );
 
 #endif /* REF_AGENTS_H */
