5eca9697ed8846aa99982593887334b571f4e748
Mike Park <Mike.Park@NASA.Gov>
2017-12-18 11:50:51 -0500

    slightly shorter

--- acceptance/3d/polar-1/two/accept-3d-two.sh
+++ acceptance/3d/polar-1/two/accept-3d-two.sh
@@ -41,7 +41,7 @@ adapt_cycle accept-3d-two-04
 adapt_cycle accept-3d-two-05
 
 cat accept-3d-two-05.status
-../../../check.rb accept-3d-two-05.status 0.05 3.0
+../../../check.rb accept-3d-two-05.status 0.035 3.0
 
 
 
