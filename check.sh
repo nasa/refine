ff6b44919bd9a48ca64c2af176776909f62c6e26
Mike Park <Mike.Park@NASA.gov>
2017-12-30 10:56:40 -0500

    optional arg to limit number of jobs

--- check.sh
+++ check.sh
@@ -3,38 +3,40 @@
 set -x
 set -u
 
+jobs=${1:-}
+
 script_name_with_path=`which "${0}"`
 script_path=`dirname "${script_name_with_path}"`
 
 if test -d ${script_path}/egads
 then
   ( cd ${script_path}/egads && \
-     make -j check ) || exit
+     make -j ${jobs} check ) || exit
   ( cd ${script_path}/egads && \
-     make -j install ) || exit
+     make -j ${jobs} install ) || exit
 fi
 
 if test -d ${script_path}/zoltan
 then
   ( cd ${script_path}/zoltan && \
-     make -j check ) || exit
+     make -j ${jobs} check ) || exit
   ( cd ${script_path}/zoltan && \
-     make -j install ) || exit
+     make -j ${jobs} install ) || exit
 fi
 
 if test -d ${script_path}/parmetis
 then
   ( cd ${script_path}/parmetis && \
-     make -j check ) || exit
+     make -j ${jobs} check ) || exit
   ( cd ${script_path}/parmetis && \
-     make -j install ) || exit
+     make -j ${jobs} install ) || exit
 fi
 
 if test -d ${script_path}/strict
 then
   ( cd ${script_path}/strict && \
-    make -j check TESTS_ENVIRONMENT='valgrind --quiet  --error-exitcode=1 --leak-check=full' ) || exit
+    make -j ${jobs} check TESTS_ENVIRONMENT='valgrind --quiet  --error-exitcode=1 --leak-check=full' ) || exit
   ( cd ${script_path}/strict && \
-    make -j install ) || exit
+    make -j ${jobs} install ) || exit
 fi
 
