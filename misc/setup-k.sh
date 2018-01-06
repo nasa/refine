75a924a6ba17ee58c786267beb782bfc063502ba
Mike Park <Mike.Park@NASA.Gov>
2018-01-06 11:15:50 -0500

    hand rolled mpich gcc zoltan build

--- misc/setup-k.sh
+++ misc/setup-k.sh
@@ -6,7 +6,7 @@ set -x
 
 module_path="/u/shared/fun3d/fun3d_users/modules"
 zoltan_path="${module_path}/Zoltan/3.82-openmpi-1.10.2-intel_2017.2.174"
-
+zoltan_path="/u/mpark/local/pkgs/Zoltan_v3.82/gcc_6.2.0-mpich_3.2"
 egads_path="/u/mpark/local/pkgs/ESP111/EngSketchPad"
 
 mkdir -p egads
@@ -27,6 +27,6 @@ mkdir -p zoltan
     --with-EGADS=${egads_path} \
     CC=mpicc \
     FC=mpif90 \
-    CFLAGS='-DHAVE_MPI -g -O2 -traceback -Wall -w3 -wd1418,2259,2547,981,11074,11076,1572,47 -ftrapuv' \
+    CFLAGS='-DHAVE_MPI -g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -Wuninitialized' \
     ) \
     || exit
