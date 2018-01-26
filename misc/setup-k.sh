717940bc314cee29ce21a49e92b4d7c57f5253ca
Mike Park <Mike.Park@NASA.Gov>
2018-01-26 16:58:56 -0500

    use system egads

--- misc/setup-k.sh
+++ misc/setup-k.sh
@@ -7,7 +7,7 @@ set -x
 module_path="/u/shared/fun3d/fun3d_users/modules"
 zoltan_path="${module_path}/Zoltan/3.82-openmpi-1.10.2-intel_2017.2.174"
 zoltan_path="/u/mpark/local/pkgs/Zoltan_v3.82/gcc_6.2.0-mpich_3.2"
-egads_path="/u/mpark/local/pkgs/ESP111/EngSketchPad"
+egads_path="${module_path}/ESP/112/EngSketchPad"
 
 mkdir -p egads
 ( cd egads && \
