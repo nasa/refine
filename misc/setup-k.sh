c1eba137cecb0e79b2c7fc87f4d9c2ad6b34438e
Mike Park <Mike.Park@NASA.Gov>
2018-01-30 11:20:30 -0500

    k needs strcit too

--- misc/setup-k.sh
+++ misc/setup-k.sh
@@ -9,6 +9,15 @@ zoltan_path="${module_path}/Zoltan/3.82-openmpi-1.10.2-intel_2017.2.174"
 zoltan_path="/u/mpark/local/pkgs/Zoltan_v3.82/gcc_6.2.0-mpich_3.2"
 egads_path="${module_path}/ESP/112/EngSketchPad"
 
+mkdir -p strict
+( cd strict && \
+    ../configure \
+    --prefix=`pwd` \
+    CFLAGS='-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -Wuninitialized' \
+    FC=gfortran \
+    ) \
+    || exit
+
 mkdir -p egads
 ( cd egads && \
     ../configure \
