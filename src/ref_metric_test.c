d6d17907c5862f0bcc257cb052fea1a88cc862e6
Mike Park <Mike.Park@NASA.gov>
2018-02-08 10:06:05 -0500

    remove only (debug) call to ref_export_tec_metric_box

--- src/ref_metric_test.c
+++ src/ref_metric_test.c
@@ -313,32 +313,6 @@ int main( int argc, char *argv[] )
       return 0;
     }
 
-  if ( argc == 9 )
-    {
-      REF_GRID ref_grid;
-      REF_DBL bounding_box[6];
-      REF_INT i;
-
-      RSS( ref_import_by_extension( &ref_grid, ref_mpi,
-				    argv[1] ), "examine header" );
-
-      RSS( ref_part_metric( ref_grid_node(ref_grid), argv[2] ), "get metric");
-
-      for ( i=0; i<6; i++)
-	{
-	  bounding_box[i] = atof(argv[3+i]);
-	  printf("bounding box[%d] = %f\n",i,bounding_box[i]);
-	}
-
-      RSS( ref_export_tec_metric_box( ref_grid, "ref_metric_box", 
-				      bounding_box ), "bbox");
-
-      RSS( ref_grid_free( ref_grid ), "free");
-      RSS( ref_mpi_free(ref_mpi), "free");
-      RSS( ref_mpi_stop( ), "stop" );
-      return 0;
-    }
-
   {  /* imply metric right tet */
     REF_DBL tol = -1.0;
     REF_GRID ref_grid;
