530f0b6231bd2289d60d0b0942f9d2f1e7d8bc21
Mike Park <Mike.Park@NASA.gov>
2018-02-15 16:57:00 -0500

    valgrind off until have suppressions hooked up

--- jenkins/cfdlab.sh
+++ jenkins/cfdlab.sh
@@ -198,13 +198,13 @@ cd ${source_dir}/acceptance/cube-cylinder/uniform/two
 ./accept-cube-cylinder-uniform-two.sh ${egads_dir} > $LOG 2>&1
 trap - EXIT
 
-date
+# date
 
-LOG=${root_dir}/log.accept-cube-cylinder-uniform-valgrind
-trap "cat $LOG" EXIT
-cd ${source_dir}/acceptance/cube-cylinder/uniform/two
-./accept-cube-cylinder-uniform-two-valgrind.sh ${egads_dir} > $LOG 2>&1
-trap - EXIT
+# LOG=${root_dir}/log.accept-cube-cylinder-uniform-valgrind
+# trap "cat $LOG" EXIT
+# cd ${source_dir}/acceptance/cube-cylinder/uniform/two
+# ./accept-cube-cylinder-uniform-two-valgrind.sh ${egads_dir} > $LOG 2>&1
+# trap - EXIT
 
 date
 
