5ec7489aff28b4acd15d1eca0e8e8d6b28f3f531
Mike Park <Mike.Park@NASA.gov>
2018-02-19 07:41:11 -0500

    elast obj

---
+++ src/ref_elast.c
@@ -0,0 +1,51 @@
+
+/* Copyright 2014 United States Government as represented by the
+ * Administrator of the National Aeronautics and Space
+ * Administration. No copyright is claimed in the United States under
+ * Title 17, U.S. Code.  All Other Rights Reserved.
+ *
+ * The refine platform is licensed under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "ref_elast.h"
+#include "ref_grid.h"
+#include "ref_node.h"
+#include "ref_edge.h"
+
+#include "ref_malloc.h"
+
+REF_STATUS ref_elast_create( REF_ELAST *ref_elast_ptr, REF_GRID ref_grid )
+{
+  REF_ELAST ref_elast;
+  
+  ref_malloc( *ref_elast_ptr, 1, REF_ELAST_STRUCT );
+  
+  ref_elast = *ref_elast_ptr;
+
+  RSS( ref_comprow_create( &(ref_elast->ref_comprow), ref_grid ), "comprow" );
+
+return REF_SUCCESS;
+}
+
+REF_STATUS ref_elast_free( REF_ELAST ref_elast )
+{
+  if ( NULL == (void *)ref_elast ) return REF_NULL;
+  
+  ref_comprow_free( ref_elast->ref_comprow );
+  
+  ref_free( ref_elast );
+  
+  return REF_SUCCESS;
+}
---
+++ src/ref_elast.h
@@ -0,0 +1,45 @@
+
+/* Copyright 2014 United States Government as represented by the
+ * Administrator of the National Aeronautics and Space
+ * Administration. No copyright is claimed in the United States under
+ * Title 17, U.S. Code.  All Other Rights Reserved.
+ *
+ * The refine platform is licensed under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#ifndef REF_ELAST_H
+#define REF_ELAST_H
+
+#include "ref_defs.h"
+
+BEGIN_C_DECLORATION
+typedef struct REF_ELAST_STRUCT REF_ELAST_STRUCT;
+typedef REF_ELAST_STRUCT * REF_ELAST;
+END_C_DECLORATION
+
+#include "ref_grid.h"
+#include "ref_comprow.h"
+
+BEGIN_C_DECLORATION
+
+struct REF_ELAST_STRUCT {
+  REF_COMPROW ref_comprow;
+};
+
+#define ref_elast_comprow(ref_elast) ((ref_elast)->ref_comprow)
+
+REF_STATUS ref_elast_create( REF_ELAST *ref_elast, REF_GRID ref_grid );
+REF_STATUS ref_elast_free( REF_ELAST ref_elast );
+
+END_C_DECLORATION
+
+#endif /* REF_ELAST_H */
