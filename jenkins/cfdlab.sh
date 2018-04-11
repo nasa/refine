fa5a5915a48f84e43a25868dd637a57bc77087bc
Mike Park <Mike.Park@NASA.Gov>
2018-04-11 11:03:16 -0400

    add migrate to build

--- jenkins/cfdlab.sh
+++ jenkins/cfdlab.sh
@@ -132,6 +132,7 @@ mpiexec -np 2 ./ref_metric_test >> $LOG 2>&1
 mpiexec -np 2 ./ref_mpi_test >> $LOG 2>&1
 mpiexec -np 2 ./ref_node_test >> $LOG 2>&1
 mpiexec -np 2 ./ref_part_test >> $LOG 2>&1
+mpiexec -np 2 ./ref_migrate_test >> $LOG 2>&1
 mpiexec -np 2 ./ref_cavity_test >> $LOG 2>&1
 mpiexec -np 2 ./ref_elast_test >> $LOG 2>&1
 mpiexec -np 8 ./ref_agents_test >> $LOG 2>&1
@@ -142,6 +143,7 @@ mpiexec -np 8 ./ref_metric_test >> $LOG 2>&1
 mpiexec -np 8 ./ref_mpi_test >> $LOG 2>&1
 mpiexec -np 8 ./ref_node_test >> $LOG 2>&1
 mpiexec -np 8 ./ref_part_test >> $LOG 2>&1
+mpiexec -np 8 ./ref_migrate_test >> $LOG 2>&1
 mpiexec -np 8 ./ref_cavity_test >> $LOG 2>&1
 mpiexec -np 8 ./ref_elast_test >> $LOG 2>&1
 trap - EXIT
