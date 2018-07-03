e03241a0ebb3ab202efcd1e2286058f248233315
Mike Park <Mike.Park@NASA.Gov>
2018-07-03 15:45:28 -0400

    add ref_geom_eval_edge_face_uv to end of add between

--- src/ref_geom.c
+++ src/ref_geom.c
@@ -1466,7 +1466,7 @@ REF_STATUS ref_geom_add_between(REF_GRID ref_grid, REF_INT node0, REF_INT node1,
   REF_INT sense, cell, nodes[REF_CELL_MAX_SIZE_PER];
   REF_STATUS status;
   REF_INT i, ncell, cells[2];
-  REF_INT geom, geom0, geom1;
+  REF_INT face_geom, geom0, geom1;
   REF_BOOL support0, support1;
 
   RSS(ref_geom_supported(ref_geom, node0, &support0), "node0 supported");
@@ -1543,16 +1543,44 @@ REF_STATUS ref_geom_add_between(REF_GRID ref_grid, REF_INT node0, REF_INT node1,
         param[1] = 0.5 * (param0[1] + param1[1]);
       }
     }
+
     RSS(ref_geom_add(ref_geom, new_node, type, id, param), "new geom");
+    RSS(ref_geom_find(ref_geom, new_node, type, id, &face_geom),
+        "new face geom");
 
     RSS(ref_geom_find(ref_geom, node0, type, id, &geom0), "face geom");
     RSS(ref_geom_find(ref_geom, node1, type, id, &geom1), "face geom");
     if (0 != ref_geom_jump(ref_geom, geom0) &&
         0 != ref_geom_jump(ref_geom, geom1) &&
         ref_geom_jump(ref_geom, geom0) == ref_geom_jump(ref_geom, geom1)) {
-      RSS(ref_geom_find(ref_geom, new_node, type, id, &geom), "new face geom");
-      ref_geom_jump(ref_geom, geom) = ref_geom_jump(ref_geom, geom0);
+      ref_geom_jump(ref_geom, face_geom) = ref_geom_jump(ref_geom, geom0);
     }
+
+#ifdef HAVE_EGADS
+    if (ref_geom_model_loaded(ref_geom) && has_edge_support) {
+      ego *edges, *faces;
+      ego edge, face;
+      REF_INT faceid, edgeid;
+      REF_DBL t;
+      edgeid = ref_geom_id(ref_geom, edge_geom);
+      edges = (ego *)(ref_geom->edges);
+      edge = edges[edgeid - 1];
+      faces = (ego *)(ref_geom->faces);
+      faceid = ref_geom_id(ref_geom, face_geom);
+      face = faces[faceid - 1];
+      t = ref_geom_param(ref_geom, 0, edge_geom);
+      sense = 0;
+      if (0 != ref_geom_jump(ref_geom, face_geom)) sense = 1;
+      REIB(EGADS_SUCCESS, EG_getEdgeUV(face, edge, sense, t, param), 
+           "edge uv", {
+             printf("edge %d face %d sense %d\n", edgeid, faceid, sense);
+             ref_geom_tattle(ref_geom, ref_geom_node(ref_geom, edge_geom));
+           });
+      ref_geom_param(ref_geom, 0, face_geom) = param[0];
+      ref_geom_param(ref_geom, 1, face_geom) = param[1];
+    }
+#endif
+
   }
 
   /* if there is an edge between, set the face uv based on edge t */
