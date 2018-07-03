903b380687d6d80869912c39774bdd71f11cde83
Mike Park <Mike.Park@NASA.gov>
2018-07-03 06:32:13 -0400

    test

---
+++ acceptance/revolve-pencil/gen/revolve-pencil-gen.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -x # echo commands
+set -e # exit on first error
+set -u # Treat unset variables as error
+
+if [ $# -gt 0 ] ; then
+    one=$1/one
+    two=$1/src
+else
+    one=${HOME}/refine/egads/one
+    two=${HOME}/refine/egads/src
+fi
+
+${two}/ref_geom_test --tetgen \
+    pencil.egads \
+    pencil.meshb \
+    5 5 90 | tee revolve-pencil-gen.out
+
