86ea1aed0ab1f8e6fcf779fba115bc2f9ac36e49
Mike Park <Mike.Park@NASA.Gov>
2018-07-10 16:07:01 -0400

    gen

---
+++ acceptance/cube-wire/tess/accept-tess.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -x # echo commands
+set -e # exit on first error
+set -u # Treat unset variables as error
+
+if [ $# -gt 0 ] ; then
+    one=$1/one
+    two=$1/src
+else
+    one=${HOME}/refine/egads/one
+    two=${HOME}/refine/egads/src
+fi
+
+${two}/ref_geom_test --tess ../geom/cube-wire.egads cube-wire01.meshb 1.0 0.1 30
+
+
+
+
