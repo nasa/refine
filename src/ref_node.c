adb673c89a4bad0b6fe623cf617fdf32541c8831
Mike Park <Mike.Park@NASA.gov>
2018-07-10 06:07:32 -0400

    format

--- src/ref_node.c
+++ src/ref_node.c
@@ -477,9 +477,11 @@ REF_STATUS ref_node_initialize_n_global_from_locals(REF_NODE ref_node) {
   REF_MPI ref_mpi = ref_node_mpi(ref_node);
   REF_INT node, local_nnode, global_nnode;
   local_nnode = 0;
-  each_ref_node_valid_node(ref_node, node) if (ref_mpi_rank(ref_mpi) ==
-                                               ref_node_part(ref_node, node))
+  each_ref_node_valid_node(ref_node, node) {
+    if (ref_node_owned(ref_node, node)) {
       local_nnode++;
+    }
+  }
   RSS(ref_mpi_sum(ref_mpi, &local_nnode, &global_nnode, 1, REF_INT_TYPE),
       "sum");
   RSS(ref_mpi_bcast(ref_mpi, &global_nnode, 1, REF_INT_TYPE), "bcast");
