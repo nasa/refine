aecca21295f66d9a560a0c397079907b754460a4
Mike Park <Mike.Park@NASA.Gov>
2018-07-17 10:10:43 -0400

    k enviroment

--- misc/setup-k.sh
+++ misc/setup-k.sh
@@ -5,9 +5,9 @@ set -x
 ./bootstrap
 
 module_path="/u/shared/fun3d/fun3d_users/modules"
-zoltan_path="${module_path}/Zoltan/3.82-openmpi-1.10.2-intel_2017.2.174"
-parmetis_path="${module_path}/ParMETIS/4.0.3-openmpi-1.10.7-intel_2017.2.174"
-egads_path="${module_path}/ESP/113/EngSketchPad"
+zoltan_path="${module_path}/Zoltan/3.82-mpt-2.16-intel_2017.2.174"
+parmetis_path="${module_path}/ParMETIS/4.0.3-mpt-2.16-intel_2017.2.174"
+egads_path="${module_path}/ESP/113.1/EngSketchPad"
 
 mkdir -p strict
 ( cd strict && \
@@ -35,9 +35,10 @@ mkdir -p zoltan
     --with-zoltan=${zoltan_path} \
     --with-EGADS=${egads_path} \
     --enable-lite \
-    CC=mpicc \
-    FC=mpif90 \
-    CFLAGS='-DHAVE_MPI -g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -Wuninitialized -Wl,--disable-new-dtags' \
+    CC=icc \
+    FC=ifort \
+    CFLAGS='-DHAVE_MPI -g -O2 -traceback -Wall -w3 -wd1418,2259,2547,981,11074,11076,1572,49,1419 -ftrapuv' \
+    LIBS=-lmpi \
     ) \
     || exit
 
@@ -48,8 +49,9 @@ mkdir -p parmetis
     --with-parmetis=${parmetis_path} \
     --with-EGADS=${egads_path} \
     --enable-lite \
-    CC=mpicc \
-    FC=mpif90 \
-    CFLAGS='-DHAVE_MPI -g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -Wuninitialized -Wl,--disable-new-dtags' \
+    CC=icc \
+    FC=ifort \
+    CFLAGS='-DHAVE_MPI -g -O2 -traceback -Wall -w3 -wd1418,2259,2547,981,11074,11076,1572,49,1419 -ftrapuv' \
+    LIBS=-lmpi \
     ) \
     || exit
