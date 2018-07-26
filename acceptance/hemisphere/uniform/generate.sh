32e23ff4236b54904d0b36e035b6db589dd36480
Mike Park <Mike.Park@NASA.gov>
2018-07-26 10:34:46 -0400

    gen script

--- acceptance/hemisphere/uniform/generate.sh
+++ acceptance/hemisphere/uniform/generate.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/env bash
+
+set -x # echo commands
+set -e # exit on first error
+set -u # Treat unset variables as error
 
 serveCSM -batch hemisphere.csm
 
-ref_geom_test --tetgen hemisphere.egads hemicurve.meshb .25  1  15.00
+ref_geom_test --tetgen hemisphere.egads hemicurve.meshb .5  1  15.00
