6516d0c18d24636e0943bdc5c2f82f6f336a6b24
Mike Park <Mike.Park@NASA.gov>
2018-08-02 16:41:56 -0400

    allow metis path to be configured seperately from parmetis

--- misc/setup-mac.sh
+++ misc/setup-mac.sh
@@ -7,7 +7,8 @@ set -x
 zoltan_path="/Users/mpark/spack/opt/spack/darwin-elcapitan-x86_64/gcc-7.2.0/zoltan-3.83-xxyq2a3qxwtcrdt3otmiflhgqaz7gial"
 egads_path="/Users/mpark/esp/EngSketchPad"
 gccflags='-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -Wuninitialized'
-parmetis_path="/Users/mpark/local/pkgs/parmetis-fake"
+parmetis_path="/Users/mpark/spack/opt/spack/darwin-elcapitan-x86_64/gcc-7.2.0/parmetis-4.0.3-lxz27qnvcp7blhtqltxvngthbnabs24h"
+metis_path="/Users/mpark/spack/opt/spack/darwin-elcapitan-x86_64/gcc-7.2.0/metis-5.1.0-x5ptqjut6c5wzhzs74pcje3lpwfl5lct"
 
 # set DYLD_LIBRARY_PATH for libzoltan.dylib
 
@@ -49,6 +50,7 @@ mkdir -p parmetis
 ( cd parmetis && \
     ../configure \
     --prefix=`pwd` \
+    --with-metis=${metis_path} \
     --with-parmetis=${parmetis_path} \
     --with-EGADS=${egads_path} \
     --enable-lite \
