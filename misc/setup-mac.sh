75921416778205d152052a8122b9f0ac06790dc3
Mike Park <Mike.Park@NASA.gov>
2018-08-07 15:34:50 -0400

    paths

--- misc/setup-mac.sh
+++ misc/setup-mac.sh
@@ -4,11 +4,12 @@ set -x
 
 ./bootstrap
 
-zoltan_path="/Users/mpark/spack/opt/spack/darwin-elcapitan-x86_64/gcc-7.2.0/zoltan-3.83-xxyq2a3qxwtcrdt3otmiflhgqaz7gial"
+zoltan_path="/Users/mpark/spack/opt/spack/darwin-highsierra-x86_64/gcc-8.2.0/zoltan-3.83-6gpgaz4xpcw73nqwhup5wkq45m5xehb2"
 egads_path="/Users/mpark/local/pkgs/EngSketchPad"
+opencascade_path="/Users/mpark/local/pkgs/OpenCASCADE-6.8.1"
 gccflags='-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -Wuninitialized'
-parmetis_path="/Users/mpark/spack/opt/spack/darwin-elcapitan-x86_64/gcc-7.2.0/parmetis-4.0.3-lxz27qnvcp7blhtqltxvngthbnabs24h"
-metis_path="/Users/mpark/spack/opt/spack/darwin-elcapitan-x86_64/gcc-7.2.0/metis-5.1.0-x5ptqjut6c5wzhzs74pcje3lpwfl5lct"
+parmetis_path="/Users/mpark/spack/opt/spack/darwin-highsierra-x86_64/gcc-8.2.0/parmetis-4.0.3-xasrm372i7dgoygsou7f2m7pqppyubmd"
+metis_path="/Users/mpark/spack/opt/spack/darwin-highsierra-x86_64/gcc-8.2.0/metis-5.1.0-nugla3v2bdfmvp5ajz67ypecdymm6wmv"
 
 # set DYLD_LIBRARY_PATH for libzoltan.dylib
 
@@ -27,22 +28,10 @@ mkdir -p egads
     ../configure \
     --prefix=`pwd` \
     --with-EGADS=${egads_path} \
+    --with-OpenCASCADE=${opencascade_path} \
     CFLAGS="${gccflags}" \
     CC=gcc-8 \
     FC=gfortran-8 \
-    ) \
-    || exit
-
-mkdir -p zoltan
-( cd zoltan && \
-    ../configure \
-    --prefix=`pwd` \
-    --with-zoltan=${zoltan_path} \
-    --with-EGADS=${egads_path} \
-    --enable-lite \
-    CC=mpicc \
-    FC=mpif90 \
-    CFLAGS="-DHAVE_MPI ${gccflags} -Wno-long-long" \
     ) \
     || exit
 
@@ -59,3 +48,17 @@ mkdir -p parmetis
     CFLAGS="-DHAVE_MPI ${gccflags} -Wno-long-long" \
     ) \
     || exit
+
+mkdir -p zoltan
+( cd zoltan && \
+    ../configure \
+    --prefix=`pwd` \
+    --with-zoltan=${zoltan_path} \
+    --with-EGADS=${egads_path} \
+    --enable-lite \
+    CC=mpicc \
+    FC=mpif90 \
+    CFLAGS="-DHAVE_MPI ${gccflags} -Wno-long-long" \
+    ) \
+    || exit
+
