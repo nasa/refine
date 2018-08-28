dbf3e4a5a65de1928ae8fb9103761d3419ca580f
Mike Park <Mike.Park@NASA.gov>
2018-08-28 09:38:50 -0400

    add ref_metric_limit_h_at_complexity

--- src/ref_metric.c
+++ src/ref_metric.c
@@ -1327,6 +1327,29 @@ REF_STATUS ref_metric_limit_h(REF_DBL *metric, REF_GRID ref_grid, REF_DBL hmin,
   return REF_SUCCESS;
 }
 
+REF_STATUS ref_metric_limit_h_at_complexity(REF_DBL *metric, REF_GRID ref_grid,
+                                            REF_DBL hmin, REF_DBL hmax,
+                                            REF_DBL target_complexity) {
+  REF_NODE ref_node = ref_grid_node(ref_grid);
+  REF_INT i, node, relaxations;
+  REF_DBL current_complexity;
+
+  /* global scaling and h limits */
+  for (relaxations = 0; relaxations < 10; relaxations++) {
+    RSS(ref_metric_complexity(metric, ref_grid, &current_complexity), "cmp");
+    if (!ref_math_divisible(target_complexity, current_complexity)) {
+      return REF_DIV_ZERO;
+    }
+    each_ref_node_valid_node(ref_node, node) for (i = 0; i < 6; i++) {
+      metric[i + 6 * node] *=
+          pow(target_complexity / current_complexity, 2.0 / 3.0);
+    }
+    RSS(ref_metric_limit_h(metric, ref_grid, hmin, hmax), "limit h");
+  }
+
+  return REF_SUCCESS;
+}
+
 REF_STATUS ref_metric_lp(REF_DBL *metric, REF_GRID ref_grid, REF_DBL *scalar,
                          REF_METRIC_RECONSTRUCTION reconstruction,
                          REF_INT p_norm, REF_DBL gradation,
