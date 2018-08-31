52ab46aefa19462331bf9362ff560b241f664255
Mike Park <Mike.Park@NASA.gov>
2018-08-30 20:55:21 -0400

    masabl exe paths

--- acceptance/2d/masabl/two/accept-2d-two.sh
+++ acceptance/2d/masabl/two/accept-2d-two.sh
@@ -1,24 +1,34 @@
 #!/usr/bin/env bash
 
-bin=${HOME}/refine/strict/src
+set -x # echo commands
+set -e # exit on first error
+set -u # Treat unset variables as error
 
-${bin}/ref_acceptance 2 ref_adapt_test.b8.ugrid
+if [ $# -gt 0 ] ; then
+    one=$1/one
+    two=$1/src
+else
+    one=${HOME}/refine/strict/one
+    two=${HOME}/refine/strict/src
+fi
+
+${two}/ref_acceptance 2 ref_adapt_test.b8.ugrid
 
 function adapt_cycle {
     proj=$1
 
     cp ref_adapt_test.b8.ugrid ${proj}.b8.ugrid
 
-    ${bin}/ref_translate ${proj}.b8.ugrid ${proj}.html
-    ${bin}/ref_translate ${proj}.b8.ugrid ${proj}.tec
+    ${two}/ref_translate ${proj}.b8.ugrid ${proj}.html
+    ${two}/ref_translate ${proj}.b8.ugrid ${proj}.tec
 
-    ${bin}/ref_acceptance ${proj}.b8.ugrid ${proj}.metric -masabl
-    ${bin}/ref_driver -i ${proj}.b8.ugrid -m ${proj}.metric -o ref_adapt_test -t | tee ${proj}.out || exit 1
+    ${two}/ref_acceptance ${proj}.b8.ugrid ${proj}.metric -masabl
+    ${two}/ref_driver -i ${proj}.b8.ugrid -m ${proj}.metric -o ref_adapt_test -t | tee ${proj}.out || exit 1
 
     cp ref_gather_movie.tec ${proj}_movie.tec
     cp ref_gather_histo.tec ${proj}_histo.tec
 
-    ${bin}/ref_metric_test ${proj}.b8.ugrid ${proj}.metric > ${proj}.status
+    ${two}/ref_metric_test ${proj}.b8.ugrid ${proj}.metric > ${proj}.status
     cp ref_metric_test_s00_n1_p0_ellipse.tec ${proj}_metric_ellipse.tec
 }
 
