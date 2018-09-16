c673b060c87d90aa633bd4a4c1666e128ce9604e
Mike Park <Mike.Park@NASA.gov>
2018-09-16 15:01:08 -0400

    added unit testing of para recon

--- jenkins/cfdlab.sh
+++ jenkins/cfdlab.sh
@@ -137,6 +137,7 @@ mpiexec -np 2 ./ref_part_test >> $LOG 2>&1
 mpiexec -np 2 ./ref_migrate_test >> $LOG 2>&1
 mpiexec -np 2 ./ref_cavity_test >> $LOG 2>&1
 mpiexec -np 2 ./ref_elast_test >> $LOG 2>&1
+mpiexec -np 2 ./ref_recon_test >> $LOG 2>&1
 mpiexec -np 8 ./ref_agents_test >> $LOG 2>&1
 mpiexec -np 8 ./ref_edge_test >> $LOG 2>&1
 mpiexec -np 8 ./ref_gather_test >> $LOG 2>&1
@@ -148,6 +149,7 @@ mpiexec -np 8 ./ref_part_test >> $LOG 2>&1
 mpiexec -np 8 ./ref_migrate_test >> $LOG 2>&1
 mpiexec -np 8 ./ref_cavity_test >> $LOG 2>&1
 mpiexec -np 8 ./ref_elast_test >> $LOG 2>&1
+mpiexec -np 8 ./ref_recon_test >> $LOG 2>&1
 trap - EXIT
 
 LOG=${root_dir}/log.accept-2d-linear-two
