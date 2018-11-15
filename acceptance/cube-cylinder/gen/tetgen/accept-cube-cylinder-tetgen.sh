92df39cc6bd60e6f64e7a470abc977e39afc791b
Mike Park <Mike.Park@NASA.gov>
2018-11-15 08:44:03 -0500

    test tetgen

---
+++ acceptance/cube-cylinder/gen/tetgen/accept-cube-cylinder-tetgen.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -x # echo commands
+set -e # exit on first error
+set -u # Treat unset variables as error
+
+if [ $# -gt 0 ] ; then
+    one=$1/one
+    two=$1/src
+else
+    one=${HOME}/refine/egads/one
+    two=${HOME}/refine/egads/src
+fi
+
+geomfile=../csm/cube-cylinder.egads
+
+ref_geom_test --tetgen ${geomfile} cube-cylinder.meshb 4e-02 2e-03 15.0
+
+
