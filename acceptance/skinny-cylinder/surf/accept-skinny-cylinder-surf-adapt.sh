6b2c1d78abb4c6787944e75fe197d0dc51a9b9c1
Mike Park <Mike.Park@NASA.gov>
2018-11-15 06:05:56 -0500

    testcase

---
+++ acceptance/skinny-cylinder/surf/accept-skinny-cylinder-surf-adapt.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -x # echo commands
+set -e # exit on first error
+set -u # Treat unset variables as error
+
+if [ $# -gt 0 ] ; then
+    one=$1/one
+    two=$1/src
+else
+    one=${HOME}/refine/egads/one
+    two=${HOME}/refine/egads/src
+fi
+
+geomfile=skinny-cylinder.egads
+
+ref_geom_test --surf ${geomfile} skinny-cylinder.meshb
