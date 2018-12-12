f8490f9aff0e8344afc7469b567ed8279c7ece1c
Mike Park <Mike.Park@NASA.gov>
2018-12-12 14:45:37 -0500

    insert sort

--- src/ref_edge.c
+++ src/ref_edge.c
@@ -521,10 +521,33 @@ static REF_STATUS ref_edge_min_degree_node(REF_EDGE ref_edge, REF_NODE ref_node,
 
 static REF_STATUS ref_edge_rcm_queue_node(REF_INT node, REF_INT degree,
                                           REF_INT *queue, REF_INT *nqueue) {
-  queue[0 + 2 * (*nqueue)] = node;
-  queue[1 + 2 * (*nqueue)] = degree;
+  REF_INT location, insert_point;
+
+  /* smallest degree first */
+
+  insert_point = (*nqueue);
+  for (location = 0; location < (*nqueue); location++) {
+    if (queue[1 + 2 * location] > degree) {
+      insert_point = location;
+      break;
+    }
+  }
+
+  for (location = (*nqueue); location > insert_point; location--) {
+    queue[0 + 2 * location] = queue[0 + 2 * (location - 1)];
+    queue[1 + 2 * location] = queue[1 + 2 * (location - 1)];
+  }
+
+  queue[0 + 2 * insert_point] = node;
+  queue[1 + 2 * insert_point] = degree;
   (*nqueue)++;
-  /* sort */
+
+  printf("nqueue %d of %d (%d) ins %d\n", *nqueue, node, degree, insert_point);
+  for (location = 0; location < (*nqueue); location++) {
+    printf("%d: %d (%d)\n", location, queue[0 + 2 * location],
+           queue[1 + 2 * location]);
+  }
+
   return REF_SUCCESS;
 }
 
@@ -536,6 +559,8 @@ static REF_STATUS ref_edge_rcm_queue(REF_EDGE ref_edge, REF_INT node,
   n2o[(*ndone)] = node;
   o2n[node] = (*ndone);
   (*ndone)++;
+
+  printf("%d done %d\n", *ndone, node);
 
   each_ref_adj_node_item_with_ref(ref_adj, node, item, ref) {
     other = ref_edge_e2n(ref_edge, 0, ref);
@@ -573,12 +598,6 @@ REF_STATUS ref_edge_rcm(REF_EDGE ref_edge, REF_NODE ref_node) {
   RSS(ref_edge_rcm_queue(ref_edge, min_degree_node, o2n, n2o, &ndone, queue,
                          &nqueue),
       "min");
-
-  n2o[ndone] = min_degree_node;
-  o2n[min_degree_node] = ndone;
-  ndone++;
-
-  /* queue neighbors of node */
 
   /* drain queue */
 
