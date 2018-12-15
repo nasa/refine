11d0db520cfec697e32f6438b661681a1850805c
Mike Park <Mike.Park@NASA.gov>
2018-12-15 18:24:54 -0500

    format accepts args

--- src/format.sh
+++ src/format.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 
+set -e
+set -u
+
+if [ -z "$1" ] ; then
+    for filename in `ls *.h *.c` ; do
+	$0 ${filename}
+    done
+    exit 0
+fi
+
+if (( $# > 1 )) ; then
+    for filename in "$@" ; do
+	$0 ${filename}
+    done
+    exit 0
+fi
+
 #uncrustify -c ../uncrustify-gnu.cfg --replace $1*.h $1*.c
 
-clang-format -style=Google -i *.c *.h
+clang-format -style=Google -i $1
 
