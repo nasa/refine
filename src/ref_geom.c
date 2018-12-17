9267febf60b0c27d841a7c49990888be0698be05
Mike Park <Mike.Park@NASA.gov>
2018-12-17 15:13:22 -0500

    removes node edge loop, feature size only based on no adjecent edges

--- src/ref_geom.c
+++ src/ref_geom.c
@@ -2695,13 +2695,12 @@ REF_STATUS ref_geom_egads_diagonal(REF_GEOM ref_geom, REF_DBL *diag) {
 REF_STATUS ref_geom_feature_size(REF_GEOM ref_geom, REF_INT node, REF_DBL *xyz,
                                  REF_DBL *length) {
 #ifdef HAVE_EGADS
-  REF_BOOL on_node, on_edge;
-  REF_INT item, edge_geom, face_geom, edgeid, faceid, iloop;
+  REF_INT edge_item, face_item, edge_geom, face_geom, edgeid, faceid, iloop;
   ego ref, *cadnodes, *edges, *loops;
   int oclass, mtype, ncadnode, nedge, nloop, *senses;
   double data[18];
   double trange[2];
-  REF_DBL xyz0[3], xyz1[3];
+  REF_DBL xyz1[3];
   REF_INT ineligible_cad_node0, ineligible_cad_node1;
   REF_INT cad_node0, cad_node1;
   REF_DBL param[2];
@@ -2710,88 +2709,65 @@ REF_STATUS ref_geom_feature_size(REF_GEOM ref_geom, REF_INT node, REF_DBL *xyz,
 
   RSS(ref_geom_egads_diagonal(ref_geom, length), "bbox diag init");
 
-  RSS(ref_geom_is_a(ref_geom, node, REF_GEOM_NODE, &on_node), "on node");
-  if (on_node) {
-    each_ref_geom_having_node(ref_geom, node, item, edge_geom) {
-      if (REF_GEOM_EDGE == ref_geom_type(ref_geom, edge_geom)) {
-        edgeid = ref_geom_id(ref_geom, edge_geom);
-        REIS(EGADS_SUCCESS,
-             EG_getTopology(((ego *)(ref_geom->edges))[edgeid - 1], &ref,
-                            &oclass, &mtype, trange, &ncadnode, &cadnodes,
-                            &senses),
-             "EG topo node");
-        if (mtype == ONENODE || mtype == DEGENERATE) continue;
-        RSS(ref_geom_eval_at(ref_geom, REF_GEOM_EDGE, edgeid, &(trange[0]),
-                             xyz0, NULL),
-            "eval at tmin");
-        RSS(ref_geom_eval_at(ref_geom, REF_GEOM_EDGE, edgeid, &(trange[1]),
-                             xyz1, NULL),
-            "eval at tmax");
-        *length = MIN(*length, sqrt(pow(xyz1[0] - xyz0[0], 2) +
-                                    pow(xyz1[1] - xyz0[1], 2) +
-                                    pow(xyz1[2] - xyz0[2], 2)));
+  each_ref_geom_having_node(ref_geom, node, edge_item, edge_geom) {
+    if (REF_GEOM_EDGE == ref_geom_type(ref_geom, edge_geom)) {
+      edgeid = ref_geom_id(ref_geom, edge_geom);
+      REIS(EGADS_SUCCESS,
+           EG_getTopology(((ego *)(ref_geom->edges))[edgeid - 1], &ref, &oclass,
+                          &mtype, trange, &ncadnode, &cadnodes, &senses),
+           "EG topo node");
+      RAS(mtype != DEGENERATE, "edge interior DEGENERATE");
+      RAS(0 < ncadnode && ncadnode < 3, "edge children");
+      ineligible_cad_node0 = EG_indexBodyTopo(ref_geom->solid, cadnodes[0]);
+      if (2 == ncadnode) {
+        ineligible_cad_node1 = EG_indexBodyTopo(ref_geom->solid, cadnodes[1]);
+      } else {
+        ineligible_cad_node1 = ineligible_cad_node0; /* ONENODE edge */
       }
-    }
-  }
-
-  RSS(ref_geom_is_a(ref_geom, node, REF_GEOM_EDGE, &on_edge), "on edge");
-  if (on_edge && !on_node) {
-    RSS(ref_geom_unique_id(ref_geom, node, REF_GEOM_EDGE, &edgeid),
-        "assume uniq edge id");
-    REIS(EGADS_SUCCESS,
-         EG_getTopology(((ego *)(ref_geom->edges))[edgeid - 1], &ref, &oclass,
-                        &mtype, trange, &ncadnode, &cadnodes, &senses),
-         "EG topo node");
-    RAS(mtype != DEGENERATE, "edge interior DEGENERATE");
-    RAS(0 < ncadnode && ncadnode < 3, "edge children");
-    ineligible_cad_node0 = EG_indexBodyTopo(ref_geom->solid, cadnodes[0]);
-    if (2 == ncadnode) {
-      ineligible_cad_node1 = EG_indexBodyTopo(ref_geom->solid, cadnodes[1]);
-    } else {
-      ineligible_cad_node1 = ineligible_cad_node0; /* ONENODE edge */
-    }
-    each_ref_geom_having_node(ref_geom, node, item, face_geom) {
-      if (REF_GEOM_FACE == ref_geom_type(ref_geom, face_geom)) {
-        faceid = ref_geom_id(ref_geom, face_geom);
-        REIS(EGADS_SUCCESS,
-             EG_getTopology(((ego *)(ref_geom->faces))[faceid - 1], &ref,
-                            &oclass, &mtype, data, &nloop, &loops, &senses),
-             "topo");
-        for (iloop = 0; iloop < nloop; iloop++) {
-          /* loop through all Edges associated with this Loop */
+      each_ref_geom_having_node(ref_geom, node, face_item, face_geom) {
+        if (REF_GEOM_FACE == ref_geom_type(ref_geom, face_geom)) {
+          faceid = ref_geom_id(ref_geom, face_geom);
           REIS(EGADS_SUCCESS,
-               EG_getTopology(loops[iloop], &ref, &oclass, &mtype, data, &nedge,
-                              &edges, &senses),
+               EG_getTopology(((ego *)(ref_geom->faces))[faceid - 1], &ref,
+                              &oclass, &mtype, data, &nloop, &loops, &senses),
                "topo");
-          for (iedge = 0; iedge < nedge; iedge++) {
-            other_edgeid =
-                EG_indexBodyTopo((ego)(ref_geom->solid), edges[iedge]);
-            /* qualified? does not share geom nodes */
+          for (iloop = 0; iloop < nloop; iloop++) {
+            /* loop through all Edges associated with this Loop */
             REIS(EGADS_SUCCESS,
-                 EG_getTopology(((ego *)(ref_geom->edges))[other_edgeid - 1],
-                                &ref, &oclass, &mtype, trange, &ncadnode,
-                                &cadnodes, &senses),
-                 "EG topo node");
-            if (mtype == DEGENERATE) continue; /* skip DEGENERATE */
-            RAS(0 < ncadnode && ncadnode < 3, "edge children");
-            cad_node0 = EG_indexBodyTopo(ref_geom->solid, cadnodes[0]);
-            if (2 == ncadnode) {
-              cad_node1 = EG_indexBodyTopo(ref_geom->solid, cadnodes[1]);
-            } else {
-              cad_node1 = cad_node0; /* ONENODE edge */
+                 EG_getTopology(loops[iloop], &ref, &oclass, &mtype, data,
+                                &nedge, &edges, &senses),
+                 "topo");
+            for (iedge = 0; iedge < nedge; iedge++) {
+              other_edgeid =
+                  EG_indexBodyTopo((ego)(ref_geom->solid), edges[iedge]);
+              /* qualified? does not share geom nodes */
+              REIS(EGADS_SUCCESS,
+                   EG_getTopology(((ego *)(ref_geom->edges))[other_edgeid - 1],
+                                  &ref, &oclass, &mtype, trange, &ncadnode,
+                                  &cadnodes, &senses),
+                   "EG topo node");
+              if (mtype == DEGENERATE) continue; /* skip DEGENERATE */
+              RAS(0 < ncadnode && ncadnode < 3, "edge children");
+              cad_node0 = EG_indexBodyTopo(ref_geom->solid, cadnodes[0]);
+              if (2 == ncadnode) {
+                cad_node1 = EG_indexBodyTopo(ref_geom->solid, cadnodes[1]);
+              } else {
+                cad_node1 = cad_node0; /* ONENODE edge */
+              }
+              if (cad_node0 == ineligible_cad_node0 ||
+                  cad_node0 == ineligible_cad_node1 ||
+                  cad_node1 == ineligible_cad_node0 ||
+                  cad_node1 == ineligible_cad_node1)
+                continue;
+              /* inverse projection */
+              status =
+                  EG_invEvaluate(((ego *)(ref_geom->edges))[other_edgeid - 1],
+                                 xyz, param, xyz1);
+              REIS(EGADS_SUCCESS, status, "EG_invEvaluate opp edge");
+              *length = MIN(*length, sqrt(pow(xyz1[0] - xyz[0], 2) +
+                                          pow(xyz1[1] - xyz[1], 2) +
+                                          pow(xyz1[2] - xyz[2], 2)));
             }
-            if (cad_node0 == ineligible_cad_node0 ||
-                cad_node0 == ineligible_cad_node1 ||
-                cad_node1 == ineligible_cad_node0 ||
-                cad_node1 == ineligible_cad_node1)
-              continue;
-            /* inverse projection */
-            status = EG_invEvaluate(
-                ((ego *)(ref_geom->edges))[other_edgeid - 1], xyz, param, xyz1);
-            REIS(EGADS_SUCCESS, status, "EG_invEvaluate opp edge");
-            *length = MIN(*length, sqrt(pow(xyz1[0] - xyz[0], 2) +
-                                        pow(xyz1[1] - xyz[1], 2) +
-                                        pow(xyz1[2] - xyz[2], 2)));
           }
         }
       }
