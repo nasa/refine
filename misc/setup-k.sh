5ae79e3110f33bcf55ce1aff1dabb77f0b2c6f4c
Mike Park <Mike.Park@NASA.Gov>
2019-02-07 07:36:17 -0500

    use gcc and self-compiled zoltan and parmetis on k

--- misc/setup-k.sh
+++ misc/setup-k.sh
@@ -5,15 +5,17 @@ set -x
 ./bootstrap
 
 module_path="/u/shared/fun3d/fun3d_users/modules"
-zoltan_path="${module_path}/Zoltan/3.82-mpt-2.16-intel_2017.2.174"
-parmetis_path="${module_path}/ParMETIS/4.0.3-mpt-2.16-intel_2017.2.174"
+zoltan_path="/u/mpark/local/pkgs/Zoltan_v3.82/gcc_6.2.0-mpt-2.18a108"
+parmetis_path="/u/mpark/local/pkgs/parmetis-4.0.3/build/Linux-x86_64"
 egads_path="${module_path}/ESP/114/EngSketchPad"
+
+gcc_flags="-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -Wuninitialized"
 
 mkdir -p strict
 ( cd strict && \
     ../configure \
     --prefix=`pwd` \
-    CFLAGS='-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -Wuninitialized' \
+    CFLAGS="${gcc_flags}" \
     ) \
     || exit
 
@@ -22,7 +24,7 @@ mkdir -p egads
     ../configure \
     --prefix=`pwd` \
     --with-EGADS=${egads_path} \
-    CFLAGS='-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -Wuninitialized' \
+    CFLAGS="${gcc_flags}" \
     ) \
     || exit
 
@@ -33,8 +35,8 @@ mkdir -p zoltan
     --with-zoltan=${zoltan_path} \
     --with-EGADS=${egads_path} \
     --enable-lite \
-    CC=icc \
-    CFLAGS='-DHAVE_MPI -g -O2 -traceback -Wall -w3 -wd1418,2259,2547,981,11074,11076,1572,49,1419' \
+    CC=mpicc \
+    CFLAGS="-DHAVE_MPI ${gcc_flags}" \
     LIBS=-lmpi \
     ) \
     || exit
@@ -46,8 +48,10 @@ mkdir -p parmetis
     --with-parmetis=${parmetis_path} \
     --with-EGADS=${egads_path} \
     --enable-lite \
-    CC=icc \
-    CFLAGS='-DHAVE_MPI -g -O2 -traceback -Wall -w3 -wd1418,2259,2547,981,11074,11076,1572,49,1419' \
+    CC=mpicc \
+    CFLAGS="-DHAVE_MPI ${gcc_flags}" \
     LIBS=-lmpi \
     ) \
     || exit
+
+
