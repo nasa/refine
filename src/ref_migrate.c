72ea4ff802276e2bfd03ec176b601bc7f35fc4b9
Mike Park <Mike.Park@NASA.gov>
2019-02-08 10:03:27 -0500

    enhance consistency by synchronize_global in single_part

--- src/ref_migrate.c
+++ src/ref_migrate.c
@@ -243,6 +243,8 @@ static REF_STATUS ref_migrate_single_part(REF_GRID ref_grid) {
   REF_NODE ref_node = ref_grid_node(ref_grid);
   REF_INT node;
 
+  RSS(ref_node_synchronize_globals(ref_node), "sync global nodes");
+
   for (node = 0; node < ref_node_max(ref_node); node++)
     ref_node_part(ref_node, node) = 0;
 
