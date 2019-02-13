7821a182d68d56995f5f61b10086eb5d3644d5ee
Mike Park <Mike.Park@NASA.Gov>
2019-02-13 12:30:13 -0500

    first subcommand bootstrap

--- src/ref_subcommand.c
+++ src/ref_subcommand.c
@@ -21,16 +21,74 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "ref_defs.h"
+
 #include "ref_mpi.h"
+#include "ref_args.h"
+
+static void usage(const char *name) {
+    printf("usage: \n %s [--help] <command> [<args>]\n", name);
+    printf("\n");
+    printf("These are common ref commands:\n");
+    printf("  boostrap Create initial grid from EGADS file\n");
+}
+static void bootstrap_help(const char *name) {
+    printf("usage: \n %s boostrap project.egads\n", name);
+    printf("\n");
+}
+
+static REF_STATUS bootstrap(int argc, char *argv[]) {
+  size_t end_of_string;
+  char project[1004];
+  if (argc<3) goto shutdown;
+  end_of_string = strlen(argv[2]);
+  if (7>end_of_string ||
+      strncmp(&(argv[2][end_of_string - 6]), ".egads", 6) != 0)
+    goto shutdown;
+  strncpy(argv[2], project, end_of_string - 7);
+  project[end_of_string - 7] = '\0';
+
+  return REF_SUCCESS;
+  shutdown:
+  bootstrap_help(argv[0]);
+  return REF_FAILURE;
+}
 
 int main(int argc, char *argv[]) {
   REF_MPI ref_mpi;
+  REF_INT help_pos = REF_EMPTY;
 
   RSS(ref_mpi_start(argc, argv), "start");
   RSS(ref_mpi_create(&ref_mpi), "make mpi");
   ref_mpi_stopwatch_start(ref_mpi);
 
+  RXS(ref_args_find(argc, argv, "--help", &help_pos), REF_NOT_FOUND,
+      "arg search");
+  if (REF_EMPTY == help_pos) {
+    RXS(ref_args_find(argc, argv, "-h", &help_pos), REF_NOT_FOUND,
+        "arg search");
+  }
+
+  if (1 == argc || 1 == help_pos) {
+    usage(argv[0]);
+    goto shutdown;
+  }
+
+  if (strncmp(argv[1], "b", 1) == 0) {
+    if (REF_EMPTY == help_pos) {
+      RSS(bootstrap(argc, argv), "bootstrap");
+    }else{
+      bootstrap_help(argv[0]);
+      goto shutdown;
+    }
+  } else {
+    usage(argv[0]);
+    goto shutdown;
+  }
+
+
   ref_mpi_stopwatch_stop(ref_mpi, "done.");
+ shutdown:
   RSS(ref_mpi_free(ref_mpi), "mpi free");
   RSS(ref_mpi_stop(), "stop");
 
