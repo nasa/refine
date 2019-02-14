14a498c42a65f1c902dd8650fecb9be6724f7be0
Mike Park <Mike.Park@NASA.gov>
2019-02-14 04:17:44 -0500

    use git diff to only format files that need commit

--- src/format.sh
+++ src/format.sh
@@ -3,6 +3,17 @@
 set -e
 set -u
 
+
+if [ "$1" = "-g" ] ; then
+    for filename in `git diff --name-only` ; do
+	if [[ "$filename" == *.c ]] || [[ "$filename" == *.h ]] ; then
+	    echo $filename
+	    $0 ../$filename
+	fi
+    done
+    exit 0
+fi
+
 if [ $# -eq 0 ] ; then
     for filename in `ls *.h *.c` ; do
 	$0 ${filename}
