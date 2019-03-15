a2763f8eaa855d894a755ec303d158e524fe179b
Mike Park <Mike.Park@NASA.gov>
2019-03-15 17:15:28 -0400

    different name

--- acceptance/3d/polar-1/two/accept-3d-two-para.sh
+++ acceptance/3d/polar-1/two/accept-3d-two-para.sh
@@ -12,37 +12,37 @@ else
     two=${HOME}/refine/zoltan/src
 fi
 
-${two}/ref_acceptance 1 ref_adapt_test.b8.ugrid
+${two}/ref_acceptance 1 ref_adapt_para.b8.ugrid
 
 function adapt_cycle {
     proj=$1
     field=polar-1
     
-    cp ref_adapt_test.b8.ugrid ${proj}.b8.ugrid
+    cp ref_adapt_para.b8.ugrid ${proj}.b8.ugrid
 
     ${two}/ref_translate ${proj}.b8.ugrid ${proj}.html
     ${two}/ref_translate ${proj}.b8.ugrid ${proj}.tec
 
     ${two}/ref_acceptance -ugawg ${field} ${proj}.b8.ugrid ${proj}.metric
     
-    rm ref_adapt_test.b8.ugrid
+    rm ref_adapt_para.b8.ugrid
     mpiexec -np 4 ${two}/ref_driver -i ${proj}.b8.ugrid -m ${proj}.metric \
-            -o ref_adapt_test -t
+            -o ref_adapt_para -t
     cp ref_gather_movie.tec ${proj}_movie.tec
     
     ${two}/ref_metric_test ${proj}.b8.ugrid ${proj}.metric > ${proj}.status
     cp ref_metric_test_s00_n1_p0_ellipse.tec ${proj}_metric_ellipse.tec
 }
 
-adapt_cycle accept-3d-two-00
-adapt_cycle accept-3d-two-01
-adapt_cycle accept-3d-two-02
-adapt_cycle accept-3d-two-03
-adapt_cycle accept-3d-two-04
-adapt_cycle accept-3d-two-05
+adapt_cycle accept-3d-para-00
+adapt_cycle accept-3d-para-01
+adapt_cycle accept-3d-para-02
+adapt_cycle accept-3d-para-03
+adapt_cycle accept-3d-para-04
+adapt_cycle accept-3d-para-05
 
-cat accept-3d-two-05.status
-../../../check.rb accept-3d-two-05.status 0.05 3.0
+cat accept-3d-para-05.status
+../../../check.rb accept-3d-para-05.status 0.05 3.0
 
 
 
