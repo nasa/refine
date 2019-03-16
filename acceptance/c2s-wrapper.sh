92555de3af810fb314562214049aaae3b4f8348c
Mike Park <Mike.Park@NASA.gov>
2019-03-16 03:20:55 -0400

    new smaller repo

--- acceptance/c2s-wrapper.sh
+++ acceptance/c2s-wrapper.sh
@@ -26,7 +26,7 @@ cd ${build_directory_root} && \
   cd ${BUILD_TAG} && \
     pwd && \
     git clone ${CI_REPOSITORY_URL} && \
-    git clone git@gitlab.larc.nasa.gov:cad-to-solution/C2S.git && \
+    git clone git@gitlab.larc.nasa.gov:cad-to-solution/acceptance.git && \
     cd refine && \
       pwd && \
       ${checkout_cmd} && \
