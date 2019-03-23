2435fe19cecd2e19aeb40c530582c59736aafa9c
Mike Park <Mike.Park@NASA.gov>
2019-03-23 06:13:31 -0400

    export system

--- src/ref_phys_test.c
+++ src/ref_phys_test.c
@@ -342,10 +342,11 @@ int main(int argc, char *argv[]) {
     REF_GRID ref_grid;
     REF_NODE ref_node;
     REF_CELL ref_cell;
-    REF_DBL *dual_flux, *res, *flux;
+    REF_DBL *dual_flux, *system, *flux;
     REF_INT ldim;
     REF_INT equ, dir, node, cell, cell_node, nodes[REF_CELL_MAX_SIZE_PER];
     REF_DBL cell_vol, flux_grad[3];
+    REF_INT nsystem = 11;
 
     REIS(1, cont_res_pos,
          "required args: --cont-res grid.meshb dual_flux.solb");
@@ -366,11 +367,11 @@ int main(int argc, char *argv[]) {
     REIS(20, ldim, "expected 20 (5*adj,5*xflux,5*yflux,5*zflux) scalar");
 
     ref_malloc(flux, ref_node_max(ref_grid_node(ref_grid)), REF_DBL);
-    ref_malloc_init(res, 5 * ref_node_max(ref_grid_node(ref_grid)), REF_DBL,
-                    0.0);
+    ref_malloc_init(system, nsystem * ref_node_max(ref_grid_node(ref_grid)),
+                    REF_DBL, 0.0);
 
-    for (equ = 0; equ < 5; equ++) {
-      for (dir = 0; dir < 3; dir++) {
+    for (dir = 0; dir < 3; dir++) {
+      for (equ = 0; equ < 5; equ++) {
         each_ref_node_valid_node(ref_node, node) {
           flux[node] = dual_flux[equ + dir * 5 + 5 + ldim * node];
         }
@@ -378,18 +379,31 @@ int main(int argc, char *argv[]) {
           RSS(ref_node_tet_vol(ref_node, nodes, &cell_vol), "vol");
           RSS(ref_node_tet_grad(ref_node, nodes, flux, flux_grad), "grad");
           each_ref_cell_cell_node(ref_cell, cell_node) {
-            res[equ + 5 * nodes[cell_node]] += 0.25 * flux_grad[dir] * cell_vol;
+            system[equ + nsystem * nodes[cell_node]] +=
+                0.25 * flux_grad[dir] * cell_vol;
           }
         }
       }
     }
 
-    if (ref_mpi_once(ref_mpi)) printf("writing res.tec\n");
-    RSS(ref_gather_scalar_by_extension(ref_grid, 5, res, NULL, "res.tec"),
+    each_ref_node_valid_node(ref_node, node) {
+      for (equ = 0; equ < 5; equ++) {
+        system[equ + 5 + nsystem * node] = dual_flux[equ + ldim * node];
+      }
+    }
+    each_ref_node_valid_node(ref_node, node) {
+      for (equ = 0; equ < 5; equ++) {
+        system[nsystem - 1 + nsystem * node] +=
+            ABS(dual_flux[equ + ldim * node] * system[equ + 6 * node]);
+      }
+    }
+    if (ref_mpi_once(ref_mpi)) printf("writing system.tec\n");
+    RSS(ref_gather_scalar_by_extension(ref_grid, nsystem, system, NULL,
+                                       "system.tec"),
         "export primitive_dual");
 
     ref_free(flux);
-    ref_free(res);
+    ref_free(system);
     ref_free(dual_flux);
 
     RSS(ref_grid_free(ref_grid), "free");
