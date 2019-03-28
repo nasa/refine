9e38349c17bbaec67648e4731ce62ab8fe1b956b
Mike Park <Mike.Park@NASA.gov>
2019-03-28 15:01:30 -0400

    need occ

--- misc/setup-pfe.sh
+++ misc/setup-pfe.sh
@@ -8,6 +8,7 @@ module_path="/u/fun3d/shared/n1337/modules"
 zoltan_path="${module_path}/Zoltan/3.82_mpt-2.17r13_ifort-2018.3.222"
 parmetis_path="${module_path}/ParMETIS/4.0.3_mpt-2.17r13_ifort-2018.3.222"
 egads_path="${module_path}/ESP/114/EngSketchPad"
+opencascade_path="${module_path}/ESP/114/OpenCASCADE-6.8.1/Linux"
 
 gcc_flags="-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -Wuninitialized"
 
@@ -24,6 +25,7 @@ mkdir -p egads
     ../configure \
     --prefix=`pwd` \
     --with-EGADS=${egads_path} \
+    --with-OpenCASCADE=${opencascade_path} \
     CFLAGS="${gcc_flags}" \
     ) \
     || exit
