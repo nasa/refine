24c0f55de2d752186378eaab2a62af2260ffe441
Mike Park <Mike.Park@NASA.gov>
2019-03-28 05:04:15 -0400

    gen

---
+++ acceptance/inflate/box.sh
@@ -0,0 +1,12 @@
+
+rm -f box.egads
+serveCSM -batch box.csm | tee box-csm.txt
+
+rm -f box-vol.meshb
+ref bootstrap box.egads | tee box-boot.txt
+
+rm -f box01.meshb
+ref_driver -i box-vol.meshb \
+	   -g box.egads \
+	   -x box.meshb | tee box-crv.txt
+
