e90d72a4437389897bfdbff124cec767c1432043
Mike Park <Mike.Park@NASA.gov>
2019-03-28 05:12:15 -0400

    drive inflate

---
+++ acceptance/inflate/normal/inflate.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -x # echo commands
+set -e # exit on first error
+set -u # Treat unset variables as error
+
+if [ $# -gt 0 ] ; then
+    one=$1/one
+    two=$1/src
+else
+    one=${HOME}/refine/egads/one
+    two=${HOME}/refine/egads/src
+fi
+
+${two}/ref_inflatable  \
+    ../box.meshb \
+    10 \
+    0.01 \
+    1.0 \
+    1.68 \
+    4 5 6
+
+exit
