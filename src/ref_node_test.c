bb3ffb1092f480b13a48ae30d663f847c9dd8913
Mike Park <Mike.Park@NASA.gov>
2019-04-24 17:13:40 -0400

    test add many

--- src/ref_node_test.c
+++ src/ref_node_test.c
@@ -2132,6 +2132,19 @@ int main(int argc, char *argv[]) {
     RSS(ref_node_free(ref_node), "node free");
   }
 
+  { /* store lots */
+    REF_NODE ref_node;
+    REF_INT global;
+    REF_INT max;
+    RSS(ref_node_create(&ref_node, ref_mpi), "create");
+    max = ref_node_max_unused(ref_node);
+    for (global = 0; global <= max; global++) {
+      RSS(ref_node_push_unused(ref_node, global), "store");
+    }
+    RAS(ref_node_max_unused(ref_node) > max, "more?");
+    RSS(ref_node_free(ref_node), "free");
+  }
+
   RSS(ref_mpi_free(ref_mpi), "mpi free");
   RSS(ref_mpi_stop(), "stop");
 
