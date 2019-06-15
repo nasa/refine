30c594388320e045f033b5ca4e38e3610c15c508
Mike Park <Mike.Park@NASA.gov>
2019-06-15 15:03:10 -0400

    remove ref_dict aux, moved to cloud for long globals

--- src/ref_dict.h
+++ src/ref_dict.h
@@ -32,22 +32,17 @@ struct REF_DICT_STRUCT {
   REF_INT n, max, naux;
   REF_INT *key;
   REF_INT *value;
-  REF_DBL *aux;
 };
 
 REF_STATUS ref_dict_create(REF_DICT *ref_dict);
-REF_STATUS ref_dict_includes_aux_value(REF_DICT ref_dict, REF_INT naux);
 REF_STATUS ref_dict_free(REF_DICT ref_dict);
 
 REF_STATUS ref_dict_deep_copy(REF_DICT *ref_dict, REF_DICT original);
 
 #define ref_dict_n(ref_dict) ((ref_dict)->n)
 #define ref_dict_max(ref_dict) ((ref_dict)->max)
-#define ref_dict_naux(ref_dict) ((ref_dict)->naux)
 #define ref_dict_key(ref_dict, key_index) ((ref_dict)->key[(key_index)])
 #define ref_dict_keyvalue(ref_dict, key_index) ((ref_dict)->value[(key_index)])
-#define ref_dict_keyvalueaux(ref_dict, aux_index, key_index) \
-  ((ref_dict)->aux[(aux_index) + ref_dict_naux(ref_dict) * (key_index)])
 
 #define ref_dict_valid(ref_dict, key_index) \
   (0 <= (key_index) && (key_index) < ref_dict_n(ref_dict))
@@ -74,12 +69,7 @@ REF_STATUS ref_dict_deep_copy(REF_DICT *ref_dict, REF_DICT original);
 #define each_ref_dict_key_index(ref_dict, key_index) \
   for ((key_index) = 0; (key_index) < ref_dict_n(ref_dict); (key_index)++)
 
-#define each_ref_dict_aux_index(ref_dict, aux_index) \
-  for ((aux_index) = 0; (aux_index) < ref_dict_naux(ref_dict); (aux_index)++)
-
 REF_STATUS ref_dict_store(REF_DICT ref_dict, REF_INT key, REF_INT value);
-REF_STATUS ref_dict_store_with_aux(REF_DICT ref_dict, REF_INT key,
-                                   REF_INT value, REF_DBL *aux);
 REF_STATUS ref_dict_location(REF_DICT ref_dict, REF_INT key, REF_INT *location);
 REF_STATUS ref_dict_remove(REF_DICT ref_dict, REF_INT key);
 REF_STATUS ref_dict_value(REF_DICT ref_dict, REF_INT key, REF_INT *value);
