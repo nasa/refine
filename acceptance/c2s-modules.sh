6b205905cc0939dac2d28f27cd2cb15916a141d9
Mike Park <Mike.Park@NASA.gov>
2019-07-01 10:55:56 -0400

    module swap

--- acceptance/c2s-modules.sh
+++ acceptance/c2s-modules.sh
@@ -3,6 +3,9 @@ module use --append /u/shared/fun3d/fun3d_users/modulefiles
 
 module load FUN3D/13.4.1-576-g0fd6f27
 
+module rm mpt-2.17r14
+module load mpt-2.19
+
 module load gcc_6.2.0
 module load ESP/114
 module load tetgen/1.5.0
