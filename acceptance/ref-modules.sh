cf4177d18a35f2a0f553e18ed93193f15bb343d0
Mike Park <Mike.Park@NASA.gov>
2019-07-03 12:20:52 -0400

    intel name

--- acceptance/ref-modules.sh
+++ acceptance/ref-modules.sh
@@ -1,7 +1,7 @@
 
 module load gcc_6.2.0
 module load openmpi_2.1.1_intel_2017
-module load intel.2017.2.174
+module load intel_2017.2.174
 
 module use --append /u/shared/fun3d/fun3d_users/modulefiles
 module load tetgen/1.5.0
