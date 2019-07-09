6215c3a92cf4fe94cf503c022c8111a3d14358eb
Mike Park <Mike.Park@NASA.gov>
2019-07-09 15:54:23 -0400

    reduce chatter

--- src/ref_subdiv.c
+++ src/ref_subdiv.c
@@ -828,7 +828,7 @@ REF_STATUS ref_subdiv_unmark_neg_tet_relax(REF_SUBDIV ref_subdiv) {
                                ref_subdiv_mpi(ref_subdiv), ref_subdiv->mark),
         "ghost mark");
 
-    if (nsweeps > 5 || ref_subdiv->instrument) {
+    if (nsweeps > 5) {
       RSS(ref_subdiv_mark_n(ref_subdiv, &nmark), "count");
       if (ref_mpi_once(ref_subdiv_mpi(ref_subdiv)))
         printf(" %d edges marked after %d unmark neg geom tet relaxations\n",
