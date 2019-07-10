52adbd78a6cdb0c63fe908d4eedc3c628ab02a42
Mike Park <Mike.Park@NASA.gov>
2019-07-10 06:29:25 -0400

    switch to 13.5 fun3d with mpt 2.19 depend

--- acceptance/c2s-modules.sh
+++ acceptance/c2s-modules.sh
@@ -1,10 +1,7 @@
 
 module use --append /u/shared/fun3d/fun3d_users/modulefiles
 
-module load FUN3D/13.4.1-576-g0fd6f27
-
-module rm mpt-2.17r14
-module load mpt-2.19
+module load FUN3D/13.5
 
 module load gcc_6.2.0
 module load ESP/114
