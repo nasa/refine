0631be190840ed6ce52c9aa45bdae01017a7516b
Mike Park <Mike.Park@NASA.gov>
2019-07-23 15:29:56 -0400

    sane to limit first pass

--- acceptance/cube-cylinder/polar-2/two/accept-cube-cylinder-polar-2-two.sh
+++ acceptance/cube-cylinder/polar-2/two/accept-cube-cylinder-polar-2-two.sh
@@ -31,7 +31,7 @@ function adapt_cycle {
 
 ${two}/ref_acceptance -ugawg ${field} ega.meshb ega-metric.solb
 
-adapt_cycle ega cycle01 15
+adapt_cycle ega cycle01 2
 adapt_cycle cycle01 cycle02 15
 adapt_cycle cycle02 cycle03 15
 adapt_cycle cycle03 cycle04 15
