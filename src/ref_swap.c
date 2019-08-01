6b75328cd2c797bbc82534f70817b4beac91b93b
Mike Park <Mike.Park@NASA.gov>
2019-08-01 16:40:24 -0400

    report details

--- src/ref_swap.c
+++ src/ref_swap.c
@@ -264,6 +264,7 @@ REF_STATUS ref_swap_node23(REF_GRID ref_grid, REF_INT node0, REF_INT node1,
   if (node1 == nodes[2] && node0 == nodes[0]) *node3 = nodes[1];
 
   RUB(REF_EMPTY, *node2, "node2 not found", {
+    printf("node2 %d node3 %d\n", *node2, *node3);
     ref_cell_nodes(ref_cell, cell_to_swap[0], nodes);
     printf("cell %d node %d %d %d %d\n", cell_to_swap[0], nodes[0], nodes[1],
            nodes[2], nodes[3]);
@@ -275,6 +276,7 @@ REF_STATUS ref_swap_node23(REF_GRID ref_grid, REF_INT node0, REF_INT node1,
     ref_export_by_extension(ref_grid, "ref_swap_node23.tec");
   });
   RUB(REF_EMPTY, *node3, "node3 not found", {
+    printf("node2 %d node3 %d\n", *node2, *node3);
     ref_cell_nodes(ref_cell, cell_to_swap[0], nodes);
     printf("cell %d node %d %d %d %d\n", cell_to_swap[0], nodes[0], nodes[1],
            nodes[2], nodes[3]);
