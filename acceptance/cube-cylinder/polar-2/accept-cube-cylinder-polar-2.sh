77f6dd35280a1449173657397c77f0329136a6cc
Mike Park <Mike.Park@NASA.gov>
2019-08-13 14:07:22 -0400

    viz polar-2 case

--- acceptance/cube-cylinder/polar-2/accept-cube-cylinder-polar-2.sh
+++ acceptance/cube-cylinder/polar-2/accept-cube-cylinder-polar-2.sh
@@ -20,8 +20,8 @@ function adapt_cycle {
     sweeps=$3
 
     ${two}/ref_driver -i ${inproj}.meshb -g ega.egads -m ${inproj}-metric.solb -o ${outproj} -s ${sweeps} -t
-    mv ref_gather_movie.tec ${inproj}_movie.tec
-    mv ref_gather_histo.tec ${inproj}_histo.tec
+    cp ref_gather_movie.tec ${inproj}_movie.tec
+    cp ref_gather_histo.tec ${inproj}_histo.tec
     ${two}/ref_acceptance -ugawg ${field} ${outproj}.meshb ${outproj}-metric.solb
     ${two}/ref_metric_test ${outproj}.meshb ${outproj}-metric.solb > ${outproj}.status
 }
