0ea855c22cb911ec63e95518d1037b57af5e4467
Mike Park <Mike.Park@NASA.gov>
2019-09-10 07:14:37 -0400

    compatable metric

--- acceptance/annulus/uniform/accept-annulus-uniform.sh
+++ acceptance/annulus/uniform/accept-annulus-uniform.sh
@@ -16,19 +16,19 @@ geomfile=annulus.egads
 
 # ${two}/ref_geom_test ${geomfile} annulus.meshb
 
-${two}/ref_acceptance annulus.meshb annulus.metric 0.1
-${two}/ref_driver -i annulus.meshb -g ${geomfile} -m annulus.metric -o ref_driver1 -t
+${two}/ref_acceptance -ugawg polar-2 annulus.meshb annulus-metric.solb
+${two}/ref_driver -i annulus.meshb -g ${geomfile} -m annulus-metric.solb -o ref_driver1 -t
 mv ref_gather_movie.tec ref_driver1_movie.tec
-${two}/ref_acceptance ref_driver1.meshb ref_driver1.metric 0.1
-${two}/ref_metric_test ref_driver1.meshb ref_driver1.metric > accept-annulus-uniform-01.status
+${two}/ref_acceptance -ugawg polar-2 ref_driver1.meshb ref_driver1-metric.solb
+${two}/ref_metric_test ref_driver1.meshb ref_driver1-metric.solb > accept-annulus-uniform-01.status
 
-${two}/ref_driver -i ref_driver1.meshb -g ${geomfile} -m ref_driver1.metric -o ref_driver2 -t
+${two}/ref_driver -i ref_driver1.meshb -g ${geomfile} -m ref_driver1-metric.solb -o ref_driver2 -t
 mv ref_gather_movie.tec ref_driver2_movie.tec
-${two}/ref_acceptance ref_driver2.meshb ref_driver2.metric 0.1
-${two}/ref_metric_test ref_driver2.meshb ref_driver2.metric > accept-annulus-uniform-02.status
+${two}/ref_acceptance -ugawg polar-2 ref_driver2.meshb ref_driver2-metric.solb
+${two}/ref_metric_test ref_driver2.meshb ref_driver-metric.solb > accept-annulus-uniform-02.status
 
 cat accept-annulus-uniform-02.status
-../../check.rb accept-annulus-uniform-02.status 0.1 2.6
+../../check.rb accept-annulus-uniform-02.status 0.3 3.0
 
 
 
