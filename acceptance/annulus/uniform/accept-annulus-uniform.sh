b71dc3e4eec0e5fcec1afd8816ebf1e599a19de3
Mike Park <Mike.Park@NASA.gov>
2019-09-10 09:13:40 -0400

    repair path

--- acceptance/annulus/uniform/accept-annulus-uniform.sh
+++ acceptance/annulus/uniform/accept-annulus-uniform.sh
@@ -25,7 +25,7 @@ ${two}/ref_metric_test ref_driver1.meshb ref_driver1-metric.solb > accept-annulu
 ${two}/ref_driver -i ref_driver1.meshb -g ${geomfile} -m ref_driver1-metric.solb -o ref_driver2 -t
 mv ref_gather_movie.tec ref_driver2_movie.tec
 ${two}/ref_acceptance -ugawg polar-2 ref_driver2.meshb ref_driver2-metric.solb
-${two}/ref_metric_test ref_driver2.meshb ref_driver-metric.solb > accept-annulus-uniform-02.status
+${two}/ref_metric_test ref_driver2.meshb ref_driver2-metric.solb > accept-annulus-uniform-02.status
 
 cat accept-annulus-uniform-02.status
 ../../check.rb accept-annulus-uniform-02.status 0.3 3.0
