73e3dbb20097297c5cb3640419c3c617711a74d2
Mike Park <Mike.Park@NASA.gov>
2019-09-17 09:26:45 -0400

    moved to ESP/116

--- misc/module-builder/pfe/refine.sh
+++ misc/module-builder/pfe/refine.sh
@@ -29,7 +29,7 @@ cd       _build_$VERSION
 ../${TOPDIR}/configure \
  --prefix=${MODULE_DEST} \
  --with-EGADS=${MODULE_ROOT}/${ESP}/EngSketchPad \
- --with-OpenCASCADE=${MODULE_ROOT}/${ESP}/OpenCASCADE-6.8.1 \
+ --with-OpenCASCADE=${MODULE_ROOT}/${ESP}/OpenCASCADE-7.3.1 \
  CC=icc \
  CFLAGS='-g -O2 -traceback -Wall -w3 -wd1418,2259,2547,981,11074,11076,1572,49,1419 -ftrapuv'
 
