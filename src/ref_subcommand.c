356c98e4226ba52b2b7bc0a326299cfb89902dd2
Mike Park <Mike.Park@NASA.gov>
2019-09-18 14:08:28 -0400

    as loop

--- src/ref_subcommand.c
+++ src/ref_subcommand.c
@@ -56,11 +56,10 @@ static void usage(const char *name) {
   printf("  bootstrap    Create initial mesh from EGADS file\n");
   printf("  fun3d        Extract a scalar from the primitive solution\n");
   printf("  interpolate  Interpolate a field from one mesh to another\n");
-  printf("  location     Report the locations of verticies in the mesh\n");
+  printf("  loop         Multiscale metric, adapt, and interpolation.\n");
   printf("  multiscale   Compute a multiscale metric.\n");
   printf("  surface      Extract mesh surface.\n");
   printf("  translate    Convert mesh formats.\n");
-  printf("  whole        Multiscale metric, adapt, and interpolation.\n");
   printf("\n");
   printf("'ref <command> -h' provides details on a specific subcommand.\n");
 }
@@ -97,32 +96,16 @@ static void interpolate_help(const char *name) {
       name);
   printf("\n");
 }
+/*
 static void location_help(const char *name) {
   printf("usage: \n %s location input.meshb node_index node_index ...\n", name);
   printf("  node_index is zero-based\n");
   printf("\n");
 }
-static void multiscale_help(const char *name) {
+*/
+static void loop_help(const char *name) {
   printf(
-      "usage: \n %s multiscale input_mesh.extension scalar.solb "
-      "complexity metric.solb\n",
-      name);
-  printf("   complexity is approximately half the target number of vertices\n");
-  printf("\n");
-}
-static void surface_help(const char *name) {
-  printf("usage: \n %s surface input_mesh.extension [surface_mesh.tec] \n",
-         name);
-  printf("\n");
-}
-static void translate_help(const char *name) {
-  printf("usage: \n %s translate input_mesh.extension output_mesh.extension \n",
-         name);
-  printf("\n");
-}
-static void whole_help(const char *name) {
-  printf(
-      "usage: \n %s whole input_project_name output_project_name"
+      "usage: \n %s loop input_project_name output_project_name"
       " complexity [<options>]\n",
       name);
   printf("\n");
@@ -154,6 +137,24 @@ static void whole_help(const char *name) {
   printf("       negative: mixed-space gradation.\n");
   printf("   --buffer coarsens the metric appraoching the x max boundary.\n");
 
+  printf("\n");
+}
+static void multiscale_help(const char *name) {
+  printf(
+      "usage: \n %s multiscale input_mesh.extension scalar.solb "
+      "complexity metric.solb\n",
+      name);
+  printf("   complexity is approximately half the target number of vertices\n");
+  printf("\n");
+}
+static void surface_help(const char *name) {
+  printf("usage: \n %s surface input_mesh.extension [surface_mesh.tec] \n",
+         name);
+  printf("\n");
+}
+static void translate_help(const char *name) {
+  printf("usage: \n %s translate input_mesh.extension output_mesh.extension \n",
+         name);
   printf("\n");
 }
 
@@ -567,7 +568,7 @@ shutdown:
   if (ref_mpi_once(ref_mpi)) fun3d_help(argv[0]);
   return REF_FAILURE;
 }
-
+/*
 static REF_STATUS location(REF_MPI ref_mpi, int argc, char *argv[]) {
   char *in_file;
   REF_INT pos, global, local;
@@ -597,199 +598,9 @@ shutdown:
   if (ref_mpi_once(ref_mpi)) location_help(argv[0]);
   return REF_FAILURE;
 }
+*/
 
-static REF_STATUS surface(REF_MPI ref_mpi, int argc, char *argv[]) {
-  char *out_file;
-  char *in_file;
-  char filename[1024];
-  REF_GRID ref_grid = NULL;
-
-  if (argc < 3) goto shutdown;
-  in_file = argv[2];
-  if (argc < 4) {
-    RAS(strlen(in_file) < 1014, "input filename too long (>1014)");
-    sprintf(filename, "%s-surf.tec", in_file);
-    out_file = filename;
-  } else {
-    out_file = argv[3];
-  }
-
-  ref_mpi_stopwatch_start(ref_mpi);
-
-  if (ref_mpi_para(ref_mpi)) {
-    if (ref_mpi_once(ref_mpi)) printf("part %s\n", in_file);
-    RSS(ref_part_by_extension(&ref_grid, ref_mpi, in_file), "part");
-    ref_mpi_stopwatch_stop(ref_mpi, "part");
-  } else {
-    if (ref_mpi_once(ref_mpi)) printf("import %s\n", in_file);
-    RSS(ref_import_by_extension(&ref_grid, ref_mpi, in_file), "import");
-    ref_mpi_stopwatch_stop(ref_mpi, "import");
-  }
-
-  if (ref_mpi_para(ref_mpi)) {
-    if (ref_mpi_once(ref_mpi)) printf("gather %s\n", out_file);
-    RSS(ref_gather_scalar_surf_tec(ref_grid, 0, NULL, NULL, out_file),
-        "gather surf tec");
-    ref_mpi_stopwatch_stop(ref_mpi, "gather");
-  } else {
-    if (ref_mpi_once(ref_mpi)) printf("export %s\n", out_file);
-    RSS(ref_export_tec_surf(ref_grid, out_file), "export tec surf");
-    ref_mpi_stopwatch_stop(ref_mpi, "export");
-  }
-
-  RSS(ref_grid_free(ref_grid), "free grid");
-
-  return REF_SUCCESS;
-shutdown:
-  if (ref_mpi_once(ref_mpi)) surface_help(argv[0]);
-  return REF_FAILURE;
-}
-
-static REF_STATUS multiscale(REF_MPI ref_mpi, int argc, char *argv[]) {
-  char *out_metric;
-  char *in_mesh;
-  char *in_scalar;
-  REF_GRID ref_grid = NULL;
-  REF_INT ldim;
-  REF_DBL *scalar, *metric;
-  REF_INT p;
-  REF_DBL gradation, complexity, current_complexity;
-  REF_RECON_RECONSTRUCTION reconstruction = REF_RECON_L2PROJECTION;
-  REF_INT pos;
-  REF_BOOL buffer;
-
-  if (argc < 6) goto shutdown;
-  in_mesh = argv[2];
-  in_scalar = argv[3];
-  complexity = atof(argv[4]);
-  out_metric = argv[5];
-
-  p = 2;
-  RXS(ref_args_find(argc, argv, "-p", &pos), REF_NOT_FOUND, "arg search");
-  if (REF_EMPTY != pos) {
-    if (pos >= argc - 1) {
-      printf("option missing value: -p <norm power>\n");
-      goto shutdown;
-    }
-    p = atoi(argv[pos + 1]);
-  }
-
-  gradation = -1.0;
-  RXS(ref_args_find(argc, argv, "-g", &pos), REF_NOT_FOUND, "arg search");
-  if (REF_EMPTY != pos) {
-    if (pos >= argc - 1) {
-      printf("option missing value: -g <gradation>\n");
-      goto shutdown;
-    }
-    gradation = atof(argv[pos + 1]);
-  }
-
-  buffer = REF_FALSE;
-  RXS(ref_args_find(argc, argv, "--buffer", &pos), REF_NOT_FOUND, "arg search");
-  if (REF_EMPTY != pos) {
-    buffer = REF_TRUE;
-  }
-
-  if (ref_mpi_once(ref_mpi)) {
-    printf("complexity %f\n", complexity);
-    printf("Lp=%d\n", p);
-    printf("gradation %f\n", gradation);
-    printf("reconstruction %d\n", (int)reconstruction);
-    printf("buffer %d (zero is inactive)\n", buffer);
-  }
-
-  ref_mpi_stopwatch_start(ref_mpi);
-
-  if (ref_mpi_para(ref_mpi)) {
-    if (ref_mpi_once(ref_mpi)) printf("part %s\n", in_mesh);
-    RSS(ref_part_by_extension(&ref_grid, ref_mpi, in_mesh), "part");
-    ref_mpi_stopwatch_stop(ref_mpi, "part");
-  } else {
-    if (ref_mpi_once(ref_mpi)) printf("import %s\n", in_mesh);
-    RSS(ref_import_by_extension(&ref_grid, ref_mpi, in_mesh), "import");
-    ref_mpi_stopwatch_stop(ref_mpi, "import");
-  }
-
-  if (ref_mpi_once(ref_mpi)) printf("part scalar %s\n", in_scalar);
-  RSS(ref_part_scalar(ref_grid_node(ref_grid), &ldim, &scalar, in_scalar),
-      "part scalar");
-  REIS(1, ldim, "expected one scalar");
-  ref_mpi_stopwatch_stop(ref_mpi, "part scalar");
-
-  if (ref_mpi_once(ref_mpi)) printf("reconstruct Hessian, compute metric\n");
-  ref_malloc(metric, 6 * ref_node_max(ref_grid_node(ref_grid)), REF_DBL);
-  RSS(ref_metric_lp(metric, ref_grid, scalar, NULL, reconstruction, p,
-                    gradation, complexity),
-      "lp norm");
-  ref_mpi_stopwatch_stop(ref_mpi, "compute metric");
-
-  if (buffer) {
-    if (ref_mpi_once(ref_mpi)) printf("buffer at complexity %e\n", complexity);
-    RSS(ref_metric_buffer_at_complexity(metric, ref_grid, complexity),
-        "buffer at complexity");
-    ref_mpi_stopwatch_stop(ref_mpi, "buffer");
-  }
-
-  RSS(ref_metric_complexity(metric, ref_grid, &current_complexity), "cmp");
-  if (ref_mpi_once(ref_mpi))
-    printf("actual complexity %e\n", current_complexity);
-  RSS(ref_metric_to_node(metric, ref_grid_node(ref_grid)), "set node");
-
-  ref_free(metric);
-  ref_free(scalar);
-
-  if (ref_mpi_once(ref_mpi)) printf("gather %s\n", out_metric);
-  RSS(ref_gather_metric(ref_grid, out_metric), "gather metric");
-  ref_mpi_stopwatch_stop(ref_mpi, "gather metric");
-
-  RSS(ref_grid_free(ref_grid), "free grid");
-
-  return REF_SUCCESS;
-shutdown:
-  if (ref_mpi_once(ref_mpi)) multiscale_help(argv[0]);
-  return REF_FAILURE;
-}
-
-static REF_STATUS translate(REF_MPI ref_mpi, int argc, char *argv[]) {
-  char *out_file;
-  char *in_file;
-  REF_GRID ref_grid = NULL;
-
-  if (argc < 4) goto shutdown;
-  in_file = argv[2];
-  out_file = argv[3];
-
-  ref_mpi_stopwatch_start(ref_mpi);
-
-  if (ref_mpi_para(ref_mpi)) {
-    if (ref_mpi_once(ref_mpi)) printf("part %s\n", in_file);
-    RSS(ref_part_by_extension(&ref_grid, ref_mpi, in_file), "part");
-    ref_mpi_stopwatch_stop(ref_mpi, "part");
-  } else {
-    if (ref_mpi_once(ref_mpi)) printf("import %s\n", in_file);
-    RSS(ref_import_by_extension(&ref_grid, ref_mpi, in_file), "import");
-    ref_mpi_stopwatch_stop(ref_mpi, "import");
-  }
-
-  if (ref_mpi_para(ref_mpi)) {
-    if (ref_mpi_once(ref_mpi)) printf("gather %s\n", out_file);
-    RSS(ref_gather_by_extension(ref_grid, out_file), "gather");
-    ref_mpi_stopwatch_stop(ref_mpi, "gather");
-  } else {
-    if (ref_mpi_once(ref_mpi)) printf("export %s\n", out_file);
-    RSS(ref_export_by_extension(ref_grid, out_file), "export");
-    ref_mpi_stopwatch_stop(ref_mpi, "export");
-  }
-
-  RSS(ref_grid_free(ref_grid), "free grid");
-
-  return REF_SUCCESS;
-shutdown:
-  if (ref_mpi_once(ref_mpi)) translate_help(argv[0]);
-  return REF_FAILURE;
-}
-
-static REF_STATUS whole(REF_MPI ref_mpi, int argc, char *argv[]) {
+static REF_STATUS loop(REF_MPI ref_mpi, int argc, char *argv[]) {
   char *in_project = NULL;
   char *out_project = NULL;
   char filename[1024];
@@ -1009,6 +820,197 @@ shutdown:
   return REF_FAILURE;
 }
 
+static REF_STATUS multiscale(REF_MPI ref_mpi, int argc, char *argv[]) {
+  char *out_metric;
+  char *in_mesh;
+  char *in_scalar;
+  REF_GRID ref_grid = NULL;
+  REF_INT ldim;
+  REF_DBL *scalar, *metric;
+  REF_INT p;
+  REF_DBL gradation, complexity, current_complexity;
+  REF_RECON_RECONSTRUCTION reconstruction = REF_RECON_L2PROJECTION;
+  REF_INT pos;
+  REF_BOOL buffer;
+
+  if (argc < 6) goto shutdown;
+  in_mesh = argv[2];
+  in_scalar = argv[3];
+  complexity = atof(argv[4]);
+  out_metric = argv[5];
+
+  p = 2;
+  RXS(ref_args_find(argc, argv, "-p", &pos), REF_NOT_FOUND, "arg search");
+  if (REF_EMPTY != pos) {
+    if (pos >= argc - 1) {
+      printf("option missing value: -p <norm power>\n");
+      goto shutdown;
+    }
+    p = atoi(argv[pos + 1]);
+  }
+
+  gradation = -1.0;
+  RXS(ref_args_find(argc, argv, "-g", &pos), REF_NOT_FOUND, "arg search");
+  if (REF_EMPTY != pos) {
+    if (pos >= argc - 1) {
+      printf("option missing value: -g <gradation>\n");
+      goto shutdown;
+    }
+    gradation = atof(argv[pos + 1]);
+  }
+
+  buffer = REF_FALSE;
+  RXS(ref_args_find(argc, argv, "--buffer", &pos), REF_NOT_FOUND, "arg search");
+  if (REF_EMPTY != pos) {
+    buffer = REF_TRUE;
+  }
+
+  if (ref_mpi_once(ref_mpi)) {
+    printf("complexity %f\n", complexity);
+    printf("Lp=%d\n", p);
+    printf("gradation %f\n", gradation);
+    printf("reconstruction %d\n", (int)reconstruction);
+    printf("buffer %d (zero is inactive)\n", buffer);
+  }
+
+  ref_mpi_stopwatch_start(ref_mpi);
+
+  if (ref_mpi_para(ref_mpi)) {
+    if (ref_mpi_once(ref_mpi)) printf("part %s\n", in_mesh);
+    RSS(ref_part_by_extension(&ref_grid, ref_mpi, in_mesh), "part");
+    ref_mpi_stopwatch_stop(ref_mpi, "part");
+  } else {
+    if (ref_mpi_once(ref_mpi)) printf("import %s\n", in_mesh);
+    RSS(ref_import_by_extension(&ref_grid, ref_mpi, in_mesh), "import");
+    ref_mpi_stopwatch_stop(ref_mpi, "import");
+  }
+
+  if (ref_mpi_once(ref_mpi)) printf("part scalar %s\n", in_scalar);
+  RSS(ref_part_scalar(ref_grid_node(ref_grid), &ldim, &scalar, in_scalar),
+      "part scalar");
+  REIS(1, ldim, "expected one scalar");
+  ref_mpi_stopwatch_stop(ref_mpi, "part scalar");
+
+  if (ref_mpi_once(ref_mpi)) printf("reconstruct Hessian, compute metric\n");
+  ref_malloc(metric, 6 * ref_node_max(ref_grid_node(ref_grid)), REF_DBL);
+  RSS(ref_metric_lp(metric, ref_grid, scalar, NULL, reconstruction, p,
+                    gradation, complexity),
+      "lp norm");
+  ref_mpi_stopwatch_stop(ref_mpi, "compute metric");
+
+  if (buffer) {
+    if (ref_mpi_once(ref_mpi)) printf("buffer at complexity %e\n", complexity);
+    RSS(ref_metric_buffer_at_complexity(metric, ref_grid, complexity),
+        "buffer at complexity");
+    ref_mpi_stopwatch_stop(ref_mpi, "buffer");
+  }
+
+  RSS(ref_metric_complexity(metric, ref_grid, &current_complexity), "cmp");
+  if (ref_mpi_once(ref_mpi))
+    printf("actual complexity %e\n", current_complexity);
+  RSS(ref_metric_to_node(metric, ref_grid_node(ref_grid)), "set node");
+
+  ref_free(metric);
+  ref_free(scalar);
+
+  if (ref_mpi_once(ref_mpi)) printf("gather %s\n", out_metric);
+  RSS(ref_gather_metric(ref_grid, out_metric), "gather metric");
+  ref_mpi_stopwatch_stop(ref_mpi, "gather metric");
+
+  RSS(ref_grid_free(ref_grid), "free grid");
+
+  return REF_SUCCESS;
+shutdown:
+  if (ref_mpi_once(ref_mpi)) multiscale_help(argv[0]);
+  return REF_FAILURE;
+}
+
+static REF_STATUS surface(REF_MPI ref_mpi, int argc, char *argv[]) {
+  char *out_file;
+  char *in_file;
+  char filename[1024];
+  REF_GRID ref_grid = NULL;
+
+  if (argc < 3) goto shutdown;
+  in_file = argv[2];
+  if (argc < 4) {
+    RAS(strlen(in_file) < 1014, "input filename too long (>1014)");
+    sprintf(filename, "%s-surf.tec", in_file);
+    out_file = filename;
+  } else {
+    out_file = argv[3];
+  }
+
+  ref_mpi_stopwatch_start(ref_mpi);
+
+  if (ref_mpi_para(ref_mpi)) {
+    if (ref_mpi_once(ref_mpi)) printf("part %s\n", in_file);
+    RSS(ref_part_by_extension(&ref_grid, ref_mpi, in_file), "part");
+    ref_mpi_stopwatch_stop(ref_mpi, "part");
+  } else {
+    if (ref_mpi_once(ref_mpi)) printf("import %s\n", in_file);
+    RSS(ref_import_by_extension(&ref_grid, ref_mpi, in_file), "import");
+    ref_mpi_stopwatch_stop(ref_mpi, "import");
+  }
+
+  if (ref_mpi_para(ref_mpi)) {
+    if (ref_mpi_once(ref_mpi)) printf("gather %s\n", out_file);
+    RSS(ref_gather_scalar_surf_tec(ref_grid, 0, NULL, NULL, out_file),
+        "gather surf tec");
+    ref_mpi_stopwatch_stop(ref_mpi, "gather");
+  } else {
+    if (ref_mpi_once(ref_mpi)) printf("export %s\n", out_file);
+    RSS(ref_export_tec_surf(ref_grid, out_file), "export tec surf");
+    ref_mpi_stopwatch_stop(ref_mpi, "export");
+  }
+
+  RSS(ref_grid_free(ref_grid), "free grid");
+
+  return REF_SUCCESS;
+shutdown:
+  if (ref_mpi_once(ref_mpi)) surface_help(argv[0]);
+  return REF_FAILURE;
+}
+
+static REF_STATUS translate(REF_MPI ref_mpi, int argc, char *argv[]) {
+  char *out_file;
+  char *in_file;
+  REF_GRID ref_grid = NULL;
+
+  if (argc < 4) goto shutdown;
+  in_file = argv[2];
+  out_file = argv[3];
+
+  ref_mpi_stopwatch_start(ref_mpi);
+
+  if (ref_mpi_para(ref_mpi)) {
+    if (ref_mpi_once(ref_mpi)) printf("part %s\n", in_file);
+    RSS(ref_part_by_extension(&ref_grid, ref_mpi, in_file), "part");
+    ref_mpi_stopwatch_stop(ref_mpi, "part");
+  } else {
+    if (ref_mpi_once(ref_mpi)) printf("import %s\n", in_file);
+    RSS(ref_import_by_extension(&ref_grid, ref_mpi, in_file), "import");
+    ref_mpi_stopwatch_stop(ref_mpi, "import");
+  }
+
+  if (ref_mpi_para(ref_mpi)) {
+    if (ref_mpi_once(ref_mpi)) printf("gather %s\n", out_file);
+    RSS(ref_gather_by_extension(ref_grid, out_file), "gather");
+    ref_mpi_stopwatch_stop(ref_mpi, "gather");
+  } else {
+    if (ref_mpi_once(ref_mpi)) printf("export %s\n", out_file);
+    RSS(ref_export_by_extension(ref_grid, out_file), "export");
+    ref_mpi_stopwatch_stop(ref_mpi, "export");
+  }
+
+  RSS(ref_grid_free(ref_grid), "free grid");
+
+  return REF_SUCCESS;
+shutdown:
+  if (ref_mpi_once(ref_mpi)) translate_help(argv[0]);
+  return REF_FAILURE;
+}
+
 static void echo_argv(int argc, char *argv[]) {
   int pos;
   printf("\n");
@@ -1071,9 +1073,9 @@ int main(int argc, char *argv[]) {
     }
   } else if (strncmp(argv[1], "l", 1) == 0) {
     if (REF_EMPTY == help_pos) {
-      RSS(location(ref_mpi, argc, argv), "location");
+      RSS(loop(ref_mpi, argc, argv), "loop");
     } else {
-      if (ref_mpi_once(ref_mpi)) location_help(argv[0]);
+      if (ref_mpi_once(ref_mpi)) loop_help(argv[0]);
       goto shutdown;
     }
   } else if (strncmp(argv[1], "m", 1) == 0) {
@@ -1095,13 +1097,6 @@ int main(int argc, char *argv[]) {
       RSS(translate(ref_mpi, argc, argv), "translate");
     } else {
       if (ref_mpi_once(ref_mpi)) translate_help(argv[0]);
-      goto shutdown;
-    }
-  } else if (strncmp(argv[1], "w", 1) == 0) {
-    if (REF_EMPTY == help_pos) {
-      RSS(whole(ref_mpi, argc, argv), "whole");
-    } else {
-      if (ref_mpi_once(ref_mpi)) whole_help(argv[0]);
       goto shutdown;
     }
   } else {
