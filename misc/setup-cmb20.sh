462e4f003d51fb8d96699e1e65e35b053d5c35a1
Mike Park <Mike.Park@NASA.Gov>
2019-09-18 13:11:18 -0400

    close, but no egadslite

--- misc/setup-cmb20.sh
+++ misc/setup-cmb20.sh
@@ -5,6 +5,7 @@ set -x
 ./bootstrap
 
 module_path="/ump/fldmd/home/casb-shared/fun3d/fun3d_users/modules"
+mpi_path="/usr/local/pkgs-modules/openmpi_1.10.2_intel_2017"
 zoltan_path="${module_path}/Zoltan/3.82-1.10.2_intel_2017-2017.2.174"
 egads_path="${module_path}/ESP/114/EngSketchPad"
 opencascade_path="${module_path}/ESP/114/OpenCASCADE-6.8.1/Linux"
@@ -23,6 +24,7 @@ mkdir -p egads
 ( cd egads && \
     ../configure \
     --prefix=`pwd` \
+    --with-mpi=${mpi_path} \
     --with-EGADS=${egads_path} \
     --with-OpenCASCADE=${opencascade_path} \
     CFLAGS='-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -Wuninitialized' \
@@ -34,6 +36,7 @@ mkdir -p parmetis
     ../configure \
     --prefix=`pwd` \
     --with-parmetis=${parmetis_path} \
+    --with-zoltan=${zoltan_path} \
     --with-EGADS=${egads_path} \
     --enable-lite \
     CC=mpicc \
@@ -41,19 +44,18 @@ mkdir -p parmetis
     ) \
     || exit
 
+exit
+
 mkdir -p zoltan
 ( cd zoltan && \
     ../configure \
     --prefix=`pwd` \
-    --with-zoltan=${zoltan_path} \
     --with-EGADS=${egads_path} \
     --enable-lite \
     CC=mpicc \
     CFLAGS='-DHAVE_MPI -g -O2 -traceback -Wall -w3 -wd1418,2259,2547,981,11074,11076,1572,49,1419 -fp-stack-check -fstack-protector-all -fstack-security-check' \
     ) \
     || exit
-
-exit
 
 mkdir -p sanitize
 ( cd sanitize && \
