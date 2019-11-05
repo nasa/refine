cfe46578573dfcddb39f08c9f9c61349968ccb4f
Mike Park <Mike.Park@NASA.gov>
2019-11-05 11:07:15 -0500

    intersection tests

--- src/ref_dist_test.c
+++ src/ref_dist_test.c
@@ -124,6 +124,56 @@ int main(int argc, char *argv[]) {
     RSS(ref_grid_free(ref_grid), "free");
   }
 
+  {
+    REF_GRID ref_grid;
+    REF_NODE ref_node;
+    REF_CELL ref_cell;
+
+    REF_INT global, node, n;
+    REF_INT cell, nodes[REF_CELL_MAX_SIZE_PER];
+
+    RSS(ref_grid_create(&ref_grid, ref_mpi), "create");
+    ref_node = ref_grid_node(ref_grid);
+    ref_cell = ref_grid_tri(ref_grid);
+    RSS(ref_grid_create(&ref_grid, ref_mpi), "create");
+    ref_node = ref_grid_node(ref_grid);
+    ref_cell = ref_grid_tri(ref_grid);
+    global = 0;
+    RSS(ref_node_add(ref_node, global, &node), "node");
+    ref_node_xyz(ref_node, 0, node) = 0.0;
+    ref_node_xyz(ref_node, 1, node) = 0.0;
+    ref_node_xyz(ref_node, 2, node) = 0.0;
+    global = 1;
+    RSS(ref_node_add(ref_node, global, &node), "node");
+    ref_node_xyz(ref_node, 0, node) = 1.0;
+    ref_node_xyz(ref_node, 1, node) = 0.0;
+    ref_node_xyz(ref_node, 2, node) = 0.0;
+    global = 2;
+    RSS(ref_node_add(ref_node, global, &node), "node");
+    ref_node_xyz(ref_node, 0, node) = 0.0;
+    ref_node_xyz(ref_node, 1, node) = 1.0;
+    ref_node_xyz(ref_node, 2, node) = 0.0;
+    nodes[0] = 0;
+    nodes[1] = 1;
+    nodes[2] = 2;
+    nodes[3] = 10;
+    RSS(ref_cell_add(ref_cell, nodes, &cell), "tri");
+    global = 3;
+    RSS(ref_node_add(ref_node, global, &node), "node");
+    ref_node_xyz(ref_node, 0, node) = 1.0;
+    ref_node_xyz(ref_node, 1, node) = 1.0;
+    ref_node_xyz(ref_node, 2, node) = 0.0;
+    nodes[0] = 2;
+    nodes[1] = 1;
+    nodes[2] = 3;
+    nodes[3] = 10;
+    RSS(ref_cell_add(ref_cell, nodes, &cell), "tri");
+
+    RSS(ref_dist_collisions(ref_grid, REF_FALSE, &n), "collisions");
+    REIS(0, n, "no collisions expected, adjacent");
+    RSS(ref_grid_free(ref_grid), "free");
+  }
+
   RSS(ref_mpi_free(ref_mpi), "free");
   RSS(ref_mpi_stop(), "stop");
   return 0;
