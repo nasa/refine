e3c9b4f93bf68c288019dd414c3fa4c7ea978e17
Mike Park <Mike.Park@NASA.gov>
2019-11-16 21:29:36 -0500

    cleaned

--- acceptance/2d/masabl/accept-2d-masabl.sh
+++ acceptance/2d/masabl/accept-2d-masabl.sh
@@ -12,23 +12,23 @@ else
     two=${HOME}/refine/strict/src
 fi
 
-${two}/ref_acceptance 2 ref_adapt_test.b8.ugrid
+${two}/ref_acceptance 2 ref_adapt_test.meshb
 
 function adapt_cycle {
     proj=$1
 
-    cp ref_adapt_test.b8.ugrid ${proj}.b8.ugrid
+    cp ref_adapt_test.meshb ${proj}.meshb
 
-    ${two}/ref_translate ${proj}.b8.ugrid ${proj}.html
-    ${two}/ref_translate ${proj}.b8.ugrid ${proj}.tec
+    ${two}/ref_translate ${proj}.meshb ${proj}.html
+    ${two}/ref_translate ${proj}.meshb ${proj}.tec
 
-    ${two}/ref_acceptance ${proj}.b8.ugrid ${proj}.metric -masabl
-    ${two}/ref_driver -i ${proj}.b8.ugrid -m ${proj}.metric -o ref_adapt_test -t | tee ${proj}.out || exit 1
+    ${two}/ref_acceptance ${proj}.meshb ${proj}.metric -masabl
+    ${two}/ref_driver -i ${proj}.meshb -m ${proj}.metric -x ref_adapt_test.meshb -t | tee ${proj}.out || exit 1
 
     cp ref_gather_movie.tec ${proj}_movie.tec
     cp ref_gather_histo.tec ${proj}_histo.tec
 
-    ${two}/ref_metric_test ${proj}.b8.ugrid ${proj}.metric > ${proj}.status
+    ${two}/ref_metric_test ${proj}.meshb ${proj}.metric > ${proj}.status
     cp ref_metric_test_s00_n1_p0_ellipse.tec ${proj}_metric_ellipse.tec
 }
 
