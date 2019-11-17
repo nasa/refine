8fc23421e7e6c489a29409298c45fc08027e9b2d
Mike Park <Mike.Park@NASA.gov>
2019-11-17 03:01:24 -0500

    cleaned up

--- acceptance/2d/radial-1/accept-2d-radial-1.sh
+++ acceptance/2d/radial-1/accept-2d-radial-1.sh
@@ -12,31 +12,37 @@ else
     two=${HOME}/refine/strict/src
 fi
 
-${two}/ref_acceptance 2 ref_adapt_test.b8.ugrid
+tecplot=-t
+metric="-twod radial-1"
 
 function adapt_cycle {
-    proj=$1
+    inproj=$1
+    outproj=$2
+    sweeps=$3
 
-    cp ref_adapt_test.b8.ugrid ${proj}.b8.ugrid
+    ${two}/ref_acceptance ${metric} ${inproj}.meshb \
+	  ${inproj}.solb
 
-    ${two}/ref_translate ${proj}.b8.ugrid ${proj}.tec
+    ${two}/ref_driver -i ${inproj}.meshb -m ${inproj}.solb \
+          -x ${outproj}.meshb \
+	  -s ${sweeps} ${tecplot}
 
-    ${two}/ref_acceptance -twod radial-1 ${proj}.b8.ugrid ${proj}.solb
-    ${two}/ref_driver -i ${proj}.b8.ugrid -m ${proj}.solb -x ref_adapt_test.b8.ugrid -t | tee ${proj}.out || exit 1
-
-    cp ref_gather_movie.tec ${proj}_movie.tec
-    cp ref_gather_histo.tec ${proj}_histo.tec
-
-    ${two}/ref_metric_test ${proj}.b8.ugrid ${proj}.solb > ${proj}.status
-    cp ref_metric_test_s00_n1_p0_ellipse.tec ${proj}_metric_ellipse.tec
+    mv ref_gather_histo.tec ${outproj}_histo.tec
+    mv ref_gather_movie.tec ${outproj}_movie.tec
+    ${two}/ref_acceptance ${metric} ${outproj}.meshb \
+	  ${outproj}.solb
+    ${two}/ref_metric_test ${outproj}.meshb ${outproj}.solb \
+	  > ${outproj}.status
 }
 
-adapt_cycle accept-2d-radial-1-00
-adapt_cycle accept-2d-radial-1-01
-adapt_cycle accept-2d-radial-1-02
-adapt_cycle accept-2d-radial-1-03
-adapt_cycle accept-2d-radial-1-04
+inproj=cycle00
+${two}/ref_acceptance 2 cycle00.meshb
 
-cat accept-2d-radial-1-04.status
-../../check.rb accept-2d-radial-1-04.status 0.40 1.6
+adapt_cycle cycle00 cycle01 10
+adapt_cycle cycle01 cycle02 10
+adapt_cycle cycle02 cycle03 10
+adapt_cycle cycle03 cycle04 10
+
+cat cycle04.status
+../../check.rb cycle04.status 0.25 1.6
 
