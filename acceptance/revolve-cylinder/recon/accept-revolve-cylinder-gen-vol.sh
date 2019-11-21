19d600916495192cfd7962df1ac91e71818b4594
Mike Park <Mike.Park@NASA.gov>
2019-11-21 09:51:46 +0900

    boostrap

--- acceptance/revolve-cylinder/recon/accept-revolve-cylinder-gen-vol.sh
+++ acceptance/revolve-cylinder/recon/accept-revolve-cylinder-gen-vol.sh
@@ -5,15 +5,13 @@ set -e # exit on first error
 set -u # Treat unset variables as error
 
 if [ $# -gt 0 ] ; then
-    one=$1/one
-    two=$1/src
+    src=$1/src
 else
-    one=${HOME}/refine/egads/one
-    two=${HOME}/refine/egads/src
+    src=${HOME}/refine/egads/src
 fi
 
 geomfile=cylinder.egads
 
-${two}/ref_geom_test --tetgen ${geomfile} cylinder-vol.meshb 1 1 90 \
+${src}/ref boostrap ${geomfile} cylinder-vol.meshb \
     | tee accept-revolve-cylinder-gen-vol.status
 
