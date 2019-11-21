656df3668bde5166d3ac8e425c82b07c6b45c67a
Mike Park <Mike.Park@NASA.gov>
2019-11-21 09:57:07 +0900

    boostrap

--- acceptance/skinny-cylinder/surf/accept-skinny-cylinder-surf-adapt.sh
+++ acceptance/skinny-cylinder/surf/accept-skinny-cylinder-surf-adapt.sh
@@ -5,16 +5,10 @@ set -e # exit on first error
 set -u # Treat unset variables as error
 
 if [ $# -gt 0 ] ; then
-    one=$1/one
-    two=$1/src
+    src=$1/src
 else
-    one=${HOME}/refine/egads/one
-    two=${HOME}/refine/egads/src
+    src=${HOME}/refine/egads/src
 fi
 
-geomfile=skinny-cylinder.egads
-
-${two}/ref_geom_test --tetgen skinny-cylinder.egads skinny-cylinder.meshb \
-      1 0.1 15.0 \
-      --surf
+${src}/ref bootstrap skinny-cylinder.egads
 
