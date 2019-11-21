54b2fbb4ffd3a909187469658c33602f86bd914e
Mike Park <Mike.Park@NASA.gov>
2019-11-21 10:52:26 +0900

    sync meshb and egads and egadslite

--- acceptance/hemisphere/uniform/accept-hemisphere-uniform-para.sh
+++ acceptance/hemisphere/uniform/accept-hemisphere-uniform-para.sh
@@ -5,11 +5,9 @@ set -e # exit on first error
 set -u # Treat unset variables as error
 
 if [ $# -gt 0 ] ; then
-    one=$1/one
-    two=$1/src
+    src=$1/src
 else
-    one=${HOME}/refine/zoltan/one
-    two=${HOME}/refine/zoltan/src
+    src=${HOME}/refine/parmetis/src
 fi
 
 geomfile=hemisphere.egads
@@ -18,14 +16,14 @@ geomfile=hemisphere.egads
 # ref_geom_test hemisphere.egads hemisphere.meshb
 # ref_driver -i hemisphere.meshb -g hemisphere.egads -r 4 -o hemicurve
 
-${two}/ref_acceptance hemicurve.meshb hemicurve-metric.solb 0.1
-mpiexec -np 4 ${two}/ref_driver -i hemicurve.meshb -g ${geomfile} -m hemicurve-metric.solb -o hemicurve1 -r 1 -s 20
-${two}/ref_acceptance hemicurve1.meshb hemicurve1-metric.solb 0.1
-${two}/ref_metric_test hemicurve1.meshb hemicurve1-metric.solb > accept-hemisphere-uniform-para-01.status
+${src}/ref_acceptance hemicurve.meshb hemicurve-metric.solb 0.1
+mpiexec -np 4 ${src}/ref_driver -i hemicurve.meshb -g ${geomfile} -m hemicurve-metric.solb -o hemicurve1 -r 1 -s 20
+${src}/ref_acceptance hemicurve1.meshb hemicurve1-metric.solb 0.1
+${src}/ref_metric_test hemicurve1.meshb hemicurve1-metric.solb > accept-hemisphere-uniform-para-01.status
 
 cat accept-hemisphere-uniform-para-01.status
 ../../check.rb accept-hemisphere-uniform-para-01.status 0.1 6.0
 
-mpiexec -np 8 ${two}/ref_interp_test hemicurve.meshb hemicurve1.meshb
-mpiexec -np 8 ${two}/ref_interp_test hemicurve1.meshb hemicurve.meshb
+mpiexec -np 8 ${src}/ref_interp_test hemicurve.meshb hemicurve1.meshb
+mpiexec -np 8 ${src}/ref_interp_test hemicurve1.meshb hemicurve.meshb
 
--- acceptance/hemisphere/uniform/generate.sh
+++ acceptance/hemisphere/uniform/generate.sh
@@ -7,3 +7,6 @@ set -u # Treat unset variables as error
 serveCSM -batch hemisphere.csm
 
 ref bootstrap hemisphere.egads
+
+ref adapt hemisphere-vol.meshb -g hemisphere.egads -x hemicurve.meshb
+
