0759efe01e99723fb6d2e89da30fc523a80a5a6b
Mike Park <Mike.Park@NASA.gov>
2019-11-30 08:35:04 -0500

    all 116

--- acceptance/c2s-modules.sh
+++ acceptance/c2s-modules.sh
@@ -9,5 +9,5 @@ module load tetgen/1.5.0
 
 module list
 
-export egads_path="/u/shared/fun3d/fun3d_users/modules/ESP/114/EngSketchPad"
+export egads_path="/u/shared/fun3d/fun3d_users/modules/ESP/116/EngSketchPad"
 export parmetis_path="/u/shared/fun3d/fun3d_users/modules/ParMETIS/4.0.3-mpt-2.17r14-gcc_6.2.0"
