e5ea6fe016f3ffb4086dbfc924484ad73ac0ccb8
Mike Park <Mike.Park@NASA.gov>
2019-12-19 06:20:52 -0500

    reports state verbosely on diag fail

--- src/ref_matrix.c
+++ src/ref_matrix.c
@@ -507,7 +507,16 @@ REF_STATUS ref_matrix_intersect(REF_DBL *m1, REF_DBL *m2, REF_DBL *m12) {
   }
   RSS(sqrt_m1_status, "sqrt_m m1");
   RSS(ref_matrix_mult_m0m1m0(m1neghalf, m2, m2bar), "m2bar=m1half*m2*m1half");
-  RSS(ref_matrix_diag_m(m2bar, m12bar_system), "diag m12bar");
+  RSB(ref_matrix_diag_m(m2bar, m12bar_system), "diag m12bar", {
+    printf("m1\n");
+    ref_matrix_show_m(m1);
+    printf("m2\n");
+    ref_matrix_show_m(m2);
+    printf("m2bar\n");
+    ref_matrix_show_m(m2bar);
+    printf("m1neghalf\n");
+    ref_matrix_show_m(m1neghalf);
+  });
   ref_matrix_eig(m12bar_system, 0) = MAX(1.0, ref_matrix_eig(m12bar_system, 0));
   ref_matrix_eig(m12bar_system, 1) = MAX(1.0, ref_matrix_eig(m12bar_system, 1));
   ref_matrix_eig(m12bar_system, 2) = MAX(1.0, ref_matrix_eig(m12bar_system, 2));
