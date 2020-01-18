8c091509c70173b9af892af8722181ec6f955e54
Mike Park <Mike.Park@NASA.gov>
2020-01-18 14:14:03 -0500

    setup spalding law-of-wall interp error control acceptance

---
+++ acceptance/facebody/spalding/accept-spalding.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -x # echo commands
+set -e # exit on first error
+set -u # Treat unset variables as error
+
+if [ $# -gt 0 ] ; then
+    src=$1/src
+else
+    src=${HOME}/refine/egads/src
+fi
+
+tecplot=-t
+field="-u uplus100"
+egads="-g square.egads"
+
+function adapt_cycle {
+    inproj=$1
+    outproj=$2
+
+    ${src}/ref_acceptance ${field} ${inproj}.meshb \
+	  ${inproj}.solb
+
+    ${src}/ref multiscale ${inproj}.meshb ${inproj}.solb \
+	  1000 ${inproj}-metric.solb
+
+    ${src}/ref adapt ${inproj}.meshb ${egads} -m ${inproj}-metric.solb \
+	  -x ${outproj}.meshb -f ${outproj}.tec
+
+    ${src}/ref_acceptance ${field} ${outproj}.meshb \
+	  ${outproj}.solb
+}
+
+cp square.meshb cycle00.meshb
+
+adapt_cycle cycle00 cycle01
+adapt_cycle cycle01 cycle02
+adapt_cycle cycle02 cycle03
+adapt_cycle cycle03 cycle04
+adapt_cycle cycle04 cycle05
+adapt_cycle cycle05 cycle06
+
+
