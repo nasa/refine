c9a635b1ab5a83d2583e1a627890b674c5efeb3e
Mike Park <Mike.Park@NASA.gov>
2020-01-31 15:10:50 -0500

    triger-build

--- acceptance/boot-modules.sh
+++ acceptance/boot-modules.sh
@@ -10,4 +10,3 @@ module load tetgen/1.5.0
 export egads_path="/u/shared/fun3d/fun3d_users/modules/ESP/117-rc.1/EngSketchPad"
 export parmetis_path="/u/shared/fun3d/fun3d_users/modules/ParMETIS/4.0.3-mpt-2.17r14-gcc_6.2.0"
 export mpi_path="/opt/hpe/hpc/mpt/mpt-2.19"
-
