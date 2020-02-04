6860a5c102e03c79b4959ec6607ae704d60e44b4
Mike Park <Mike.Park@NASA.gov>
2020-02-03 20:58:54 -0500

    trigger-build

--- acceptance/boot-modules.sh
+++ acceptance/boot-modules.sh
@@ -15,3 +15,4 @@ export parmetis_path="${module_path}/ParMETIS/4.0.3-mpt-2.17r14-gcc_6.2.0"
 
 export egads_path="${module_path}/ESP/117-rc.1/EngSketchPad"
 export opencascade_path="${module_path}/ESP/117/OpenCASCADE-7.3.1/lib"
+
