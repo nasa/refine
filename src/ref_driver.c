43ba4d0928075f87fb996f3e9774e85f2735bbe4
Mike Park <Mike.Park@NASA.gov>
2020-02-10 08:41:33 -0500

    versions

--- src/ref_driver.c
+++ src/ref_driver.c
@@ -88,7 +88,7 @@ int main(int argc, char *argv[]) {
   output_project[0] = '\0';
 
   if (ref_mpi_once(ref_mpi)) {
-    printf("version %s, on or after 1.9.0\n", VERSION);
+    printf("version %s, on or after 1.9.1\n", VERSION);
     echo_argv(argc, argv);
   }
 
