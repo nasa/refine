1be2ac4d05341b21566778d3230b49592bb7fbf6
Mike Park <Mike.Park@NASA.gov>
2020-02-10 08:50:34 -0500

    pfe-env

--- misc/setup-pfe.sh
+++ misc/setup-pfe.sh
@@ -4,11 +4,13 @@ set -x
 
 ./bootstrap
 
-module_path="/u/fun3d/shared/n1337/modules"
+module_path="/swbuild/fun3d/shared/fun3d_users/modules"
 zoltan_path="${module_path}/Zoltan/3.82_mpt-2.17r13_ifort-2018.3.222"
 parmetis_path="${module_path}/ParMETIS/4.0.3_mpt-2.17r13_ifort-2018.3.222"
-egads_path="${module_path}/ESP/114/EngSketchPad"
-opencascade_path="${module_path}/ESP/114/OpenCASCADE-6.8.1/Linux"
+egads_path="${module_path}/ESP/117/EngSketchPad"
+opencascade_path="${module_path}/ESP/117/OpenCASCADE-7.3.1"
+
+mpi_path="/nasa/hpe/mpt/2.17r13"
 
 gcc_flags="-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -Wuninitialized"
 
@@ -25,7 +27,9 @@ mkdir -p egads
     ../configure \
     --prefix=`pwd` \
     --with-EGADS=${egads_path} \
-    --with-OpenCASCADE=${opencascade_path} \
+    --with-OpenCASCADE=${occ_path} \
+    --with-mpi=${mpi_path} \
+    --with-parmetis=${parmetis_path} \
     CFLAGS="${gcc_flags}" \
     ) \
     || exit
