df1b422ae1f86cea418b2f599959c7cc87fc16eb
Mike Park <Mike.Park@NASA.gov>
2020-02-23 04:29:50 -0500

    merge differences, add ref_collapse_edge_tet_quality

--- src/ref_collapse.c
+++ src/ref_collapse.c
@@ -1388,6 +1388,10 @@ REF_STATUS ref_collapse_face_remove_node1(REF_GRID ref_grid,
     if (!allowed && verbose) printf("%d qual\n", node);
     if (!allowed) continue;
 
+    RSS(ref_collapse_edge_tet_quality(ref_grid, node0, node1, &allowed),
+        "tet qual");
+    if (!allowed && verbose) printf("   tet qual\n");
+
     RSS(ref_collapse_face_local_pris(ref_grid, node0, node1, &allowed),
         "colloc");
     if (!allowed && verbose) printf("%d loca\n", node);
