33c792292bfa97ace0f36e32c2bddd335c9c180c
Mike Park <Mike.Park@NASA.gov>
2020-02-23 05:24:36 -0500

    not hooked up, para 2d accept

---
+++ acceptance/2d/masabl/accept-2d-masabl-para.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+set -x # echo commands
+set -e # exit on first error
+set -u # Treat unset variables as error
+
+if [ $# -gt 0 ] ; then
+    one=$1/one
+    two=$1/src
+else
+    one=${HOME}/refine/parmetis/one
+    two=${HOME}/refine/parmetis/src
+fi
+
+tecplot=-t
+metric="-twod masabl-1"
+
+function adapt_cycle {
+    inproj=$1
+    outproj=$2
+    sweeps=$3
+
+    ${two}/ref_acceptance ${metric} ${inproj}.meshb \
+	  ${inproj}.solb
+
+    mpiexec -np 4 ${two}/ref_driver -i ${inproj}.meshb -m ${inproj}.solb \
+          -x ${outproj}.meshb \
+	  -s ${sweeps} ${tecplot}
+
+    mv ref_gather_histo.tec ${outproj}_histo.tec
+    mv ref_gather_movie.tec ${outproj}_movie.tec
+    ${two}/ref_acceptance ${metric} ${outproj}.meshb \
+	  ${outproj}.solb
+    ${two}/ref_metric_test ${outproj}.meshb ${outproj}.solb \
+	  > ${outproj}.status
+}
+
+inproj=para00
+${two}/ref_acceptance 2 para00.meshb
+
+adapt_cycle para00 para01 10
+adapt_cycle para01 para02 10
+adapt_cycle para02 para03 10
+
+cat para03.status
+../../check.rb para03.status 0.40 1.6
+
