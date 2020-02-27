c3c9e9af52dea3a9a0200e316b67fe3de85a77d8
Mike Park <Mike.Park@NASA.gov>
2020-02-27 13:18:22 -0500

    ref needs path

--- acceptance/bite/coarse/accept-bite-coarse.sh
+++ acceptance/bite/coarse/accept-bite-coarse.sh
@@ -12,18 +12,13 @@ fi
 
 serveCSM -batch bite.csm \
 	 > accept-bite-coarse-csm.txt
-ref bootstrap bite.egads --global 10000 10000 90 -t \
+${src}/ref bootstrap bite.egads --global 10000 10000 90 -t \
     > accept-bite-coarse-bootstrap.txt
 
-grep 'topo angle' accept-bite-coarse-bootstrap.txt
-grep 'faceid near' accept-bite-coarse-bootstrap.txt
-
-exit 0
-
-ref adapt bite-vol.meshb -g bite.egads -f bite-final.tec \
+${src}/ref adapt bite-vol.meshb -g bite.egads -f bite-final.tec \
     > accept-bite-coarse-status.txt
 
-cat accept-bite-coarse-status.txt
+${src}/cat accept-bite-coarse-status.txt
 ../../check.rb accept-bite-coarse-status.txt 0.3 3.0
 
 
