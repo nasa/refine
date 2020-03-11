56bb40fea85ab3e0de9294dbda4b28769f3f592b
Mike Park <Mike.Park@NASA.gov>
2020-03-11 07:13:04 -0400

    lays

---
+++ acceptance/3d/static/accept-3d-static.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -x # echo commands
+set -e # exit on first error
+set -u # Treat unset variables as error
+
+if [ $# -gt 0 ] ; then
+    src=$1/src
+else
+    src=${HOME}/refine/egads/src
+fi
+
+function adapt_cycle {
+    inproj=$1
+    outproj=$2
+    sweeps=$3
+    complexity=2000
+
+    ${src}/ref_acceptance -u u5 ${inproj}.meshb ${inproj}.solb
+    ${src}/ref_metric_test --lp ${inproj}.meshb ${inproj}.solb 2 -1 ${complexity}  ${inproj}-metric.meshb
+    ${src}/ref_driver -i ${inproj}.meshb -m ${inproj}-metric.meshb -x ${outproj}.meshb -s ${sweeps}
+    ${src}/ref_acceptance -u u5 ${outproj}.meshb ${outproj}.solb
+    ${src}/ref_gather_test ${outproj}.meshb ${outproj}.solb ${outproj}.tec
+}
+
+${src}/ref_acceptance 1 cycle00.meshb
+
+adapt_cycle cycle00 cycle01 10
+adapt_cycle cycle01 cycle02 10
+adapt_cycle cycle02 cycle03 10
+adapt_cycle cycle03 cycle04 10
+
+
+#cat cycle02.status
+#../../check.rb cycle02.status 0.3 3.0
+
+exit
