a3e83196d1c98d9cd2427dfb19a4a9bed4578f63
Mike Park <Mike.Park@NASA.gov>
2020-03-16 12:49:46 -0400

    para interp --face too

--- acceptance/3d/subset/accept-3d-subset-para.sh
+++ acceptance/3d/subset/accept-3d-subset-para.sh
@@ -13,17 +13,20 @@ fi
 ${src}/ref_acceptance 1 whole.meshb
 ${src}/ref_acceptance -u u5 whole.meshb whole.solb
 
-${src}/ref_translate whole.meshb target.meshb --scale 0.5 --shift 0.25 0.25 0.25
+${src}/ref_translate whole.meshb target.meshb \
+      --scale 0.5 --shift 0.25 0.25 0.25
 ${src}/ref_acceptance -u u5 target.meshb target.solb
 
-mpiexec -np 4 ${src}/ref_gather_test --subset whole.meshb whole.solb 0.24 0.2 0.2 0.02 0.6 0.6 subset.meshb subset.solb
+mpiexec -np 4 ${src}/ref_gather_test --subset whole.meshb whole.solb \
+	0.24 0.2 0.2 0.02 0.6 0.6 \
+	subset.meshb subset.solb
 ${src}/ref_gather_test whole.meshb whole.solb whole.tec
 ${src}/ref_gather_test subset.meshb subset.solb subset.tec
 
 ${src}/ref translate subset.meshb subset.lb8.ugrid
 ${src}/ref translate subset.meshb subset-trans.tec
 
-${src}/ref_interp_test --face subset.meshb subset.solb \
+mpiexec -np 4 ${src}/ref_interp_test --face subset.meshb subset.solb \
       target.meshb target.solb target_merged.solb 1 
 
 ${src}/ref_gather_test target.meshb \
