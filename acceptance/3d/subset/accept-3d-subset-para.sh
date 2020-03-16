5f8f286a00ad233ed28d5a7276a3bd9423c38f23
Mike Park <Mike.Park@NASA.gov>
2020-03-16 11:39:23 -0400

    gather all members of a bounding box intersection

--- acceptance/3d/subset/accept-3d-subset-para.sh
+++ acceptance/3d/subset/accept-3d-subset-para.sh
@@ -13,7 +13,7 @@ fi
 ${src}/ref_acceptance 1 whole.meshb
 ${src}/ref_acceptance -u u5 whole.meshb whole.solb
 
-mpiexec -np 4 ${src}/ref_gather_test --subset whole.meshb whole.solb -1 -1 -1 1.4 1.6 1.8 subset.meshb subset.solb
+mpiexec -np 4 ${src}/ref_gather_test --subset whole.meshb whole.solb 0.5 0.5 0.5 0 0 0 subset.meshb subset.solb
 ${src}/ref_gather_test whole.meshb whole.solb whole.tec
 ${src}/ref_gather_test subset.meshb subset.solb subset.tec
 
