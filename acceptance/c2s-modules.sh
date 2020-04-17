cf6cb80c4a8393594b60132ddf4a41aacf900b37
Mike Park <Mike.Park@NASA.gov>
2020-04-17 17:20:58 -0400

    acceptance test with ESP/118-rc.3

--- acceptance/c2s-modules.sh
+++ acceptance/c2s-modules.sh
@@ -4,7 +4,7 @@ module use --append /u/shared/fun3d/fun3d_users/modulefiles
 module load FUN3D/13.6
 
 module load gcc_6.2.0
-module load ESP/118-rc.2
+module load ESP/118-rc.3
 module load tetgen/1.5.0
 
 module list
@@ -15,6 +15,6 @@ export mpi_path="/opt/hpe/hpc/mpt/mpt-2.19"
 
 export parmetis_path="${module_path}/ParMETIS/4.0.3-mpt-2.17r14-gcc_6.2.0"
 
-export egads_path="${module_path}/ESP/118-rc.2/EngSketchPad"
+export egads_path="${module_path}/ESP/118-rc.3/EngSketchPad"
 export opencascade_path="${module_path}/ESP/117/OpenCASCADE-7.3.1"
 
