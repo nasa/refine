f836bc1a348ae70076dc354c7a4c425239766df8
Mike Park <Mike.Park@NASA.Gov>
2020-04-21 17:01:25 -0400

    lastest ESP beta on k

--- misc/setup-k.sh
+++ misc/setup-k.sh
@@ -7,8 +7,8 @@ set -x
 module_path="/u/shared/fun3d/fun3d_users/modules"
 zoltan_path="${module_path}/Zoltan/3.82-mpt-2.19-intel_2018.3.222"
 parmetis_path="${module_path}/ParMETIS/4.0.3-mpt-2.19-intel_2018.3.222"
-egads_path="${module_path}/ESP/117/EngSketchPad"
-occ_path="${module_path}/ESP/117/OpenCASCADE-7.3.1"
+egads_path="${module_path}/ESP/118-rc.3/EngSketchPad"
+occ_path="${module_path}/ESP/118-rc.3/OpenCASCADE-7.3.1"
 
 mpi_path="/opt/hpe/hpc/mpt/mpt-2.19"
 
