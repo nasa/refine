6f84cd2a8d188869f8964b9df4ada068ef55dc09
Mike Park <Mike.Park@NASA.gov>
2020-04-27 04:35:40 -0400

    unused

--- src/ref_export.h
+++ src/ref_export.h
@@ -36,10 +36,6 @@ BEGIN_C_DECLORATION
 
 REF_STATUS ref_export_tec_surf(REF_GRID ref_grid, const char *filename);
 
-REF_STATUS ref_export_tec_int(REF_GRID ref_grid, REF_INT *scalar,
-                              const char *filename);
-
-REF_STATUS ref_export_tec_part(REF_GRID ref_grid, const char *root_filename);
 REF_STATUS ref_export_metric_xyzdirlen(REF_GRID ref_grid, const char *filename);
 REF_STATUS ref_export_tec_metric_axis(REF_GRID ref_grid,
                                       const char *root_filename);
