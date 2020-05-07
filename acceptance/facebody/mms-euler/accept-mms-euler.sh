96ffa32a5f78393d20f42a1ac70e94b5c3db4649
Mike Park <Mike.Park@NASA.gov>
2020-05-07 06:18:40 -0400

    restricts --entropyadj to adj, use ref_phys_test --euler-flux for fluxes

--- acceptance/facebody/mms-euler/accept-mms-euler.sh
+++ acceptance/facebody/mms-euler/accept-mms-euler.sh
@@ -22,7 +22,10 @@ function adapt_cycle {
 	  ${inproj}-primal.solb
 
     ${src}/ref_interp_test --entropyadj ${inproj}.meshb \
-	  ${inproj}-primal.solb ${inproj}-adjflux.solb
+	  ${inproj}-primal.solb ${inproj}-primdual.solb
+
+    ${src}/ref_phys_test --euler-flux ${inproj}.meshb \
+	  ${inproj}-primdual.solb ${inproj}-adjflux.solb
 
     ${src}/ref_metric_test --opt-goal ${inproj}.meshb \
 	  ${inproj}-adjflux.solb 1 -1 1000 ${inproj}-metric.solb
@@ -34,12 +37,10 @@ function adapt_cycle {
 	  ${outproj}-primal.solb
 
     ${src}/ref_interp_test --entropyadj ${outproj}.meshb \
-	  ${outproj}-primal.solb ${outproj}-adjflux.solb
+	  ${outproj}-primal.solb ${outproj}-primdual.solb
 
-    ${src}/ref_gather_test ${outproj}.meshb ${outproj}-primal.solb \
-	  ${outproj}-primal.tec
-    ${src}/ref_gather_test ${outproj}.meshb ${outproj}-adjflux.solb \
-	  ${outproj}-adjflux.tec
+    ${src}/ref_gather_test ${outproj}.meshb ${outproj}-primdual.solb \
+	  ${outproj}-primdual.tec
 }
 
 ./square.sh
