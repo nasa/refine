492bc97f77c2f01ccf522dccda25a7b74d08ee2b
Mike Park <Mike.Park@NASA.gov>
2020-05-29 03:18:05 -0400

    format

--- util/assert_mpi_min_loc.c
+++ util/assert_mpi_min_loc.c
@@ -1,46 +1,41 @@
 /* mpicc -g -o assert_mpi_min_loc assert_mpi_min_loc.c */
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "mpi.h"
-int main( int argc, char *argv[] )
-{
+int main(int argc, char *argv[]) {
   int i, n = 1403;
   int nproc, rank;
   typedef struct MY_MPI_DBLWHO MY_MPI_DBLWHO;
-  struct MY_MPI_DBLWHO { 
+  struct MY_MPI_DBLWHO {
     double val;
-    int   rank;
+    int rank;
   };
   MY_MPI_DBLWHO *in, *out;
 
-  if ( argc > 1 )
-    {
-       n = atoi(argv[1]);
-    }	
+  if (argc > 1) {
+    n = atoi(argv[1]);
+  }
 
-  MPI_Init(&argc,&argv);
-  MPI_Comm_size(MPI_COMM_WORLD,&nproc);
-  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  in = (MY_MPI_DBLWHO *)malloc(n*sizeof(MY_MPI_DBLWHO));
-  out = (MY_MPI_DBLWHO *)malloc(n*sizeof(MY_MPI_DBLWHO));
+  in = (MY_MPI_DBLWHO *)malloc(n * sizeof(MY_MPI_DBLWHO));
+  out = (MY_MPI_DBLWHO *)malloc(n * sizeof(MY_MPI_DBLWHO));
 
-  for (i=0; i<n; i++) 
-    { 
-      in[i].val = (double)rank; 
-      in[i].rank = rank; 
-    }
-  if ( 0 == rank )
+  for (i = 0; i < n; i++) {
+    in[i].val = (double)rank;
+    in[i].rank = rank;
+  }
+  if (0 == rank)
     printf("calling MPI_Allreduce( %d, MPI_DOUBLE_INT, MPI_MINLOC) with %d\n",
-	   n, nproc);
-  MPI_Allreduce( in, out, n, MPI_DOUBLE_INT, MPI_MINLOC, 
-		 MPI_COMM_WORLD );
-  if ( 0 == rank )
-    printf("MPI_Allreduce returned\n");
+           n, nproc);
+  MPI_Allreduce(in, out, n, MPI_DOUBLE_INT, MPI_MINLOC, MPI_COMM_WORLD);
+  if (0 == rank) printf("MPI_Allreduce returned\n");
 
-  free( out );
-  free( in );
-  
-  MPI_Finalize( );
+  free(out);
+  free(in);
+
+  MPI_Finalize();
   return 0;
 }
--- util/assert_mpi_reduce.c
+++ util/assert_mpi_reduce.c
@@ -1,38 +1,35 @@
 /* mpicc -g -o assert_mpi_reduce assert_mpi_reduce.c */
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "mpi.h"
-int main( int argc, char *argv[] )
-{
+int main(int argc, char *argv[]) {
   int n = 10;
   int nproc, rank;
   double *in, *out;
 
-  if ( argc > 1 ) n = atoi(argv[1]);
-    	
-  MPI_Init(&argc,&argv);
-  MPI_Comm_size(MPI_COMM_WORLD,&nproc);
-  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  if (argc > 1) n = atoi(argv[1]);
 
-  in = (double *)malloc(n*sizeof(double));
-  out = (double *)malloc(n*sizeof(double));
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  for (i=0; i<n; i++) in[i] = 0;
-  if ( 0 == rank ){
-    printf("calling MPI_Reduce( %d, MPI_DOUBLE, MPI_SUM) with %d\n",
-	   n, nproc);
+  in = (double *)malloc(n * sizeof(double));
+  out = (double *)malloc(n * sizeof(double));
+
+  for (i = 0; i < n; i++) in[i] = 0;
+  if (0 == rank) {
+    printf("calling MPI_Reduce( %d, MPI_DOUBLE, MPI_SUM) with %d\n", n, nproc);
     fflush(stdout);
   }
-  MPI_Allreduce( in, out, n, MPI_DOUBLE, MPI_SUM, 
-		 MPI_COMM_WORLD );
-  if ( 0 == rank ){
+  MPI_Allreduce(in, out, n, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+  if (0 == rank) {
     printf("MPI_Allreduce returned\n");
     fflush(stdout);
   }
 
-  free( out );
-  free( in );
-  
-  MPI_Finalize( );
+  free(out);
+  free(in);
+
+  MPI_Finalize();
   return 0;
 }
