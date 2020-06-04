61d58d35649c1ea10746bcb20dc5b3e85a54c618
Mike Park <Mike.Park@NASA.gov>
2020-06-04 12:56:53 -0400

    archive

--- misc/module-builder/pfe/esp.sh
+++ misc/module-builder/pfe/esp.sh
@@ -15,7 +15,7 @@ echo Build ${PACKAGE} ${VERSION}
 # https://acdl.mit.edu/ESP/archive/
 
 rm -f ESP${VERSION}Lin.tgz
-wget --no-check-certificate https://acdl.mit.edu/ESP/PreBuilts/ESP${VERSION}Lin.tgz
+wget --no-check-certificate https://acdl.mit.edu/ESP/archive/ESP${VERSION}Lin.tgz
 mkdir ${MODULE_DEST}
 tar xzf ESP${VERSION}Lin.tgz -C ${MODULE_DEST} --strip-components=1
 
