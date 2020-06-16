dbdefa87244d910f61955a0513e773121fc55614
Mike Park <Mike.Park@NASA.gov>
2020-06-16 16:40:24 -0400

    egads self-contained, remove EGADS_INCLUDE_DIRS

--- cmake/FindEGADS.cmake
+++ cmake/FindEGADS.cmake
@@ -1,16 +1,11 @@
 # - Try to find egads
 # Once done this will define
 #  EGADS_FOUND - System has EGADS
-#  EGADS_INCLUDE_DIRS - The EGADS include directories
-#  EGADS_LIBRARIES - The libraries needed to use EGADS
-#  EGADS_DEFINITIONS - Compiler switches required for using EGADS
 
 find_path(EGADS_INCLUDE_DIR egads.h)
 
 find_library(EGADS_LIBRARY egads)
 find_library(EGADSLITE_LIBRARY egadslite)
-
-set(EGADS_INCLUDE_DIRS ${EGADS_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set EGADS_FOUND to TRUE
