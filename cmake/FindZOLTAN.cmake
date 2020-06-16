7eb33db17fac5fb63e2fb25b8a402a47f12950eb
Kyle Thompson <kyle.b.thompson@nasa.gov>
2020-06-16 10:10:16 -0400

    towards making third party libraries cmake targets

--- cmake/FindZOLTAN.cmake
+++ cmake/FindZOLTAN.cmake
@@ -1,23 +1,20 @@
-# - Try to find zoltan
-# Once done this will define
-#  ZOLTAN_FOUND - System has ZOLTAN
-#  ZOLTAN_INCLUDE_DIRS - The ZOLTAN include directories
-#  ZOLTAN_LIBRARIES - The libraries needed to use ZOLTAN
-#  ZOLTAN_DEFINITIONS - Compiler switches required for using ZOLTAN
-
 find_path(ZOLTAN_INCLUDE_DIR zoltan.h)
 find_library(ZOLTAN_LIBRARY zoltan)
 
-set(ZOLTAN_LIBRARIES ${ZOLTAN_LIBRARY} )
-set(ZOLTAN_INCLUDE_DIRS ${ZOLTAN_INCLUDE_DIR} )
-
 include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set ZOLTAN_FOUND to TRUE
-# if all listed variables are TRUE
 find_package_handle_standard_args(
         ZOLTAN
         DEFAULT_MSG
         ZOLTAN_LIBRARY ZOLTAN_INCLUDE_DIR
 )
 
-mark_as_advanced(ZOLTAN_INCLUDE_DIR ZOLTAN_LIBRARY )
+mark_as_advanced(ZOLTAN_INCLUDE_DIR ZOLTAN_LIBRARY)
+
+if(ZOLTAN_FOUND AND NOT TARGET ZOLTAN::ZOLTAN)
+    message(STATUS "ZOLTAN Found: ${ZOLTAN_LIBRARY}")
+    add_library(ZOLTAN::ZOLTAN UNKNOWN IMPORTED)
+    set_target_properties(ZOLTAN::ZOLTAN PROPERTIES
+            IMPORTED_LOCATION ${ZOLTAN_LIBRARY}
+            INTERFACE_INCLUDE_DIRECTORIES ${ZOLTAN_INCLUDE_DIR}
+            )
+endif()
