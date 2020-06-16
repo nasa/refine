a1c75a6621978d1a5d3b864ae27d7407366137e9
Kyle Thompson <kyle.b.thompson@nasa.gov>
2020-06-16 10:21:16 -0400

    towards making third party libraries cmake targets

--- cmake/FindEGADS.cmake
+++ cmake/FindEGADS.cmake
@@ -10,8 +10,6 @@ find_path(EGADS_INCLUDE_DIR egads.h)
 find_library(EGADS_LIBRARY egads)
 find_library(EGADSLITE_LIBRARY egadslite)
 
-set(EGADS_LIBRARIES ${EGADS_LIBRARY} )
-set(EGADSLITE_LIBRARIES ${EGADSLITE_LIBRARY} )
 set(EGADS_INCLUDE_DIRS ${EGADS_INCLUDE_DIR} )
 
 include(FindPackageHandleStandardArgs)
@@ -22,6 +20,23 @@ find_package_handle_standard_args(
         DEFAULT_MSG
         EGADS_LIBRARY EGADSLITE_LIBRARY EGADS_INCLUDE_DIR
 )
+
+if(EGADS_FOUND AND NOT TARGET EGADS::EGADS)
+    message(STATUS "EGADS Found: ${EGADS_LIBRARY}")
+    add_library(EGADS::EGADS UNKNOWN IMPORTED)
+    set_target_properties(EGADS::EGADS PROPERTIES
+            IMPORTED_LOCATION ${EGADS_LIBRARY}
+            INTERFACE_INCLUDE_DIRECTORIES ${EGADS_INCLUDE_DIR}
+            )
+    if(NOT TARGET EGADS::EGADSLITE)
+        message(STATUS "EGADSLITE Found: ${EGADSLITE_LIBRARY}")
+        add_library(EGADS::EGADSLITE UNKNOWN IMPORTED)
+        set_target_properties(EGADS::EGADSLITE PROPERTIES
+                IMPORTED_LOCATION ${EGADSLITE_LIBRARY}
+                INTERFACE_INCLUDE_DIRECTORIES ${EGADS_INCLUDE_DIR}
+                )
+    endif()
+endif()
 
 mark_as_advanced(EGADS_INCLUDE_DIR EGADS_LIBRARY EGADSLITE_LIBRARY)
 
