11b4cb58960e254ab19e65f557eaf7501ec45b7f
Mike Park <Mike.Park@NASA.gov>
2020-07-02 13:37:59 -0400

    prints once

--- src/ref_subcommand.c
+++ src/ref_subcommand.c
@@ -871,13 +871,15 @@ static REF_STATUS loop(REF_MPI ref_mpi, int argc, char *argv[]) {
       "arg search");
   if (REF_EMPTY != pos && pos < argc - 1) {
     ref_grid_partitioner(ref_grid) = (REF_MIGRATE_PARTIONER)atoi(argv[pos + 1]);
-    printf("--partioner %d partitioner\n", (int)ref_grid_partitioner(ref_grid));
+    if (ref_mpi_once(ref_mpi))
+      printf("--partioner %d partitioner\n",
+             (int)ref_grid_partitioner(ref_grid));
   }
 
   RXS(ref_args_find(argc, argv, "--topo", &pos), REF_NOT_FOUND, "arg search");
   if (REF_EMPTY != pos) {
     ref_grid_adapt(ref_grid, watch_topo) = REF_TRUE;
-    printf("--topo checks active\n");
+    if (ref_mpi_once(ref_mpi)) printf("--topo checks active\n");
   }
 
   RXS(ref_args_find(argc, argv, "--meshlink", &pos), REF_NOT_FOUND,
