e6e3478ec2d1f207b64b49e5243845390260087c
Mike Park <Mike.Park@NASA.gov>
2020-07-12 02:59:12 -0400

    cyl uplus adapt

--- acceptance/cube-cylinder/rspal/accept-cube-cylinder-rspal.sh
+++ acceptance/cube-cylinder/rspal/accept-cube-cylinder-rspal.sh
@@ -10,27 +10,37 @@ else
     src=${HOME}/refine/egads/src
 fi
 
-field=polar-2
+field="-u cyluplus"
+egads="-g cube-cylinder.egads"
 
 function adapt_cycle {
     inproj=$1
     outproj=$2
-    sweeps=$3
+    complexity=$3
+    sweeps=10
 
-    egads="-g cube-cylinder.egads"
+    ${src}/ref_acceptance ${field} ${inproj}.meshb ${inproj}-uplus.solb
 
-    ${src}/ref_acceptance -ugawg ${field} ${inproj}.meshb ${inproj}-metric.solb
-    ${src}/ref adapt ${inproj}.meshb ${egads} -m ${inproj}-metric.solb -x ${outproj}.meshb -s ${sweeps} -t -f ${outproj}_stat.tec | tee ${outproj}_refine_out
+    ${src}/ref multiscale ${inproj}.meshb ${inproj}.solb \
+	  ${complexity} ${inproj}-metric.solb \
+	  --gradation 10 | tee ${inproj}-multi.txt
+    
+    ${src}/ref \
+	  adapt ${inproj}.meshb \
+	  ${egads} \
+	  -m ${inproj}-metric.solb \
+	  -x ${outproj}.meshb -s ${sweeps} \
+	  -t -f ${outproj}_stat.tec | tee ${outproj}_refine_out
     cp ref_gather_movie.tec ${outproj}_movie.tec
     cp ref_gather_histo.tec ${outproj}_histo.tec
-    ${src}/ref_acceptance -ugawg ${field} ${outproj}.meshb ${outproj}-metric.solb
-    ${src}/ref_metric_test ${outproj}.meshb ${outproj}-metric.solb > ${outproj}.status
+    
+    ${src}/ref_acceptance ${field} ${outproj}.meshb ${outproj}-uplus.solb
 }
 
-adapt_cycle cube-cylinder00 cube-cylinder01 2
-adapt_cycle cube-cylinder01 cube-cylinder02 5
-adapt_cycle cube-cylinder02 cube-cylinder03 5
-adapt_cycle cube-cylinder03 cube-cylinder04 5
+adapt_cycle cube-cylinder00 cube-cylinder01 500
+adapt_cycle cube-cylinder01 cube-cylinder02 500
+adapt_cycle cube-cylinder02 cube-cylinder03 1000
+adapt_cycle cube-cylinder03 cube-cylinder04 2000
 
 cat cube-cylinder04.status
 ../../check.rb cube-cylinder04.status 0.2 3.0
