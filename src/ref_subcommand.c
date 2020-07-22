76671091aff00c8b2dd469e45977b0b96792828c
Mike Park <Mike.Park@NASA.gov>
2020-07-22 15:40:52 -0400

    uses three surface adapts to untangle after blend

--- src/ref_subcommand.c
+++ src/ref_subcommand.c
@@ -563,6 +563,10 @@ static REF_STATUS bootstrap(REF_MPI ref_mpi, int argc, char *argv[]) {
     if (ref_mpi_once(ref_mpi)) printf("constrain all\n");
     RSS(ref_geom_constrain_all(ref_grid), "constrain");
     ref_mpi_stopwatch_stop(ref_mpi, "constrain param");
+    RSS(ref_adapt_surf_to_geom(ref_grid, 3), "ad");
+    ref_mpi_stopwatch_stop(ref_mpi, "untangle");
+    RSS(ref_grid_pack(ref_grid), "pack");
+    ref_mpi_stopwatch_stop(ref_mpi, "pack");
   }
 
   if (ref_geom_manifold(ref_grid_geom(ref_grid))) {
