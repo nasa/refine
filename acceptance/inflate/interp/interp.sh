2594de4c8da8b3a0dbfc11569246330a3480becf
Mike Park <Mike.Park@NASA.gov>
2020-09-09 03:32:20 -0400

    path

--- acceptance/inflate/interp/interp.sh
+++ acceptance/inflate/interp/interp.sh
@@ -5,19 +5,17 @@ set -e # exit on first error
 set -u # Treat unset variables as error
 
 if [ $# -gt 0 ] ; then
-    one=$1/one
-    two=$1/src
+    src=$1/src
 else
-    one=${HOME}/refine/egads/one
-    two=${HOME}/refine/egads/src
+    src=${HOME}/refine/egads/src
 fi
 
 serveCSM -batch poly.csm 
 
-${two}/ref bootstrap poly.egads
+${src}/ref bootstrap poly.egads
 
 rm -rf inflated.b8.ugrid
-${two}/ref_inflatable  \
+${src}/ref_inflatable  \
     poly-vol.meshb \
     10 \
     0.1 \
@@ -25,16 +23,16 @@ ${two}/ref_inflatable  \
     1.68 \
     2 3 4 5 6 7 8 9
 
-${two}/ref translate inflated.b8.ugrid surf.meshb
+${src}/ref translate inflated.b8.ugrid surf.meshb
 
-${two}/ref \
+${src}/ref \
       adapt poly-vol.meshb \
       -g poly.egads \
       -x poly-curve.meshb \
       -f poly-curve-prop.tec
 
 rm -rf inflated.b8.ugrid
-${two}/ref_inflatable  \
+${src}/ref_inflatable  \
     poly-curve.meshb \
     10 \
     0.1 \
@@ -42,10 +40,10 @@ ${two}/ref_inflatable  \
     1.68 \
     2 3 4 5 6 7 8 9
 
-${two}/ref translate inflated.b8.ugrid curve.meshb
+${src}/ref translate inflated.b8.ugrid curve.meshb
 
-${two}/ref_acceptance -u 5 surf.meshb surf.solb
+${src}/ref_acceptance -u 5 surf.meshb surf.solb
 
-${two}/ref interp surf.meshb surf.solb curve.meshb curve.solb
+${src}/ref interp surf.meshb surf.solb curve.meshb curve.solb
 
 exit
