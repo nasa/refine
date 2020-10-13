2d07c9e2dee396b3cd241d8bc5f2ed18ce0f734d
Mike Park <Mike.Park@NASA.gov>
2020-10-13 05:26:51 -0400

    quiet

--- acceptance/facebody/trig-euler/accept-facebody-trig-euler.sh
+++ acceptance/facebody/trig-euler/accept-facebody-trig-euler.sh
@@ -17,6 +17,7 @@ egads="-g square.egads"
 function adapt_cycle {
     inproj=$1
     outproj=$2
+    complexity=$3
 
     ${src}/ref_acceptance ${field} ${inproj}.meshb \
 	  ${inproj}-primal.solb
@@ -28,10 +29,10 @@ function adapt_cycle {
 	  ${inproj}-primdual.solb ${inproj}-adjflux.solb
 
     ${src}/ref_metric_test --opt-goal ${inproj}.meshb \
-	  ${inproj}-adjflux.solb 1 -1 1000 ${inproj}-metric.solb
+	  ${inproj}-adjflux.solb 1 -1 ${complexity} ${inproj}-metric.solb
 
     ${src}/ref adapt ${inproj}.meshb ${egads} -m ${inproj}-metric.solb \
-	  -x ${outproj}.meshb -f ${outproj}.tec
+	  -x ${outproj}.meshb -f ${outproj}.tec > ${inproj}-adapt.txt
 
     ${src}/ref_acceptance ${field} ${outproj}.meshb \
 	  ${outproj}-primal.solb
@@ -43,14 +44,15 @@ function adapt_cycle {
 	  ${outproj}-primdual.tec
 }
 
-./square.sh
-cp square.meshb cycle00.meshb
+serveCSM -batch square.csm > square-servecsm.txt
+${src}/ref bootstrap square.egads > square-bootstrap.txt
+mv square-vol.meshb cycle00.meshb
 
-adapt_cycle cycle00 cycle01
-adapt_cycle cycle01 cycle02
-adapt_cycle cycle02 cycle03
-adapt_cycle cycle03 cycle04
-adapt_cycle cycle04 cycle05
-adapt_cycle cycle05 cycle06
+adapt_cycle cycle00 cycle01 1000
+adapt_cycle cycle01 cycle02 1000
+adapt_cycle cycle02 cycle03 1000
+adapt_cycle cycle03 cycle04 1000
+adapt_cycle cycle04 cycle05 1000
+adapt_cycle cycle05 cycle06 1000
 
 
