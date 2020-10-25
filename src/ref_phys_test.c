e20962cc27cb3fe9281fb97ca3810aaae9c31bdd
Mike Park <Mike.Park@NASA.gov>
2020-10-25 16:52:34 -0400

    adds framework for entropy function

--- src/ref_phys_test.c
+++ src/ref_phys_test.c
@@ -41,6 +41,7 @@ int main(int argc, char *argv[]) {
   REF_INT mask_pos = REF_EMPTY;
   REF_INT cont_res_pos = REF_EMPTY;
   REF_INT uplus_pos = REF_EMPTY;
+  REF_INT inviscid_entropy_flux_pos = REF_EMPTY;
 
   REF_MPI ref_mpi;
   RSS(ref_mpi_start(argc, argv), "start");
@@ -58,6 +59,9 @@ int main(int argc, char *argv[]) {
       "arg search");
   RXS(ref_args_find(argc, argv, "--uplus", &uplus_pos), REF_NOT_FOUND,
       "arg search");
+  RXS(ref_args_find(argc, argv, "--inviscid-entropy-flux",
+                    &inviscid_entropy_flux_pos),
+      REF_NOT_FOUND, "arg search");
 
   if (laminar_flux_pos != REF_EMPTY) {
     REF_GRID ref_grid;
@@ -564,6 +568,36 @@ int main(int argc, char *argv[]) {
     ref_mpi_stopwatch_stop(ref_mpi, "write uplus");
 
     ref_free(scalar);
+
+    RSS(ref_grid_free(ref_grid), "free");
+    RSS(ref_mpi_free(ref_mpi), "free");
+    RSS(ref_mpi_stop(), "stop");
+    return 0;
+  }
+
+  if (inviscid_entropy_flux_pos != REF_EMPTY) {
+    REF_GRID ref_grid;
+    REF_DBL *volume;
+    REF_INT ldim;
+    ref_mpi_stopwatch_start(ref_mpi);
+    REIS(1, inviscid_entropy_flux_pos,
+         "required args: --inviscid-entropy-flux grid.meshb volume.solb");
+    if (4 > argc) {
+      printf("required args: --inviscid-entropy-flux grid.meshb volume.solb");
+      return REF_FAILURE;
+    }
+    if (ref_mpi_once(ref_mpi)) printf("reading grid %s\n", argv[2]);
+    RSS(ref_part_by_extension(&ref_grid, ref_mpi, argv[2]),
+        "unable to load target grid in position 2");
+    ref_mpi_stopwatch_stop(ref_mpi, "read grid");
+
+    if (ref_mpi_once(ref_mpi)) printf("reading volume %s\n", argv[3]);
+    RSS(ref_part_scalar(ref_grid_node(ref_grid), &ldim, &volume, argv[3]),
+        "unable to load volume in position 3");
+    RAS(ldim >= 5, "expected a ldim of at least 5");
+    ref_mpi_stopwatch_stop(ref_mpi, "read volume");
+
+    ref_free(volume);
 
     RSS(ref_grid_free(ref_grid), "free");
     RSS(ref_mpi_free(ref_mpi), "free");
