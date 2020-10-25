dc55e07bcaa88ec1310456d6f98fc6e26dd96a4a
Mike Park <Mike.Park@NASA.gov>
2020-10-25 16:45:24 -0400

    adds entropy flux function

--- src/ref_phys.h
+++ src/ref_phys.h
@@ -30,6 +30,7 @@ BEGIN_C_DECLORATION
 REF_STATUS ref_phys_make_primitive(REF_DBL *conserved, REF_DBL *primitive);
 REF_STATUS ref_phys_make_conserved(REF_DBL *primitive, REF_DBL *conserved);
 REF_STATUS ref_phys_entropy_adjoint(REF_DBL *primitive, REF_DBL *dual);
+REF_STATUS ref_phys_entropy_flux(REF_DBL *primitive, REF_DBL *flux);
 
 REF_STATUS ref_phys_euler(REF_DBL *state, REF_DBL *direction, REF_DBL *flux);
 REF_STATUS ref_phys_euler_jac(REF_DBL *state, REF_DBL *direction,
