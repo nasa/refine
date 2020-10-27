581593ad38659938db5859b688bf47822787c454
Mike Park <Mike.Park@NASA.gov>
2020-10-27 18:00:39 -0400

    decomp

--- src/ref_metric_test.c
+++ src/ref_metric_test.c
@@ -1647,11 +1647,11 @@ int main(int argc, char *argv[]) {
     REF_GRID ref_grid;
     REF_NODE ref_node;
     REF_DBL *decomp;
-    REF_INT ldim;
+    REF_INT ldim, node;
 
     REIS(1, decompose_pos,
          "required args: --decompose grid.ext metric.solb decomp.solb");
-    REIS(9, argc,
+    REIS(5, argc,
          "required args: --decompose grid.ext metric.solb decomp.solb");
     if (ref_mpi_once(ref_mpi)) printf("part grid %s\n", argv[2]);
     RSS(ref_part_by_extension(&ref_grid, ref_mpi, argv[2]),
@@ -1663,8 +1663,21 @@ int main(int argc, char *argv[]) {
         "unable to load metric in position 3");
     ref_mpi_stopwatch_stop(ref_mpi, "read metric");
 
-    ldim = 5;
+    ldim = 3;
     ref_malloc(decomp, ldim * ref_node_max(ref_grid_node(ref_grid)), REF_DBL);
+
+    each_ref_node_valid_node(ref_node, node) {
+      REF_DBL m[6], d[12];
+      RSS(ref_node_metric_get(ref_node, node, m), "get");
+      RSS(ref_matrix_diag_m(m, d), "decomp");
+      RSS(ref_matrix_ascending_eig(d), "ascend");
+      decomp[0 + ldim * node] = 1.0 / sqrt(ref_matrix_eig(d, 0));
+      decomp[1 + ldim * node] = (1.0 / sqrt(ref_matrix_eig(d, 1))) /
+                                (1.0 / sqrt(ref_matrix_eig(d, 0)));
+      decomp[2 + ldim * node] =
+          atan2(ref_matrix_vec(d, 1, 0), ref_matrix_vec(d, 0, 0));
+      if (decomp[2 + ldim * node] < 0.0) decomp[2 + ldim * node] += ref_math_pi;
+    }
 
     if (ref_mpi_once(ref_grid_mpi(ref_grid)))
       printf("writing decomp %s\n", argv[4]);
