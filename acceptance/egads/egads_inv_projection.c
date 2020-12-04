6d00d0c8fb7a7b8f698242a7a9c1e9f5ff6c0114
Mike Park <Mike.Park@NASA.gov>
2020-12-04 11:16:43 -0500

    format

--- acceptance/egads/egads_inv_projection.c
+++ acceptance/egads/egads_inv_projection.c
@@ -1,38 +1,38 @@
 /*
 
-gcc-7  -g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -Wuninitialized  -I/Users/mpark/esp/EngSketchPad/include -o egads_inv_projection egads_inv_projection.c -Wl,-rpath,/Users/mpark/esp/EngSketchPad/lib -L/Users/mpark/esp/EngSketchPad/lib -legads   -lm
+gcc-7  -g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -Wuninitialized
+-I/Users/mpark/esp/EngSketchPad/include -o egads_inv_projection
+egads_inv_projection.c -Wl,-rpath,/Users/mpark/esp/EngSketchPad/lib
+-L/Users/mpark/esp/EngSketchPad/lib -legads   -lm
 
 */
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "egads.h"
 
-#define is_equal(a,b,msg)                                    \
-  {                                                          \
-    int ref_private_status_ai,ref_private_status_bi;     \
-    ref_private_status_ai = ( a );                           \
-    ref_private_status_bi = ( b );                           \
-    if (ref_private_status_ai!=ref_private_status_bi) {      \
-        printf("%s: %d: %s: %s\nexpected %d was %d\n",       \
-               __FILE__,__LINE__,__func__,( msg ),           \
-               ref_private_status_ai,ref_private_status_bi); \
-        return 1;                                            \
-      }                                                      \
+#define is_equal(a, b, msg)                                                  \
+  {                                                                          \
+    int ref_private_status_ai, ref_private_status_bi;                        \
+    ref_private_status_ai = (a);                                             \
+    ref_private_status_bi = (b);                                             \
+    if (ref_private_status_ai != ref_private_status_bi) {                    \
+      printf("%s: %d: %s: %s\nexpected %d was %d\n", __FILE__, __LINE__,     \
+             __func__, (msg), ref_private_status_ai, ref_private_status_bi); \
+      return 1;                                                              \
+    }                                                                        \
   }
 
-#define is_true(a,msg)                                                 \
+#define is_true(a, msg)                                                \
   {                                                                    \
-    if (!( a )) {                                                      \
-        printf("%s: %d: %s: %s\n",__FILE__,__LINE__,__func__,( msg )); \
-        return 1;                                                      \
-      }                                                                \
+    if (!(a)) {                                                        \
+      printf("%s: %d: %s: %s\n", __FILE__, __LINE__, __func__, (msg)); \
+      return 1;                                                        \
+    }                                                                  \
   }
 
-
-int main( void )
-{
+int main(void) {
   ego context;
   ego model = NULL;
   ego geom, *bodies, *children;
@@ -44,54 +44,53 @@ int main( void )
   double input_xyz[3];
   double param[2];
   double output_xyz[3];
-  
-  is_equal( EGADS_SUCCESS, EG_open(&context), "EG open");
+
+  is_equal(EGADS_SUCCESS, EG_open(&context), "EG open");
   /* Success returns the old output level. (0-silent to 3-debug) */
-  is_true( EG_setOutLevel(context, 3) >= 0, "make verbose");
+  is_true(EG_setOutLevel(context, 3) >= 0, "make verbose");
 
-  is_equal( EGADS_SUCCESS,
-            EG_loadModel(context, 0, "onera-m6-sharp-te.egads", &model),
-            "EG load");
+  is_equal(EGADS_SUCCESS,
+           EG_loadModel(context, 0, "onera-m6-sharp-te.egads", &model),
+           "EG load");
 
-  is_equal( EGADS_SUCCESS,
-            EG_getTopology(model, &geom, &oclass, &mtype, NULL,
-                           &nbody, &bodies, &senses), "EG topo bodies");
-  is_equal( 1, nbody, "expected 1 body" );
+  is_equal(EGADS_SUCCESS,
+           EG_getTopology(model, &geom, &oclass, &mtype, NULL, &nbody, &bodies,
+                          &senses),
+           "EG topo bodies");
+  is_equal(1, nbody, "expected 1 body");
   solid = bodies[0];
-  is_equal( EGADS_SUCCESS,
-            EG_getTopology(solid, &geom, &oclass, &mtype,
-                           NULL, &nchild, &children, &senses),
-            "EG topo body type");
-  is_equal( SOLIDBODY, mtype, "expected SOLIDBODY" );
-  
-  is_equal( EGADS_SUCCESS,
-	EG_getBodyTopos(solid, NULL, FACE, &nface, &faces), "EG face topo");
+  is_equal(EGADS_SUCCESS,
+           EG_getTopology(solid, &geom, &oclass, &mtype, NULL, &nchild,
+                          &children, &senses),
+           "EG topo body type");
+  is_equal(SOLIDBODY, mtype, "expected SOLIDBODY");
+
+  is_equal(EGADS_SUCCESS, EG_getBodyTopos(solid, NULL, FACE, &nface, &faces),
+           "EG face topo");
 
   faceid = 2;
 
-  input_xyz[0] =  25.0;
-  input_xyz[1] =   0.0;
-  input_xyz[2] =  25.0;
-  is_equal( EGADS_SUCCESS,
-            EG_invEvaluate(faces[faceid - 1],
-                           input_xyz,
-                           param, output_xyz), "EG inv  eval");
+  input_xyz[0] = 25.0;
+  input_xyz[1] = 0.0;
+  input_xyz[2] = 25.0;
+  is_equal(EGADS_SUCCESS,
+           EG_invEvaluate(faces[faceid - 1], input_xyz, param, output_xyz),
+           "EG inv  eval");
 
-  printf(" input %f %f %f\n",input_xyz[0],input_xyz[1],input_xyz[2]);
-  printf("output %f %f %f\n",output_xyz[0],output_xyz[1],output_xyz[2]);
-  printf(" param %f %f\n",param[0],param[1]);
-  
-  input_xyz[0] =  25.0;
-  input_xyz[1] =   0.0;
+  printf(" input %f %f %f\n", input_xyz[0], input_xyz[1], input_xyz[2]);
+  printf("output %f %f %f\n", output_xyz[0], output_xyz[1], output_xyz[2]);
+  printf(" param %f %f\n", param[0], param[1]);
+
+  input_xyz[0] = 25.0;
+  input_xyz[1] = 0.0;
   input_xyz[2] = -25.0;
-  is_equal( EGADS_SUCCESS,
-            EG_invEvaluate(faces[faceid - 1],
-                           input_xyz,
-                           param, output_xyz), "EG inv  eval");
+  is_equal(EGADS_SUCCESS,
+           EG_invEvaluate(faces[faceid - 1], input_xyz, param, output_xyz),
+           "EG inv  eval");
 
-  printf(" input %f %f %f\n",input_xyz[0],input_xyz[1],input_xyz[2]);
-  printf("output %f %f %f\n",output_xyz[0],output_xyz[1],output_xyz[2]);
-  printf(" param %f %f\n",param[0],param[1]);
-    
+  printf(" input %f %f %f\n", input_xyz[0], input_xyz[1], input_xyz[2]);
+  printf("output %f %f %f\n", output_xyz[0], output_xyz[1], output_xyz[2]);
+  printf(" param %f %f\n", param[0], param[1]);
+
   return 0;
 }
