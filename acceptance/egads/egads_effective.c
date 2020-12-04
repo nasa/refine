1716d3b2d6c67b153d392be0ff2935e2c6f2e3f9
Mike Park <Mike.Park@NASA.gov>
2020-12-04 14:31:37 -0500

    adds bob suggestion

--- acceptance/egads/egads_effective.c
+++ acceptance/egads/egads_effective.c
@@ -1,9 +1,10 @@
 /*
 
-gcc-10  -g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -Wuninitialized
--I/Users/mpark/local/pkgs/EGADS/trunk/include -o egads_effective
-egads_effective.c -Wl,-rpath,/Users/mpark/local/pkgs/EGADS/trunk/lib
--L/Users/mpark/local/pkgs/EGADS/trunk/lib -legads   -lm
+gcc-10  -g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -Wuninitialized \
+-I/Users/mpark/local/pkgs/EGADS/trunk/include -o egads_effective \
+egads_effective.c -Wl,-rpath,/Users/mpark/local/pkgs/EGADS/trunk/lib \
+-L/Users/mpark/local/pkgs/EGADS/trunk/lib -legads   -lm \
+&& ./egads_effective
 
 */
 
@@ -37,16 +38,14 @@ int main(void) {
   ego model = NULL;
   ego geom, *bodies;
   int oclass, mtype, nbody, *senses;
-  ego solid;
+  ego newBodies[3], newModel;
   double params[3];
-  ego tess;
   int tess_status, nvert;
   double angle;
-  ego ebody;
 
   is_equal(EGADS_SUCCESS, EG_open(&context), "EG open");
   /* Success returns the old output level. (0-silent to 3-debug) */
-  is_true(EG_setOutLevel(context, 3) >= 0, "make verbose");
+  is_true(EG_setOutLevel(context, 2) >= 0, "make verbose");
 
   is_equal(EGADS_SUCCESS, EG_loadModel(context, 0, "boxbox.egads", &model),
            "EG load");
@@ -56,23 +55,42 @@ int main(void) {
                           &senses),
            "EG topo bodies");
   is_equal(1, nbody, "expected 1 body");
-  solid = bodies[0];
 
+  /* copy the Body so we can use/save it later */
+  is_equal(EGADS_SUCCESS, EG_copyObject(bodies[0], NULL, &newBodies[0]),
+           "EG copy object");
+  EG_deleteObject(model);
+
+  /* make the tessellation object */
   params[0] = 0.1;
   params[1] = 0.01;
   params[2] = 20.0;
 
-  is_equal(EGADS_SUCCESS, EG_makeTessBody(solid, params, &tess), "EG tess");
-  is_equal(EGADS_SUCCESS, EG_statusTessBody(tess, &geom, &tess_status, &nvert),
+  is_equal(EGADS_SUCCESS, EG_makeTessBody(newBodies[0], params, &newBodies[1]),
+           "EG tess");
+  is_equal(EGADS_SUCCESS,
+           EG_statusTessBody(newBodies[1], &geom, &tess_status, &nvert),
            "EG tess");
   is_equal(1, tess_status, "tess not closed");
 
+  /* make the effective topology object */
   angle = 10.0;
-  is_equal(EGADS_SUCCESS, EG_virtualize(tess, angle, &ebody), "virt");
-  is_equal(EGADS_SUCCESS, EG_finalize(ebody), "fin")
+  is_equal(EGADS_SUCCESS, EG_initEBody(newBodies[1], angle, &newBodies[2]),
+           "initEB");
+  is_equal(EGADS_SUCCESS, EG_finishEBody(newBodies[2]), "finEB");
 
-      is_equal(EGADS_SUCCESS, EG_saveModel(model, "boxboxeff.egads"),
-               "EG save eff");
+  /* make the model with the body, tessellation and effective topology body
+     notes: 1) mtype = 3 is the total number of objects in the model
+               Body Objects must be first
+            2) nchild = 1 are the number of actual Body Objects */
+  is_equal(EGADS_SUCCESS,
+           EG_makeTopology(context, NULL, MODEL, 3, NULL, 1, newBodies, NULL,
+                           &newModel),
+           "make Topo Model");
+
+  is_equal(EGADS_SUCCESS, EG_saveModel(newModel, "boxboxeff.egads"),
+           "EG save eff");
+  EG_deleteObject(newModel);
 
   is_equal(EGADS_SUCCESS, EG_close(context), "EG close");
   return 0;
