605aeeb41bac848f700fa30a2c711f844914dcc4
Mike Park <Mike.Park@NASA.gov>
2020-12-08 15:43:34 -0500

    use svn EGADS on mac

--- misc/setup-mac.sh
+++ misc/setup-mac.sh
@@ -16,6 +16,7 @@ gcc9flags='-g -O2 -Werror -pedantic-errors -Wall -Wextra -Wunused -Wuninitialize
 
 zoltan_path="/Users/mpark/spack/opt/spack/darwin-mojave-x86_64/gcc-9.1.0/zoltan-3.83-5uh3ojfi7bp5ge7aavovf6lldduugwep"
 egads_path="/Users/mpark/local/pkgs/EngSketchPad"
+egads_svn_path="/Users/mpark/local/pkgs/EGADS/trunk"
 opencascade_path="/Users/mpark/local/pkgs/OpenCASCADE"
 meshlink_path="/Users/mpark/local/pkgs/MeshLink"
 
@@ -24,19 +25,6 @@ parmetis_path="/Users/mpark/local/pkgs/parmetis-4.0.3"
 metis_path="/Users/mpark/local/pkgs/parmetis-4.0.3"
 mpi_path="/Users/mpark/local/pkgs/openmpi-4.0.5/build"
 
-mkdir -p svn
-( cd svn && \
-    ../configure \
-    --prefix=`pwd` \
-    --with-mpi=${mpi_path} \
-    --with-metis=${metis_path} \
-    --with-parmetis=${parmetis_path} \
-    --with-EGADS=/Users/mpark/local/pkgs/EGADS/trunk \
-    --with-OpenCASCADE=${opencascade_path} \
-    CFLAGS=" -DHAVE_EGADS_EFFECTIVE ${clangflags}" \
-    ) \
-    || exit
-
 mkdir -p egads
 ( cd egads && \
     ../configure \
@@ -44,10 +32,10 @@ mkdir -p egads
     --with-mpi=${mpi_path} \
     --with-metis=${metis_path} \
     --with-parmetis=${parmetis_path} \
-    --with-EGADS=${egads_path} \
+    --with-EGADS=${egads_svn_path} \
     --with-OpenCASCADE=${opencascade_path} \
     --with-MeshLink=${meshlink_path} \
-    CFLAGS="${clangflags}" \
+    CFLAGS=" -DHAVE_EGADS_EFFECTIVE ${clangflags}" \
     ) \
     || exit
 
