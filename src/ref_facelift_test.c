9cf981fe115e35d4122d3bb0ebd4cc5344a27ad0
Mike Park <Mike.Park@NASA.gov>
2020-12-24 06:31:17 -0500

    P2 and P3 surrogates

--- src/ref_facelift_test.c
+++ src/ref_facelift_test.c
@@ -189,7 +189,7 @@ int main(int argc, char *argv[]) {
     RSS(ref_facelift_free(ref_facelift), "free");
   }
 
-  if (ref_egads_allows_construction()) { /* steinmetz */
+  if (ref_egads_allows_construction()) { /* steinmetz P2 surrogate */
     REF_GRID ref_grid, surrogate;
     REF_FACELIFT ref_facelift;
     REF_DBL gap;
@@ -200,6 +200,25 @@ int main(int argc, char *argv[]) {
     RSS(ref_geom_constrain_all(ref_grid), "constrain");
     RSS(ref_grid_deep_copy(&surrogate, ref_grid), "free grid");
     RSS(ref_geom_enrich2(surrogate), "enrich2");
+    RSS(ref_facelift_create(&ref_facelift, surrogate, REF_TRUE), "create");
+    ref_geom_facelift(ref_grid_geom(ref_grid)) = ref_facelift;
+    RSS(ref_geom_constrain_all(ref_grid), "constrain");
+    RSS(ref_geom_max_gap(ref_grid, &gap), "geom gap");
+    RAB(gap < 1.0e-14, "expected watertight", { printf("gap %e\n", gap); });
+    RSS(ref_grid_free(ref_grid), "free grid");
+  }
+
+  if (ref_egads_allows_construction()) { /* steinmetz P3 surrogate */
+    REF_GRID ref_grid, surrogate;
+    REF_FACELIFT ref_facelift;
+    REF_DBL gap;
+    RSS(ref_grid_create(&ref_grid, ref_mpi), "create grid");
+    RSS(ref_egads_construct(ref_grid_geom(ref_grid), "steinmetz"),
+        "create cylinder");
+    RSS(ref_egads_tess(ref_grid, 0, NULL), "tess");
+    RSS(ref_geom_constrain_all(ref_grid), "constrain");
+    RSS(ref_grid_deep_copy(&surrogate, ref_grid), "free grid");
+    RSS(ref_geom_enrich3(surrogate), "enrich2");
     RSS(ref_facelift_create(&ref_facelift, surrogate, REF_TRUE), "create");
     ref_geom_facelift(ref_grid_geom(ref_grid)) = ref_facelift;
     RSS(ref_geom_constrain_all(ref_grid), "constrain");
