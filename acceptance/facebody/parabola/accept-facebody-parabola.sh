2957541cd1a36be133be750df2c888d67eedfaa9
Mike Park <Mike.Park@NASA.gov>
2021-01-08 20:01:33 -0500

    wires up ref_metric_multigrad with tests

--- acceptance/facebody/parabola/accept-facebody-parabola.sh
+++ acceptance/facebody/parabola/accept-facebody-parabola.sh
@@ -11,14 +11,14 @@ else
 fi
 
 tecplot=-t
-field="-u parabola"
+field="parabola"
 egads="-g square.egads"
 
-function adapt_cycle {
+function func_cycle {
     inproj=$1
     outproj=$2
 
-    ${src}/ref_acceptance ${field} ${inproj}.meshb \
+    ${src}/ref_acceptance -u ${field} ${inproj}.meshb \
 	  ${inproj}.solb
 
     ${src}/ref multiscale ${inproj}.meshb ${inproj}.solb \
@@ -27,7 +27,24 @@ function adapt_cycle {
     ${src}/ref adapt ${inproj}.meshb ${egads} -m ${inproj}-metric.solb \
 	  -x ${outproj}.meshb -f ${outproj}.tec
 
-    ${src}/ref_acceptance ${field} ${outproj}.meshb \
+    ${src}/ref_acceptance -u ${field} ${outproj}.meshb \
+	  ${outproj}.solb
+}
+
+function grad_cycle {
+    inproj=$1
+    outproj=$2
+
+    ${src}/ref_acceptance -g ${field} ${inproj}.meshb \
+	  ${inproj}.solb
+
+    ${src}/ref_metric_test --multigrad ${inproj}.meshb ${inproj}.solb \
+	  2 -1 1000 ${inproj}-metric.solb
+
+    ${src}/ref adapt ${inproj}.meshb ${egads} -m ${inproj}-metric.solb \
+	  -x ${outproj}.meshb -f ${outproj}.tec
+
+    ${src}/ref_acceptance -g ${field} ${outproj}.meshb \
 	  ${outproj}.solb
 }
 
@@ -35,9 +52,16 @@ serveCSM -batch square.csm
 ${src}/ref bootstrap square.egads
 mv square-vol.meshb func00.meshb
 
-adapt_cycle func00 func01
-adapt_cycle func01 func02
-adapt_cycle func02 func03
-adapt_cycle func03 func04
+func_cycle func00 func01
+func_cycle func01 func02
+func_cycle func02 func03
+func_cycle func03 func04
+
+cp func00.meshb grad00.meshb
+
+grad_cycle grad00 grad01
+grad_cycle grad01 grad02
+grad_cycle grad02 grad03
+grad_cycle grad03 grad04
 
 
