cd84815cfaba431ae1f9d3652e2f710281fd7194
Mike Park <Mike.Park@NASA.Gov>
2021-02-28 05:13:35 -0800

    uses beta ESP

--- misc/setup-pfe.sh
+++ misc/setup-pfe.sh
@@ -7,8 +7,8 @@ set -x
 module_path="/swbuild/fun3d/shared/fun3d_users/modules"
 zoltan_path="${module_path}/Zoltan/3.82_mpt-2.17r13_ifort-2018.3.222"
 parmetis_path="${module_path}/ParMETIS/4.0.3_mpt-2.17r13_ifort-2018.3.222"
-egads_path="${module_path}/ESP/118/EngSketchPad"
-opencascade_path="${module_path}/ESP/118/OpenCASCADE-7.3.1"
+egads_path="${module_path}/ESP/119-beta.2020.10.27.0909/EngSketchPad"
+occ_path="${module_path}/ESP/119-beta.2020.10.27.0909/OpenCASCADE-7.3.1"
 
 mpi_path="/nasa/hpe/mpt/2.17r13"
 
