4c2b0d5b9055e4f2151f44ad4eccc26899acb2f9
Mike Park <Mike.Park@NASA.gov>
2021-03-11 14:48:26 -0500

    set mpi

--- misc/setup-rome.sh
+++ misc/setup-rome.sh
@@ -10,6 +10,8 @@ egads_path="${module_path}/ESP/119-beta.2021.03.05.1211/EngSketchPad"
 occ_path="${module_path}/ESP/119-beta.2021.03.05.1211/OpenCASCADE-7.3.1"
 parmetis_path="/nasa/parmetis/4.0.3-sles12"
 
+mpi_path="/nasa/hpe/mpt/2.23_sles15_patch11654"
+
 gcc_flags="-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -Wuninitialized"
 
 mkdir -p rome
