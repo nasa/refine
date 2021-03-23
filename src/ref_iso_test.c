80004c5b719214784013c77899ffbe7244cdd61d
Mike Park <Mike.Park@NASA.gov>
2021-03-23 14:32:12 -0400

    adds test for tet brick

--- src/ref_iso_test.c
+++ src/ref_iso_test.c
@@ -379,6 +379,48 @@ int main(int argc, char *argv[]) {
     ref_grid_free(ref_grid);
   }
 
+  { /* cast tet brick */
+    REF_GRID ref_grid, iso_grid;
+    REF_NODE ref_node;
+    REF_INT ldim = 1;
+    REF_DBL *field, *iso_field;
+    REF_INT node;
+    REF_DBL segment0[3], segment1[3];
+    REF_BOOL debug = REF_FALSE;
+
+    RSS(ref_fixture_tet_brick_grid(&ref_grid, ref_mpi), "tri");
+    ref_node = ref_grid_node(ref_grid);
+    ref_malloc(field, ldim * ref_node_max(ref_node), REF_DBL);
+    each_ref_node_valid_node(ref_node, node) {
+      field[node] = ref_node_xyz(ref_node, 0, node);
+    }
+    segment0[0] = -1.0;
+    segment0[1] = 0.2;
+    segment0[2] = 0.2;
+    segment1[0] = 2.0;
+    segment1[1] = 0.2;
+    segment1[2] = 0.2;
+
+    RSS(ref_iso_cast(&iso_grid, &iso_field, ref_grid, field, ldim, segment0,
+                     segment1),
+        "cast");
+    if (!ref_mpi_para(ref_mpi)) {
+      REIS(10, ref_node_n(ref_grid_node(iso_grid)), "three nodes");
+      REIS(0, ref_cell_n(ref_grid_tri(iso_grid)), "zero tri");
+      REIS(9, ref_cell_n(ref_grid_edg(iso_grid)), "one edg");
+    }
+    if (debug) {
+      ref_gather_scalar_by_extension(iso_grid, ldim, iso_field, NULL,
+                                     "iso-edge.tec");
+      ref_gather_scalar_by_extension(ref_grid, ldim, field, NULL,
+                                     "iso-orig.tec");
+    }
+    ref_free(iso_field);
+    ref_grid_free(iso_grid);
+    ref_free(field);
+    ref_grid_free(ref_grid);
+  }
+
   RSS(ref_mpi_free(ref_mpi), "mpi free");
   RSS(ref_mpi_stop(), "stop");
   return 0;
