d5ca56529c575dd2845085bcac2784127b4d6c5d
Mike Park <Mike.Park@NASA.gov>
2021-03-24 14:32:49 -0400

    aoa tests

--- src/ref_iso_test.c
+++ src/ref_iso_test.c
@@ -525,6 +525,82 @@ int main(int argc, char *argv[]) {
     ref_grid_free(ref_grid);
   }
 
+  { /* segment, phi 45 */
+    REF_GRID ref_grid;
+    REF_NODE ref_node;
+    REF_DBL center[3] = {0, 0, 0};
+    REF_DBL aoa = 0;
+    REF_DBL phi = 45.0;
+    REF_DBL h = 20;
+    REF_DBL segment0[3], segment1[3];
+    REF_INT node;
+    REF_GLOB global;
+
+    RSS(ref_grid_create(&ref_grid, ref_mpi), "tri");
+    ref_node = ref_grid_node(ref_grid);
+    global = 0;
+    RSS(ref_node_add(ref_node, global, &node), "first add");
+    ref_node_xyz(ref_node, 0, node) = 0.0;
+    ref_node_xyz(ref_node, 1, node) = 0.0;
+    ref_node_xyz(ref_node, 2, node) = 0.0;
+    global = 1;
+    RSS(ref_node_add(ref_node, global, &node), "second add");
+    ref_node_xyz(ref_node, 0, node) = 1.0;
+    ref_node_xyz(ref_node, 1, node) = 0.0;
+    ref_node_xyz(ref_node, 2, node) = 0.0;
+
+    RSS(ref_iso_segment(ref_grid, center, aoa, phi, h, segment0, segment1),
+        "seg");
+
+    {
+      REF_DBL r;
+      r = h / sqrt(2.0);
+      RWDS(r, segment0[1], -1.0, "y");
+      RWDS(-r, segment0[2], -1.0, "z");
+      RWDS(r, segment1[1], -1.0, "y");
+      RWDS(-r, segment1[2], -1.0, "z");
+    }
+
+    ref_grid_free(ref_grid);
+  }
+
+  { /* segment, aoa 45 */
+    REF_GRID ref_grid;
+    REF_NODE ref_node;
+    REF_DBL center[3] = {0, 0, 0};
+    REF_DBL aoa = 45;
+    REF_DBL phi = 0.0;
+    REF_DBL h = 0;
+    REF_DBL segment0[3], segment1[3];
+    REF_INT node;
+    REF_GLOB global;
+
+    RSS(ref_grid_create(&ref_grid, ref_mpi), "tri");
+    ref_node = ref_grid_node(ref_grid);
+    global = 0;
+    RSS(ref_node_add(ref_node, global, &node), "first add");
+    ref_node_xyz(ref_node, 0, node) = -1.0;
+    ref_node_xyz(ref_node, 1, node) = 0.0;
+    ref_node_xyz(ref_node, 2, node) = 0.0;
+    global = 1;
+    RSS(ref_node_add(ref_node, global, &node), "second add");
+    ref_node_xyz(ref_node, 0, node) = 1.0;
+    ref_node_xyz(ref_node, 1, node) = 0.0;
+    ref_node_xyz(ref_node, 2, node) = 0.0;
+
+    RSS(ref_iso_segment(ref_grid, center, aoa, phi, h, segment0, segment1),
+        "seg");
+
+    {
+      RWDS(0, segment0[1], -1.0, "y");
+      RWDS(-segment0[0], segment0[2], -1.0, "z");
+      RWDS(0, segment1[1], -1.0, "y");
+      RWDS(-segment1[0], segment1[2], -1.0, "z");
+    }
+
+    ref_grid_free(ref_grid);
+  }
+
   RSS(ref_mpi_free(ref_mpi), "mpi free");
   RSS(ref_mpi_stop(), "stop");
   return 0;
