4ca0b04b92397b6ed236620b4969535c5300ad3f
Mike Park <Mike.Park@NASA.gov>
2021-03-24 14:15:31 -0400

    tests-phi

--- src/ref_iso_test.c
+++ src/ref_iso_test.c
@@ -421,7 +421,7 @@ int main(int argc, char *argv[]) {
     ref_grid_free(ref_grid);
   }
 
-  { /* segment */
+  { /* segment, case values */
     REF_GRID ref_grid;
     REF_NODE ref_node;
     REF_DBL center[3] = {0, 0, 1.58087742328643799};
@@ -447,6 +447,80 @@ int main(int argc, char *argv[]) {
 
     RSS(ref_iso_segment(ref_grid, center, aoa, phi, h, segment0, segment1),
         "seg");
+
+    ref_grid_free(ref_grid);
+  }
+
+  { /* segment, phi 0 */
+    REF_GRID ref_grid;
+    REF_NODE ref_node;
+    REF_DBL center[3] = {0, 0, 0};
+    REF_DBL aoa = 0;
+    REF_DBL phi = 0.0;
+    REF_DBL h = 15;
+    REF_DBL segment0[3], segment1[3];
+    REF_INT node;
+    REF_GLOB global;
+
+    RSS(ref_grid_create(&ref_grid, ref_mpi), "tri");
+    ref_node = ref_grid_node(ref_grid);
+    global = 0;
+    RSS(ref_node_add(ref_node, global, &node), "first add");
+    ref_node_xyz(ref_node, 0, node) = 0.0;
+    ref_node_xyz(ref_node, 1, node) = 0.0;
+    ref_node_xyz(ref_node, 2, node) = 0.0;
+    global = 1;
+    RSS(ref_node_add(ref_node, global, &node), "second add");
+    ref_node_xyz(ref_node, 0, node) = 1.0;
+    ref_node_xyz(ref_node, 1, node) = 0.0;
+    ref_node_xyz(ref_node, 2, node) = 0.0;
+
+    RSS(ref_iso_segment(ref_grid, center, aoa, phi, h, segment0, segment1),
+        "seg");
+
+    RWDS(0.0, segment0[1], -1.0, "y");
+    RWDS(-15.0, segment0[2], -1.0, "z");
+    RWDS(0.0, segment1[1], -1.0, "y");
+    RWDS(-15.0, segment1[2], -1.0, "z");
+
+    ref_grid_free(ref_grid);
+  }
+
+  { /* segment, phi -30 */
+    REF_GRID ref_grid;
+    REF_NODE ref_node;
+    REF_DBL center[3] = {0, 0, 0};
+    REF_DBL aoa = 0;
+    REF_DBL phi = -30.0;
+    REF_DBL h = 20;
+    REF_DBL segment0[3], segment1[3];
+    REF_INT node;
+    REF_GLOB global;
+
+    RSS(ref_grid_create(&ref_grid, ref_mpi), "tri");
+    ref_node = ref_grid_node(ref_grid);
+    global = 0;
+    RSS(ref_node_add(ref_node, global, &node), "first add");
+    ref_node_xyz(ref_node, 0, node) = 0.0;
+    ref_node_xyz(ref_node, 1, node) = 0.0;
+    ref_node_xyz(ref_node, 2, node) = 0.0;
+    global = 1;
+    RSS(ref_node_add(ref_node, global, &node), "second add");
+    ref_node_xyz(ref_node, 0, node) = 1.0;
+    ref_node_xyz(ref_node, 1, node) = 0.0;
+    ref_node_xyz(ref_node, 2, node) = 0.0;
+
+    RSS(ref_iso_segment(ref_grid, center, aoa, phi, h, segment0, segment1),
+        "seg");
+
+    {
+      REF_DBL z;
+      z = 10.0 * sqrt(3.0);
+      RWDS(-10, segment0[1], -1.0, "y");
+      RWDS(-z, segment0[2], -1.0, "z");
+      RWDS(-10, segment1[1], -1.0, "y");
+      RWDS(-z, segment1[2], -1.0, "z");
+    }
 
     ref_grid_free(ref_grid);
   }
