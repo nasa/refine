72bcd5f5b2018263f389def8b864e48425556953
Mike Park <Mike.Park@NASA.gov>
2021-06-23 12:19:15 -0400

    creates a new collar test

---
+++ acceptance/inflate/collar/inflate.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -x # echo commands
+set -e # exit on first error
+set -u # Treat unset variables as error
+
+if [ $# -gt 0 ] ; then
+    src=$1/src
+else
+    src=${HOME}/refine/egads/src
+fi
+
+serveCSM -batch poly.csm 
+
+${src}/ref bootstrap poly.egads
+
+${src}/ref collar  \
+    poly-vol.meshb \
+    10 \
+    0.1 \
+    2.0 \
+    1.68
+
+exit
