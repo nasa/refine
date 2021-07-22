f6b5899b3370c00edadd5040660373bb8d7c32e2
Mike Park <Mike.Park@NASA.gov>
2021-07-22 15:46:02 -0400

    new MPT ESP setup

--- misc/setup-k.sh
+++ misc/setup-k.sh
@@ -5,15 +5,15 @@ set -x
 ./bootstrap
 
 module_path="/u/shared/fun3d/fun3d_users/modules"
-zoltan_path="${module_path}/Zoltan/3.82-mpt-2.19-intel_2018.3.222"
-parmetis_path="${module_path}/ParMETIS/4.0.3-mpt-2.19-intel_2018.3.222"
-gcc_parmetis_path="${module_path}/ParMETIS/4.0.3-mpt-2.17r14-gcc_6.2.0"
-egads_path="${module_path}/ESP/119-beta.2021.05.05.1543/EngSketchPad"
+zoltan_path="${module_path}/Zoltan/3.82-mpt-2.23-intel_2018.3.22"
+parmetis_path="${module_path}/ParMETIS/4.0.3-mpt-2.23-intel_2018.3.222"
+gcc_parmetis_path="${module_path}/ParMETIS/4.0.3-mpt-2.23-gcc_6.2.0"
+egads_path="${module_path}/ESP/119/EngSketchPad"
 egads_svn_path="/u/mpark/local/pkgs/EGADS/trunk"
-occ_path="${module_path}/ESP/119-beta.2021.05.05.1543/OpenCASCADE-7.3.1"
+occ_path="${module_path}/ESP/119/OpenCASCADE-7.3.1"
 meshlink_path="/u/mpark/local/pkgs/MeshLink"
 
-mpi_path="/opt/hpe/hpc/mpt/mpt-2.19"
+mpi_path="/opt/hpe/hpc/mpt/mpt-2.23"
 
 gcc_flags="-g -O2 -pedantic-errors -Wall -Wextra -Werror -Wunused -Wuninitialized"
 icc_flags="-g -O2 -traceback -Wall -w3 -wd1418,2259,2547,981,11074,11076,1572,49,1419 -ftrapuv"
