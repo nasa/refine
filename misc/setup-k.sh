0045f546bbeade16099d5e746401f34a78abb680
Mike Park <Mike.Park@NASA.gov>
2021-08-10 14:39:34 -0400

    explicit OCC path

--- misc/setup-k.sh
+++ misc/setup-k.sh
@@ -9,7 +9,7 @@ parmetis_path="${module_path}/ParMETIS/4.0.3-mpt-2.23-intel_2018.3.222"
 gcc_parmetis_path="${module_path}/ParMETIS/4.0.3-mpt-2.23-gcc_6.2.0"
 egads_path="${module_path}/ESP/119/EngSketchPad"
 egads_svn_path="/u/mpark/local/pkgs/EGADS/trunk"
-occ_path="${module_path}/ESP/119/OpenCASCADE"
+occ_path="${module_path}/ESP/119/OpenCASCADE-7.4.1"
 meshlink_path="/u/mpark/local/pkgs/MeshLink"
 
 mpi_path="/opt/hpe/hpc/mpt/mpt-2.23"
