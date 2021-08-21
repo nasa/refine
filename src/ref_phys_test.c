9f17094b822ee240e8a7830e9e8a08704a596b04
Mike Park <Mike.Park@NASA.gov>
2021-08-21 18:19:04 -0400

    tests timing for moderate case with --timing

--- src/ref_phys_test.c
+++ src/ref_phys_test.c
@@ -186,6 +186,7 @@ int main(int argc, char *argv[]) {
   REF_INT uplus_pos = REF_EMPTY;
   REF_INT turb1_pos = REF_EMPTY;
   REF_INT entropy_output_pos = REF_EMPTY;
+  REF_INT timing_pos = REF_EMPTY;
 
   REF_MPI ref_mpi;
   RSS(ref_mpi_start(argc, argv), "start");
@@ -207,6 +208,8 @@ int main(int argc, char *argv[]) {
       "arg search");
   RXS(ref_args_find(argc, argv, "--entropy-output", &entropy_output_pos),
       REF_NOT_FOUND, "arg search");
+  RXS(ref_args_find(argc, argv, "--timing", &timing_pos), REF_NOT_FOUND,
+      "arg search");
 
   if (laminar_flux_pos != REF_EMPTY) {
     REF_GRID ref_grid;
@@ -1854,6 +1857,38 @@ int main(int argc, char *argv[]) {
     char grid_file[] = "ref_phys_test_brick_wall_dist.lb8.ugrid";
     if (ref_mpi_once(ref_mpi)) {
       RSS(ref_fixture_tet_brick_grid(&ref_grid, ref_mpi), "set up tet");
+      RSS(ref_export_by_extension(ref_grid, grid_file), "export");
+      RSS(ref_grid_free(ref_grid), "free");
+    }
+    RSS(ref_part_by_extension(&ref_grid, ref_mpi, grid_file), "import");
+    ref_node = ref_grid_node(ref_grid);
+
+    RSS(ref_dict_create(&ref_dict), "dict");
+    ref_malloc_init(distance, ref_node_max(ref_node), REF_DBL, -1.0);
+    RSS(ref_dict_store(ref_dict, 5, 4000), "store");
+    RSS(ref_phys_wall_distance(ref_grid, ref_dict, distance), "store");
+
+    each_ref_node_valid_node(ref_node, node) {
+      RWDS(ref_node_xyz(ref_node, 2, node), distance[node], -1, "dist=z");
+    }
+
+    ref_free(distance);
+    ref_dict_free(ref_dict);
+    ref_grid_free(ref_grid);
+    if (ref_mpi_once(ref_mpi)) REIS(0, remove(grid_file), "test clean up");
+  }
+
+  if (timing_pos != REF_EMPTY) { /* timing large brick wall dist */
+    REF_GRID ref_grid;
+    REF_NODE ref_node;
+    REF_DICT ref_dict;
+    REF_DBL *distance;
+    REF_INT node;
+    char grid_file[] = "ref_phys_test_brick_wall_dist.lb8.ugrid";
+    if (ref_mpi_once(ref_mpi)) {
+      RSS(ref_fixture_tet_brick_args_grid(&ref_grid, ref_mpi, 0, 1, 0, 1, 0, 1,
+                                          50, 50, 50),
+          "set up tet");
       RSS(ref_export_by_extension(ref_grid, grid_file), "export");
       RSS(ref_grid_free(ref_grid), "free");
     }
