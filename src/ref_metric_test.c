01a0e605cc9f1d52afbe6d8edc7fba2fc4c8b093
Mike Park <Mike.Park@NASA.gov>
2022-01-03 05:51:28 -0800

    tests imply metric for tri

--- src/ref_metric_test.c
+++ src/ref_metric_test.c
@@ -2264,6 +2264,32 @@ int main(int argc, char *argv[]) {
     RSS(ref_grid_free(ref_grid), "free");
   }
 
+  { /* imply metric right tri */
+    REF_DBL tol = -1.0;
+    REF_GRID ref_grid;
+    REF_DBL *metric;
+    REF_INT node;
+
+    RSS(ref_fixture_tri_grid(&ref_grid, ref_mpi), "tri");
+
+    ref_malloc(metric, 6 * ref_node_max(ref_grid_node(ref_grid)), REF_DBL);
+
+    RSS(ref_metric_imply_from(metric, ref_grid), "imply");
+
+    each_ref_node_valid_node(ref_grid_node(ref_grid), node) {
+      RWDS(1.0, metric[0 + 6 * node], tol, "m[0]");
+      RWDS(0.5, metric[1 + 6 * node], tol, "m[1]");
+      RWDS(0.0, metric[2 + 6 * node], tol, "m[2]");
+      RWDS(1.0, metric[3 + 6 * node], tol, "m[3]");
+      RWDS(0.0, metric[4 + 6 * node], tol, "m[4]");
+      RWDS(1.0, metric[5 + 6 * node], tol, "m[5]");
+    }
+
+    ref_free(metric);
+
+    RSS(ref_grid_free(ref_grid), "free");
+  }
+
   { /* imply metric right prism */
     REF_DBL tol = 0.00001;
     REF_GRID ref_grid;
