d17ad10df891b16b541f68b959c966883a7649b4
Mike Park <Mike.Park@NASA.gov>
2022-01-28 17:02:11 -0500

    loads intel_2017.2.174 at runtime due to openmpi link mangling

--- acceptance/ref-modules.sh
+++ acceptance/ref-modules.sh
@@ -3,10 +3,11 @@
 module use --append /u/shared/fun3d/fun3d_users/modulefiles
 module use --append /u/shared/wtjones1/Modules/modulefiles
 
-export compile_modules="gcc_6.2.0 intel_2017.2.174"
+export compile_modules="gcc_6.2.0"
 export compiler_rpath="/usr/local/pkgs-modules/intel_2017.0.098/compilers_and_libraries_2017.2.174/linux/compiler/lib/intel64_lin"
 
-export run_modules="openmpi_2.1.1_intel_2017 tetgen ESP/120 GEOLAB/geolab_64 GEOLAB/AFLR3-16.28.5 valgrind_3.13.0"
+# expected to move intel to compile mods, but not working with openmpi
+export run_modules="intel_2017.2.174 openmpi_2.1.1_intel_2017 tetgen ESP/120 GEOLAB/geolab_64 GEOLAB/AFLR3-16.28.5 valgrind_3.13.0"
 
 module load ${compile_modules} ${run_modules}
 
