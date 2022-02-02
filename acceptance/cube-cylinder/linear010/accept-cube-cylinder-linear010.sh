cd1572d2091ae5e4a5299400c6d344d49482fbd8
Mike Park <Mike.Park@NASA.gov>
2022-02-02 06:06:20 -0500

    fixes cl -o -x typo

--- acceptance/cube-cylinder/linear010/accept-cube-cylinder-linear010.sh
+++ acceptance/cube-cylinder/linear010/accept-cube-cylinder-linear010.sh
@@ -21,7 +21,7 @@ ${two}/ref adapt ega.meshb -g ega.egads -m ega.metric -x ega1.meshb -d
 ${two}/ref_acceptance ega1.meshb ega1.metric ${h}
 ${two}/ref_metric_test ega1.meshb ega1.metric > accept-cube-cylinder-linear010-01.status
 
-${two}/ref adapt ega1.meshb -g ega.egads -m ega1.metric -o ega2.meshb -d
+${two}/ref adapt ega1.meshb -g ega.egads -m ega1.metric -x ega2.meshb -d
 ${two}/ref_acceptance ega2.meshb ega2.metric ${h}
 ${two}/ref_metric_test ega2.meshb ega2.metric > accept-cube-cylinder-linear010-02.status
 
