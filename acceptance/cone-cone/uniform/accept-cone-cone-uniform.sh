4f0b36b06e45f51583e67a3e6215a6010dc0a85e
Mike Park <Mike.Park@NASA.gov>
2022-02-02 09:31:52 -0500

    exports metric to test confromity

--- acceptance/cone-cone/uniform/accept-cone-cone-uniform.sh
+++ acceptance/cone-cone/uniform/accept-cone-cone-uniform.sh
@@ -17,12 +17,18 @@ geomfile=cone-cone.egads
 # ${two}/ref_geom_test ${geomfile} cone-cone.meshb
 
 ${two}/ref_acceptance cone-cone.meshb cone-cone.metric 0.1
-${two}/ref adapt cone-cone.meshb -g ${geomfile} -m cone-cone.metric -x cone-cone1.meshb -t
+${two}/ref adapt cone-cone.meshb -g ${geomfile} -m cone-cone.metric \
+      -x cone-cone1.meshb \
+      --export-metric-as cone-cone1-final-metric.solb \
+      -t
 mv ref_gather_movie.tec cone-cone1_movie.tec
 ${two}/ref_acceptance cone-cone1.meshb cone-cone1.metric 0.1
 ${two}/ref_metric_test cone-cone1.meshb cone-cone1-final-metric.solb > accept-cone-cone-uniform-01.status
 
-${two}/ref adapt cone-cone1.meshb -g ${geomfile} -m cone-cone1.metric -x cone-cone2.meshb -t
+${two}/ref adapt cone-cone1.meshb -g ${geomfile} -m cone-cone1.metric \
+      -x cone-cone2.meshb \
+      --export-metric-as cone-cone2-final-metric.solb \
+      -t
 mv ref_gather_movie.tec cone-cone2_movie.tec
 ${two}/ref_acceptance cone-cone2.meshb cone-cone2.metric 0.1
 ${two}/ref_metric_test cone-cone2.meshb cone-cone2-final-metric.solb > accept-cone-cone-uniform-02.status
