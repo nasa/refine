06454a1498dfeaa840a2f316b9b52e86d05c310d
Mike Park <Mike.Park@NASA.gov>
2022-02-02 06:45:45 -0500

    fixes -x

--- acceptance/cube-cylinder/uniform/accept-cube-cylinder-uniform.sh
+++ acceptance/cube-cylinder/uniform/accept-cube-cylinder-uniform.sh
@@ -17,12 +17,12 @@ geomfile=ega.egads
 # ${two}/ref_geom_test ${geomfile} ega.meshb
 
 ${two}/ref_acceptance ega.meshb ega.metric 0.1
-${two}/ref adapt ega.meshb -g ${geomfile} -m ega.metric -o ega1.meshb -t
+${two}/ref adapt ega.meshb -g ${geomfile} -m ega.metric -x ega1.meshb -t
 mv ref_gather_movie.tec ega1_movie.tec
 ${two}/ref_acceptance ega1.meshb ega1.metric 0.1
 ${two}/ref_metric_test ega1.meshb ega1.metric > accept-cube-cylinder-uniform-01.status
 
-${two}/ref adapt ega1.meshb -g ${geomfile} -m ega1.metric -o ega2.meshb -t
+${two}/ref adapt ega1.meshb -g ${geomfile} -m ega1.metric -x ega2.meshb -t
 mv ref_gather_movie.tec ega2_movie.tec
 ${two}/ref_acceptance ega2.meshb ega2.metric 0.1
 ${two}/ref_metric_test ega2.meshb ega2.metric > accept-cube-cylinder-uniform-02.status
