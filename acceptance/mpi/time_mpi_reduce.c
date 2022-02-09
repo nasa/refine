9bb716a91498d74a4fb00f8eba500df5a73192be
Mike Park <Mike.Park@NASA.gov>
2022-02-09 13:32:01 -0500

    totals the count

--- acceptance/mpi/time_mpi_reduce.c
+++ acceptance/mpi/time_mpi_reduce.c
@@ -1,35 +1,55 @@
-/* mpicc -g -o assert_mpi_reduce assert_mpi_reduce.c */
+/* mpicc -g -o time_mpi_reduce time_mpi_reduce.c */
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "mpi.h"
 int main(int argc, char *argv[]) {
-  int i, n = 10;
+  int i, nnode = 1000000, ldim = 6, chunk, n;
   int nproc, rank;
   double *in, *out;
-  long total;
+  double start_time = 0, end_time = 0, delta_time = 0, total_time = 0;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &nproc);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  if (argc > 1) n = atoi(argv[1]);
+  if (0 == rank) {
+    printf("usage: \n %s <nnode=1M> <ldim=6>\n", argv[0]);
+  }
+  if (argc > 1) nnode = atoi(argv[1]);
+  if (argc > 2) ldim = atoi(argv[2]);
 
+  chunk = nnode / nproc + 1;
+
+  n = (ldim + 1) * chunk;
   in = (double *)malloc(n * sizeof(double));
   out = (double *)malloc(n * sizeof(double));
 
-  for (i = 0; i < n; i++) in[i] = 0;
   if (0 == rank) {
-    printf("calling MPI_Reduce( %d, MPI_DOUBLE, MPI_SUM) with %d\n", n, nproc);
-    total = (long)n * (long)nproc;
-    printf("for a total of %ld\n", total);
-    fflush(stdout);
-  }
-  MPI_Allreduce(in, out, n, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-  if (0 == rank) {
-    printf("MPI_Allreduce returned\n");
+    printf("chunk size %d doubles %lu bytes\n", n, n * sizeof(double));
     fflush(stdout);
   }
 
+  for (i = 0; i < n; i++) in[i] = 0.0;
+  for (i = 0; i < nproc; i++) {
+    if (0 == rank) {
+      printf("calling MPI_Reduce( %d, MPI_DOUBLE, MPI_SUM) on %d\n", n, nproc);
+      fflush(stdout);
+      start_time = MPI_Wtime();
+    }
+    MPI_Reduce(in, out, n, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (0 == rank) {
+      end_time = MPI_Wtime();
+      delta_time = end_time - start_time;
+      total_time += delta_time;
+      printf("MPI_Allreduce returned in %f sec\n", delta_time);
+      fflush(stdout);
+    }
+  }
+  if (0 == rank) {
+    printf("total %f sec\n", total_time);
+    fflush(stdout);
+  }
   free(out);
   free(in);
 
