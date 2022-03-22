f88cc53395b5bc759cce0615b81b9d9007f9b245
Mike Park <Mike.Park@NASA.gov>
2022-03-22 15:08:12 -0400

    tests-linear

--- src/ref_geom_test.c
+++ src/ref_geom_test.c
@@ -1283,6 +1283,22 @@ int main(int argc, char *argv[]) {
   }
 
   {
+    REF_INT degree = 1;
+    REF_INT n_control_points = 2;
+    REF_DBL knots[] = {0, 0, 1, 1};
+    REF_DBL t;
+    REF_INT span;
+    REF_DBL N[2];
+    REF_DBL tol = -1.0;
+    t = 0.5;
+    RSS(ref_geom_bspline_span_index(degree, n_control_points, knots, t, &span),
+        "index");
+    RSS(ref_geom_bspline_basis(degree, knots, t, span, N), "basis");
+    RWDS(1.0 / 2.0, N[0], tol, "N0,1");
+    RWDS(1.0 / 2.0, N[1], tol, "N1,1");
+  }
+
+  {
     REF_INT degree = 2;
     REF_INT n_control_points = 7;
     REF_DBL knots[] = {0, 0, 0, 1, 2, 3, 4, 4, 5, 5, 5};
