cd33b6dcf56a1b611ed851dc4f057c3ee461c091
Mike Park <Mike.Park@NASA.gov>
2022-03-28 09:36:02 -0400

    status

--- src/ref_recon_test.c
+++ src/ref_recon_test.c
@@ -74,6 +74,7 @@ int main(int argc, char *argv[]) {
     REIS(1, pos, "required args: --limiter-effect grid.ext [p,vol,phi].solb");
     if (ref_mpi_once(ref_mpi)) printf("import grid %s\n", argv[2]);
     RSS(ref_import_by_extension(&ref_grid, ref_mpi, argv[2]), "argv import");
+    if (ref_mpi_once(ref_mpi)) printf("extract ref_node\n");
     ref_node = ref_grid_node(ref_grid);
     ref_mpi_stopwatch_stop(ref_grid_mpi(ref_grid), "grid import");
     if (ref_mpi_once(ref_mpi)) printf("reading field %s\n", argv[2]);
