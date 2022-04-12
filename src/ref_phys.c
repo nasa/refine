ff6fa3c9a9495731cde8e5d6dfe96d9bbfd12d85
Mike Park <Mike.Park@NASA.gov>
2022-04-12 12:12:58 -0400

    removes tau_wall unused compiler warning/error

--- src/ref_phys.c
+++ src/ref_phys.c
@@ -674,7 +674,7 @@ REF_STATUS ref_phys_spalding_uplus(REF_DBL yplus, REF_DBL *uplus) {
 REF_STATUS ref_phys_yplus_dist(REF_DBL mach, REF_DBL re, REF_DBL reference_t_k,
                                REF_DBL rho, REF_DBL t, REF_DBL y, REF_DBL u,
                                REF_DBL *yplus_dist) {
-  REF_DBL mu, nu_mach_re, tau_wall, u_tau;
+  REF_DBL mu, nu_mach_re, u_tau;
   *yplus_dist = -1.0;
   RSS(viscosity_law(t, reference_t_k, &mu), "sutherlands");
   nu_mach_re = mu / rho * mach / re;
