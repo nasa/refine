05e600c8c6f011cc69084e85245d7efec0e7a010
Mike Park <Mike.Park@NASA.gov>
2022-05-23 06:42:45 -0400

    decore

---
+++ misc/decorate.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+def decore(filename)
+puts filename
+lines = IO.readlines(filename)
+
+File.open(filename,'w') do |f|
+  lines.each do |line|
+    line.chomp!
+    line.sub!(/^REF_STATUS ref/, "REF_FCN REF_STATUS ref")
+    line.sub!(/^static REF_STATUS ref/, "REF_FCN static REF_STATUS ref")
+    f.puts line
+  end
+end
+end
+
+decore(ARGV[0])
