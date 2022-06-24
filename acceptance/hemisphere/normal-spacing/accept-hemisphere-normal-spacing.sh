7d67fbc89e9c3aca85b7f31e9631daa4b52ed950
Mike Park <Mike.Park@NASA.gov>
2022-06-24 06:34:35 -0400

    adds --faceid-spacing example

---
+++ acceptance/hemisphere/normal-spacing/accept-hemisphere-normal-spacing.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -x # echo commands
+set -e # exit on first error
+set -u # Treat unset variables as error
+
+if [ $# -gt 0 ] ; then
+    src=$1/src
+else
+    src=${HOME}/refine/egads/src
+fi
+
+geomfile=hemisphere.egads
+
+if [ ! -f hemisphere-vol.meshb ]
+then
+    serveCSM -batch -skipTess ../hemisphere.csm
+    ${src}/ref bootstrap ${geomfile}
+fi
+
+${src}/ref_acceptance hemisphere-vol.meshb hemisphere-vol-metric.solb 0.1
+${src}/ref adapt hemisphere-vol.meshb \
+      --egads ${geomfile} \
+      --metric hemisphere-vol-metric.solb \
+      --fun3d-mapbc hemisphere-vol.mapbc \
+      --spalding 0.1 2000 \
+      --faceid-spacing 3 0.05 -1 -1 \
+      -x heminorm1.meshb \
+      -x heminorm1.plt \
+      -x heminorm1.lb8.ugrid \
+      -s 5
+
