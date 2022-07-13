619fb84f5e5fcb2552af130554f82aa187feb67e
Mike Park <Mike.Park@NASA.gov>
2022-07-13 09:35:47 -0600

    extracts esp_module

--- acceptance/boot-modules.sh
+++ acceptance/boot-modules.sh
@@ -1,10 +1,12 @@
 
 module use --append /u/shared/fun3d/fun3d_users/modulefiles
 
+esp_module=ESP/120
+
 module load mpt-2.23
 
 module load gcc_6.2.0
-module load ESP/120
+module load ${esp_module}
 module load tetgen
 
 export module_path="/u/shared/fun3d/fun3d_users/modules"
@@ -13,6 +15,6 @@ export mpi_path="/opt/hpe/hpc/mpt/mpt-2.23"
 
 export parmetis_path="${module_path}/ParMETIS/4.0.3-mpt-2.23-gcc_6.2.0"
 
-export egads_path="${module_path}/ESP/120/EngSketchPad"
-export opencascade_path="${module_path}/ESP/120/OpenCASCADE"
+export egads_path="${module_path}/${esp_module}/EngSketchPad"
+export opencascade_path="${module_path}/${esp_module}/OpenCASCADE"
 
