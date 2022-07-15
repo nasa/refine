14e1fa65cf653d5ea7638234146aef606d310e5d
Mike Park <Mike.Park@NASA.gov>
2022-07-15 05:27:48 -0600

    mpt and nl

--- misc/module-builder/pfe/common.sh
+++ misc/module-builder/pfe/common.sh
@@ -9,7 +9,7 @@ PARMETIS_VERSION='4.0.3'
 ESP_VERSION='120'
 
 INTEL_MODULE="comp-intel/${INTEL_VERSION}"
-MPT_MODULE="mpi-hpe/mpt.${MPT_VERSION}"
+MPT_MODULE="mpt/${MPT_VERSION}"
 ESP_MODULE="ESP/${ESP_VERSION}"
 
 PREFIX="/swbuild/fun3d/shared/fun3d_users" # where everything is anchored
@@ -27,3 +27,4 @@ MODFILE_DEST="${MODFILE_BASE}/${VERSION}"
 
 . /usr/share/modules/init/bash
 module use ${MODFILE_ROOT}
+
